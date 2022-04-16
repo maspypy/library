4c84542fe5c63674527344ad00c300af631b51e5
maspypy <maspypy.gmail.com>
2022-04-16 23:37:16 +0900

    fix test

--- test/aoj/DPL_2_A_minimum_hamiltonian_cycle.test.cpp
+++ test/aoj/DPL_2_A_minimum_hamiltonian_cycle.test.cpp
@@ -1,52 +1,25 @@
-#pragma once
+#define PROBLEM \
+  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A"
+#include "my_template.hpp"
+#include "other/io.hpp"
 #include "graph/base.hpp"
+#include "graph/minimum_hamiltonian_cycle.hpp"
 
-/*
-return [cost, cycle]
-cycle なしの場合：{-1, {}}
-*/
-template <typename T, typename Graph>
-pair<T, vc<int>> minimum_hamiltonian_cycle(Graph& G) {
-  assert(G.is_prepared());
-  T INF = numeric_limits<T>::max();
-  int n = G.N;
-  vv(T, dist, n, n, INF);
-  FOR(v, n) {
-    for (auto&& e: G[v]) chmin(dist[v][e.to], e.cost);
-  }
-  n -= 1;
-  vv(T, dp, 1 << n, n, INF);
-  FOR(v, n) chmin(dp[1 << v][v], dist[n][v]);
-  FOR(s, 1 << n) FOR(frm, n) if (dp[s][frm] < INF) {
-    FOR(to, n) {
-      int t = s | 1 << to;
-      T cost = dist[frm][to];
-      if (s < t && cost < INF) chmin(dp[t][to], dp[s][frm] + cost);
-    }
-  }
-  int s = (1 << n) - 1;
-  T res = INF;
-  int best_v = -1;
-  FOR(v, n) if (dist[v][n] < INF && dp[s][v] < INF) {
-    if (chmin(res, dp[s][v] + dist[v][n])) best_v = v;
-  }
-  if (res == INF) return {-1, {}};
-  vc<int> C = {n, best_v};
-  int t = s;
-  while (len(C) <= n) {
-    int to = C.back();
-    int frm = [&]() -> int {
-      FOR(frm, n) {
-        int s = t ^ (1 << to);
-        if (dp[s][frm] < INF && dist[frm][to] < INF
-            && dp[s][frm] + dist[frm][to] == dp[t][to])
-          return frm;
-      }
-      return -1;
-    }();
-    C.eb(frm);
-    t ^= 1 << to;
-  }
-  reverse(all(C));
-  return {res, C};
+void solve() {
+  LL(N, M);
+  Graph<int, 1> G(N);
+  G.read_graph(M, 1, 0);
+  print(minimum_hamiltonian_cycle<int>(G).fi);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
 }
