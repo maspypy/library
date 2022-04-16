f212e7b80d18c7d6e4826ba51a551cf3a46d65b8
maspypy <maspypy.gmail.com>
2022-04-16 19:33:30 +0900

    add test

---
+++ test/aoj/DPL_3_B_largest_ractangle.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM \
+  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_3_B"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/cartesian_tree.hpp"
+
+void solve() {
+  LL(H, W);
+  vi A(W);
+  ll ANS = 0;
+  FOR(i, H) {
+    FOR(j, W) {
+      LL(x);
+      A[j] = (x == 0 ? A[j] + 1 : 0);
+    }
+    CartesianTree<ll> CT(A);
+    chmax(ANS, CT.max_rectangle_area());
+  }
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
