a8baf1dee3d6840819717583ad9d0f198ce0f89c
maspypy <maspypy.gmail.com>
2022-04-23 01:23:52 +0900

    add phi table

--- nt/euler_phi.hpp
+++ nt/euler_phi.hpp
@@ -1,6 +1,15 @@
+#include "nt/zeta.hpp"
 #include "nt/factor.hpp"
+
 ll euler_phi(ll n) {
   auto pf = factor(n);
   for (auto&& [p, e]: pf) n -= n / p;
   return n;
+}
+
+vi euler_phi_table(ll n) {
+  vi A(n + 1);
+  iota(all(A), 0);
+  divisor_mobius(A);
+  return A;
 }
