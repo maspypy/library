468aa7ae6a346b8d0c9c2d952485b2e86d1944cf
maspypy <maspypy.gmail.com>
2022-04-23 01:10:55 +0900

    fix

--- dp/monge.hpp
+++ dp/monge.hpp
@@ -1,4 +1,5 @@
 #include "ds/larsch.hpp"
+#include "dp/monotone_minima.hpp"
 
 // 定義域 [0, N] の範囲で f の monge 性を確認
 template <typename F>
@@ -12,7 +13,7 @@ bool check_monge(int N, F f) {
 }
 
 template <ll INF, typename F>
-vi shortest_path_monge(ll N, F f) {
+vi monge_shortest_path(int N, F f) {
   vi dp(N + 1, INF);
   dp[0] = 0;
   LARSCH<ll> larsch(N, [&](int i, int j) {
@@ -27,17 +28,34 @@ vi shortest_path_monge(ll N, F f) {
   return dp;
 }
 
+template <typename T, typename F>
+vc<vc<T>> monge_shortest_path_d_edge_dense(int N, F f, T INF, int d_max) {
+  vv(T, DP, d_max + 1, N + 1, INF);
+  DP[0][0] = 0;
+  FOR(d, d_max) {
+    auto& dp = DP[d];
+    auto& newdp = DP[d + 1];
+    auto g = [&](int r, int l) -> T {
+      //      print(l, r);
+      if (r <= l) return INF;
+      return dp[l] + f(l, r);
+    };
+    auto ret = monotone_minima<T>(N + 1, N + 1, g);
+    FOR(j, N + 1) newdp[j] = ret[j].se;
+  }
+  return DP;
+}
+
 /*
 https://dic.kimiyuki.net/d-edge-shortest-path-monge
 上凸関数 calc_L(lambda) の最大値を求める問題に帰着
 |f| の上限 f_lim も渡す
 */
-template <typename F>
-ll d_edge_shortest_path_monge(ll N, F f, ll d, ll f_lim) {
-  const ll INF = 1LL << 60;
+template <ll INF, typename F>
+ll monge_shortest_path_d_edge(ll N, F f, ll d, ll f_lim) {
   auto calc_L = [&](ll lambda) -> ll {
     auto cost = [&](int frm, int to) -> ll { return f(frm, to) + lambda; };
-    auto dp = shortest_path_monge(N, cost);
+    auto dp = monge_shortest_path<INF>(N, cost);
     return dp.back() - lambda * d;
   };
 
