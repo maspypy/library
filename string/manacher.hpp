4be2e02134879ec0d560f5a786f7b35c60df349b
maspypy <maspypy.gmail.com>
2022-04-23 14:12:28 +0900

    fix

--- string/manacher.hpp
+++ string/manacher.hpp
@@ -6,24 +6,26 @@ vc<pair<int, int>> manacher(STRING s) {
   s.resize(2 * n - 1);
   FOR_R(i, n) s[2 * i] = s[i];
   FOR(i, n - 1) s[2 * i + 1] = '~';
-  vector<int> A(len(s));
+  vector<int> dp(len(s));
   int i = 0, j = 0;
   while (i < len(s)) {
     while (i - j >= 0 && i + j < len(s) && s[i - j] == s[i + j]) ++j;
-    A[i] = j;
+    dp[i] = j;
     int k = 1;
-    while (i - k >= 0 && i + k < len(s) && k + A[i - k] < j) {
-      A[i + k] = A[i - k];
+    while (i - k >= 0 && i + k < len(s) && k + dp[i - k] < j) {
+      dp[i + k] = dp[i - k];
       ++k;
     }
     i += k, j -= k;
   }
-  if (CALC_ALL) {
-    FOR(i, len(A)) {
-      if (!((i ^ A[i]) & 1)) A[i]--;
-    }
-  } else {
-    for (auto&& x: A) x = 2 * x - 1;
+  FOR(i, len(dp)) if (!((i ^ dp[i]) & 1))-- dp[i];
+  vc<pair<int, int>> res;
+  res.reserve(len(dp));
+  FOR(i, len(dp)) {
+    if (dp[i] == 0) continue;
+    int l = (i - dp[i] + 1) / 2;
+    int r = (i + dp[i] + 1) / 2;
+    res.eb(l, r);
   }
-  return A;
+  return res;
 }
--- test/library_checker/string/enumerate_palindromes.test.cpp
+++ test/library_checker/string/enumerate_palindromes.test.cpp
@@ -1,38 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
 #include "my_template.hpp"
 #include "other/io.hpp"
-
-// 極大回文 [L, R) を列挙する
-template <typename STRING>
-vc<pair<int, int>> manacher(STRING s) {
-  int n = len(s);
-  assert(n > 0);
-  s.resize(2 * n - 1);
-  FOR_R(i, n) s[2 * i] = s[i];
-  FOR(i, n - 1) s[2 * i + 1] = '~';
-  vector<int> dp(len(s));
-  int i = 0, j = 0;
-  while (i < len(s)) {
-    while (i - j >= 0 && i + j < len(s) && s[i - j] == s[i + j]) ++j;
-    dp[i] = j;
-    int k = 1;
-    while (i - k >= 0 && i + k < len(s) && k + dp[i - k] < j) {
-      dp[i + k] = dp[i - k];
-      ++k;
-    }
-    i += k, j -= k;
-  }
-  FOR(i, len(dp)) if (!((i ^ dp[i]) & 1))-- dp[i];
-  vc<pair<int, int>> res;
-  res.reserve(len(dp));
-  FOR(i, len(dp)) {
-    if (dp[i] == 0) continue;
-    int l = (i - dp[i] + 1) / 2;
-    int r = (i + dp[i] + 1) / 2;
-    res.eb(l, r);
-  }
-  return res;
-}
+#include "string/manacher.hpp"
 
 void solve() {
   STR(S);
