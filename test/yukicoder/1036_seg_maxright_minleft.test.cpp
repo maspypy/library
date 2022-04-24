ddaf96a15543f8acdbcb1d8c0ac6c30e2ee4aa39
maspypy <maspypy.gmail.com>
2022-04-24 16:57:10 +0900

    add test

---
+++ test/yukicoder/1036_seg_maxright_minleft.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1036"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/segtree.hpp"
+#include "alg/monoid_gcd.hpp"
+
+void solve() {
+  LL(N);
+  VEC(ll, A, N);
+  SegTree<Monoid_Gcd<ll>> seg(A);
+  ll ANS1 = 0;
+  FOR(L, N) {
+    auto check = [&](auto e) -> bool { return e != 1; };
+    auto R = seg.max_right(check, L);
+    ANS1 += N - R;
+  }
+  ll ANS2 = 0;
+  FOR3(R, 1, N + 1) {
+    auto check = [&](auto e) -> bool { return e != 1; };
+    auto L = seg.min_left(check, R);
+    ANS2 += L;
+  }
+  assert(ANS1 == ANS2);
+  print(ANS1);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
---
+++ test/yukicoder/1036_swag.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1036"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/swag.hpp"
+#include "alg/monoid_gcd.hpp"
+
+void solve() {
+  LL(N);
+  VEC(ll, A, N);
+  SWAG<Monoid_Gcd<ll>> swag;
+  ll R = 0;
+  ll ANS = 0;
+  FOR(L, N) {
+    chmax(R, L);
+    while (R < N && gcd(swag.prod(), A[R]) != 1) swag.push(A[R++]);
+    ANS += N - R;
+    if (len(swag)) swag.pop();
+  }
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
