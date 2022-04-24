fd47b5b19b85e2b54e31b495fcd9b717838ecace
maspypy <maspypy.gmail.com>
2022-04-24 16:42:51 +0900

    add xor sparse table

---
+++ test/yukicoder/1891_xor_sparse.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1891"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/modint.hpp"
+#include "alg/group_affine.hpp"
+#include "ds/xor_sparsetable.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N, Q);
+  ll LOG = topbit(N);
+  using Mono = Group_Affine<mint>;
+  using X = typename Mono::value_type;
+  VEC(X, A, N);
+  Xor_SparseTable<Mono> seg(A);
+  FOR_(Q) {
+    LL(l, r, p, x);
+    auto [a, b] = seg.prod(l, r, p);
+    print(a * mint(x) + b);
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
