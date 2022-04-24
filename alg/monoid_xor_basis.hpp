6061caf02551eae2521dcf30c4e4efb3a6de126d
maspypy <maspypy.gmail.com>
2022-04-24 15:03:19 +0900

    change value type

--- alg/monoid_xor_basis.hpp
+++ alg/monoid_xor_basis.hpp
@@ -1,24 +1,26 @@
-template <typename INT, int LOG>
+template <typename INT>
 struct Monoid_XorBasis {
-  using value_type = array<INT, LOG>;
+  using value_type = vector<INT>;
   using X = value_type;
-  static void add_element(X& x, INT v) {
-    int n = 0;
+  static bool add_element(X& x, INT v) {
     for (auto&& e: x) {
       if (e == 0 || v == 0) break;
-      ++n;
       chmin(v, v ^ e);
     }
-    if (v) x[n] = v;
+    if (v) {
+      x.eb(v);
+      return true;
+    }
+    return false;
   }
 
   static X op(const X& x, const X& y) {
     X z = x;
-    int n = 0;
-    while (n < LOG && z[n]) ++n;
     for (auto v: y) { add_element(z, v); }
     return z;
   }
+
+  // x ^ v の最大値
   static INT get_max(const X& x) {
     INT res = 0;
     for (auto&& a: x) chmax(res, res ^ a);
