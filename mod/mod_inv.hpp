196d03909c8241992ea37763861e0de78ecaf31c
maspypy <maspypy.gmail.com>
2022-04-27 05:05:30 +0900

    ll

--- mod/mod_inv.hpp
+++ mod/mod_inv.hpp
@@ -1,5 +1,6 @@
+// long でも大丈夫
 ll mod_inv(ll val, ll mod) {
-  int a = val, b = mod, u = 1, v = 0, t;
+  ll a = val, b = mod, u = 1, v = 0, t;
   while (b > 0) {
     t = a / b;
     swap(a -= t * b, b), swap(u -= t * v, v);
--- mod/mod_pow.hpp
+++ mod/mod_pow.hpp
@@ -1,4 +1,5 @@
 #include "mod/fast_div.hpp"
+// int
 ll mod_pow(ll a, ll n, int mod){
   fast_div fd(mod);
   a = a % fd;
@@ -7,6 +8,18 @@ ll mod_pow(ll a, ll n, int mod){
   while(n){
     if(n & 1) v = v * p % fd;
     p = p * p % fd;
+    n >>= 1;
+  }
+  return v;
+}
+
+ll mod_pow_long(ll a, ll n, ll mod){
+  a %= mod;
+  ll p = a;
+  ll v = 1;
+  while(n){
+    if(n & 1) v = i128(v) * p % mod;
+    p = i128(p) * p % mod;
     n >>= 1;
   }
   return v;
