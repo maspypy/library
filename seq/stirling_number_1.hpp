ec9c6a7d49f644976d9a548972db0c4942ac2f69
maspypy <maspypy.gmail.com>
2022-04-27 03:25:13 +0900

    add fixed-k type

--- seq/stirling_number_1.hpp
+++ seq/stirling_number_1.hpp
@@ -1,17 +1,44 @@
 #include "poly/poly_taylor_shift.hpp"
+#include "poly/fps_pow.hpp"
 
-// x(x-1)...(x-n+1) の係数 s(n, k)
-// 分割統治 + taylor shift で O(nlogn) 時間
+// x(x+1)...(x+n-1) の係数 c(n, k)
+// [n] の順列のうち、k 個のサイクルに分かれるものの個数。
+// n を固定したときの列挙を O(n log n) で行う。
 template <typename mint>
-vc<mint> stirling_number_1(int n) {
-  if (n == 0) return {1};
-  if (n == 1) return {0, 1};
-  auto f = stirling_number_1<mint>(n / 2);
-  auto g = poly_taylor_shift(f, -mint(n / 2));
-  f = convolution(f, g);
-  if (n & 1) {
-    g = {-(n - 1), 1};
+vc<mint> stirling_number_1_n(int n, bool sgn = false) {
+  auto dfs = [&](auto self, int n) -> vc<mint> {
+    if (n == 0) return {1};
+    if (n == 1) return {0, 1};
+    auto f = self(self, n / 2);
+    auto g = poly_taylor_shift(f, mint(n / 2));
     f = convolution(f, g);
-  }
+    if (n & 1) {
+      g = {(n - 1), 1};
+      f = convolution(f, g);
+    }
+    return f;
+  };
+  auto f = dfs(dfs, n);
+  if (sgn) { FOR(i, n + 1) if ((n + i) % 2 == 0) f[i] = -f[i]; }
   return f;
+}
+
+// k を固定したときの c(n, k) の列挙。
+template <typename mint>
+vc<mint> stirling_number_1_k(int k, int n_max, bool sgn = false) {
+  if (n_max < k) {
+    vc<mint> f(n_max + 1);
+    return f;
+  }
+  int LIM = n_max - k;
+  vc<mint> f(LIM + 1);
+  FOR(i, LIM + 1) f[i] = inv<mint>(i + 1);
+  f = fps_pow(f, k);
+  if (sgn) { FOR(i, LIM + 1) if (i % 2 == 1) f[i] = -f[i]; }
+
+  mint cf = fact_inv<mint>(k);
+  vc<mint> res(n_max + 1);
+  FOR(i, len(f)) res[k + i] = cf * f[i] * fact<mint>(k + i);
+
+  return res;
 }
