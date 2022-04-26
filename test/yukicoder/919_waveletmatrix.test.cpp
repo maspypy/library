dcd14107154f9b9b9b897632c2f38e9f752b76ff
maspypy <maspypy.gmail.com>
2022-04-27 05:04:26 +0900

    add test

---
+++ test/yukicoder/919_waveletmatrix.test.cpp
@@ -0,0 +1,46 @@
+#define PROBLEM "https://yukicoder.me/problems/no/919"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/waveletmatrix.hpp"
+
+void solve() {
+  LL(N);
+  VEC(ll, A, N);
+  WaveletMatrix<ll> WM(A);
+  const ll INF = 1LL << 60;
+  ll ANS = -INF;
+
+  auto get = [&](ll L, ll R) -> ll {
+    assert(L < R);
+    ll n = R - L;
+    return WM.kth(L, R, (n - 1) / 2);
+  };
+
+  FOR3(K, 1, N + 1) {
+    // とる最大回数
+    ll LIM = N / K;
+    vi left(LIM);
+    vi right(LIM);
+    FOR(i, LIM) left[i] = get(K * i, K * i + K);
+    FOR(i, LIM) right[i] = get(N - K * i - K, N - K * i);
+    left = cumsum(left);
+    right = cumsum(right);
+    // x 回以下とるときの～
+    FOR(i, LIM) chmax(left[i + 1], left[i]);
+    FOR(i, LIM) chmax(right[i + 1], right[i]);
+    FOR(i, LIM + 1) chmax(ANS, K * (left[i] + right[LIM - i]));
+  }
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
---
+++ test/yukicoder/924.test.cpp
@@ -0,0 +1,69 @@
+#define PROBLEM "https://yukicoder.me/problems/no/924"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/waveletmatrix.hpp"
+#include "ds/fenwick.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  auto Ac = cumsum(A);
+  vi X = A;
+  UNIQUE(X);
+  for (auto&& a: A) a = LB(X, a);
+  WaveletMatrix<ll> WM(A);
+  VEC(pi, query, Q);
+  for (auto&& [l, r]: query) --l;
+  vc<int> med(Q);
+  vi low_sum(Q), low_cnt(Q);
+  FOR(q, Q) {
+    auto [l, r] = query[q];
+    ll n = r - l;
+    med[q] = WM.kth(l, r, n / 2);
+  }
+
+  vvc<int> QID(N);
+  FOR(q, Q) QID[med[q]].eb(q);
+
+  vvc<int> AID(N);
+  FOR(i, N) AID[A[i]].eb(i);
+
+  FenwickTree<Group_Add<ll>> bit_c(N), bit_s(N);
+
+  FOR(x, N) {
+    for (auto&& i: AID[x]) {
+      bit_c.add(i, 1);
+      bit_s.add(i, X[x]);
+    }
+    for (auto&& q: QID[x]) {
+      auto [l, r] = query[q];
+      low_cnt[q] = bit_c.sum(l, r);
+      low_sum[q] = bit_s.sum(l, r);
+    }
+  }
+
+  FOR(q, Q) {
+    auto [l, r] = query[q];
+    ll x = X[med[q]];
+    ll lc = low_cnt[q], ls = low_sum[q];
+    ll hc = r - l - lc;
+    ll hs = Ac[r] - Ac[l] - ls;
+    ll ANS = 0;
+    ANS += x * lc - ls;
+    ANS += hs - x * hc;
+    //    print(x, lc, ls, hc, hs);
+    print(ANS);
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
