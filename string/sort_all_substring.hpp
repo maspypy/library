7a5a3f77efd8ab2a9b1b926d75ff328e1add48b9
maspypy <maspypy.gmail.com>
2022-05-01 01:13:40 +0900

    add limited ver

--- string/sort_all_substring.hpp
+++ string/sort_all_substring.hpp
@@ -24,3 +24,26 @@ vc<vc<int>> sort_all_substring(STRING S) {
   }
   return DP;
 }
+
+// 長さの上限あり
+// (開始位置、長さ) に対してソート順を埋める
+template <typename STRING>
+vc<vc<int>> sort_all_substring_limit(STRING S, ll LIM) {
+  int n = len(S);
+  SuffixArray sa(S);
+  auto& SA = sa.SA;
+  auto& LCP = sa.LCP;
+  int nxt = 0;
+  vv(int, DP, n, LIM + 1, -1);
+  FOR(i, len(SA)) {
+    auto L = SA[i];
+    FOR3(k, 1, min(n - L, LIM) + 1) {
+      int R = L + k;
+      if (i > 0 && LCP[i - 1] >= k)
+        DP[L][R - L] = DP[SA[i - 1]][k];
+      else
+        DP[L][R - L] = nxt++;
+    }
+  }
+  return DP;
+}
