4e17774fb7b67b8f26cb369b4faade1602a12a8c
maspypy <maspypy.gmail.com>
2022-05-02 00:47:58 +0900

    add test

---
+++ test/yukicoder/1332_range_freq.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1332"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/waveletmatrix.hpp"
+
+void solve() {
+  LL(N);
+  VEC(ll, X, N);
+  WaveletMatrix<ll> WM(X);
+  const ll INF = 1LL << 60;
+  LL(Q);
+  FOR_(Q) {
+    LL(l, r, x);
+    --l;
+    ll ANS = INF;
+    ll n = WM.freq(l, r, 0, x);
+    if (n > 0) chmin(ANS, abs(x - WM.kth(l, r, n - 1)));
+    if (n < r - l) chmin(ANS, abs(x - WM.kth(l, r, n)));
+    print(ANS);
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
