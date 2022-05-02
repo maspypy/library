12906442bd31a8a35b69fe8fd63214ff2a6f5369
maspypy <maspypy.gmail.com>
2022-05-02 14:12:28 +0900

    add sparse/tuning

--- poly/fps_pow.hpp
+++ poly/fps_pow.hpp
@@ -1,7 +1,10 @@
+#pragma once
+#include "poly/count_terms.hpp"
 #include "poly/fps_exp.hpp"
 #include "poly/fps_log.hpp"
 
 // fps の k 乗を求める。k >= 0 の前提である。
+// 定数項が 1 で、k が mint の場合には、fps_pow_1 を使うこと。
 // ・dense な場合： log, exp を使う O(NlogN)
 // ・sparse な場合： O(NK)
 template <typename mint>
@@ -22,34 +25,35 @@ vc<mint> fps_pow(const vc<mint>& f, ll k, bool force_sparse = 0) {
   return h;
 }
 
-// 定数項が 1 の場合限定。指数は modint なので、負数や有理数でも可。
-// ・dense な場合： log, exp を使う O(NlogN)
-// ・sparse な場合： O(NK)
 template <typename mint>
-vc<mint> fps_pow_1(const vc<mint>& f, mint K, bool force_sparse = 0) {
-  assert(f[0] == mint(1));
-  int terms = 0;
-  FOR(i, len(f)) if (f[i] != mint(0))++ terms;
-  bool is_sparse = (terms <= 50) || force_sparse;
-  if (!is_sparse) {
-    auto log_f = fps_log(f);
-    FOR(i, len(f)) log_f[i] *= K;
-    return fps_exp(log_f);
-  }
-
-  // sparse pow
+vc<mint> fps_pow_1_sparse(const vc<mint>& f, mint K) {
   int N = len(f);
   vc<pair<int, mint>> dat;
   FOR3(i, 1, N) if (f[i] != mint(0)) dat.eb(i, f[i]);
   vc<mint> g(N);
   g[0] = 1;
   FOR(n, N - 1) {
+    mint& x = g[n + 1];
     for (auto&& [d, cf]: dat) {
-      if (d == 0 || d > n + 1) continue;
+      if (d > n + 1) break;
       mint t = cf * g[n - d + 1];
-      g[n + 1] += t * (K * mint(d) - mint(n - d + 1));
+      x += t * (K.val * d - (n - d + 1));
     }
-    g[n + 1] *= inv<mint>(n + 1);
+    x *= inv<mint>(n + 1);
   }
   return g;
+}
+
+template <typename mint>
+vc<mint> fps_pow_1_dense(const vc<mint>& f, mint K) {
+  assert(f[0] == mint(1));
+  auto log_f = fps_log(f);
+  FOR(i, len(f)) log_f[i] *= K;
+  return fps_exp(log_f);
+}
+
+template <typename mint>
+vc<mint> fps_pow_1(const vc<mint>& f) {
+  if (count_terms(f) <= 100) return fps_pow_1_sparse(f);
+  return fps_pow_1_dense(f);
 }
