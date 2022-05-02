538577515e00a017b7fa2edd4dd1c6d96387e1a5
maspypy <maspypy.gmail.com>
2022-05-02 12:36:42 +0900

    split files

--- poly/convolution.hpp
+++ poly/convolution.hpp
@@ -1,175 +1,8 @@
 #pragma once
 #include "mod/modint.hpp"
-template <class T>
-vector<T> convolution_naive(const vector<T>& a, const vector<T>& b) {
-  int n = int(a.size()), m = int(b.size());
-  vector<T> ans(n + m - 1);
-  if (n < m) {
-    FOR(j, m) FOR(i, n) ans[i + j] += a[i] * b[j];
-  } else {
-    FOR(i, n) FOR(j, m) ans[i + j] += a[i] * b[j];
-  }
-  return ans;
-}
-
-template <class mint>
-struct fft_info {
-  static constexpr int bsf_constexpr(unsigned int n) {
-    int x = 0;
-    while (!(n & (1 << x))) x++;
-    return x;
-  }
-
-  static constexpr int rank2 = bsf_constexpr(mint::get_mod() - 1);
-  array<mint, rank2 + 1> root;
-  array<mint, rank2 + 1> iroot;
-  array<mint, max(0, rank2 - 1)> rate2;
-  array<mint, max(0, rank2 - 1)> irate2;
-  array<mint, max(0, rank2 - 2)> rate3;
-  array<mint, max(0, rank2 - 2)> irate3;
-
-  fft_info() {
-    int g = primitive_root(mint::get_mod());
-    root[rank2] = mint(g).pow((mint::get_mod() - 1) >> rank2);
-    iroot[rank2] = mint(1) / root[rank2];
-    FOR_R(i, rank2) {
-      root[i] = root[i + 1] * root[i + 1];
-      iroot[i] = iroot[i + 1] * iroot[i + 1];
-    }
-
-    {
-      mint prod = 1, iprod = 1;
-      for (int i = 0; i <= rank2 - 2; i++) {
-        rate2[i] = root[i + 2] * prod;
-        irate2[i] = iroot[i + 2] * iprod;
-        prod *= iroot[i + 2];
-        iprod *= root[i + 2];
-      }
-    }
-    {
-      mint prod = 1, iprod = 1;
-      for (int i = 0; i <= rank2 - 3; i++) {
-        rate3[i] = root[i + 3] * prod;
-        irate3[i] = iroot[i + 3] * iprod;
-        prod *= iroot[i + 3];
-        iprod *= root[i + 3];
-      }
-    }
-  }
-
-  constexpr int primitive_root(int m) {
-    if (m == 167772161) return 3;
-    if (m == 469762049) return 3;
-    if (m == 754974721) return 11;
-    if (m == 880803841) return 26;
-    if (m == 998244353) return 3;
-    return -1;
-  }
-};
-
-template <class mint>
-void ntt(vector<mint>& a, bool inverse) {
-  int n = int(a.size());
-  int h = topbit(n);
-  assert(n == 1 << h);
-  static const fft_info<mint> info;
-  if (!inverse) {
-    int len = 0; // a[i, i+(n>>len), i+2*(n>>len), ..] is transformed
-    while (len < h) {
-      if (h - len == 1) {
-        int p = 1 << (h - len - 1);
-        mint rot = 1;
-        FOR(s, 1 << len) {
-          int offset = s << (h - len);
-          FOR(i, p) {
-            auto l = a[i + offset];
-            auto r = a[i + offset + p] * rot;
-            a[i + offset] = l + r;
-            a[i + offset + p] = l - r;
-          }
-          rot *= info.rate2[topbit(~s & -~s)];
-        }
-        len++;
-      } else {
-        int p = 1 << (h - len - 2);
-        mint rot = 1, imag = info.root[2];
-        for (int s = 0; s < (1 << len); s++) {
-          mint rot2 = rot * rot;
-          mint rot3 = rot2 * rot;
-          int offset = s << (h - len);
-          for (int i = 0; i < p; i++) {
-            auto mod2 = 1ULL * mint::get_mod() * mint::get_mod();
-            auto a0 = 1ULL * a[i + offset].val;
-            auto a1 = 1ULL * a[i + offset + p].val * rot.val;
-            auto a2 = 1ULL * a[i + offset + 2 * p].val * rot2.val;
-            auto a3 = 1ULL * a[i + offset + 3 * p].val * rot3.val;
-            auto a1na3imag = 1ULL * mint(a1 + mod2 - a3).val * imag.val;
-            auto na2 = mod2 - a2;
-            a[i + offset] = a0 + a2 + a1 + a3;
-            a[i + offset + 1 * p] = a0 + a2 + (2 * mod2 - (a1 + a3));
-            a[i + offset + 2 * p] = a0 + na2 + a1na3imag;
-            a[i + offset + 3 * p] = a0 + na2 + (mod2 - a1na3imag);
-          }
-          rot *= info.rate3[topbit(~s & -~s)];
-        }
-        len += 2;
-      }
-    }
-  } else {
-    mint coef = mint(1) / mint(len(a));
-    FOR(i, len(a)) a[i] *= coef;
-    int len = h;
-    while (len) {
-      if (len == 1) {
-        int p = 1 << (h - len);
-        mint irot = 1;
-        FOR(s, 1 << (len - 1)) {
-          int offset = s << (h - len + 1);
-          FOR(i, p) {
-            auto l = a[i + offset];
-            auto r = a[i + offset + p];
-            a[i + offset] = l + r;
-            a[i + offset + p]
-                = (unsigned long long)(mint::get_mod() + l.val - r.val)
-                  * irot.val;
-            ;
-          }
-          irot *= info.irate2[topbit(~s & -~s)];
-        }
-        len--;
-      } else {
-        int p = 1 << (h - len);
-        mint irot = 1, iimag = info.iroot[2];
-        FOR(s, (1 << (len - 2))) {
-          mint irot2 = irot * irot;
-          mint irot3 = irot2 * irot;
-          int offset = s << (h - len + 2);
-          for (int i = 0; i < p; i++) {
-            auto a0 = 1ULL * a[i + offset + 0 * p].val;
-            auto a1 = 1ULL * a[i + offset + 1 * p].val;
-            auto a2 = 1ULL * a[i + offset + 2 * p].val;
-            auto a3 = 1ULL * a[i + offset + 3 * p].val;
-
-            auto a2na3iimag
-                = 1ULL * mint((mint::get_mod() + a2 - a3) * iimag.val).val;
-
-            a[i + offset] = a0 + a1 + a2 + a3;
-            a[i + offset + 1 * p]
-                = (a0 + (mint::get_mod() - a1) + a2na3iimag) * irot.val;
-            a[i + offset + 2 * p]
-                = (a0 + a1 + (mint::get_mod() - a2) + (mint::get_mod() - a3))
-                  * irot2.val;
-            a[i + offset + 3 * p]
-                = (a0 + (mint::get_mod() - a1) + (mint::get_mod() - a2na3iimag))
-                  * irot3.val;
-          }
-          irot *= info.irate3[topbit(~s & -~s)];
-        }
-        len -= 2;
-      }
-    }
-  }
-}
+#include "poly/convolution_naive.hpp"
+#include "poly/ntt.hpp"
+#include "poly/fft.hpp"
 
 template <class mint>
 vector<mint> convolution_ntt(vector<mint> a, vector<mint> b) {
@@ -178,7 +11,7 @@ vector<mint> convolution_ntt(vector<mint> a, vector<mint> b) {
   while (sz < n + m - 1) sz *= 2;
 
   // sz = 2^k のときの高速化。分割統治的なやつで損しまくるので。
-  if((n+m-3) <= sz / 2){
+  if ((n + m - 3) <= sz / 2) {
     auto a_last = a.back(), b_last = b.back();
     a.pop_back(), b.pop_back();
     auto c = convolution(a, b);
@@ -190,11 +23,10 @@ vector<mint> convolution_ntt(vector<mint> a, vector<mint> b) {
     return c;
   }
 
-
   a.resize(sz), b.resize(sz);
   bool same = a == b;
   ntt(a, 0);
-  if(same){
+  if (same) {
     b = a;
   } else {
     ntt(b, 0);
@@ -236,72 +68,13 @@ vector<mint> convolution_garner(const vector<mint>& a, const vector<mint>& b) {
   return c;
 }
 
-namespace CFFT {
-using real = double;
-
-struct C {
-  real x, y;
-
-  C() : x(0), y(0) {}
-
-  C(real x, real y) : x(x), y(y) {}
-  inline C operator+(const C& c) const { return C(x + c.x, y + c.y); }
-  inline C operator-(const C& c) const { return C(x - c.x, y - c.y); }
-  inline C operator*(const C& c) const {
-    return C(x * c.x - y * c.y, x * c.y + y * c.x);
-  }
-
-  inline C conj() const { return C(x, -y); }
-};
-
-const real PI = acosl(-1);
-int base = 1;
-vector<C> rts = {{0, 0}, {1, 0}};
-vector<int> rev = {0, 1};
-
-void ensure_base(int nbase) {
-  if (nbase <= base) return;
-  rev.resize(1 << nbase);
-  rts.resize(1 << nbase);
-  for (int i = 0; i < (1 << nbase); i++) {
-    rev[i] = (rev[i >> 1] >> 1) + ((i & 1) << (nbase - 1));
-  }
-  while (base < nbase) {
-    real angle = PI * 2.0 / (1 << (base + 1));
-    for (int i = 1 << (base - 1); i < (1 << base); i++) {
-      rts[i << 1] = rts[i];
-      real angle_i = angle * (2 * i + 1 - (1 << base));
-      rts[(i << 1) + 1] = C(cos(angle_i), sin(angle_i));
-    }
-    ++base;
-  }
-}
-
-void fft(vector<C>& a, int n) {
-  assert((n & (n - 1)) == 0);
-  int zeros = __builtin_ctz(n);
-  ensure_base(zeros);
-  int shift = base - zeros;
-  for (int i = 0; i < n; i++) {
-    if (i < (rev[i] >> shift)) { swap(a[i], a[rev[i] >> shift]); }
-  }
-  for (int k = 1; k < n; k <<= 1) {
-    for (int i = 0; i < n; i += 2 * k) {
-      for (int j = 0; j < k; j++) {
-        C z = a[i + j + k] * rts[j + k];
-        a[i + j + k] = a[i + j] - z;
-        a[i + j] = a[i + j] + z;
-      }
-    }
-  }
-}
-
 template <typename R>
 vc<double> convolution_fft(const vc<R>& a, const vc<R>& b) {
+  using C = CFFT::C;
   int need = (int)a.size() + (int)b.size() - 1;
   int nbase = 1;
   while ((1 << nbase) < need) nbase++;
-  ensure_base(nbase);
+  CFFT::ensure_base(nbase);
   int sz = 1 << nbase;
   vector<C> fa(sz);
   for (int i = 0; i < sz; i++) {
@@ -309,7 +82,7 @@ vc<double> convolution_fft(const vc<R>& a, const vc<R>& b) {
     int y = (i < (int)b.size() ? b[i] : 0);
     fa[i] = C(x, y);
   }
-  fft(fa, sz);
+  CFFT::fft(fa, sz);
   C r(0, -0.25 / (sz >> 1)), s(0, 1), t(0.5, 0);
   for (int i = 0; i <= (sz >> 1); i++) {
     int j = (sz - i) & (sz - 1);
@@ -319,17 +92,16 @@ vc<double> convolution_fft(const vc<R>& a, const vc<R>& b) {
   }
   for (int i = 0; i < (sz >> 1); i++) {
     C A0 = (fa[i] + fa[i + (sz >> 1)]) * t;
-    C A1 = (fa[i] - fa[i + (sz >> 1)]) * t * rts[(sz >> 1) + i];
+    C A1 = (fa[i] - fa[i + (sz >> 1)]) * t * CFFT::rts[(sz >> 1) + i];
     fa[i] = A0 + A1 * s;
   }
-  fft(fa, sz >> 1);
+  CFFT::fft(fa, sz >> 1);
   vector<double> ret(need);
   for (int i = 0; i < need; i++) {
     ret[i] = (i & 1 ? fa[i >> 1].y : fa[i >> 1].x);
   }
   return ret;
 }
-} // namespace CFFT
 
 vector<ll> convolution(const vector<ll>& a, const vector<ll>& b) {
   int n = len(a), m = len(b);
@@ -339,22 +111,24 @@ vector<ll> convolution(const vector<ll>& a, const vector<ll>& b) {
   FOR(i, n) abs_sum_a += abs(a[i]);
   FOR(i, m) abs_sum_b += abs(b[i]);
   assert(abs_sum_a * abs_sum_b < 1e15);
-  vc<double> c = CFFT::convolution_fft(a, b);
+  vc<double> c = convolution_fft(a, b);
   vc<ll> res(len(c));
   FOR(i, len(c)) res[i] = ll(floor(c[i] + .5));
   return res;
 }
 
-template<typename mint>
-enable_if_t<is_same<mint, modint998>::value, vc<mint>> convolution(const vc<mint>& a, const vc<mint>& b) {
+template <typename mint>
+enable_if_t<is_same<mint, modint998>::value, vc<mint>> convolution(
+    const vc<mint>& a, const vc<mint>& b) {
   int n = len(a), m = len(b);
   if (!n || !m) return {};
   if (min(n, m) <= 60) return convolution_naive(a, b);
   return convolution_ntt(a, b);
 }
 
-template<typename mint>
-enable_if_t<!is_same<mint, modint998>::value, vc<mint>> convolution(const vc<mint>& a, const vc<mint>& b) {
+template <typename mint>
+enable_if_t<!is_same<mint, modint998>::value, vc<mint>> convolution(
+    const vc<mint>& a, const vc<mint>& b) {
   int n = len(a), m = len(b);
   if (!n || !m) return {};
   if (min(n, m) <= 60) return convolution_naive(a, b);
