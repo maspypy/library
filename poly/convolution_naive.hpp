9d1ef23119b6566c6f75e71f83bbd0aeef743bab
maspypy <maspypy.gmail.com>
2022-05-02 12:36:42 +0900

    split files

---
+++ poly/convolution_naive.hpp
@@ -0,0 +1,11 @@
+template <class T>
+vector<T> convolution_naive(const vector<T>& a, const vector<T>& b) {
+  int n = int(a.size()), m = int(b.size());
+  vector<T> ans(n + m - 1);
+  if (n < m) {
+    FOR(j, m) FOR(i, n) ans[i + j] += a[i] * b[j];
+  } else {
+    FOR(i, n) FOR(j, m) ans[i + j] += a[i] * b[j];
+  }
+  return ans;
+}
