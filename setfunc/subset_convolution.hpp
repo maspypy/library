a1054d9dcceace7cbb16451e9609a8b4a8d0d75f
maspypy <maspypy.gmail.com>
2022-05-06 11:34:29 +0900

    setfunc/subset_convolution.hpp

--- setfunc/subset_convolution.hpp
+++ setfunc/subset_convolution.hpp
@@ -3,13 +3,12 @@
 template <typename T>
 vc<T> subset_convolution(vc<T>& A, vc<T>& B) {
   auto RA = ranked_zeta(A);
-  auto RB = ranked_zeta(A);
+  auto RB = ranked_zeta(B);
   int n = len(RA[0]);
   FOR(s, len(RA)) {
     vc<T> f(n + 1);
     FOR(i, n + 1) FOR(j, n - i + 1) f[i + j] += RA[s][i] * RB[s][j];
     RA[s] = f;
   }
-  FOR(s, len(RA)) RA[s] *= RB[s];
   return ranked_mobius(RA);
 }
