38c2b3110f5a4042aa1602b2d64553346834b780
maspypy <maspypy.gmail.com>
2022-05-09 20:05:47 +0900

    add test

---
+++ test/yukicoder/1641_tree_abel.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1641"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/treeabelgroup.hpp"
+#include "alg/group_xor.hpp"
+
+void solve() {
+  LL(N, Q);
+  Graph G(N);
+  VEC(int, A, N);
+  G.read_tree();
+  HLD hld(G);
+  TreeAbelGroup<decltype(hld), Group_Xor<int>, 0, 0, 1> TA(hld, A);
+  FOR_(Q) {
+    LL(t, x, y);
+    --x;
+    if (t == 1) { TA.add(x, y); }
+    if (t == 2) { print(TA.prod_subtree(x)); }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
