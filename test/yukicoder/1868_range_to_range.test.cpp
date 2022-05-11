761af881c48ab17616362de359bd153212049f0d
maspypy <maspypy.gmail.com>
2022-05-12 05:29:29 +0900

    add test

---
+++ test/yukicoder/1868_range_to_range.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1868"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/range_to_range_graph.hpp"
+#include "graph/bfs01.hpp"
+
+void solve() {
+  LL(N);
+  Range_to_Range_Graph<int> RRG(N);
+  FOR(i, N - 1) {
+    LL(r);
+    RRG.add_to_range(i, i + 1, r, 1);
+  }
+  auto G = RRG.build();
+  auto [dist, par] = bfs01(G, 0);
+  print(dist[N - 1]);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
