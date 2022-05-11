6d5721f68b5e2e11e8309481a03b2fdfa24096f7
maspypy <maspypy.gmail.com>
2022-05-12 01:09:27 +0900

    add offset

--- seq/interpolate_linear_rec.hpp
+++ seq/interpolate_linear_rec.hpp
@@ -4,7 +4,10 @@
 #include "seq/coef_of_rational_fps.hpp"
 
 template <typename mint>
-mint interpolate_linear_rec(vector<mint>& A, ll N) {
+mint interpolate_linear_rec(vector<mint>& A, ll N, int off) {
+  if(N < len(A)) return A[N];
+  A = {A.begin() + off, A.end()};
+  N -= off;
   auto G = find_linear_rec(A);
   auto F = convolution(A, G);
   F.resize(len(G) - 1);
