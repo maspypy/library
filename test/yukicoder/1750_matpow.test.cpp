c1ddc6c140c4e6837f7fba4e2eda5134dff57a8e
maspypy <maspypy.gmail.com>
2022-05-13 03:41:35 +0900

    add test

---
+++ test/yukicoder/1750_matpow.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1750"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/modint.hpp"
+#include "linalg/mat_pow.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N, M, T);
+  vv(mint, A, N, N);
+  FOR(M) {
+    LL(a, b);
+    A[a][b] = 1;
+    A[b][a] = 1;
+  }
+  A = mat_pow(A, T);
+  print(A[0][0]);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
