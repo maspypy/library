3fa8d71b50672a5f252dc280619b591f49531a59
maspypy <maspypy.gmail.com>
2022-05-15 20:12:52 +0900

    add dsu on tree

---
+++ test/aoj/2995_dsu.test.cpp
@@ -0,0 +1,65 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2995"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/dsu_on_tree.hpp"
+#include "ds/unionfind.hpp"
+
+void solve() {
+  LL(N, K);
+  Graph G(N);
+  G.read_tree();
+  HLD hld(G);
+  VEC(pi, CD, N);
+  for (auto&& [c, d]: CD) --c, --d;
+  /*
+  頂点になっている：+x
+  連結成分ごとに、ロスの min を持たせる
+  */
+  vc<int> tree(K, 1);
+  vc<int> history;
+  ll ans = 0;
+  vc<int> ANS(N);
+  UnionFind uf(K);
+
+  auto eval
+      = [&](int v) -> int { return (tree[v] ? uf.size[v] - 1 : uf.size[v]); };
+
+  auto ADD = [&](int v) -> void {
+    auto [c, d] = CD[v];
+    history.eb(c), history.eb(d);
+    c = uf[c], d = uf[d];
+    if (c == d) {
+      if (tree[c]) tree[c] = 0, ans++;
+      return;
+    }
+    ans -= eval(c) + eval(d);
+    uf.merge(c, d);
+    int r = uf[c];
+    tree[r] = tree[c] && tree[d];
+    ans += eval(r);
+  };
+  auto QUERY = [&](int v) -> void { ANS[v] = ans; };
+  auto RESET = [&]() -> void {
+    for (auto&& v: history) {
+      uf.size[v] = 1;
+      uf.par[v] = v;
+      tree[v] = 1;
+    }
+    ans = 0;
+    history.clear();
+  };
+  DSU_on_Tree(hld, ADD, QUERY, RESET);
+  for (auto&& x: ANS) print(x);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
---
+++ test/library_checker/datastructure/vertex_add_subtree_sum_dsu.test.cpp
@@ -0,0 +1,64 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_subtree_sum"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/dsu_on_tree.hpp"
+#include "ds/fenwick.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  Graph<int, 1> G(N);
+  FOR(v, 1, N) {
+    LL(p);
+    G.add(p, v);
+  }
+  G.build();
+  HLD hld(G);
+
+  vi ANS(Q, -1);
+  vvc<pair<int, int>> query(N);
+  FOR(q, Q) {
+    LL(t);
+    if (t == 0) {
+      LL(v, x);
+      query[v].eb(q, x);
+    }
+    if (t == 1) {
+      LL(v);
+      query[v].eb(q, -1);
+    }
+  }
+
+  FenwickTree<Group_Add<ll>> bit(Q);
+  vc<pair<int, int>> history;
+  ll base_sum = 0;
+
+  auto ADD = [&](int v) -> void {
+    base_sum += A[v];
+    for (auto&& [q, x]: query[v]) {
+      if (x != -1) {
+        bit.add(q, x);
+        history.eb(q, x);
+      }
+    }
+  };
+  auto QUERY = [&](int v) -> void {
+    for (auto&& [q, x]: query[v]) {
+      if (x == -1) ANS[q] = base_sum + bit.sum(q);
+    }
+  };
+  auto RESET = [&]() -> void {
+    base_sum = 0;
+    for (auto&& [q, x]: history) bit.add(q, -x);
+    history.clear();
+  };
+  DSU_on_Tree(hld, ADD, QUERY, RESET);
+  for (auto&& ans: ANS)
+    if (ans != -1) print(ans);
+}
+
+signed main() {
+  solve();
+
+  return 0;
+}
