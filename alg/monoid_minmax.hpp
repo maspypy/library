27d02f549a3561ca21679bb4587234caf94e2b14
maspypy <maspypy.gmail.com>
2022-05-15 15:49:16 +0900

    rm INF

--- alg/monoid_minmax.hpp
+++ alg/monoid_minmax.hpp
@@ -1,10 +1,14 @@
-template <class X, X INF>
+#pragma once
+
+template <class X>
 struct Monoid_MinMax {
   using P = pair<X, X>;
   using value_type = P;
   static constexpr P op(const P x, const P y) noexcept {
     return {min(x.fi, y.fi), max(x.se, y.se)};
   }
-  static constexpr P unit() { return {INF, -INF}; }
+  static constexpr P unit() {
+    return {numeric_limits<X>::max(), numeric_limits<X>::lowest()};
+  }
   static constexpr bool commute = true;
 };
