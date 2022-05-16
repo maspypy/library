e5474ce93fc7ade8b1da1b0d7c20123ad460bb32
maspypy <maspypy.gmail.com>
2022-05-16 18:02:11 +0900

    add alg

---
+++ alg/lazy_maxidx_add.hpp
@@ -0,0 +1,16 @@
+#include "alg/group_add.hpp"
+#include "alg/monoid_max_idx.hpp"
+
+template <typename E, bool tie_is_left = true>
+struct Lazy_MaxIdx_Add {
+  using MX = Monoid_Max_Idx<E, tie_is_left>;
+  using MA = Group_Add<E>;
+  using X_structure = MX;
+  using A_structure = MA;
+  using X = typename MX::value_type;
+  using A = typename MA::value_type;
+  static constexpr X act(const X &x, const A &a) {
+    if (x.fi == numeric_limits<E>::lowest()) return x;
+    return {x.fi + a, x.se};
+  }
+};
---
+++ alg/lazy_minidx_add.hpp
@@ -0,0 +1,16 @@
+#include "alg/group_add.hpp"
+#include "alg/monoid_min_idx.hpp"
+
+template <typename E, bool tie_is_left = true>
+struct Lazy_MinIdx_Add {
+  using MX = Monoid_Min_Idx<E, tie_is_left>;
+  using MA = Group_Add<E>;
+  using X_structure = MX;
+  using A_structure = MA;
+  using X = typename MX::value_type;
+  using A = typename MA::value_type;
+  static constexpr X act(const X &x, const A &a) {
+    if (x.fi == numeric_limits<E>::max()) return x;
+    return {x.fi + a, x.se};
+  }
+};
