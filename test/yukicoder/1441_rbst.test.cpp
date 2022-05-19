932b67fcb79498f988d75be91ab8210666a52977
maspypy <maspypy.gmail.com>
2022-05-19 13:44:35 +0900

    add test

---
+++ test/yukicoder/1441_rbst.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1441"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "bbst/splaytree_monoid.hpp"
+#include "alg/group_add.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  SplayTree_Monoid<Group_Add<ll>> ST;
+  auto root = ST.new_node(A);
+  FOR(Q) {
+    LL(t, l, r);
+    --l;
+    if (t == 1) {
+      auto nr = ST.split(root, r);
+      auto nm = ST.split(root, l);
+      nm = ST.new_node(nm->prod);
+      ST.merge(root, nm);
+      ST.merge(root, nr);
+    }
+    if (t == 2) { print(ST.prod(root, l, r)); }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
