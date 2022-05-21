c28db5423e2c7ee2ad4b36a9d3ee54f5df55018e
maspypy <maspypy.gmail.com>
2022-05-21 17:44:31 +0900

    surj

---
+++ seq/surjection.hpp
@@ -0,0 +1,15 @@
+// n 元集合からの全射の数え上げ
+template <typename mint>
+vc<mint> surjection_n(int n, int k_max) {
+  auto f = stirling_number_2_n<mint>(n, k_max);
+  FOR(i, k_max + 1) f[i] *= fact<mint>(i);
+  return f;
+}
+
+// k 元集合へのの全射の数え上げ
+template <typename mint>
+vc<mint> surjection_k(int k, int n_max) {
+  auto f = stirling_number_2_k<mint>(k, n_max);
+  FOR(i, n_max + 1) f[i] *= fact<mint>(k);
+  return f;
+}
