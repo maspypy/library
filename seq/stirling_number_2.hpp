0498cc395928781268b11ec24d54d699b23e04f5
maspypy <maspypy.gmail.com>
2022-05-21 17:44:21 +0900

    improve algorithm

--- seq/stirling_number_2.hpp
+++ seq/stirling_number_2.hpp
@@ -1,20 +1,23 @@
 #include "poly/convolution.hpp"
 #include "poly/fps_pow.hpp"
+#include "mod/powertable.hpp"
 
 // n 個のもの (labeled) を k グループ (no label) に分ける方法
+// label をつけることで、全射の数え上げに利用できる
 template <typename mint>
-vc<mint> stirling_number_2_n(int n) {
-  vc<mint> a(n + 1), b(n + 1);
-  FOR(i, n + 1) a[i] = mint(i).pow(n);
-  FOR(i, n + 1) b[i] = (i % 2 == 0 ? 1 : -1);
-  FOR(i, n + 1) a[i] *= fact_inv<mint>(i);
-  FOR(i, n + 1) b[i] *= fact_inv<mint>(i);
+vc<mint> stirling_number_2_n(int n, int k_max) {
+  vc<mint> a = powertable_2<mint>(n, k_max + 1);
+  FOR(i, k_max + 1) a[i] *= fact_inv<mint>(i);
+  vc<mint> b(k_max + 1);
+  FOR(i, 1, k_max + 1, 2) b[i] = -b[i];
+  FOR(i, k_max + 1) b[i] *= fact_inv<mint>(i);
   auto f = convolution(a, b);
-  f.resize(n + 1);
+  f.resize(k_max + 1);
   return f;
 }
 
 // n 個のもの (labeled) を k グループ (no label) に分ける方法
+// label をつけることで、全射の数え上げに利用できる
 template <typename mint>
 vc<mint> stirling_number_2_k(int k, int n_max) {
   if (k > n_max) { return vc<mint>(n_max + 1); }
