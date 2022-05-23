ac978b0206f87a5140fa296c7201ac777b91e517
maspypy <maspypy.gmail.com>
2022-05-23 16:56:22 +0900

    path binary search(edge)

--- graph/lazytreemonoid.hpp
+++ graph/lazytreemonoid.hpp
@@ -46,7 +46,7 @@ struct LazyTreeMonoid {
     for (auto &&[a, b]: pd) {
       X x = (a <= b ? seg.prod(a, b + 1)
                     : (MonoX::commute ? seg.prod(b, a + 1)
-                                       : seg_r.prod(b, a + 1)));
+                                      : seg_r.prod(b, a + 1)));
       val = MonoX::op(val, x);
     }
     return val;
@@ -57,16 +57,14 @@ struct LazyTreeMonoid {
     return seg.prod(l + edge, r);
   }
 
-  X prod_all() {
-    return seg.prod_all();
-  }
+  X prod_all() { return seg.prod_all(); }
 
   void apply_path(int u, int v, A a) {
     auto pd = hld.get_path_decomposition(u, v, edge);
     for (auto &&[x, y]: pd) {
       int l = min(x, y), r = max(x, y);
       seg.apply(l, r + 1, a);
-      if(!MonoX::commute) seg_r.apply(l, r + 1, a);
+      if (!MonoX::commute) seg_r.apply(l, r + 1, a);
     }
   }
 
@@ -82,8 +80,75 @@ struct LazyTreeMonoid {
     seg_r.debug();
   }
 
-  void doc() {
-    print("HL分解 + セグ木。");
-    print("部分木クエリ O(logN) 時間、パスクエリ O(log^2N) 時間。");
+  template <class F>
+  int max_path(F &check, int u, int v) {
+    if (edge) return max_path_edge(check, u, v);
+    if (!check(prod_path(u, u))) return -1;
+    auto pd = hld.get_path_decomposition(u, v, edge);
+    X val = Monoid::unit();
+    for (auto &&[a, b]: pd) {
+      X x = (a <= b ? seg.prod(a, b + 1)
+                    : (Monoid::commute ? seg.prod(b, a + 1)
+                                       : seg_r.prod(b, a + 1)));
+      if (check(Monoid::op(val, x))) {
+        val = Monoid::op(val, x);
+        u = (hld.V[b]);
+        continue;
+      }
+      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
+      if (a <= b) {
+        // 下り
+        auto i = seg.max_right(check_tmp, a);
+        return (i == a ? u : hld.V[i - 1]);
+      } else {
+        // 上り
+        auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
+                                  : seg_r.min_left(check_tmp, a + 1));
+        if (i == a + 1) return u;
+        return hld.parent[hld.V[i]];
+      }
+    }
+    return v;
+  }
+
+private:
+  template <class F>
+  int max_path_edge(F &check, int u, int v) {
+    assert(edge);
+    if (!check(Monoid::unit())) return -1;
+    int lca = hld.lca(u, v);
+    auto pd = hld.get_path_decomposition(u, lca, edge);
+    X val = Monoid::unit();
+
+    // climb
+    for (auto &&[a, b]: pd) {
+      assert(a >= b);
+      X x = (Monoid::commute ? seg.prod(b, a + 1) : seg_r.prod(b, a + 1));
+      if (check(Monoid::op(val, x))) {
+        val = Monoid::op(val, x);
+        u = (hld.parent[hld.V[b]]);
+        continue;
+      }
+      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
+      auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
+                                : seg_r.min_left(check_tmp, a + 1));
+      if (i == a + 1) return u;
+      return hld.parent[hld.V[i]];
+    }
+    // down
+    pd = hld.get_path_decomposition(lca, v, edge);
+    for (auto &&[a, b]: pd) {
+      assert(a <= b);
+      X x = seg.prod(a, b + 1);
+      if (check(Monoid::op(val, x))) {
+        val = Monoid::op(val, x);
+        u = (hld.V[b]);
+        continue;
+      }
+      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
+      auto i = seg.max_right(check_tmp, a);
+      return (i == a ? u : hld.V[i - 1]);
+    }
+    return v;
   }
 };
