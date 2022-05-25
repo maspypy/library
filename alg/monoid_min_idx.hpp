1b030cb7dd4beb25cf5d945bc0e16d860dde2b00
maspypy <maspypy.gmail.com>
2022-05-26 00:06:40 +0900

    fix

--- alg/monoid_min_idx.hpp
+++ alg/monoid_min_idx.hpp
@@ -1,10 +1,11 @@
 template <typename T, bool tie_is_left = true>
 struct Monoid_Min_Idx {
-  using value_type = pair<T, ll>;
+  using value_type = pair<T, int>;
   using X = value_type;
   static X op(X x, X y) {
     if (x.fi < y.fi) return x;
     if (x.fi > y.fi) return y;
+    if (x.se > y.se) swap(x, y);
     return (tie_is_left ? x : y);
   }
   static constexpr X unit() { return {numeric_limits<T>::max(), -1}; }
