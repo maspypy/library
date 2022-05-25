d348efe82445071ccacecac06988fb54c322709f
maspypy <maspypy.gmail.com>
2022-05-26 00:09:03 +0900

    add limit shape option

--- seq/enumerate_partitions.hpp
+++ seq/enumerate_partitions.hpp
@@ -8,14 +8,16 @@ partition は、減少列として辞書式の降順に列挙する
 ・N = 80（15796476）：1100 ms
 */
 template <typename F>
-void enumerate_partitions(int N, F query) {
+void enumerate_partitions(int N, F query, int LIM_len = -1, int LIM_val = -1) {
   assert(N >= 0);
   auto dfs = [&](auto self, vc<int>& p, int sum) -> void {
     if (sum == N) {
       query(p);
       return;
     }
+    if (LIM_len != -1 && len(p) == LIM_len) return;
     int nxt = (len(p) == 0 ? N : p.back());
+    if (LIM_val != -1) chmin(nxt, LIM_val);
     chmin(nxt, N - sum);
     p.eb(0);
     FOR3_R(x, 1, nxt + 1) {
