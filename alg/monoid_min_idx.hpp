50e5d2b9140190e683f7a3711106d8564c848346
maspypy <maspypy.gmail.com>
2022-05-27 16:21:11 +0900

    tie break

--- alg/monoid_min_idx.hpp
+++ alg/monoid_min_idx.hpp
@@ -2,12 +2,12 @@ template <typename T, bool tie_is_left = true>
 struct Monoid_Min_Idx {
   using value_type = pair<T, int>;
   using X = value_type;
-  static X op(X x, X y) {
-    if (x.fi < y.fi) return x;
-    if (x.fi > y.fi) return y;
-    if (x.se > y.se) swap(x, y);
-    return (tie_is_left ? x : y);
+  static constexpr bool is_small(const X& x, const X& y) {
+    if (x.fi < y.fi) return true;
+    if (x.fi > y.fi) return false;
+    return (tie_is_left ? (x.se < y.se) : (x.se >= y.se));
   }
+  static X op(X x, X y) { return (is_small(x, y) ? x : y); }
   static constexpr X unit() { return {numeric_limits<T>::max(), -1}; }
   static constexpr bool commute = true;
 };
