07c315115a3eb0088b0958de9d9e4c1ad38fff55
maspypy <maspypy.gmail.com>
2022-05-27 14:32:07 +0900

    add test

---
+++ test/library_checker/datastructure/segment_add_get_min_dynamic.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/segment_add_get_min"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/dynamic_lichao.hpp"
+
+void solve() {
+  LL(N, Q);
+  ll max = 1'000'000'000;
+  Dynamic_LiChaoTree<ll, 14'000'000> seg(-max, max + 1);
+  FOR(N) {
+    LL(l, r, a, b);
+    seg.add_segment(l, r, a, b);
+  }
+  const ll INF = 1LL << 60;
+  FOR(Q) {
+    LL(t);
+    if (t == 0) {
+      LL(l, r, a, b);
+      seg.add_segment(l, r, a, b);
+    }
+    if (t == 1) {
+      LL(x);
+      ll y = seg.query(x);
+      if (y >= INF)
+        print("INFINITY");
+      else
+        print(y);
+    }
+  }
+}
+
+signed main() {
+  solve();
+
+  return 0;
+}
---
+++ test/yukicoder/1326_bct.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1326"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/biconnected_component.hpp"
+#include "graph/treeabelgroup.hpp"
+#include "alg/group_add.hpp"
+
+void solve() {
+  LL(N, M);
+  Graph<int> G(N);
+  G.read_graph(M);
+  Biconnected_Component<decltype(G)> BC(G);
+
+  auto T = BC.BCT();
+  HLD hld(T);
+
+  vc<int> dat(T.N);
+  FOR(v, N) if (BC.is_articulation(v)) { dat[BC.BCT_idx_v(v)]++; }
+  TreeAbelGroup<decltype(hld), Group_Add<int>, 0, 1, 0> TA(hld, dat);
+
+  LL(Q);
+  FOR(Q) {
+    LL(x, y);
+    --x, --y;
+    auto a = BC.BCT_idx_v(x);
+    auto b = BC.BCT_idx_v(y);
+    if (a == b) {
+      print(0);
+      continue;
+    }
+    ll k = TA.prod_path(a, b);
+    if (BC.is_articulation(x)) --k;
+    if (BC.is_articulation(y)) --k;
+    print(k);
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
