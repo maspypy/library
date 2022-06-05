84f3ceaf4eba10bd1e1fd3b595cd0ae471604204
maspypy <maspypy.gmail.com>
2022-06-05 15:16:23 +0900

    abstract discrete log

--- other/discrete_log.hpp
+++ other/discrete_log.hpp
@@ -1,34 +1,44 @@
 // log_a b の計算
-// 「*」 が定義されたクラス、ハッシュ関数 H を持たせる
-// lb 以上の解をかえす
-template <typename X>
-ll discrete_log(X a, X b, ll LIM, function<ll(X)> H, int lb = 0) {
+// ハッシュ関数 H : X -> long long を持たせる
+// [lb, rb) の最初の解をかえす
+// なければ -1
+template <typename Group, typename X>
+ll discrete_log(Group::X a, Group::X b, function<ll(Group::X)> H, ll lb, ll ub) {
+  if (lb >= ub) return -1;
   {
-    int n = lb;
+    ll n = lb;
     X p = a;
+    X x = Group::unit();
     while (n) {
-      if (n & 1) b /= p;
-      p *= p;
+      if (n & 1) x = Group::op(x, a);
+      p = Group::op(p, p);
       n /= 2;
     }
+    x = Group::inverse(x);
+    b = Group::op(b, x);
   }
+  ll LIM = ub - lb;
 
   ll K = 1;
   while (K * K < LIM) ++K;
 
-  unordered_map<ll, int> MP;
-  MP.reserve(K + 1);
-  X p = 1;
+  static HashMapLL<int, 20> MP;
+  MP.reset();
+
+  X p = Group::unit();
   FOR(k, K + 1) {
     auto key = H(p);
     if (!MP.count(key)) MP[key] = k;
-    if (k != K) p = p * a;
+    if (k != K) p = Group::op(p, a);
   }
-  p = X(1) / p;
+  p = Group::inverse(p);
   FOR(k, K + 1) {
     auto key = H(b);
-    if (MP.count(key)) return k * K + MP[key] + lb;
-    b *= p;
+    if (MP.count(key)) {
+      ll res = k * K + MP[key] + lb;
+      return (res >= ub ? -1 : res);
+    }
+    b = Group::op(b, p);
   }
   return -1;
 }
