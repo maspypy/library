5ca5e0c91e1b3fc3089e853be169a66f26a1ad7f
maspypy <maspypy.gmail.com>
2022-06-05 16:18:07 +0900

    add missing X

--- alg/group_mul.hpp
+++ alg/group_mul.hpp
@@ -1,6 +1,7 @@
-template <class X>
+template <class T>
 struct Group_Mul {
-  using value_type = X;
+  using value_type = T;
+  using X = T;
   static constexpr X op(const X &x, const X &y) noexcept { return x * y; }
   static constexpr X inverse(const X &x) noexcept { return X(1) / x; }
   static constexpr X unit() { return X(1); }
