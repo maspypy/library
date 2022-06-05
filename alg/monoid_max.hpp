c703f59f6f91e5ef58a77b8039cec17c5f6e04cf
maspypy <maspypy.gmail.com>
2022-06-05 12:22:23 +0900

    fix

--- alg/monoid_max.hpp
+++ alg/monoid_max.hpp
@@ -3,6 +3,6 @@ template <class X>
 struct Monoid_Max {
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return max(x, y); }
-  static constexpr X unit() { return -numeric_limits<X>::lowest(); }
+  static constexpr X unit() { return numeric_limits<X>::lowest(); }
   static constexpr bool commute = true;
 };
