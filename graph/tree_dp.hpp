3e77346a549b25e5357140dc4867d171b957ce12
maspypy <maspypy.gmail.com>
2022-06-08 22:19:28 +0900

    add root

--- graph/tree_dp.hpp
+++ graph/tree_dp.hpp
@@ -3,7 +3,7 @@
 // 木全体に対する結果を return する。
 // 部分木ごとに処理したい場合は、fev の最後に行うようにせよ。
 template <typename Graph, typename Data, typename F1, typename F2, typename F3>
-Data tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit) {
+Data tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit, int root = 0) {
   int N = G.N;
 
   auto dfs = [&](auto& dfs, int v, int p) -> Data {
@@ -14,5 +14,5 @@ Data tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit) {
     }
     return fev(x, v);
   };
-  return dfs(dfs, 0, -1);
+  return dfs(dfs, root, -1);
 }
