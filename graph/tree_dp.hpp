c960f5b485b8eddea82f5b8c60dd66dea33eeed8
maspypy <maspypy.gmail.com>
2022-06-08 14:41:16 +0900

    stop create dp table

--- graph/tree_dp.hpp
+++ graph/tree_dp.hpp
@@ -1,33 +1,18 @@
-// https://codeforces.com/contest/735/problem/E
 #include "graph/base.hpp"
-#include "graph/hld.hpp"
 
+// 木全体に対する結果を return する。
+// 部分木ごとに処理したい場合は、fev の最後に行うようにせよ。
 template <typename Graph, typename Data, typename F1, typename F2, typename F3>
-vc<Data> tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit) {
-  using E = typename Graph::edge_type;
-
+Data tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit) {
   int N = G.N;
-  HLD hld(G);
-  auto V = hld.V;
-  auto VR = V;
-  reverse(all(VR));
-  auto par = hld.parent;
 
-  vc<Data> dpv(N);
-  vc<Data> dpe1(N);
-
-  for (auto&& v: VR) {
-    auto val = unit;
-    E e0;
+  auto dfs = [&](auto& dfs, int v, int p) -> Data {
+    Data x = unit;
     for (auto&& e: G[v]) {
-      if (e.to != par[v]) {
-        val = fee(val, dpe1[e.to]);
-      } else {
-        e0 = e;
-      }
+      if (e.to == p) continue;
+      x = fee(x, fve(dfs(dfs, e.to, v), e));
     }
-    dpv[v] = fev(val, v);
-    dpe1[v] = (v ? fve(dpv[v], e0) : unit);
-  }
-  return dpv;
+    return fev(x, v);
+  };
+  return dfs(dfs, 0, -1);
 }
