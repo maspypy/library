0c23ad9f1f474af7db3fd249cf98d701683a88a1
maspypy <maspypy.gmail.com>
2022-06-27 16:36:33 +0900

    add X

--- alg/group_add.hpp
+++ alg/group_add.hpp
@@ -1,10 +1,12 @@
 #pragma once
-template <class X>
+
+template <typename E>
 struct Group_Add {
+  using X = E;
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return x + y; }
   static constexpr X inverse(const X &x) noexcept { return -x; }
-  static constexpr X power(const X &x, ll n) noexcept { return n * x; }
+  static constexpr X power(const X &x, ll n) noexcept { return X(n) * x; }
   static constexpr X unit() { return X(0); }
   static constexpr bool commute = true;
 };
