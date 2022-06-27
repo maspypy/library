7cca67a3f66295d112a5fbdee98e5498f3bfec44
maspypy <maspypy.gmail.com>
2022-06-27 16:37:50 +0900

    reduce template

--- ds/point_add_rectangle_sum.hpp
+++ ds/point_add_rectangle_sum.hpp
@@ -1,46 +1,53 @@
+#pragma once
 #include "ds/fenwick.hpp"
 
-// 点群はクエリより前に全部入力すること (add_pt)。
-// 同じ点群に対してクエリをやり直せる。
-// SMALL=true にすると、座圧をしないため少し高速
+
 template <typename AbelGroup, bool SMALL = false>
 struct Point_Add_Rectangle_Sum {
   using G = typename AbelGroup::value_type;
   int Q;
-  vi X, Y;
-  vc<G> WT;
-  vc<tuple<ll, ll, ll, ll>> rect;
+  vector<ll> X, Y;
+  vector<G> WT;
+  vector<tuple<ll, ll, ll, ll>> rect;
 
   Point_Add_Rectangle_Sum() {}
 
-  void add_query(ll x, ll y, G w) { X.eb(x), Y.eb(y), WT.eb(w); }
+  void add_query(ll x, ll y, G w) {
+    X.emplace_back(x), Y.emplace_back(y), WT.emplace_back(w);
+  }
 
-  void sum_query(ll xl, ll yl, ll xr, ll yr) { rect.eb(xl, yl, xr, yr); }
+  void sum_query(ll xl, ll yl, ll xr, ll yr) {
+    rect.emplace_back(xl, yl, xr, yr);
+  }
 
-  vc<G> calc() {
-    int N = len(X), Q = len(rect);
-    if (N == 0) return vc<G>(Q, AbelGroup::unit());
+  vector<G> calc() {
+    int N = X.size(), Q = rect.size();
+    if (N == 0) return vector<G>(Q, AbelGroup::unit());
     int NX = 0, NY = 0;
     if (!SMALL) {
-      vi keyX = X, keyY = Y;
-      UNIQUE(keyX), UNIQUE(keyY);
-      NX = len(keyX), NY = len(keyY);
-      for (auto&& x: X) x = LB(keyX, x);
-      for (auto&& y: Y) y = LB(keyY, y);
-      for (auto&& [xl, yl, xr, yr]: rect) {
-        xl = LB(keyX, xl);
-        xr = LB(keyX, xr);
-        yl = LB(keyY, yl);
-        yr = LB(keyY, yr);
+      vector<ll> keyX = X, keyY = Y;
+      sort(all(keyX));
+      keyX.erase(unique(all(keyX)), keyX.end());
+      sort(all(keyY));
+      keyY.erase(unique(all(keyY)), keyY.end());
+      NX = keyX.size(), NY = keyY.size();
+      for (auto &&x: X) x = lower_bound(all(keyX), x) - keyX.begin();
+      for (auto &&y: Y) y = lower_bound(all(keyY), y) - keyY.begin();
+      for (auto &&[xl, yl, xr, yr]: rect) {
+        xl = lower_bound(all(keyX), xl) - keyX.begin();
+        xr = lower_bound(all(keyX), xr) - keyX.begin();
+        yl = lower_bound(all(keyY), yl) - keyY.begin();
+        yr = lower_bound(all(keyY), yr) - keyY.begin();
       }
     }
     if (SMALL) {
-      ll mx = MIN(X);
-      ll my = MIN(Y);
-      for (auto&& x: X) x -= mx;
-      for (auto&& y: Y) y -= my;
-      NX = MAX(X) + 1, NY = MAX(Y) + 1;
-      for (auto&& [xl, yl, xr, yr]: rect) {
+      ll mx = *(min_element(X.begin(), X.end()));
+      ll my = *(min_element(Y.begin(), Y.end()));
+      for (auto &&x: X) x -= mx;
+      for (auto &&y: Y) y -= my;
+      NX = *(max_element(X.begin(), X.end())) + 1;
+      NY = *(max_element(Y.begin(), Y.end())) + 1;
+      for (auto &&[xl, yl, xr, yr]: rect) {
         xl -= mx, xr -= mx;
         yl -= my, yr -= my;
         xl = max(0, min<int>(xl, NX));
@@ -49,29 +56,29 @@ struct Point_Add_Rectangle_Sum {
         yr = max(0, min<int>(yr, NY));
       }
     }
-    vvc<pair<int, G>> ADD(NY + 1);
-    vvc<tuple<int, int, int>> CALC_1(NY + 1);
-    vvc<tuple<int, int, int>> CALC_2(NY + 1);
-    FOR(n, N) { ADD[Y[n]].eb(X[n], WT[n]); }
-    FOR(q, Q) {
-      auto& [xl, yl, xr, yr] = rect[q];
-      CALC_1[yr].eb(xl, xr, q);
-      CALC_2[yl].eb(xl, xr, q);
+    vector<vector<pair<int, G>>> ADD(NY + 1);
+    vector<vector<tuple<int, int, int>>> CALC_1(NY + 1);
+    vector<vector<tuple<int, int, int>>> CALC_2(NY + 1);
+    for (int n = 0; n < N; ++n) { ADD[Y[n]].emplace_back(X[n], WT[n]); }
+    for (int q = 0; q < Q; ++q) {
+      auto &[xl, yl, xr, yr] = rect[q];
+      CALC_1[yr].emplace_back(xl, xr, q);
+      CALC_2[yl].emplace_back(xl, xr, q);
     };
 
-    vc<G> res(Q, AbelGroup::unit());
+    vector<G> res(Q, AbelGroup::unit());
     FenwickTree<AbelGroup> bit(NX);
 
-    FOR(y, NY + 1) {
-      for (auto&& [xl, xr, q]: CALC_1[y]) {
+    for (int y = 0; y <= NY; ++y) {
+      for (auto &&[xl, xr, q]: CALC_1[y]) {
         auto x = bit.sum(xl, xr);
         res[q] = AbelGroup::op(res[q], x);
       }
-      for (auto&& [xl, xr, q]: CALC_2[y]) {
+      for (auto &&[xl, xr, q]: CALC_2[y]) {
         auto x = AbelGroup::inverse(bit.sum(xl, xr));
         res[q] = AbelGroup::op(res[q], x);
       }
-      for (auto&& [x, g]: ADD[y]) { bit.add(x, g); }
+      for (auto &&[x, g]: ADD[y]) { bit.add(x, g); }
     }
     return res;
   }
