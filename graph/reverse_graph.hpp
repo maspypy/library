70c196cb6cd4d44bd2162e2ba64c5d07da3b80f1
maspypy <maspypy.gmail.com>
2022-07-10 21:47:01 +0900

    fix

--- graph/reverse_graph.hpp
+++ graph/reverse_graph.hpp
@@ -4,6 +4,6 @@ Graph reverse_graph(Graph& G) {
   assert(G.is_directed());
   Graph G1(G.N);
   for (auto&& e: G.edges) { G1.add(e.to, e.frm, e.cost, e.id); }
-  G1.prepare();
+  G1.build();
   return G1;
 }
