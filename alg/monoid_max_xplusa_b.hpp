aa3fd24cee31fc7d78d6469da0048f8d45514b65
maspypy <maspypy.gmail.com>
2022-07-10 21:46:47 +0900

    refactoring

--- alg/monoid_max_xplusa_b.hpp
+++ alg/monoid_max_xplusa_b.hpp
@@ -5,16 +5,22 @@ template <typename T>
 struct Monoid_Max_xplusa_b {
   using value_type = pi;
   using X = value_type;
+  static const T LIM = numeric_limits<T>::lowest();
   static T add(T x, T y) {
-    if (x == numeric_limits<T>::lowest()) return x;
-    if (y == numeric_limits<T>::lowest()) return y;
+    if (x == LIM) return LIM;
+    if (y == LIM) return LIM;
     return x + y;
   }
+  static T eval(X f, T x) {
+    auto [a, b] = f;
+    return max(x + a, b);
+  }
+
   static X op(X A, X B) {
     auto [a, b] = A;
     auto [c, d] = B;
     return {add(a, c), max(add(b, c), d)};
   }
-  static constexpr X unit() { return {0, numeric_limits<T>::lowest()}; }
+  static constexpr X unit() { return {0, LIM}; }
   static constexpr bool commute = false;
 };
