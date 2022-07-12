132b36476902d4b6b298558ad7bcbfa4e41bbf49
maspypy <maspypy.gmail.com>
2022-07-12 23:22:22 +0900

    add test

---
+++ test/library_checker/datastructure/range_affine_point_add.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_point_get"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "ds/dualsegtree.hpp"
+#include "alg/group_affine.hpp"
+#include "mod/modint.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N, Q);
+  VEC(mint, A, N);
+  DualSegTree<Group_Affine<mint>> seg(N);
+  FOR(Q) {
+    LL(t);
+    if (t == 0) {
+      LL(l, r, b, c);
+      seg.apply(l, r, {mint(b), mint(c)});
+    }
+    if (t == 1) {
+      LL(idx);
+      mint x = A[idx];
+      auto [a, b] = seg.get(idx);
+      print(a * x + b);
+    }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
