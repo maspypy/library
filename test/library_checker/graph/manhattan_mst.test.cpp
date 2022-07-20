af430ed3bb02a4ea333ed4cd0d3e3aa8c8fb0e05
maspypy <maspypy.gmail.com>
2022-07-20 17:24:10 +0900

    manhattan mst

---
+++ test/library_checker/graph/manhattan_mst.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/manhattanmst"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "geo/manhattan_mst.hpp"
+
+void solve() {
+  LL(N);
+  using P = pair<int, int>;
+  VEC(P, XY, N);
+  auto G = Manhattan_MST<int>(XY);
+  ll ANS = 0;
+  for (auto&& e: G.edges) { ANS += e.cost; }
+  print(ANS);
+  for (auto&& e: G.edges) { print(e.frm, e.to); }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
