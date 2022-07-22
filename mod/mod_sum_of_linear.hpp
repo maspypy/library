2b172179c57159ebf0e48f1f42002b401bebdce4
maspypy <maspypy.gmail.com>
2022-07-22 22:14:52 +0900

    change name

---
+++ mod/mod_sum_of_linear.hpp
@@ -0,0 +1,11 @@
+#include "mod/floor_sum_of_linear.hpp"
+i128 mod_sum_of_linear(ll L, ll R, ll a, ll b, ll mod) {
+  /*
+  sum_{x in [L,R)} (ax + b mod mod)
+  */
+  i128 s = a * L + b;
+  i128 t = a * (R - 1) + b;
+  i128 sum = (s + t) * (R - L) / 2;
+  i128 fsum = floor_sum(L, R, a, b, mod);
+  return sum - fsum * mod;
+}
