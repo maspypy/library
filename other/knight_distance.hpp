8d68a7d246f2091e5283a5756007467e3198dd4a
maspypy <maspypy.gmail.com>
2022-07-31 08:50:18 +0900

    knight dist

---
+++ other/knight_distance.hpp
@@ -0,0 +1,10 @@
+ll Knight_Distance(ll x, ll y) {
+  x = abs(x), y = abs(y);
+  if (x + y == 0) return 0;
+  if (x + y == 1) return 3;
+  if (x == 2 && y == 2) return 4;
+  ll step = (max(x, y) + 1) / 2;
+  chmax(step, (x + y + 2) / 3);
+  step += (step ^ x ^ y) & 1;
+  return step;
+}
