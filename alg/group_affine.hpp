19f636f8cf3033123d5690a56bc14998fe7746d8
maspypy <maspypy.gmail.com>
2022-07-31 08:49:47 +0900

    a

--- alg/group_affine.hpp
+++ alg/group_affine.hpp
@@ -3,14 +3,14 @@ struct Group_Affine {
   using F = pair<K, K>;
   using value_type = F;
   static constexpr F op(const F &x, const F &y) noexcept {
-    return F({x.fi * y.fi, x.se * y.fi + y.se});
+    return F({x.first * y.first, x.second * y.first + y.second});
   }
-  static constexpr F inverse(const F &x) {
+  static constexpr F inversecond(const F &x) {
     auto [a, b] = x;
     a = K(1) / a;
     return {a, a * (-b)};
   }
-  static constexpr K eval(const F &f, K x) noexcept { return f.fi * x + f.se; }
+  static constexpr K eval(const F &f, K x) noexcept { return f.first * x + f.second; }
   static constexpr F unit() { return {K(1), K(0)}; }
   static constexpr bool commute = false;
 };
