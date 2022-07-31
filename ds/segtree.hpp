37237883ee9e0b2279914177d0cc175d5152f064
maspypy <maspypy.gmail.com>
2022-07-31 09:18:22 +0900

    format

--- ds/segtree.hpp
+++ ds/segtree.hpp
@@ -4,26 +4,26 @@ template <class Monoid>
 struct SegTree {
   using X = typename Monoid::value_type;
   using value_type = X;
-  vc<X> dat;
+  vector<X> dat;
   int n, log, size;
 
   SegTree() : SegTree(0) {}
-  SegTree(int n) : SegTree(vc<X>(n, Monoid::unit())) {}
-  SegTree(vc<X> v) : n(len(v)) {
+  SegTree(int n) : SegTree(vector<X>(n, Monoid::unit())) {}
+  SegTree(vector<X> v) : n(v.size()) {
     log = 1;
     while ((1 << log) < n) ++log;
     size = 1 << log;
     dat.assign(size << 1, Monoid::unit());
-    FOR(i, n) dat[size + i] = v[i];
-    FOR3_R(i, 1, size) update(i);
+    for (int i = 0; i < n; ++i) dat[size + i] = v[i];
+    for (int i = size - 1; i >= 1; --i) update(i);
   }
 
   void reset() { fill(all(dat), Monoid::unit()); }
 
-  void set_all(const vc<X>& v){
+  void set_all(const vector<X>& v) {
     dat.assign(size << 1, Monoid::unit());
-    FOR(i, n) dat[size + i] = v[i];
-    FOR3_R(i, 1, size) update(i);
+    for (int i = 0; i < n; ++i) dat[size + i] = v[i];
+    for (int i = size - 1; i >= 1; --i) update(i);
   }
 
   X operator[](int i) { return dat[size + i]; }
@@ -36,7 +36,7 @@ struct SegTree {
     while (i >>= 1) update(i);
   }
 
-  void multiply(int i, const X& x){
+  void multiply(int i, const X& x) {
     assert(i < n);
     i += size;
     dat[i] = Monoid::op(dat[i], x);
@@ -59,7 +59,7 @@ struct SegTree {
   X prod_all() { return dat[1]; }
 
   template <class F>
-  int max_right(F &check, int L) {
+  int max_right(F& check, int L) {
     assert(0 <= L && L <= n && check(Monoid::unit()));
     if (L == n) return n;
     L += size;
@@ -83,7 +83,7 @@ struct SegTree {
   }
 
   template <class F>
-  int min_left(F &check, int R) {
+  int min_left(F& check, int R) {
     assert(0 <= R && R <= n && check(Monoid::unit()));
     if (R == 0) return 0;
     R += size;
@@ -111,7 +111,7 @@ struct SegTree {
   X Xor_prod(int l, int r, int xor_val) {
     assert(Monoid::commute);
     X x = Monoid::unit();
-    FOR(k, log + 1) {
+    for (int k = 0; k < log + 1; ++k) {
       if (l >= r) break;
       if (l & 1) { x = Monoid::op(x, dat[(size >> k) + ((l++) ^ xor_val)]); }
       if (r & 1) { x = Monoid::op(x, dat[(size >> k) + ((--r) ^ xor_val)]); }
