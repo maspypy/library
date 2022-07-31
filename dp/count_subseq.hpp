af6da3d3d5447daab5566e65aff1146cfdd2b118
maspypy <maspypy.gmail.com>
2022-07-31 09:53:37 +0900

    a

--- dp/count_subseq.hpp
+++ dp/count_subseq.hpp
@@ -1,13 +1,16 @@
-template <typename mint, typename T>
-mint count_subseq(vc<T> S) {
+template <typename mint, typename T, typename STRING>
+mint count_subseq(STRING S) {
+  auto key = S;
+  UNIQUE(key);
+  for(auto&& x : S)x=LB(key,x);
+  vc<int> IDX(len(key),-1);
   ll N = len(S);
-  map<T, ll> IDX;
   vc<mint> DP(N + 1);
   DP[0] = 1;
   FOR(i, N) {
     T c = S[i];
     DP[i + 1] = DP[i] + DP[i];
-    if (IDX.count(c)) { DP[i + 1] -= DP[IDX[c]]; }
+    if (IDX[c] != -1) { DP[i + 1] -= DP[IDX[c]]; }
     IDX[c] = i;
   }
   return DP.back();
