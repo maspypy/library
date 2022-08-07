1f968439bee03d2df7c965c428c576fd981350df
maspypy <maspypy.gmail.com>
2022-08-07 22:07:21 +0900

    add test

---
+++ test/mytest/factorial_998.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "other/random.hpp"
+
+#include "mod/factorial998.hpp"
+#include "poly/prefix_product_of_poly.hpp"
+
+using mint = modint998;
+
+void test() {
+  RandomNumberGenerator RNG;
+  FOR(10) {
+    int x = RNG(0, mint::get_mod());
+    // int x = t;
+    int a = factorial998(x);
+    vc<mint> f = {1, 1};
+    int b = prefix_product_of_poly(f, x).val;
+    assert(a == b);
+  }
+}
+
+void solve() {
+  LL(a, b);
+  print(a + b);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  test();
+  solve();
+
+  return 0;
+}
