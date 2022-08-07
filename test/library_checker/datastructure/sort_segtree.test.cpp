cb660c9980688df3f1320a224eaf839faeed1014
maspypy <maspypy.gmail.com>
2022-08-07 14:21:31 +0900

    verify sort segtree

---
+++ test/library_checker/datastructure/sort_segtree.test.cpp
@@ -0,0 +1,56 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/point_set_range_sort_range_composite"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/sortable_segtree.hpp"
+#include "alg/group_affine.hpp"
+#include "mod/modint.hpp"
+
+using mint = modint998;
+
+void solve() {
+  // クエリ先読みなしの方
+
+  using AFF = Group_Affine<mint>;
+  LL(N, Q);
+  vc<int> key(N);
+  vc<pair<mint, mint>> seg_raw(N);
+  FOR(i, N) { read(key[i]), read(seg_raw[i]); }
+
+  Sortable_SegTree<AFF> seg(1LL << 30, key, seg_raw);
+
+  int i, p, l, r;
+  pair<mint, mint> f;
+  mint x;
+
+  FOR(Q) {
+    LL(t);
+    if (t == 0) {
+      read(i), read(p), read(f);
+      seg.set(i, p, f);
+    }
+    if (t == 1) {
+      read(l), read(r), read(x);
+      auto f = seg.prod(l, r);
+      print(AFF::eval(f, x));
+    }
+    if (t == 2) {
+      read(l), read(r);
+      seg.sort_inc(l, r);
+    }
+    if (t == 3) {
+      read(l), read(r);
+      seg.sort_dec(l, r);
+    }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  FOR(T) solve();
+  return 0;
+}
---
+++ test/library_checker/datastructure/sort_segtree_1.test.cpp
@@ -0,0 +1,67 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/point_set_range_sort_range_composite"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/sortable_segtree.hpp"
+#include "alg/group_affine.hpp"
+#include "mod/modint.hpp"
+
+using mint = modint998;
+
+void solve() {
+  // クエリ先読みする方
+
+  using AFF = Group_Affine<mint>;
+  LL(N, Q);
+  vc<int> key(N);
+  vc<pair<mint, mint>> seg_raw(N);
+  FOR(i, N) { read(key[i]), read(seg_raw[i]); }
+
+  vc<int> all_key = key;
+  using QT = tuple<int, int, int, int, int>;
+  vc<QT> query(Q);
+  FOR(q, Q) {
+    LL(t);
+    if (t == 0) {
+      LL(i, p, a, b);
+      query[q] = {t, i, p, a, b};
+      all_key.eb(p);
+    }
+    if (t == 1) {
+      LL(l, r, x);
+      query[q] = {t, l, r, x, 0};
+    }
+    if (t == 2 || t == 3) {
+      LL(l, r);
+      query[q] = {t, l, r, 0, 0};
+    }
+  }
+
+  UNIQUE(all_key);
+  for (auto&& k: key) k = LB(all_key, k);
+
+  Sortable_SegTree<AFF> seg(len(all_key), key, seg_raw);
+
+  for (auto&& [t, a, b, c, d]: query) {
+    if (t == 0) {
+      b = LB(all_key, b);
+      seg.set(a, b, {mint(c), mint(d)});
+    }
+    if (t == 1) {
+      auto f = seg.prod(a, b);
+      print(AFF::eval(f, c));
+    }
+    if (t == 2) { seg.sort_inc(a, b); }
+    if (t == 3) { seg.sort_dec(a, b); }
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  FOR(T) solve();
+  return 0;
+}
