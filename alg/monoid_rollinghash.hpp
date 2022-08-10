a860df9160e3ef98741a6f829d0ef519bd0a710f
maspypy <maspypy.gmail.com>
2022-08-11 02:09:15 +0900

    a

--- alg/monoid_rollinghash.hpp
+++ alg/monoid_rollinghash.hpp
@@ -2,8 +2,9 @@
 #include "other/random.hpp"
 #include "mod/modint61.hpp"
 
+// pow of base, val
 struct Monoid_Rolling_Hash {
-  using value_type = pair<modint61, modint61>; // pow of base, val
+  using value_type = pair<modint61, modint61>;
   using X = value_type;
 
   static u64& base() {
@@ -14,7 +15,7 @@ struct Monoid_Rolling_Hash {
     }
     return x;
   }
-  static X from_element(u64& x) { return {base(), x}; }
+  static X from_element(u64 x) { return {base(), x}; }
   static X op(X x, X y) { return {x.fi * y.fi, x.se * y.fi + y.se}; }
   static constexpr X unit() { return {1, 0}; }
   static constexpr bool commute = false;
