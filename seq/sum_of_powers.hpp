61a6d4d15b2f35ec7cc523044024281473ea7c0c
maspypy <maspypy.gmail.com>
2022-08-14 06:03:07 +0900

    add coef

--- seq/sum_of_powers.hpp
+++ seq/sum_of_powers.hpp
@@ -1,5 +1,6 @@
 #include "poly/convolution_all.hpp"
 #include "poly/fps_log.hpp"
+#include "poly/sum_of_rationals.hpp"
 
 // sum_{a in A} a^n を、n = 0, 1, ..., N で列挙
 template <typename T>
@@ -11,5 +12,20 @@ vc<T> sum_of_powers(const vc<T>& A, ll N) {
   f = fps_log(f);
   FOR(i, len(f)) f[i] = -f[i] * T(i);
   f[0] = len(A);
+  return f;
+}
+
+// sum ca^n を n=0,1,...,N で列挙
+template <typename T>
+vc<T> sum_of_powers_with_coef(const vc<T>& A, const vc<T>& C, int N) {
+  using P = pair<vc<T>, vc<T>>;
+  vc<P> dat;
+  FOR(i, len(A)) { dat.eb(vc<T>({C[i]}), vc<T>({1, -A[i]})); }
+  auto [num, den] = sum_of_rationals(dat);
+  num.resize(N + 1);
+  den.resize(N + 1);
+  auto f = fps_inv(den);
+  f = convolution(f, num);
+  f.resize(N + 1);
   return f;
 }
