174d4ca6886a330d3742e0d017790bb42d118564
maspypy <maspypy.gmail.com>
2022-08-16 02:50:42 +0900

    add test

---
+++ test/library_checker/math/stirline_mod_p_1.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/stirling_number_of_the_first_kind_small_p_large_n"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "seq/stirling_number_query.hpp"
+
+void solve() {
+  LL(T, p);
+  Stirling_Number_Query X(p, 1, 0);
+  FOR(T) {
+    LL(n, k);
+    print(X.S1(n, k));
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  // LL(T);
+  ll T = 1;
+  FOR(T) solve();
+
+  return 0;
+}
