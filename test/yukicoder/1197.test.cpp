a5e30aa54c6e84b00ed26aba140cd9b56a18af75
maspypy <maspypy.gmail.com>
2022-08-17 06:26:42 +0900

    add test

---
+++ test/yukicoder/1197.test.cpp
@@ -0,0 +1,62 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1197"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "graph/hld.hpp"
+#include "alg/lazy_cntsum_add.hpp"
+#include "graph/lazytreemonoid.hpp"
+
+void solve() {
+  LL(N, K, Q);
+  VEC(int, C, K);
+  for (auto&& v: C) --v;
+
+  Graph<int, 0> G(N);
+  G.read_tree();
+
+  HLD<decltype(G)> hld(G);
+  using Lazy = Lazy_CntSum_Add<ll>;
+  vc<pi> dat(N);
+  FOR(i, N) dat[i] = {1, 0};
+
+  LazyTreeMonoid<decltype(hld), Lazy> X(hld, dat);
+
+  FOR(i, K) { X.apply_path(0, C[i], 1); }
+
+  ll d_sm = 0;
+  auto& dep = hld.depth;
+  FOR(i, K) d_sm += dep[C[i]];
+
+  FOR(Q) {
+    LL(t);
+    if (t == 1) {
+      LL(k, d);
+      --k;
+      --d;
+      d_sm -= dep[C[k]];
+      X.apply_path(0, C[k], -1);
+      C[k] = d;
+      d_sm += dep[C[k]];
+      X.apply_path(0, C[k], 1);
+    }
+    if (t == 2) {
+      LL(v);
+      --v;
+      ll ANS = dep[v] * K + d_sm;
+      ANS -= 2 * X.prod_path(0, v).se;
+      ANS += 2 * K;
+      print(ANS);
+    }
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
