2842546813d23de1508ba4a65a55c71e82ebb7d4
maspypy <maspypy.gmail.com>
2022-08-16 15:06:00 +0900

    add comb dense / comb negative

--- mod/modint.hpp
+++ mod/modint.hpp
@@ -160,10 +160,37 @@ mint fact_inv(int n) {
   return dat[n];
 }
 
-template <typename mint, bool large = false>
+template <typename mint>
+mint C_dense(int n, int k) {
+  static vvc<mint> C;
+  static int H = 0, W = 0;
+
+  auto calc = [&](int i, int j) -> mint {
+    if (i == 0) return (j == 0 ? mint(1) : mint(0));
+    return C[i - 1][j] + (j ? C[i - 1][j - 1] : 0);
+  };
+
+  if (W <= k) {
+    FOR(i, H) {
+      C[i].resize(k + 1);
+      FOR(j, W, k + 1) { C[i][j] = calc(i, j); }
+    }
+    W = k + 1;
+  }
+  if (H <= n) {
+    FOR(i, H, n + 1) {
+      FOR(j, W) { C[i][j] = calc(i, j); }
+    }
+    H = n + 1;
+  }
+  return C[n][k];
+}
+
+template <typename mint, bool large = false, bool dense = false>
 mint C(ll n, ll k) {
   assert(n >= 0);
   if (k < 0 || n < k) return 0;
+  if (dense) return C_dense(n, k);
   if (!large) return fact<mint>(n) * fact_inv<mint>(k) * fact_inv<mint>(n - k);
   k = min(k, n - k);
   mint x(1);
@@ -178,6 +205,15 @@ mint C_inv(ll n, ll k) {
   assert(0 <= k && k <= n);
   if (!large) return fact_inv<mint>(n) * fact<mint>(k) * fact<mint>(n - k);
   return mint(1) / C<mint, 1>(n, k);
+}
+
+// [x^d](1-x)^{-n} の計算
+template <typename mint, bool large = false, bool dense = false>
+mint C_negative(ll n, ll d) {
+  assert(n >= 0);
+  if (d < 0) return mint(0);
+  if (n == 0) { return (d == 0 ? mint(1) : mint(0)); }
+  return C<mint, large, dense>(n + d - 1, n);
 }
 
 using modint107 = modint<1000000007>;
