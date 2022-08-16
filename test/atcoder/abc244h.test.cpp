5850fc7ee630d61280c2a86a9a5a67a882d8eb3c
maspypy <maspypy.gmail.com>
2022-08-17 05:36:09 +0900

    try atcoder verify

---
+++ test/atcoder/abc244h.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLME "https://atcoder.jp/contests/abc244/tasks/abc244_h"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "ds/cht.hpp"
+
+void solve() {
+  LL(Q);
+  CHT_xy<ll> cht;
+  FOR(Q) {
+    LL(a, b, x, y);
+    cht.add(a, b);
+    print(cht.get_max(x, y));
+  }
+}
+
+signed main() {
+  solve();
+
+  return 0;
+}
