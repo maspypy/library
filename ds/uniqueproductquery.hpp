388f0c29ab4b63290b5dc76fa4958fb73da33e29
maspypy <maspypy.gmail.com>
2022-08-17 12:44:32 +0900

    split file

--- ds/uniqueproductquery.hpp
+++ ds/uniqueproductquery.hpp
@@ -1,6 +1,5 @@
 #include "alg/group_add.hpp"
 #include "ds/segtree.hpp"
-#include "pds/segtree.hpp"
 
 // sample：https://codeforces.com/contest/703/problem/D
 // [L, R) 内の要素 (long long)を UNIQUE した上で、f(x) の総積をとったものを計算。
@@ -54,38 +53,3 @@ struct UniqueProductQuery {
 };
 
 
-
-// sample：https://codeforces.com/contest/703/problem/D
-// key = long long
-// [L, R) 内の要素 (long long) を UNIQUE した上で、
-// f(k),の総積をとったものを計算。 f: key -> Monoid value
-// Online 化したものの、メモリ定数倍が重い
-template <typename Monoid = Group_Add<int>>
-struct OnlineUniqueProductQuery {
-  using X = typename Monoid::value_type;
-  int N;
-  vc<ll> key;
-  vc<pair<int, int>> query;
-  vc<int> times;
-  PersistentSegTree<Monoid> seg;
-
-  template <typename F>
-  OnlineUniqueProductQuery(vc<ll>& key, F f) : N(len(key)), key(key), seg(N) {
-    times.eb(seg.time());
-    unordered_map<ll, int> pos;
-    pos.reserve(N);
-    FOR(i, N) {
-      ll x = key[i];
-      int t = seg.time();
-      if (pos.count(x)) { t = seg.set(t, pos[x], Monoid::unit); }
-      pos[x] = i;
-      t = seg.set(t, i, f(key[i]));
-      times.eb(t);
-    }
-  }
-  
-  X prod(int L, int R) {
-    int t = times[R];
-    return seg.prod(t, L, R);
-  }
-};
