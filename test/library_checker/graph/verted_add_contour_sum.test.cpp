b62912d57b5a9f26daf3fd50b58ab7a4784a333e
maspypy <maspypy.gmail.com>
2022-08-18 17:59:30 +0900

    add test

---
+++ test/library_checker/graph/verted_add_contour_sum.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/vertex_add_range_contour_sum_on_tree"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/base.hpp"
+#include "graph/contoursum.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  Graph<int, 0> G(N);
+  G.read_tree(0, 0);
+  ContourSum<decltype(G), Group_Add<ll>> X(G, A);
+
+  FOR(Q) {
+    LL(t);
+    if (t == 0) {
+      LL(v, x);
+      X.add(v, x);
+    }
+    if (t == 1) {
+      LL(v, l, r);
+      print(X.sum(v, l, r));
+    }
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
