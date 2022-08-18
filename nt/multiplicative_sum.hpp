a5856f330231ebe448b1fbc70973d27e769ecfdd
maspypy <maspypy.gmail.com>
2022-08-19 00:54:59 +0900

    change

--- nt/multiplicative_sum.hpp
+++ nt/multiplicative_sum.hpp
@@ -1,32 +1,28 @@
 #include "nt/primetable.hpp"
-template <typename T, typename FUNC>
-T multiplicative_sum(ll N, FUNC F, vc<T>& sum_lo, vc<T>& sum_hi) {
-  // F(p^e) を与える関数に加え、事前に計算した prime sum を持たせる
-  // black algorithm in
-  // http://baihacker.github.io/main/2020/The_prefix-sum_of_multiplicative_function_the_black_algorithm.html
+
+// f_pe：T(int p,int e), f(p^e)
+// f_psum：[1, x] での f(p) の和
+template <typename T, typename F1, typename F2>
+T multiplicative_sum(ll N, F1 f_pe, F2 f_psum) {
   ll sqN = sqrtl(N);
   auto P = primetable(sqN);
-  auto get = [&](ll d) -> T {
-    return (d <= sqN ? sum_lo[d] : sum_hi[double(N) / d]);
-  };
 
-  T ANS = T(1) + get(N); // 1 and prime
-
+  T ANS = T(1) + f_psum(N); // 1 and prime
   // t = up_i^k のときに、(t, i, k, f(t), f(u)) を持たせる
 
   auto dfs = [&](auto self, ll t, ll i, ll k, T ft, T fu) -> void {
-    T f_nxt = fu * F(P[i], k + 1);
+    T f_nxt = fu * f_pe(P[i], k + 1);
     // 子ノードを全部加算
     ANS += f_nxt;
-    ANS += ft * (get(double(N) / t) - get(P[i]));
+    ANS += ft * (f_psum(double(N) / t) - f_psum(P[i]));
 
     ll lim = sqrtl(double(N) / t);
     if (P[i] <= lim) { self(self, t * P[i], i, k + 1, f_nxt, fu); }
     FOR3(j, i + 1, len(P)) {
       if (P[j] > lim) break;
-      self(self, t * P[j], j, 1, ft * F(P[j], 1), ft);
+      self(self, t * P[j], j, 1, ft * f_pe(P[j], 1), ft);
     }
   };
-  FOR(i, len(P)) if (P[i] <= sqN) dfs(dfs, P[i], i, 1, F(P[i], 1), 1);
+  FOR(i, len(P)) if (P[i] <= sqN) dfs(dfs, P[i], i, 1, f_pe(P[i], 1), 1);
   return ANS;
 }
--- test/library_checker/math/totient_sum.test.cpp
+++ test/library_checker/math/totient_sum.test.cpp
@@ -4,21 +4,23 @@
 
 #include "nt/primesum.hpp"
 #include "nt/multiplicative_sum.hpp"
+#include "mod/modint.hpp"
+
+using mint = modint998;
 
 void solve() {
   LL(N);
-  auto [sum_lo_0, sum_hi_0] = primecnt<ll>(N);
-  auto [sum_lo, sum_hi] = primesum<i128>(N);
-  ll m = len(sum_lo_0);
-  FOR(i, m) sum_lo[i] -= sum_lo_0[i];
-  FOR(i, m) sum_hi[i] -= sum_hi_0[i];
-  auto f = [&](ll p, ll e) -> i128 {
-    ll x = p - 1;
-    FOR(e - 1) x *= p;
+  PrimeSum<mint> A(N), B(N);
+  A.calc_sum();
+  B.calc_count();
+
+  auto f_pe = [&](ll p, ll e) -> mint {
+    mint x = p - 1;
+    FOR(e - 1) x *= mint(p);
     return x;
   };
-  int mod = 998244353;
-  print(int(multiplicative_sum(N, f, sum_lo, sum_hi) % mod));
+  auto f_psum = [&](ll x) -> mint { return A[x] - B[x]; };
+  print(multiplicative_sum<mint>(N, f_pe, f_psum));
 }
 
 signed main() {
