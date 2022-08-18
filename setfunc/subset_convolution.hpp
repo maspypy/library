c64533a928707f5f61ea9f2d607db8f4637c8f6b
maspypy <maspypy.gmail.com>
2022-08-19 07:15:10 +0900

    add square

--- setfunc/subset_convolution.hpp
+++ setfunc/subset_convolution.hpp
@@ -2,6 +2,7 @@
 
 template <typename T, int LIM = 20>
 vc<T> subset_convolution(const vc<T>& A, const vc<T>& B) {
+  if (A == B) return subset_convolution_square(A);
   auto RA = ranked_zeta<T, LIM>(A);
   auto RB = ranked_zeta<T, LIM>(B);
   int n = topbit(len(RA));
@@ -10,6 +11,21 @@ vc<T> subset_convolution(const vc<T>& A, const vc<T>& B) {
     FOR_R(d, n + 1) {
       T x = 0;
       FOR(i, d + 1) x += f[i] * g[d - i];
+      f[d] = x;
+    }
+  }
+  return ranked_mobius<T, LIM>(RA);
+}
+
+template <typename T, int LIM = 20>
+vc<T> subset_convolution_square(const vc<T>& A) {
+  auto RA = ranked_zeta<T, LIM>(A);
+  int n = topbit(len(RA));
+  FOR(s, len(RA)) {
+    auto& f = RA[s];
+    FOR_R(d, n + 1) {
+      T x = 0;
+      FOR(i, d + 1) x += f[i] * f[d - i];
       f[d] = x;
     }
   }
