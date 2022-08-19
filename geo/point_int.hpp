9b1ca63ac3880cca9f1e121c8621625a91325e5a
maspypy <maspypy.gmail.com>
2022-08-20 05:58:32 +0900

    rm

--- geo/point_int.hpp
+++
@@ -1,9 +0,0 @@
-template <typenamt INT>
-struct Point {
-  INT x, y;
-  template <typename S, typename T>
-  Point_int(S x, T y) : x(x), y(y) {}
-
-  template <typename S, typename T>
-  Point_int(pair<S, T> p) : x(p.fi), y(p.se) {}
-};
