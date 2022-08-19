58a99406850d296f75a8b6de4aa30b94d88fd2f4
maspypy <maspypy.gmail.com>
2022-08-20 06:37:51 +0900

    count subrect

--- ds/cartesian_tree.hpp
+++ ds/cartesian_tree.hpp
@@ -5,15 +5,19 @@
 template <typename T, bool IS_MIN>
 struct CartesianTree {
   int n;
-  vc<T> A;
+  vc<T>& A;
   vc<pair<int, int>> range;
   vc<int> lch, rch, par;
 
-  CartesianTree(vc<T> A) : n(len(A)), A(A) {
+  CartesianTree(vc<T>& A) : n(len(A)), A(A) {
     range.assign(n, {-1, -1});
     lch.assign(n, -1);
     rch.assign(n, -1);
     par.assign(n, -1);
+    if (n == 1) {
+      range[0] = {0, 1};
+      return;
+    }
     auto is_sm = [&](int i, int j) -> bool {
       if (IS_MIN) return (A[i] < A[j]) || (A[i] == A[j] && i < j);
       return (A[i] > A[j]) || (A[i] == A[j] && i < j);
@@ -53,6 +57,17 @@ struct CartesianTree {
     FOR(i, n) {
       auto [l, r, h] = maximum_rectangle(i);
       chmax(res, (r - l) * h);
+    }
+    return res;
+  }
+
+  ll count_subrectangle(bool baseline) {
+    assert(IS_MIN);
+    ll res = 0;
+    FOR(i, n) {
+      auto [l, r, h] = maximum_rectangle(i);
+      ll x = (baseline ? h : h * (h + 1) / 2);
+      res += x * (i - l + 1) * (r - i);
     }
     return res;
   }
