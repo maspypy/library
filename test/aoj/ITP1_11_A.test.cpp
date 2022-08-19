91c12e716904ad2be5d671136a85c5f30b8ba311
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-20 06:08:53 +0900

    Update ITP1_11_A.test.cpp

--- test/aoj/ITP1_11_A.test.cpp
+++ test/aoj/ITP1_11_A.test.cpp
@@ -9,14 +9,9 @@ void solve() {
   array<int, 6> A;
   FOR(i, 6) read(A[i]);
   Dice<int> X(A);
-  auto dat = X.gen_all();
-  LL(Q);
-  FOR(Q) {
-    LL(a, b);
-    for (auto&& B: dat) {
-      if (B[0] == a && B[1] == b) print(B[2]);
-    }
-  }
+  STR(S);
+  for (auto&& s: S) { X.rotate(s); }
+  print(X.U());
 }
 
 signed main() {
