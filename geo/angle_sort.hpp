52f907d96be0fa674643dd14fc0ca2c5df59a2da
maspypy <maspypy.gmail.com>
2022-08-21 16:45:34 +0900

    angle sort

--- geo/angle_sort.hpp
+++ geo/angle_sort.hpp
@@ -1,11 +1,11 @@
 #include "geo/base.hpp"
 
 // 偏角ソートに対する argsort
-template<typename Point>
-vector<int> angle_argsort(vector<Point>& P) {
-  auto is_lower = [](Point P) { return (P.y < 0) || (P.y == 0 && P.x > 0); };
+template <typename T>
+vector<int> angle_argsort(vector<Point<T>>& P) {
+  auto is_lower = [](Point<T> P) { return (P.y < 0) || (P.y == 0 && P.x > 0); };
   vector<int> lower, origin, upper;
-  Point O = {0, 0};
+  Point<T> O = {0, 0};
   FOR(i, len(P)) {
     if (P[i] == O) origin.eb(i);
     elif (is_lower(P[i])) lower.eb(i);
@@ -19,12 +19,18 @@ vector<int> angle_argsort(vector<Point>& P) {
   return I;
 }
 
+// 偏角ソートに対する argsort
+template <typename T>
+vector<int> angle_argsort(vector<pair<T, T>> P) {
+  vc<Point<T>> tmp(len(P));
+  FOR(i, len(P)) tmp[i] = Point<T>(P[i]);
+  return angle_argsort(tmp);
+}
+
 // inplace に偏角ソートする
 // index が欲しい場合は angle_argsort
-template<typename Point>
-void angle_sort(vector<Point>& P) {
-  auto I = angle_argsort(P);
-  vc<Point> Q(len(P));
-  FOR(i, len(P)) Q[i] = P[I[i]];
-  P = Q;
+template <typename T>
+void angle_sort(vector<T>& P) {
+  auto I = angle_argsort<T>(P);
+  P = rearrange(P, I);
 }
