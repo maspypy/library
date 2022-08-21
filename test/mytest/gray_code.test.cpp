a5d3ca385fcb5c39b6862e18a73b7541275d2302
maspypy <maspypy.gmail.com>
2022-08-22 04:37:47 +0900

    add test

---
+++ test/mytest/gray_code.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "seq/gray_code.hpp"
+
+void test() {
+  FOR(LOG, 10) {
+    auto G = gray_code(LOG);
+    FOR(i, len(G) - 1) {
+      int j = (i + 1 == len(G) ? 0 : i);
+      int x = G[i] ^ G[j];
+      assert(popcnt(x) == 1);
+    }
+    assert(len(G) == 1 << LOG);
+    UNIQUE(G);
+    assert(len(G) == 1 << LOG);
+  }
+}
+
+void solve() {
+  LL(a, b);
+  print(a + b);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  test();
+  solve();
+
+  return 0;
+}
