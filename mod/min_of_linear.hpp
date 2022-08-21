6966a910d283d4a2846b4de10c4775e33dc1312e
maspypy <maspypy.gmail.com>
2022-08-21 18:00:35 +0900

    min of linear

---
+++ mod/min_of_linear.hpp
@@ -0,0 +1,19 @@
+// min_{x in [L, R)} (ax+b mod)
+pair<ll, int> min_of_linear(ll L, ll R, int a, int b, int mod) {
+  ll n = R - L;
+  b = (b + a * L) % mod;
+  if (b < 0) b += mod;
+  auto [X, DX] = min_of_linear_segments(a, b, mod);
+  int x = 0;
+  for (int i = 0; i < int(X.size()) - 1; ++i) {
+    int xl = X[i], xr = X[i + 1];
+    if (xr < n) {
+      x = xr;
+      continue;
+    }
+    x = xl + (n - 1 - x) / DX[i] * DX[i];
+    break;
+  }
+  int y = (ll(a) * x + b) % mod;
+  return {L + x, y};
+}
--- test/library_checker/math/min_of_mod_of_linear.test.cpp
+++ test/library_checker/math/min_of_mod_of_linear.test.cpp
@@ -1,22 +1,20 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/min_of_mod_of_linear"
-#include "mod/min_of_linear_segments.hpp"
 
-// min_{x in [L, R)} (ax+b mod)
-pair<ll, int> min_of_linear(ll L, ll R, int a, int b, int mod) {
-  ll n = R - L;
-  b = (b + a * L) % mod;
-  if (b < 0) b += mod;
-  auto [X, DX] = min_of_linear_segments(a, b, mod);
-  int x = 0;
-  for (int i = 0; i < int(X.size()) - 1; ++i) {
-    int xl = X[i], xr = X[i + 1];
-    if (xr < n) {
-      x = xr;
-      continue;
-    }
-    x = xl + (n - 1 - x) / DX[i] * DX[i];
-    break;
-  }
-  int y = (ll(a) * x + b) % mod;
-  return {L + x, y};
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/min_of_linear.hpp"
+
+void solve() {
+  LL(n, m, a, b);
+  auto [x, fx] = min_of_linear(0, n, a, b, m);
+  print(fx);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  LL(T);
+  FOR(T) solve();
+
+  return 0;
 }
