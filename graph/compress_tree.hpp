16dfccbb792a8ffcf8930ba98f321fe1e8495853
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-22 17:45:56 +0900

    Update compress_tree.hpp

--- graph/compress_tree.hpp
+++ graph/compress_tree.hpp
@@ -1,21 +1,21 @@
 // (圧縮された木の頂点ラベルたち、グラフ)
-template <typename HLD>
-pair<vc<int>, typename HLD::Graph_type> compress_tree(HLD& hld, vc<int> V) {
+template <typename TREE>
+pair<vc<int>, typename TREE::Graph_type> compress_tree(TREE& tree, vc<int> V) {
   // 大事な点をリストアップする
   // もともとの根は含まれるようにする
   {
     vc<int> VV = V;
-    sort(all(V), [&](auto& x, auto& y) { return hld.LID[x] < hld.LID[y]; });
-    FOR(i, len(V) - 1) { VV.eb(hld.lca(V[i], V[i + 1])); }
-    VV.eb(hld.lca(V[0], V.back()));
-    VV.eb(hld.V[0]);
+    sort(all(V), [&](auto& x, auto& y) { return tree.LID[x] < tree.LID[y]; });
+    FOR(i, len(V) - 1) { VV.eb(tree.lca(V[i], V[i + 1])); }
+    VV.eb(tree.lca(V[0], V.back()));
+    VV.eb(tree.V[0]);
     UNIQUE(VV);
     V = VV;
-    sort(all(V), [&](auto& x, auto& y) { return hld.LID[x] < hld.LID[y]; });
+    sort(all(V), [&](auto& x, auto& y) { return tree.LID[x] < tree.LID[y]; });
   }
   // 辺を張ってグラフを作る
   int n = len(V);
-  using Graph = typename HLD::Graph_type;
+  using Graph = typename TREE::Graph_type;
   using WT = typename Graph::cost_type;
   Graph G(n);
   vc<int> st = {0};
@@ -23,12 +23,12 @@ pair<vc<int>, typename HLD::Graph_type> compress_tree(HLD& hld, vc<int> V) {
     while (1) {
       int p = V[st.back()];
       int v = V[i];
-      if (hld.in_subtree(v, p)) break;
+      if (tree.in_subtree(v, p)) break;
       st.pop_back();
     }
     int p = V[st.back()];
     int v = V[i];
-    WT d = hld.depth_weighted[v] - hld.depth_weighted[p];
+    WT d = tree.depth_weighted[v] - tree.depth_weighted[p];
     G.add(st.back(), i, d);
     st.eb(i);
   }
