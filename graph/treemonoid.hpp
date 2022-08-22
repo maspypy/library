07c05caaeb1b578f9b87c385266578e87b0557bf
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-22 18:04:37 +0900

    Update treemonoid.hpp

--- graph/treemonoid.hpp
+++ graph/treemonoid.hpp
@@ -1,29 +1,29 @@
 #pragma once
 #include "ds/segtree.hpp"
-#include "graph/hld.hpp"
+#include "graph/tree.hpp"
 #include "alg/monoid_reverse.hpp"
 
-template <typename HLD, typename Monoid, bool edge = false>
+template <typename TREE, typename Monoid, bool edge = false>
 struct TreeMonoid {
   using RevMonoid = Monoid_Reverse<Monoid>;
   using X = typename Monoid::value_type;
-  HLD &hld;
+  TREE &tree;
   int N;
   SegTree<Monoid> seg;
   SegTree<RevMonoid> seg_r;
 
-  TreeMonoid(HLD &hld) : hld(hld), N(hld.N), seg(hld.N) {
-    if (!Monoid::commute) seg_r = SegTree<RevMonoid>(hld.N);
+  TreeMonoid(TREE &tree) : tree(tree), N(tree.N), seg(tree.N) {
+    if (!Monoid::commute) seg_r = SegTree<RevMonoid>(tree.N);
   }
 
-  TreeMonoid(HLD &hld, vc<X> &dat) : hld(hld), N(hld.N) {
+  TreeMonoid(TREE &tree, vc<X> &dat) : tree(tree), N(tree.N) {
     vc<X> seg_raw(N, Monoid::unit());
     if (!edge) {
-      FOR(v, N) seg_raw[hld.LID[v]] = dat[v];
+      FOR(v, N) seg_raw[tree.LID[v]] = dat[v];
     } else {
       FOR(e, N - 1) {
-        int v = hld.e_to_v(e);
-        seg_raw[hld.LID[v]] = dat[e];
+        int v = tree.e_to_v(e);
+        seg_raw[tree.LID[v]] = dat[e];
       }
     }
     seg = SegTree<Monoid>(seg_raw);
@@ -31,14 +31,14 @@ struct TreeMonoid {
   }
 
   void set(int i, X x) {
-    if (edge) i = hld.e_to_v(i);
-    i = hld.LID[i];
+    if (edge) i = tree.e_to_v(i);
+    i = tree.LID[i];
     seg.set(i, x);
     if (!Monoid::commute) seg_r.set(i, x);
   }
 
   X prod_path(int u, int v) {
-    auto pd = hld.get_path_decomposition(u, v, edge);
+    auto pd = tree.get_path_decomposition(u, v, edge);
     X val = Monoid::unit();
     for (auto &&[a, b]: pd) {
       X x = (a <= b ? seg.prod(a, b + 1)
@@ -59,7 +59,7 @@ struct TreeMonoid {
   int max_path(F &check, int u, int v) {
     if (edge) return max_path_edge(check, u, v);
     if (!check(prod_path(u, u))) return -1;
-    auto pd = hld.get_path_decomposition(u, v, edge);
+    auto pd = tree.get_path_decomposition(u, v, edge);
     X val = Monoid::unit();
     for (auto &&[a, b]: pd) {
       X x = (a <= b ? seg.prod(a, b + 1)
@@ -67,33 +67,33 @@ struct TreeMonoid {
                                        : seg_r.prod(b, a + 1)));
       if (check(Monoid::op(val, x))) {
         val = Monoid::op(val, x);
-        u = (hld.V[b]);
+        u = (tree.V[b]);
         continue;
       }
       auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
       if (a <= b) {
         // 下り
         auto i = seg.max_right(check_tmp, a);
-        return (i == a ? u : hld.V[i - 1]);
+        return (i == a ? u : tree.V[i - 1]);
       } else {
         // 上り
         auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
                                   : seg_r.min_left(check_tmp, a + 1));
         if (i == a + 1) return u;
-        return (edge ? hld.parent[hld.V[i]] : hld.V[i]);
+        return (edge ? tree.parent[tree.V[i]] : tree.V[i]);
       }
     }
     return v;
   }
 
   X prod_subtree(int u) {
-    int l = hld.LID[u], r = hld.RID[u];
+    int l = tree.LID[u], r = tree.RID[u];
     return seg.prod(l + edge, r);
   }
 
   void debug() {
     print("tree_monoid");
-    hld.debug();
+    tree.debug();
     seg.debug();
     seg_r.debug();
   }
@@ -103,8 +103,8 @@ private:
   int max_path_edge(F &check, int u, int v) {
     assert(edge);
     if (!check(Monoid::unit())) return -1;
-    int lca = hld.lca(u, v);
-    auto pd = hld.get_path_decomposition(u, lca, edge);
+    int lca = tree.lca(u, v);
+    auto pd = tree.get_path_decomposition(u, lca, edge);
     X val = Monoid::unit();
 
     // climb
@@ -113,28 +113,28 @@ private:
       X x = (Monoid::commute ? seg.prod(b, a + 1) : seg_r.prod(b, a + 1));
       if (check(Monoid::op(val, x))) {
         val = Monoid::op(val, x);
-        u = (hld.parent[hld.V[b]]);
+        u = (tree.parent[tree.V[b]]);
         continue;
       }
       auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
       auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
                                 : seg_r.min_left(check_tmp, a + 1));
       if (i == a + 1) return u;
-      return hld.parent[hld.V[i]];
+      return tree.parent[tree.V[i]];
     }
     // down
-    pd = hld.get_path_decomposition(lca, v, edge);
+    pd = tree.get_path_decomposition(lca, v, edge);
     for (auto &&[a, b]: pd) {
       assert(a <= b);
       X x = seg.prod(a, b + 1);
       if (check(Monoid::op(val, x))) {
         val = Monoid::op(val, x);
-        u = (hld.V[b]);
+        u = (tree.V[b]);
         continue;
       }
       auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
       auto i = seg.max_right(check_tmp, a);
-      return (i == a ? u : hld.V[i - 1]);
+      return (i == a ? u : tree.V[i - 1]);
     }
     return v;
   }
