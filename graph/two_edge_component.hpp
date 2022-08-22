5fc8ff2a8360aae946a9359f3976da0e738e40d6
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-22 23:56:37 +0900

    Update two_edge_component.hpp

--- graph/two_edge_component.hpp
+++ graph/two_edge_component.hpp
@@ -1,28 +1,28 @@
-#include "graph/hld.hpp"
+#include "graph/tree.hpp"
 
 // (成分数, 成分番号の vector)
 template <typename Graph>
 pair<int, vc<int>> two_edge_component(Graph& G) {
-  HLD hld(G);
+  TREE tree(G);
   int N = G.N;
   vc<int> DP(N);
   for (auto&& e: G.edges) {
-    if (!hld.in_tree[e.id]) {
+    if (!tree.in_tree[e.id]) {
       int a = e.frm, b = e.to;
-      if (hld.depth[a] < hld.depth[b]) swap(a, b);
+      if (tree.depth[a] < tree.depth[b]) swap(a, b);
       DP[a]++, DP[b]--;
     }
   }
   auto& V = hld.V;
   FOR_R(i, len(V)) {
     int v = V[i];
-    int p = hld.parent[v];
+    int p = tree.parent[v];
     if (p != -1) DP[p] += DP[v];
   }
   int C = 0;
   vc<int> comp(N, -1);
   FOR(v, N) if (DP[v] == 0) comp[v] = C++;
   for (auto&& v: V)
-    if (comp[v] == -1) comp[v] = comp[hld.parent[v]];
+    if (comp[v] == -1) comp[v] = comp[tree.parent[v]];
   return {C, comp};
 }
