7c8f3fcbc43e44aa3e122a0dc38ea429bd8ab2e2
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-22 18:02:30 +0900

    Update treegroup.hpp

--- graph/treegroup.hpp
+++ graph/treegroup.hpp
@@ -1,19 +1,19 @@
 #include "ds/segtree.hpp"
-#include "graph/hld.hpp"
+#include "graph/tree.hpp"
 #include "alg/group_reverse.hpp"
 
 // 作ってみたものの、HLD(log^2N)より遅いがち？
-template <typename HLD, typename Group, bool edge = false,
+template <typename TREE, typename Group, bool edge = false,
           bool path_query = true, bool subtree_query = false>
 struct TreeGroup {
   using RevGroup = Group_Reverse<Group>;
   using X = typename Group::value_type;
-  HLD &hld;
+  TREE &tree;
   int N;
   SegTree<Group> seg, seg_subtree;
   SegTree<RevGroup> seg_r;
 
-  TreeGroup(HLD &hld) : hld(hld), N(hld.N) {
+  TreeGroup(TREE &tree) : tree(tree), N(tree.N) {
     if (path_query) {
       seg = SegTree<Group>(2 * N);
       if (!Group::commute) seg_r = SegTree<RevGroup>(2 * N);
@@ -24,21 +24,21 @@ struct TreeGroup {
     }
   }
 
-  TreeGroup(HLD &hld, vc<X> dat) : hld(hld), N(hld.N) {
+  TreeGroup(TREE &tree, vc<X> dat) : tree(tree), N(tree.N) {
     if (path_query) {
       vc<X> seg_raw(2 * N);
       if (!edge) {
         assert(len(dat) == N);
         FOR(v, N) {
-          seg_raw[hld.ELID(v)] = dat[v];
-          seg_raw[hld.ERID(v)] = Group::inverse(dat[v]);
+          seg_raw[tree.ELID(v)] = dat[v];
+          seg_raw[tree.ERID(v)] = Group::inverse(dat[v]);
         }
       } else {
         assert(len(dat) == N - 1);
         FOR(e, N - 1) {
-          int v = hld.e_to_v(e);
-          seg_raw[hld.ELID(v)] = dat[e];
-          seg_raw[hld.ERID(v)] = Group::inverse(dat[e]);
+          int v = tree.e_to_v(e);
+          seg_raw[tree.ELID(v)] = dat[e];
+          seg_raw[tree.ERID(v)] = Group::inverse(dat[e]);
         }
       }
       seg = SegTree<Group>(seg_raw);
@@ -49,12 +49,12 @@ struct TreeGroup {
       vc<X> seg_raw(N);
       if (!edge) {
         assert(len(dat) == N);
-        FOR(v, N) seg_raw[hld.LID[v]] = dat[v];
+        FOR(v, N) seg_raw[tree.LID[v]] = dat[v];
       } else {
         assert(len(dat) == N - 1);
         FOR(e, N - 1) {
-          int v = hld.e_to_v(e);
-          seg_raw[hld.LID[v]] = dat[e];
+          int v = tree.e_to_v(e);
+          seg_raw[tree.LID[v]] = dat[e];
         }
       }
       seg_subtree = SegTree<Group>(seg_raw);
@@ -63,42 +63,42 @@ struct TreeGroup {
 
   void set_path(int v, X x) {
     X inv_x = Group::inverse(x);
-    seg.set(hld.ELID(v), x);
-    seg.set(hld.ERID(v), inv_x);
+    seg.set(tree.ELID(v), x);
+    seg.set(tree.ERID(v), inv_x);
     if (!Group::commute) {
-      seg_r.set(hld.ELID(v), x);
-      seg_r.set(hld.ERID(v), inv_x);
+      seg_r.set(tree.ELID(v), x);
+      seg_r.set(tree.ERID(v), inv_x);
     }
   }
 
-  void set_subtree(int v, X x) { seg_subtree.set(hld.LID[v], x); }
+  void set_subtree(int v, X x) { seg_subtree.set(tree.LID[v], x); }
 
   void set(int i, X x) {
-    int v = (edge ? hld.e_to_v(i) : i);
+    int v = (edge ? tree.e_to_v(i) : i);
     if (path_query) set_path(v, x);
     if (subtree_query) set_subtree(v, x);
   }
 
   X prod_path(int frm, int to) {
     assert(path_query);
-    int lca = hld.LCA(frm, to);
+    int lca = tree.LCA(frm, to);
     // [frm, lca)
-    X x1 = (Group::commute ? seg.prod(hld.ELID(lca) + 1, hld.ELID(frm) + 1)
-                           : seg_r.prod(hld.ELID(lca) + 1, hld.ELID(frm) + 1));
+    X x1 = (Group::commute ? seg.prod(tree.ELID(lca) + 1, tree.ELID(frm) + 1)
+                           : seg_r.prod(tree.ELID(lca) + 1, tree.ELID(frm) + 1));
     // edge なら (lca, to]、vertex なら [lca, to]
-    X x2 = seg.prod(hld.ELID(lca) + edge, hld.ELID(to) + 1);
+    X x2 = seg.prod(tree.ELID(lca) + edge, tree.ELID(to) + 1);
     return Group::op(x1, x2);
   }
 
   X prod_subtree(int u) {
     assert(subtree_query);
-    int l = hld.LID[u], r = hld.RID[u];
+    int l = tree.LID[u], r = tree.RID[u];
     return seg_subtree.prod(l + edge, r);
   }
 
   void debug() {
-    print("hld");
-    hld.debug();
+    print("tree");
+    tree.debug();
     print("seg");
     seg.debug();
     print("seg_r");
