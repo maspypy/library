7d572ccc7c519ce9e6315aff21c37d4edcbe8c7e
maspypy <maspypy.gmail.com>
2022-08-24 14:22:39 +0900

    fix

--- graph/statictreemonoid.hpp
+++ graph/statictreemonoid.hpp
@@ -1,28 +1,28 @@
 #include "ds/disjointsparse.hpp"
-#include "graph/hld.hpp"
+#include "graph/tree.hpp"
 #include "alg/monoid_reverse.hpp"
 
-template <typename HLD, typename Monoid, bool edge = false>
+template <typename TREE, typename Monoid, bool edge = false>
 struct StaticTreeMonoid {
   using RevMonoid = Monoid_Reverse<Monoid>;
   using X = typename Monoid::value_type;
-  HLD &hld;
+  TREE &tree;
   int N;
   DisjointSparse<Monoid> seg;
   DisjointSparse<RevMonoid> seg_r;
 
-  StaticTreeMonoid(HLD &hld) : hld(hld), N(hld.N), seg(hld.N) {
-    if (!Monoid::commute) seg_r = DisjointSparse<RevMonoid>(hld.N);
+  StaticTreeMonoid(TREE &tree) : tree(tree), N(tree.N), seg(tree.N) {
+    if (!Monoid::commute) seg_r = DisjointSparse<RevMonoid>(tree.N);
   }
 
-  StaticTreeMonoid(HLD &hld, vc<X> &dat) : hld(hld), N(hld.N) {
+  StaticTreeMonoid(TREE &tree, vc<X> &dat) : tree(tree), N(tree.N) {
     vc<X> seg_raw(N, Monoid::unit());
     if (!edge) {
-      FOR(v, N) seg_raw[hld.LID[v]] = dat[v];
+      FOR(v, N) seg_raw[tree.LID[v]] = dat[v];
     } else {
       FOR(e, N - 1) {
-        int v = hld.e_to_v(e);
-        seg_raw[hld.LID[v]] = dat[e];
+        int v = tree.e_to_v(e);
+        seg_raw[tree.LID[v]] = dat[e];
       }
     }
     seg = DisjointSparse<Monoid>(seg_raw);
@@ -30,7 +30,7 @@ struct StaticTreeMonoid {
   }
 
   X prod_path(int u, int v) {
-    auto pd = hld.get_path_decomposition(u, v, edge);
+    auto pd = tree.get_path_decomposition(u, v, edge);
     X val = Monoid::unit();
     for (auto &&[a, b]: pd) {
       X x = (a <= b ? seg.prod(a, b + 1)
@@ -51,7 +51,7 @@ struct StaticTreeMonoid {
   int max_path(F &check, int u, int v) {
     if (edge) return max_path_edge(check, u, v);
     if (!check(prod_path(u, u))) return -1;
-    auto pd = hld.get_path_decomposition(u, v, edge);
+    auto pd = tree.get_path_decomposition(u, v, edge);
     X val = Monoid::unit();
     for (auto &&[a, b]: pd) {
       X x = (a <= b ? seg.prod(a, b + 1)
@@ -59,33 +59,33 @@ struct StaticTreeMonoid {
                                        : seg_r.prod(b, a + 1)));
       if (check(Monoid::op(val, x))) {
         val = Monoid::op(val, x);
-        u = (hld.V[b]);
+        u = (tree.V[b]);
         continue;
       }
       auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
       if (a <= b) {
         // 下り
         auto i = seg.max_right(check_tmp, a);
-        return (i == a ? u : hld.V[i - 1]);
+        return (i == a ? u : tree.V[i - 1]);
       } else {
         // 上り
         auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
                                   : seg_r.min_left(check_tmp, a + 1));
         if (i == a + 1) return u;
-        return (edge ? hld.parent[hld.V[i]] : hld.V[i]);
+        return (edge ? tree.parent[tree.V[i]] : tree.V[i]);
       }
     }
     return v;
   }
 
   X prod_subtree(int u) {
-    int l = hld.LID[u], r = hld.RID[u];
+    int l = tree.LID[u], r = tree.RID[u];
     return seg.prod(l + edge, r);
   }
 
   void debug() {
     print("tree_monoid");
-    hld.debug();
+    tree.debug();
     seg.debug();
     seg_r.debug();
   }
