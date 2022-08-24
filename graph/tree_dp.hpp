8620d70ac95d370a0b05aa2a78b66d6f03c9624e
maspypy <maspypy.gmail.com>
2022-08-25 01:59:06 +0900

    upd

--- graph/tree_dp.hpp
+++ graph/tree_dp.hpp
@@ -1,18 +1,18 @@
+
 #include "graph/base.hpp"
+#include "graph/tree.hpp"
 
-// 木全体に対する結果を return する。
-// 部分木ごとに処理したい場合は、fev の最後に行うようにせよ。
-template <typename Graph, typename Data, typename F1, typename F2, typename F3>
-Data tree_dp(Graph& G, F1 fee, F2 fev, F3 fve, Data unit, int root = 0) {
-  int N = G.N;
+template <typename TREE, typename Data, typename F1, typename F2, typename F3>
+vc<Data> tree_dp(TREE& tree, F1 f_ee, F2 f_ev, F3 f_ve, const Data unit) {
+  assert(!tree.G.is_directed());
 
-  auto dfs = [&](auto& dfs, int v, int p) -> Data {
-    Data x = unit;
-    for (auto&& e: G[v]) {
-      if (e.to == p) continue;
-      x = fee(x, fve(dfs(dfs, e.to, v), e));
-    }
-    return fev(x, v);
-  };
-  return dfs(dfs, root, -1);
-}
+  int N = tree.G.N;
+  vc<Data> dp(N, unit);
+  FOR_R(i, 1, N) {
+    int v = tree.V[i];
+    dp[v] = f_ev(dp[v], v);
+    for (auto&& e: tree.G[v])
+      if (e.to == tree.parent[v]) { dp[e.to] = f_ee(dp[e.to], f_ve(dp[v], e)); }
+  }
+  return dp;
+};
