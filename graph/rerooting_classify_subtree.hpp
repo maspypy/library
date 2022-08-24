97b6569a8102cdc01f2a33f6f29b9f76a941a308
maspypy <maspypy.gmail.com>
2022-08-25 01:58:29 +0900

    add

---
+++ graph/rerooting_classify_subtree.hpp
@@ -0,0 +1,44 @@
+#include "mod/modint61.hpp"
+#include "graph/base.hpp"
+#include "graph/tree.hpp"
+#include "other/random.hpp"
+#include "graph/rerooting_dp.hpp"
+
+template <typename TREE>
+struct Rerooting_classify_subtree {
+  TREE& tree;
+  vc<ll> dp, dp_1, dp_2;
+
+  Rerooting_classify_subtree(TREE& tree) : tree(tree) {
+    int N = tree.G.N;
+    RandomNumberGenerator RNG;
+    using mint = modint61;
+    using T = pair<int, mint>;
+    T unit = {0, mint(1)};
+    vc<mint> hash_base(N + 1);
+    FOR(i, N + 1) hash_base[i] = RNG(mint::get_mod());
+
+    auto f_ee = [&](T A, T B) -> T { return {A.fi + B.fi, A.se * B.se}; };
+    auto f_ev = [&](T A, int v) -> T { return {A.fi + 1, A.se}; };
+    auto f_ve = [&](T A, const auto& e) -> T {
+      return {A.fi, A.se + hash_base[A.fi]};
+    };
+
+    Rerooting_dp<decltype(tree), T> DP(tree, f_ee, f_ev, f_ve, unit);
+    dp.resize(N), dp_1.resize(N), dp_2.resize(N);
+    FOR(v, N) dp[v] = DP.dp[v].se.val;
+    FOR(v, N) dp_1[v] = DP.dp_1[v].se.val;
+    FOR(v, N) dp_2[v] = DP.dp_2[v].se.val;
+  }
+
+  // v を根としたときの full tree
+  int operator[](int v) { return dp[v]; }
+
+  // root を根としたときの部分木 v
+  int get(int root, int v) {
+    if (root == v) return dp[v];
+    if (!tree.in_subtree(root, v)) { return dp_1[v]; }
+    int w = tree.jump(v, root);
+    return dp_2[w];
+  }
+};
