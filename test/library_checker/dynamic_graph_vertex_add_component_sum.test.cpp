ca1d0178c62c6e14fe10ad92cc8f6165c794abcb
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-24 18:37:11 +0900

    Update dynamic_graph_vertex_add_component_sum.test.cpp

--- test/library_checker/dynamic_graph_vertex_add_component_sum.test.cpp
+++ test/library_checker/dynamic_graph_vertex_add_component_sum.test.cpp
@@ -1,128 +1,98 @@
-#define PROBLEM \
-  "https://judge.yosupo.jp/problem/dynamic_graph_vertex_add_component_sum"
+#define PROBLEM "https://yukicoder.me/problems/no/1418"
 #include "my_template.hpp"
 #include "other/io.hpp"
 #include "pds/rollbackunionfind.hpp"
+#include "pds/rollbackarray.hpp"
+#include "ds/addremove_query.hpp"
 
 void solve() {
   LL(N, Q);
-  VEC(ll, a, N);
+  VEC(ll, A0, N);
 
-  using QT = tuple<int, int, int>;
-  vc<QT> query(Q);
-  FOR(q, Q) {
+  using P = pair<int, int>;
+  AddRemove_Query<P, 1> X;
+  RollbackArray<ll> A(A0);
+
+  vc<int> query;
+  FOR(Q) {
     LL(t);
     if (t == 0) {
       LL(u, v);
       if (u > v) swap(u, v);
-      query[q] = {t, u, v};
+      X.add(len(query), {u, v});
     }
     if (t == 1) {
       LL(u, v);
       if (u > v) swap(u, v);
-      query[q] = {t, u, v};
+      X.remove(len(query), {u, v});
     }
     if (t == 2) {
       LL(v, x);
-      query[q] = {t, v, x};
+      X.add(len(query), {~v, x});
     }
     if (t == 3) {
       LL(v);
-      query[q] = {t, v, -1};
+      query.eb(v);
     }
   }
-
-  map<pair<int, int>, vc<int>> MP;
-  FOR(q, Q) {
-    auto& [t, a, b] = query[q];
-    if (t == 0 || t == 1) {
-      pair<int, int> p = {a, b};
-      MP[p].eb(q);
-    }
-  }
-
-  int size = 1;
-  while (size < Q) size *= 2;
-
-  /*
-  0 a b ： merge a, b
-  1 v x : add v x
-  2 q v: answer query q
-  */
-
-  vvc<QT> query2(2 * size);
-  for (auto&& [p, Ts]: MP) {
-    if (Ts.size() % 2 == 1) Ts.eb(Q);
-    auto [a, b] = p;
-    FOR(i, 0, len(Ts), 2) {
-      ll l = Ts[i];
-      ll r = Ts[i + 1];
-      l += size;
-      r += size;
-      while (l < r) {
-        if (l % 2 == 1) { query2[l++].eb(0, a, b); }
-        if (r % 2 == 1) { query2[--r].eb(0, a, b); }
-        l /= 2, r /= 2;
-      }
-    }
-  }
-  FOR(q, Q) {
-    auto [t, a, b] = query[q];
-    if (t == 0 || t == 1) continue;
-    if (t == 2) {
-      int v = a, x = b;
-      ll l = q, r = Q;
-      l += size;
-      r += size;
-      while (l < r) {
-        if (l % 2 == 1) { query2[l++].eb(1, v, x); }
-        if (r % 2 == 1) { query2[--r].eb(1, v, x); }
-        l /= 2, r /= 2;
-      }
-    }
-    if (t == 3) { query2[q + size].eb(2, q, a); }
-  }
-
-  RollbackArray<ll> A(a);
   RollbackUnionFind uf(N);
-  vi ANS(Q);
+  // rollback_dfs
+  auto upd = X.calc(len(query));
+  vi ANS(len(query));
+  vc<int> I(len(upd));
+  iota(all(I), 0);
+  auto dfs = [&](auto& dfs, vc<int>& upd_query_I, int begin, int end) -> void {
+    // snapshot
+    int a_time = A.time();
+    int uf_time = uf.time();
 
-  auto dfs = [&](auto& dfs, int idx) -> void {
-    int time1 = A.time();
-    int time2 = uf.time();
-    //
-    for (auto&& [t, a, b]: query2[idx]) {
-      if (t == 0) {
-        a = uf[a], b = uf[b];
-        if (a == b) continue;
-        uf.merge(a, b);
-        A.set(uf[a], A.get(a) + A.get(b));
+    vc<int> IL, IR;
+    int mid = (begin + end) / 2;
+    for (auto&& i: upd_query_I) {
+      auto [a, b, X] = upd[i];
+      if (a <= begin && end <= b) {
+        // X で表される update query を処理する
+        auto [p, q] = X;
+        if (p >= 0) {
+          int u = p, v = q;
+          u = uf[u], v = uf[v];
+          if (u == v) continue;
+          uf.merge(u, v);
+          int r = uf[u];
+          A.set(r, A.get(u) + A.get(v));
+        } else {
+          int v = ~p, x = q;
+          v = uf[v];
+          A.set(v, A.get(v) + x);
+        }
+        continue;
       }
-      if (t == 1) {
-        a = uf[a];
-        A.set(a, A.get(a) + b);
-      }
-      if (t == 2) {
-        ll ans = A.get(uf[b]);
-        ANS[a] = ans;
-      }
+      if (a < mid) IL.eb(i);
+      if (mid < b) IR.eb(i);
     }
-    if (idx < size) {
-      dfs(dfs, 2 * idx + 0);
-      dfs(dfs, 2 * idx + 1);
+    if (begin + 1 == end) {
+      int v = query[begin];
+      ANS[begin] = A.get(uf[v]);
+    } else {
+      dfs(dfs, IL, begin, mid);
+      dfs(dfs, IR, mid, end);
     }
-    A.rollback(time1);
-    uf.rollback(time2);
+    // rollback
+    A.rollback(a_time);
+    uf.rollback(uf_time);
   };
-  dfs(dfs, 1);
-  FOR(q, Q) {
-    auto [t, a, b] = query[q];
-    if (t == 3) print(ANS[q]);
-  }
+  dfs(dfs, I, 0, len(query));
+  for (auto&& x: ANS) print(x);
 }
 
 signed main() {
-  solve();
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
 
   return 0;
 }
