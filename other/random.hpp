450e3bab2058628c38513cb188b66fd99b206613
maspypy <maspypy.gmail.com>
2022-08-25 01:58:56 +0900

    once

--- other/random.hpp
+++ other/random.hpp
@@ -1,14 +1,17 @@
+#pragma once
+
 struct RandomNumberGenerator {
   mt19937 mt;
 
-  RandomNumberGenerator() : mt(chrono::steady_clock::now().time_since_epoch().count()) {}
+  RandomNumberGenerator()
+      : mt(chrono::steady_clock::now().time_since_epoch().count()) {}
 
-  ll operator()(ll a, ll b) {  // [a, b)
+  ll operator()(ll a, ll b) { // [a, b)
     uniform_int_distribution<ll> dist(a, b - 1);
     return dist(mt);
   }
 
-  ll operator()(ll b) {  // [0, b)
+  ll operator()(ll b) { // [0, b)
     return (*this)(0, b);
   }
 };
