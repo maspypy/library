da1ab370234284f85f8cae3daf72b2cc74b3dc6f
maspypy <maspypy.gmail.com>
2022-08-28 02:29:21 +0900

    add test

---
+++ test/atcoder/abc266h_bit.test.cpp
@@ -0,0 +1,78 @@
+#define PROBLEM "https://atcoder.jp/contests/abc266/tasks/abc266_h"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/fenwick2d.hpp"
+#include "alg/monoid_max.hpp"
+
+using Mono = Monoid_Max<ll>;
+
+void solve() {
+  LL(N);
+  using T = tuple<ll, ll, ll, ll>;
+
+  VEC(T, dat, N);
+  dat.eb(0, 0, 0, 0);
+  ++N;
+
+  sort(all(dat), [&](auto& a, auto& b) -> bool {
+    auto [at, ax, ay, aa] = a;
+    auto [bt, bx, by, bb] = b;
+    if (ay < by) return true;
+    if (ay > by) return false;
+    return at < bt;
+  });
+
+  /*
+  右上への遷移：
+  (x,y,t) が情報 (a,b,c,v) を受け取るのは
+  a<=x, c-a-b<=t-x-y
+  ・(a,c-a-b) に情報を追加する
+  ・[-INF,x] x [-INF, t-x-y] での最大値を求める
+
+  左上への遷移：
+  (a-x)+(y-b)<=t-c
+  (-a)<=(-x), a-b+c<=x-y+t
+  */
+  vi X1(N), Y1(N), X2(N), Y2(N);
+  FOR(i, N) {
+    auto [t, x, y, v] = dat[i];
+    X1[i] = x;
+    X2[i] = -x;
+    Y1[i] = t - x - y;
+    Y2[i] = x - y + t;
+  }
+  Fenwick2D<Mono, false> seg1(X1, Y1);
+  Fenwick2D<Mono, false> seg2(X2, Y2);
+
+  const ll INF = 1LL << 60;
+
+  ll ANS = 0;
+  FOR(i, N) {
+    const auto [t, x, y, v] = dat[i];
+    const ll a = x, b = y, c = t;
+    if (i == 0) {
+      seg1.add(a, c - a - b, 0);
+      seg2.add(-a, a - b + c, 0);
+      continue;
+    }
+    ll best = -INF;
+    chmax(best, seg1.prefix_sum(x + 1, t - x - y + 1));
+    chmax(best, seg2.prefix_sum((-x) + 1, x - y + t + 1));
+    if (best < 0) continue;
+    best += v;
+    chmax(ANS, best);
+    seg1.add(a, c - a - b, best);
+    seg2.add(-a, a - b + c, best);
+  }
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
