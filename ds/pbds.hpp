686ffa90e00cbc9770c2ce2acce3c1111397401f
maspypy <maspypy.gmail.com>
2022-08-27 23:26:12 +0900

    add comment

--- ds/pbds.hpp
+++ ds/pbds.hpp
@@ -3,6 +3,7 @@
 #include <ext/pb_ds/tag_and_trait.hpp>
 using namespace __gnu_pbds;
 
+// お手軽だけど、座圧＋BITとかの方がかなり速いので注意！
 template <typename KEY>
 using pbds_set = tree<KEY, null_type, less<KEY>, rb_tree_tag,
                       tree_order_statistics_node_update>;
