e9d75ffa28484c0c30057d9090cc450e820b4116
maspypy <maspypy.gmail.com>
2022-08-27 23:25:43 +0900

    add test

---
+++ test/atcoder/abc266f.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://atcoder.jp/contests/abc266/tasks/abc266_f"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/unicyclic.hpp"
+#include "graph/tree.hpp"
+
+void solve() {
+  LL(N);
+  Graph<int, 0> G(N);
+  G.read_graph(N);
+
+  UnicyclicGraph X(G);
+  auto T = X.tree;
+  TREE<decltype(T)> tree(T, X.root);
+
+  ll root = X.root;
+  ll bottom = X.TO[X.root];
+
+  LL(Q);
+  FOR(Q) {
+    LL(a, b);
+    --a, --b;
+    ll ca = tree.lca(a, bottom);
+    ll cb = tree.lca(b, bottom);
+    Yes(ca == cb);
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
---
+++ test/atcoder/abc266h.test.cpp
@@ -0,0 +1,113 @@
+#define PROBLEM "https://atcoder.jp/contests/abc266/tasks/abc266_h"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/segtree2d.hpp"
+#include "alg/monoid_max.hpp"
+
+using Mono = Monoid_Max<ll>;
+
+void naive() {
+  LL(N);
+  using T = tuple<ll, ll, ll, ll>;
+
+  VEC(T, dat, N);
+  dat.eb(0, 0, 0, 0);
+  ++N;
+
+  sort(all(dat), [&](auto& a, auto& b) -> bool {
+    auto [at, ax, ay, aa] = a;
+    auto [bt, bx, by, bb] = b;
+    if (ay < by) return true;
+    if (ay > by) return false;
+    return at < bt;
+  });
+
+  const ll INF = 1LL << 60;
+  vi dp(N, -INF);
+  dp[0] = 0;
+  FOR(j, N) {
+    auto [tj, xj, yj, vvj] = dat[j];
+    FOR(i, j) {
+      auto [ti, xi, yi, vvi] = dat[i];
+      ll d = abs(xi - xj) + abs(yi - yj);
+      if (yi > yj) continue;
+      if (ti + d > tj) continue;
+      chmax(dp[j], dp[i] + vvj);
+    }
+    print(tj, xj, yj, vvj, dp[j]);
+  }
+  print(MAX(dp));
+}
+
+void solve() {
+  // return naive();
+
+  LL(N);
+  using T = tuple<ll, ll, ll, ll>;
+
+  VEC(T, dat, N);
+  dat.eb(0, 0, 0, 0);
+  ++N;
+
+  sort(all(dat), [&](auto& a, auto& b) -> bool {
+    auto [at, ax, ay, aa] = a;
+    auto [bt, bx, by, bb] = b;
+    if (ay < by) return true;
+    if (ay > by) return false;
+    return at < bt;
+  });
+
+  /*
+  右上への遷移：
+  (x,y,t) が情報 (a,b,c,v) を受け取るのは
+  a<=x, x+y-t<=a+b-c
+  ・(a,a+b-c) に情報を追加する
+  ・[-INF,x] x [x+y-t, INF] での最大値を求める
+
+  左上への遷移：
+  (a-x)+(y-b)<=t-c
+  x<=a, a-b+c<=x-y+t
+  */
+  vi X1(N), Y1(N), X2(N), Y2(N);
+  FOR(i, N) {
+    auto [t, x, y, v] = dat[i];
+    X1[i] = x;
+    X2[i] = x;
+    Y1[i] = x + y - t;
+    Y2[i] = x - y + t;
+  }
+  SegTree2D<Mono, ll, false> seg1(X1, Y1);
+  SegTree2D<Mono, ll, false> seg2(X2, Y2);
+
+  const ll INF = 1LL << 60;
+
+  ll ANS = 0;
+  FOR(i, N) {
+    const auto [t, x, y, v] = dat[i];
+    const ll a = x, b = y, c = t;
+    if (i == 0) {
+      seg1.multiply(a, a + b - c, 0);
+      seg2.multiply(a, a - b + c, 0);
+      continue;
+    }
+    ll best = -INF;
+    chmax(best, seg1.prod(-INF, x + y - t, x + 1, INF));
+    chmax(best, seg2.prod(x, -INF, INF, x - y + t + 1));
+    if (best < 0) continue;
+    best += v;
+    chmax(ANS, best);
+    seg1.multiply(a, a + b - c, best);
+    seg2.multiply(a, a - b + c, best);
+  }
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
