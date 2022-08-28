5d2e41a24e7d07b25ead2a01f425069e516cb22d
maspypy <maspypy.gmail.com>
2022-08-28 10:13:46 +0900

    add test

---
+++ test/aoj/ITP2_3_B.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM \
+  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP2_3_B"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/segtree.hpp"
+#include "alg/monoid_minmax.hpp"
+
+void solve() {
+  LL(N);
+  VEC(int, A, N);
+  using Mono = Monoid_MinMax<int>;
+  SegTree<Mono> seg(N, [&](int i) { return Mono::from_element(A[i]); });
+  LL(Q);
+  FOR(Q) {
+    LL(t, b, e);
+    if (t == 0) print(seg.prod(b, e).fi);
+    if (t == 1) print(seg.prod(b, e).se);
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
