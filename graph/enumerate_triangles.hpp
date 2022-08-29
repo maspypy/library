04c138ba26aad154603ef430fa7c0be51ddf4b97
maspypy <45988013+maspypy@users.noreply.github.com>
2022-08-29 20:37:09 +0900

    Update enumerate_triangles.hpp

--- graph/enumerate_triangles.hpp
+++ graph/enumerate_triangles.hpp
@@ -1,14 +1,12 @@
 #include "graph/base.hpp"
-#include "graph/degree.hpp"
 
 template <typename Gr, typename F>
 void enumerate_triangles(Gr& G, F query) {
   int N = G.N;
-  auto deg = degree(G);
   Graph<int, 1> H(N);
   for (auto&& e: G.edges) {
     // 注意：次数比較だけだと DAG にならず、サイクルができてしまう
-    if (mp(deg[e.frm], e.frm) < mp(deg[e.to], e.to))
+    if (mp(G.deg(e.frm), e.frm) < mp(G.deg(e.to), e.to))
       H.add(e.frm, e.to);
     else
       H.add(e.to, e.frm);
