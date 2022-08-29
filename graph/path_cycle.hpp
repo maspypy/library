80e155bcab29ff0a7a1e608ad8fc8834d00e8722
maspypy <maspypy.gmail.com>
2022-08-29 19:36:17 +0900

    path cycle decomposition

---
+++ graph/path_cycle.hpp
@@ -0,0 +1,37 @@
+#include "graph/degree.hpp"
+// どの点の次数も 2 以下のグラフがあるときに、
+// パスの頂点列, サイクルの頂点列
+// に分解する
+template <typename Graph>
+pair<vvc<int>, vvc<int>> path_cycle(Graph& G) {
+  int N = G.N;
+  auto deg = degree(G);
+  assert(MAX(deg) <= 2);
+
+  vc<bool> done(N);
+  auto calc_frm = [&](int v) -> vc<int> {
+    vc<int> P = {v};
+    done[v] = 1;
+    while (1) {
+      bool ok = 0;
+      for (auto&& e: G[P.back()]) {
+        if (done[e.to]) continue;
+        P.eb(e.to);
+        done[e.to] = 1;
+        ok = 1;
+      }
+      if (!ok) break;
+    }
+    return P;
+  };
+  vvc<int> paths, cycs;
+  FOR(v, N) {
+    if (done[v] || deg[v] != 1) continue;
+    paths.eb(calc_frm(v));
+  }
+  FOR(v, N) {
+    if (done[v]) continue;
+    cycs.eb(calc_frm(v));
+  }
+  return {paths, cycs};
+}
