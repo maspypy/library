645e649c21ef7b6d91244cb7159a93cd9590e79b
maspypy <maspypy.gmail.com>
2022-08-30 02:42:49 +0900

    change BCT

--- graph/biconnected_component.hpp
+++ graph/biconnected_component.hpp
@@ -1,128 +1,57 @@
 #include "graph/base.hpp"
 
 /*
-孤立点は、辺のない component で、block になる。関節点ではない。
-block cut treeの頂点番号：
-[0, n_block)：block
-[n_block, n_block + n_cut)：cut （関節点）
+block-cut tree を、block に通常の頂点を隣接させて拡張しておく
+https://twitter.com/noshi91/status/1529858538650374144?s=20&t=eznpFbuD9BDhfTb4PplFUg
+[0, n)：もとの頂点
+[n, n + n_block)：block
+関節点：[0, n) のうちで、degree >= 2 を満たすもの
+
+孤立点は、1 点だけからなる block
 */
-template <typename GT>
-struct Biconnected_Component {
-  GT& G;
-  vc<pair<int, int>> BCT_edges;
-  int n_block, n_cut;
-  vc<vc<int>> comp_e;
-  vc<vc<int>> comp_v; // block -> vs
-  vc<int> BCT_idx_edge;
-  vc<int> BCT_idx_vertex;
+template <typename GRAPH>
+Graph<int, 0> block_cut_tree(GRAPH& G) {
+  int n = G.N;
+  vc<int> low(n), ord(n), st;
+  vc<bool> used(n);
+  st.reserve(n);
+  Graph<int, 0> BCT;
+  int nxt = n;
+  int k = 0;
 
-  Biconnected_Component(GT& G) : G(G) {
-    auto [ord, low] = calculate_lowlink();
-    calculate_bcc(ord, low);
-    build_bct();
-  }
-
-  int BCT_idx_v(int v) { return BCT_idx_vertex[v]; }
-  int BCT_idx_e(int eid) { return BCT_idx_edge[eid]; }
-  Graph<int> BCT() {
-    Graph<int> bct(n_block + n_cut);
-    for (auto&& [a, b]: BCT_edges) bct.add(a, b);
-    bct.build();
-    return bct;
-  }
-  bool is_articulation(int v) { return BCT_idx_v(v) >= n_block; }
-
-private:
-  void build_bct() {
-    int n = G.N;
-    vvc<int> nbd(n);
-    n_block = len(comp_e);
-    comp_v.resize(n_block);
-    n_cut = 0;
-    BCT_idx_edge.resize(G.M);
-    BCT_idx_vertex.resize(G.N);
-
-    auto add = [&](int v, int c) -> void {
-      if (len(nbd[v]) && nbd[v].back() == c) return;
-      nbd[v].eb(c);
-    };
-
-    FOR(c, len(comp_e)) {
-      for (auto&& eid: comp_e[c]) {
-        BCT_idx_edge[eid] = c;
-        auto& e = G.edges[eid];
-        add(e.frm, c);
-        add(e.to, c);
+  auto dfs = [&](auto& dfs, int v, int p) -> void {
+    st.eb(v);
+    used[v] = 1;
+    low[v] = ord[v] = k++;
+    int child = 0;
+    for (auto&& e: G[v]) {
+      if (e.to == p) continue;
+      if (!used[e.to]) {
+        ++child;
+        int s = len(st);
+        dfs(dfs, e.to, v);
+        chmin(low[v], low[e.to]);
+        if ((p == -1 && child > 1) || (p != -1 && low[e.to] >= ord[v])) {
+          BCT.resize(nxt + 1);
+          BCT.add(nxt, v);
+          while (len(st) > s) {
+            BCT.add(nxt, st.back());
+            st.pop_back();
+          }
+          ++nxt;
+        }
+      } else {
+        chmin(low[v], ord[e.to]);
       }
     }
-
-    FOR(v, n) {
-      if (len(nbd[v]) == 0) {
-        // 孤立点は辺のない block
-        BCT_idx_vertex[v] = n_block++;
-        comp_v.eb(vc<int>({int(v)}));
-      }
-      if (len(nbd[v]) >= 2) {
-        BCT_idx_vertex[v] = n_block + n_cut;
-        for (auto&& c: nbd[v]) {
-          BCT_edges.eb(n_block + n_cut, c);
-          comp_v[c].eb(v);
-        }
-        n_cut++;
-      }
-      elif (len(nbd[v]) == 1) {
-        int c = nbd[v][0];
-        BCT_idx_vertex[v] = c;
-        comp_v[c].eb(v);
-      }
-    }
-
-    comp_e.resize(n_block);
+  };
+  FOR(v, n) if (!used[v]) {
+    dfs(dfs, v, -1);
+    BCT.resize(nxt + 1);
+    for (auto&& x: st) { BCT.add(nxt, x); }
+    ++nxt;
+    st.clear();
   }
-
-  pair<vc<int>, vc<int>> calculate_lowlink() {
-    int n = G.N;
-    vc<bool> used(n);
-    vc<int> low(n), ord(n);
-    int k = 0;
-    auto dfs = [&](auto self, int v, int eid) -> void {
-      used[v] = 1;
-      low[v] = ord[v] = k++;
-      for (auto&& e: G[v]) {
-        if (e.id == eid) continue;
-        if (!used[e.to]) {
-          self(self, e.to, e.id);
-          chmin(low[v], low[e.to]);
-        } else {
-          chmin(low[v], ord[e.to]);
-        }
-      }
-    };
-    FOR(v, n) if (!used[v]) dfs(dfs, v, -1);
-    return {ord, low};
-  }
-
-  void calculate_bcc(vc<int>& ord, vc<int>& low) {
-    int n = G.N;
-    vc<bool> used(n);
-    vc<int> buf;
-    auto dfs = [&](auto self, int v, int eid) -> void {
-      used[v] = 1;
-      for (auto&& e: G[v]) {
-        if (e.id == eid) continue;
-        if (!used[e.to] || ord[e.to] < ord[v]) buf.eb(e.id);
-        if (used[e.to]) continue;
-        self(self, e.to, e.id);
-        if (low[e.to] < ord[v]) continue;
-        vc<int> edges;
-        while (1) {
-          edges.eb(buf.back());
-          buf.pop_back();
-          if (edges.back() == e.id) break;
-        }
-        comp_e.eb(edges);
-      }
-    };
-    FOR(v, n) if (!used[v]) dfs(dfs, v, -1);
-  }
-};
+  BCT.build();
+  return BCT;
+}
