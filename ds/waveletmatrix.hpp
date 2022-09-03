9c9a59c4bfebc39f80f0e5e824a94dfddf3bfa80
maspypy <maspypy.gmail.com>
2022-09-03 17:10:30 +0900

    upd

--- ds/waveletmatrix.hpp
+++ ds/waveletmatrix.hpp
@@ -1,122 +1,85 @@
-#include <immintrin.h>
-
-struct bit_vector {
-  using u32 = uint32_t;
-  using i64 = int64_t;
-  using u64 = uint64_t;
-
-  static constexpr u32 w = 64;
-  vector<u64> block;
-  vector<u32> count;
-  u32 n, zeros;
-
-  inline u32 get(u32 i) const { return u32(block[i / w] >> (i % w)) & 1u; }
-  inline void set(u32 i) { block[i / w] |= 1LL << (i % w); }
-
-  bit_vector() {}
-  bit_vector(int _n) { init(_n); }
-  __attribute__((optimize("O3", "unroll-loops"))) void init(int _n) {
-    n = zeros = _n;
-    block.resize(n / w + 1, 0);
-    count.resize(block.size(), 0);
-  }
-
-  __attribute__((target("popcnt"))) void build() {
-    for (u32 i = 1; i < block.size(); ++i)
-      count[i] = count[i - 1] + _mm_popcnt_u64(block[i - 1]);
-    zeros = rank0(n);
-  }
-
-  inline u32 rank0(u32 i) const { return i - rank1(i); }
-  __attribute__((target("bmi2,popcnt"))) inline u32 rank1(u32 i) const {
-    return count[i / w] + _mm_popcnt_u64(_bzhi_u64(block[i / w], i % w));
-  }
-};
-
-/*
-コンストラクタ：vector<T> を渡す
-静的な列に対して次が O(log N) 時間で行える
-・freq(l, r, lower, upper)：[lower, upper) 内の要素の数え上げ
-・kth(l, r, lower, upper)：[lower, upper) 内をソートしたときの k 番目
-*/
 template <typename T>
 struct WaveletMatrix {
-  using u32 = uint32_t;
-  using i64 = int64_t;
-  using u64 = uint64_t;
+  struct BitVector {
+    vector<u64> buf;
+    vector<int> sum;
+    BitVector(const vector<char>& a = {}) {
+      int n = a.size();
+      buf.assign((n + 63) >> 6, 0);
+      sum.assign(buf.size() + 1, 0);
+      FOR(i, n) if (a[i]) {
+        buf[i >> 6] |= 1ull << (i & 63);
+        sum[(i >> 6) + 1]++;
+      }
+      FOR(i, buf.size()) sum[i + 1] += sum[i];
+    }
+    int rank(int k, bool f = 1) {
+      int ret = sum[k >> 6]
+                + __builtin_popcountll(buf[k >> 6] & ((1ull << (k & 63)) - 1));
+      if (!f)
+        return k - ret;
+      else
+        return ret;
+    }
+  };
 
-  int n, lg;
+  int N, lg;
+  vector<int> mid;
+  vector<BitVector> bv;
   vc<T> key;
-  vc<int> A;
-  vector<bit_vector> bv;
-
-  __attribute__((optimize("O3"))) WaveletMatrix(const vc<T>& dat)
-      : n(len(dat)) {
+  WaveletMatrix(vector<T>& dat) : N(dat.size()) {
     key = dat;
     UNIQUE(key);
-    A.resize(n);
-    FOR(i, n) A[i] = LB(key, dat[i]);
+    vc<int> A(N);
+    FOR(i, N) A[i] = LB(key, dat[i]);
     lg = __lg(max(MAX(A), 1)) + 1;
-    bv.assign(lg, n);
-    vc<int> cur = A, nxt(n);
-    for (int h = lg - 1; h >= 0; --h) {
-      for (int i = 0; i < n; ++i)
-        if ((cur[i] >> h) & 1) bv[h].set(i);
-      bv[h].build();
-      array<decltype(begin(nxt)), 2> it{begin(nxt), begin(nxt) + bv[h].zeros};
-      for (int i = 0; i < n; ++i) *it[bv[h].get(i)]++ = cur[i];
-      swap(cur, nxt);
-    }
-  }
 
-  inline pair<u32, u32> succ0(int l, int r, int h) const {
-    return make_pair(bv[h].rank0(l), bv[h].rank0(r));
-  }
-
-  inline pair<u32, u32> succ1(int l, int r, int h) const {
-    u32 l0 = bv[h].rank0(l);
-    u32 r0 = bv[h].rank0(r);
-    u32 zeros = bv[h].zeros;
-    return make_pair(l + zeros - l0, r + zeros - r0);
-  }
-
-  T kth(u32 l, u32 r, u32 k) const {
-    int res = 0;
-    for (int h = lg - 1; h >= 0; --h) {
-      u32 l0 = bv[h].rank0(l), r0 = bv[h].rank0(r);
-      if (k < r0 - l0)
-        l = l0, r = r0;
-      else {
-        k -= r0 - l0;
-        res |= 1 << h;
-        l += bv[h].zeros - l0;
-        r += bv[h].zeros - r0;
+    mid.resize(lg);
+    bv.resize(lg);
+    for (int d = lg - 1; d >= 0; d--) {
+      vector<char> add;
+      vector nxt(2, vector<T>());
+      for (auto& x: A) {
+        add.push_back(x >> d & 1);
+        nxt[x >> d & 1].push_back(x);
       }
+      mid[d] = (int)nxt[0].size();
+      bv[d] = BitVector(add);
+      swap(A, nxt[0]);
+      A.insert(A.end(), all(nxt[1]));
     }
-    return key[res];
   }
 
-  int freq(int l, int r, T lower, T upper) {
-    return freq_upper(l, r, upper) - freq_upper(l, r, lower);
+  // [L, R) 内にある [a, b) を数える
+  int freq(int L, int R, T a, T b) {
+    return freq_upper(L, R, b) - freq_upper(L, R, a);
   }
-
-private:
-  int freq_upper(int l, int r, T upper_t) {
-    int upper = LB(key, upper_t);
-    if (upper >= (1 << lg)) return r - l;
+  int freq_upper(int L, int R, T t) {
+    int x = LB(key, t);
+    if (x >= (1 << lg)) return R - L;
     int ret = 0;
     for (int h = lg - 1; h >= 0; --h) {
-      bool f = (upper >> h) & 1;
-      u32 l0 = bv[h].rank0(l), r0 = bv[h].rank0(r);
-      if (f) {
-        ret += r0 - l0;
-        l += bv[h].zeros - l0;
-        r += bv[h].zeros - r0;
-      } else {
-        l = l0;
-        r = r0;
-      }
+      bool f = (x >> h) & 1;
+      if (f) ret += bv[h].rank(R, 0) - bv[h].rank(L, 0);
+      L = bv[h].rank(L, f) + (f ? mid[h] : 0);
+      R = bv[h].rank(R, f) + (f ? mid[h] : 0);
     }
     return ret;
+  }
+
+  // [L, R) の中で k>=0 番目
+  T kth(int L, int R, int k) {
+    int ret = 0;
+    for (int h = lg - 1; h >= 0; h--) {
+      int l0 = bv[h].rank(L, 0), r0 = bv[h].rank(R, 0);
+      if (k < r0 - l0)
+        L = l0, R = r0;
+      else {
+        k -= r0 - l0;
+        ret |= 1 << h;
+        L += mid[h] - l0, R += mid[h] - r0;
+      }
+    }
+    return key[ret];
   }
 };
