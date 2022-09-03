13d5019c2bb9304f481605b3ef798db60b25ae10
maspypy <maspypy.gmail.com>
2022-09-03 23:33:56 +0900

    add prod of 1-x^n

---
+++ poly/product_of_one_minus_xn.hpp
@@ -0,0 +1,16 @@
+#include "poly/fps_exp.hpp"
+
+// prod (1 - x^a) を x^{LIM} まで求める
+template <typename mint, typename INT>
+vc<mint> product_of_one_minus_xn(vc<INT> A, int LIM) {
+  vc<int> CNT(LIM + 1);
+  for (auto&& x: A) {
+    if (x <= LIM) CNT[x]++;
+  }
+
+  vc<mint> logf(LIM + 1);
+  FOR(x, 1, LIM + 1) {
+    FOR(d, 1, LIM / x + 1) { logf[d * x] -= mint(CNT[x]) * inv<mint>(d); }
+  }
+  return fps_exp(logf);
+}
