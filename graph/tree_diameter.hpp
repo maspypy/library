2725a8e78f57078d0a0ad53e758dadc32c3afd81
maspypy <maspypy.gmail.com>
2022-09-05 02:43:49 +0900

    upd

--- graph/tree_diameter.hpp
+++ graph/tree_diameter.hpp
@@ -6,10 +6,10 @@ pair<T, vc<int>> tree_diameter(Graph<T>& G) {
   assert(G.is_prepared());
   int A, B;
   {
-    auto [dist, par] = bfs01(G, 0);
+    auto [dist, par] = bfs01<decltype(G), T>(G, 0);
     A = max_element(all(dist)) - dist.begin();
   }
-  auto [dist, par] = bfs01(G, A);
+  auto [dist, par] = bfs01<decltype(G), T>(G, A);
   B = max_element(all(dist)) - dist.begin();
   vc<int> P = restore_path(par, B);
   return {dist[B], P};
