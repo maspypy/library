ecfaa53a504942729a83e8679b362e82f06f41b3
maspypy <maspypy.gmail.com>
2022-09-16 08:56:30 +0900

    refactoring

--- graph/functional.hpp
+++ graph/functional.hpp
@@ -1,14 +1,13 @@
+#include "graph/tree.hpp"
 #include "ds/unionfind.hpp"
-#include "graph/base.hpp"
 
+// N が根となる木を新たに作る
 template <typename T = int>
 struct FunctionalGraph {
   int N, M;
-  Graph<T, 1> newG; // 新しい頂点 N を根として追加した有向木。逆向きの辺になる。
   vc<int> TO;
   vc<T> wt;
   vc<int> root;
-  vvc<int> cyc;
 
   FunctionalGraph() {}
   FunctionalGraph(int N) : N(N), M(0), TO(N, -1), wt(N), root(N, -1) {}
@@ -21,34 +20,37 @@ struct FunctionalGraph {
     wt[a] = c;
   }
 
-  void build() {
+  TREE<Graph<T, 1>> build() {
     assert(N == M);
     UnionFind uf(N);
     FOR(v, N) if (!uf.merge(v, TO[v])) { root[v] = v; }
     FOR(v, N) if (root[v] == v) root[uf[v]] = v;
     FOR(v, N) root[v] = root[uf[v]];
 
-    tree = Graph<T, 1>(N + 1);
+    Graph<T, 1> G(N + 1);
     FOR(v, N) {
-      if (root[v] != v)
-        tree.add(TO[v], v);
+      if (root[v] == v)
+        G.add(N, v, wt[v]);
       else
-        tree.add(N, v);
+        G.add(TO[v], v, wt[v]);
     }
-    tree.build();
-    cyc.resize(N);
-    FOR(v, N) if (root[v] == v) {
-      vc<int> C = {TO[root[v]]};
-      while (C.back() != v) C.eb(TO[C.back()]);
-      cyc[v] = C;
-    }
+
+    G.build();
+    TREE<Graph<T, 1>> tree(G, N);
+    return tree;
   }
 
-  void debug() {
-    print("TO", TO);
-    print("root");
-    print(root);
-    print("根を追加した木 tree");
-    tree.debug();
+  // functional graph に向かって進む
+  template <typename TREE>
+  int jump(TREE& tree, int v, ll step) {
+    int d = tree.depth[v];
+    if (step <= d - 1) return tree.jump(v, N, step);
+    v = tree.jump(v, N, d - 1);
+    step -= d - 1;
+    int bottom = TO[v];
+    int c = tree.depth[bottom];
+    step %= c;
+    if (step == 0) return v;
+    return tree.jump(bottom, step - 1);
   }
 };
