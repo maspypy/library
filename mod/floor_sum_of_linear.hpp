f17909ebdae6efbc48a2c38ac569e9c29cda7d42
maspypy <maspypy.gmail.com>
2022-09-19 03:25:55 +0900

    improve performance?

--- mod/floor_sum_of_linear.hpp
+++ mod/floor_sum_of_linear.hpp
@@ -6,12 +6,21 @@ i128 floor_sum_of_linear(ll L, ll R, ll a, ll b, ll mod) {
   i128 res = 0;
   b += L * a;
   ll N = R - L;
+
+  if (b < 0) {
+    ll k = ceil(-b, mod);
+    b += k * mod;
+    res -= i128(N) * k;
+  }
+
   while (N) {
     ll q;
     tie(q, a) = divmod(a, mod);
     res += i128(N) * (N - 1) / 2 * q;
-    tie(q, b) = divmod(b, mod);
-    res += i128(N) * q;
+    if (b >= mod) {
+      tie(q, b) = divmod(b, mod);
+      res += i128(N) * q;
+    }
     tie(N, b) = divmod(a * N + b, mod);
     tie(a, mod) = mp(mod, a);
   }
