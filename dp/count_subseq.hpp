fb03374ca953e47f8807eefc73812cd68e094205
maspypy <maspypy.gmail.com>
2022-09-19 03:41:05 +0900

    change typename

--- dp/count_subseq.hpp
+++ dp/count_subseq.hpp
@@ -1,14 +1,14 @@
-template <typename mint, typename T, typename STRING>
+template <typename mint, typename CHAR, typename STRING>
 mint count_subseq(STRING S) {
   auto key = S;
   UNIQUE(key);
-  for(auto&& x : S)x=LB(key,x);
-  vc<int> IDX(len(key),-1);
+  for (auto&& x: S) x = LB(key, x);
+  vc<int> IDX(len(key), -1);
   ll N = len(S);
   vc<mint> DP(N + 1);
   DP[0] = 1;
   FOR(i, N) {
-    T c = S[i];
+    CHAR c = S[i];
     DP[i + 1] = DP[i] + DP[i];
     if (IDX[c] != -1) { DP[i + 1] -= DP[IDX[c]]; }
     IDX[c] = i;
