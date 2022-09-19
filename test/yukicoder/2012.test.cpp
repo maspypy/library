74b2c5257885aae37e5cb683e9f8acc026e7ad34
maspypy <maspypy.gmail.com>
2022-09-19 13:57:26 +0900

    add test

---
+++ test/yukicoder/2012.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2012"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/cht.hpp"
+
+void solve() {
+  LL(N);
+  CHT_xy<ll> cht;
+  VEC(pi, XY, N);
+  ll ANS = 0;
+  for (auto&& [a, b]: XY) cht.add(a, -b);
+  for (auto&& [x, y]: XY) { chmax(ANS, cht.get_max(y, x)); }
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
