1d1186c5372ab35cc1d9731966701d6a6c4202f3
maspypy <maspypy.gmail.com>
2022-09-19 13:20:04 +0900

    add undirected

--- graph/cycle_detection.hpp
+++ graph/cycle_detection.hpp
@@ -1,13 +1,11 @@
 #include "graph/base.hpp"
 
-// 辺の列 or 頂点列の vector を返す
-// 見つからなかった場合には、空 vector
 template <typename Graph>
-vc<int> cycle_detection(Graph& G, bool is_edge = 0) {
+vc<int> cycle_detection_directed(Graph& G, bool is_edge = 0) {
   assert(G.is_directed());
   assert(G.is_prepared());
   if (!is_edge) {
-    auto C = cycle_detection(G, true);
+    auto C = cycle_detection_directed(G, true);
     if (len(C) == 0) return C;
     vc<int> ANS(len(C));
     FOR(i, len(C)) {
@@ -19,7 +17,6 @@ vc<int> cycle_detection(Graph& G, bool is_edge = 0) {
 
   int N = G.N;
   vc<int> used(N);
-  vc<int> path; // edge
   vc<pair<int, int>> par(N);
   vector<int> ANS;
 
@@ -46,4 +43,50 @@ vc<int> cycle_detection(Graph& G, bool is_edge = 0) {
   };
   FOR(v, N) if (!used[v]) dfs(dfs, v);
   return ANS;
+}
+
+template <typename Graph>
+vc<int> cycle_detection_undirected(Graph& G, bool is_edge = 0) {
+  assert(!G.is_directed());
+  assert(G.is_prepared());
+
+  int N = G.N;
+  vc<int> used_e(G.M);
+  vc<int> used_v(G.N);
+  vc<pair<int, int>> par(N);
+  vector<int> ANS;
+
+  auto dfs = [&](auto& dfs, int v) -> void {
+    used_v[v] = 1;
+    for (auto&& e: G[v]) {
+      if (len(ANS)) return;
+      if (used_e[e.id]) continue;
+      used_e[e.id] = 1;
+      if (!used_v[e.to]) {
+        par[e.to] = {v, e.id};
+        dfs(dfs, e.to);
+      } else {
+        if (is_edge) ANS = {e.id};
+        if (!is_edge) ANS = {v};
+        int cur = v;
+        while (cur != e.to) {
+          if (is_edge) ANS.eb(par[cur].se);
+          if (!is_edge) ANS.eb(par[cur].fi);
+          cur = par[cur].fi;
+        }
+        reverse(all(ANS));
+        return;
+      }
+    }
+  };
+  FOR(v, N) if (!used_v[v]) dfs(dfs, v);
+  return ANS;
+}
+
+// 辺の列 or 頂点列の vector を返す
+// 見つからなかった場合には、空 vector
+template <typename Graph>
+vc<int> cycle_detection(Graph& G, bool is_edge = 0) {
+  if (G.is_directed()) return cycle_detection_directed(G, is_edge);
+  return cycle_detection_undirected(G, is_edge);
 }
