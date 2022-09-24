b8c7cf8adc1a2029d363a8b1b7c8e39e1f99a621
maspypy <maspypy.gmail.com>
2022-09-24 23:43:19 +0900

    add integral

---
+++ geo/integral.hpp
@@ -0,0 +1,15 @@
+template <typename Re, typename F>
+Re integral(Re a, Re b, F f, int n) {
+  Re I = 0;
+  Re dx = (b - a) / n;
+  Re fl = 0, fr = f(a);
+  FOR(i, n) {
+    Re lx = a + dx * i;
+    Re rx = lx + dx;
+    fl = fr;
+    fr = f(rx);
+    Re fm = f((lx + rx) / 2);
+    I += fl + 4 * fm + fr;
+  }
+  return I * dx / 6;
+}
