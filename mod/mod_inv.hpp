e64614a24153fdc630a3a1e662f6012f4f14c3d7
maspypy <maspypy.gmail.com>
2022-09-24 23:40:40 +0900

    negative case

--- mod/mod_inv.hpp
+++ mod/mod_inv.hpp
@@ -1,11 +1,13 @@
 #pragma once
 // long でも大丈夫
 ll mod_inv(ll val, ll mod) {
+  val %= mod;
+  if (val < 0) val += mod;
   ll a = val, b = mod, u = 1, v = 0, t;
   while (b > 0) {
     t = a / b;
     swap(a -= t * b, b), swap(u -= t * v, v);
   }
-  if(u < 0) u += mod;
+  if (u < 0) u += mod;
   return u;
 }
