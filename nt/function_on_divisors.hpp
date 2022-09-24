ed9959d1fd4b4cb551960e17b66eba264f51e65e
maspypy <maspypy.gmail.com>
2022-09-24 23:41:03 +0900

    add func on divisors

---
+++ nt/function_on_divisors.hpp
@@ -0,0 +1,91 @@
+#include "nt/factor.hpp"
+
+template <typename T>
+struct Function_on_Divisors {
+  vc<pi> pf;
+  vc<ll> divs;
+  vc<T> dat;
+
+  Function_on_Divisors(ll N) : Function_on_Divisors(factor(N)) {}
+  Function_on_Divisors(vc<pi> pf) : pf(pf) {
+    ll n = 1;
+    for (auto&& [p, e]: pf) n *= (e + 1);
+    divs.reserve(n);
+    divs = {1};
+    for (auto&& [p, e]: pf) {
+      int n = len(divs);
+      ll q = p;
+      FOR(e) {
+        FOR(i, n) divs.eb(divs[i] * q);
+        q *= p;
+      }
+    }
+  }
+
+  // f(p, k) を与える → 乗法的に拡張
+  template <typename F>
+  void set_multiplicative(F f) {
+    dat.reserve(len(divs));
+    dat = {T(1)};
+    for (auto&& [p, e]: pf) {
+      int n = len(divs);
+      FOR(k, 1, e + 1) { FOR(i, n) dat.eb(dat[i] * f(p, k)); }
+    }
+  }
+
+  void set_euler_phi() {
+    dat.resize(len(divs));
+    FOR(i, len(divs)) dat[i] = T(divs[i]);
+    divisor_mobius();
+  }
+
+  void multiplier_zeta() {
+    ll k = 1;
+    for (auto&& [p, e]: pf) {
+      ll mod = k * (e + 1);
+      FOR(i, len(divs) / mod) {
+        FOR_R(j, mod - k) { dat[mod * i + j] += dat[mod * i + j + k]; }
+      }
+      k *= (e + 1);
+    }
+  }
+
+  void multiplier_mobius() {
+    ll k = 1;
+    for (auto&& [p, e]: pf) {
+      ll mod = k * (e + 1);
+      FOR(i, len(divs) / mod) {
+        FOR(j, mod - k) { dat[mod * i + j] -= dat[mod * i + j + k]; }
+      }
+      k *= (e + 1);
+    }
+  }
+
+  void divisor_zeta() {
+    ll k = 1;
+    for (auto&& [p, e]: pf) {
+      ll mod = k * (e + 1);
+      FOR(i, len(divs) / mod) {
+        FOR(j, mod - k) { dat[mod * i + j + k] += dat[mod * i + j]; }
+      }
+      k *= (e + 1);
+    }
+  }
+
+  void divisor_mobius() {
+    ll k = 1;
+    for (auto&& [p, e]: pf) {
+      ll mod = k * (e + 1);
+      FOR(i, len(divs) / mod) {
+        FOR_R(j, mod - k) { dat[mod * i + j + k] -= dat[mod * i + j]; }
+      }
+      k *= (e + 1);
+    }
+  }
+
+  vc<pair<ll, T>> get() {
+    vc<pair<ll, T>> res;
+    FOR(i, len(divs)) { res.eb(divs[i], dat[i]); }
+    return res;
+  }
+};
