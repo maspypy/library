a7e8081440bff7fce94c7f47613fcdc52c5b2d6d
maspypy <maspypy.gmail.com>
2022-09-29 01:09:53 +0900

    test

---
+++ test/yukicoder/1319.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1319"
+#define ERROR 0.01
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "geo/integral.hpp"
+
+void solve() {
+  DBL(R, H, D);
+  using Re = double;
+  auto f = [&](Re z) -> Re {
+    Re r = R * z / H;
+    if (2 * r <= D) return 0;
+    Re theta = acos(D / (2 * r));
+    Re S = r * r * sin(2 * theta);
+    Re T = r * r * theta * 2;
+    return T - S;
+  };
+
+  Re ANS = integral<Re>(0, H, f, 10000);
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
