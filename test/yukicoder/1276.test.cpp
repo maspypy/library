2faa7ea1e23f51fdb192505070aac89f7620628a
maspypy <maspypy.gmail.com>
2022-09-30 22:13:04 +0900

    add test

---
+++ test/yukicoder/1276.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1276"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/modint.hpp"
+#include "nt/multiplicative_sum.hpp"
+#include "nt/primesum.hpp"
+#include "other/floor_range.hpp"
+
+using mint = modint107;
+
+void solve() {
+  LL(N);
+  // zeta^3
+  // p^k -> k(k+1)/2
+  PrimeSum<mint> X(N);
+  X.calc_count();
+  auto f_psum = [&](ll n) -> mint { return X[n] * mint(3); };
+  auto f_pe = [&](ll p, ll e) -> mint { return (e + 1) * (e + 2) / 2; };
+
+  mint ANS = 0;
+  mint x = 0; // a|b
+
+  floor_range(q, l, r, N) {
+    x += mint(r - l) * q;
+    // b|c かつ distinct
+    ANS += mint(N - 2) * mint(r - l) * mint(q - 1);
+    // b|a,c かつ distinct
+    ANS -= mint(r - l) * mint((q - 1) * (q - 2));
+  }
+  // a|b|c
+  mint y = multiplicative_sum<mint>(N, f_pe, f_psum);
+  // このうち distinct なものは？
+  y -= x;
+  y -= x;
+  y += mint(N);
+  print(ANS - y);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
---
+++ test/yukicoder/1526.test.cpp
@@ -0,0 +1,43 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1526"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/beats_summin_chmax.hpp"
+
+void solve() {
+  LL(N);
+  VEC(ll, A, N);
+  for (auto&& x: A) --x;
+
+  // dp[L] := mex[L,R)>=k となる R の min
+  vi dp(N);
+  FOR(i, N) dp[i] = i + 1;
+  Beats_SumMin_Chmax X(dp);
+
+  vvc<int> IDS(N + 1);
+  FOR(i, N) IDS[A[i]].eb(i);
+
+  ll ANS = 0;
+  FOR(x, N + 1) {
+    // ANS += (N + 1) * N - SUM<int>(dp);
+    ANS += (N + 1) * N - X.prod(0, N).sum;
+    auto& I = IDS[x];
+    I.insert(I.begin(), -1);
+    I.eb(N);
+    FOR(t, len(I) - 1) {
+      auto i = I[t], j = I[t + 1];
+      // FOR(k, i + 1, j) chmax(dp[k], j + 1);
+      X.chmax(i + 1, j, j + 1);
+    }
+  }
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
