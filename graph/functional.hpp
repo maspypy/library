deacbb1b0bda12b4868b81747f3b2b93cc9135e1
maspypy <maspypy.gmail.com>
2022-09-30 22:12:56 +0900

    jump_all

--- graph/functional.hpp
+++ graph/functional.hpp
@@ -8,6 +8,7 @@ struct FunctionalGraph {
   vc<int> TO;
   vc<T> wt;
   vc<int> root;
+  Graph<T, 1> G;
 
   FunctionalGraph() {}
   FunctionalGraph(int N) : N(N), M(0), TO(N, -1), wt(N), root(N, -1) {}
@@ -27,14 +28,13 @@ struct FunctionalGraph {
     FOR(v, N) if (root[v] == v) root[uf[v]] = v;
     FOR(v, N) root[v] = root[uf[v]];
 
-    Graph<T, 1> G(N + 1);
+    G.resize(N + 1);
     FOR(v, N) {
       if (root[v] == v)
         G.add(N, v, wt[v]);
       else
         G.add(TO[v], v, wt[v]);
     }
-
     G.build();
     TREE<Graph<T, 1>> tree(G, N);
     return tree;
@@ -45,12 +45,47 @@ struct FunctionalGraph {
   int jump(TREE& tree, int v, ll step) {
     int d = tree.depth[v];
     if (step <= d - 1) return tree.jump(v, N, step);
-    v = tree.jump(v, N, d - 1);
+    v = root[v];
     step -= d - 1;
     int bottom = TO[v];
     int c = tree.depth[bottom];
     step %= c;
     if (step == 0) return v;
     return tree.jump(bottom, step - 1);
+  }
+
+  // functional graph に step 回進む
+  template <typename TREE>
+  vc<int> jump_all(TREE& tree, ll step) {
+    auto& G = tree.G;
+    vc<int> res(N, -1);
+    // v の k 個先を res[w] に入れる
+    vvc<pair<int, int>> query(N);
+    FOR(v, N) {
+      int d = tree.depth[v];
+      int r = root[v];
+      if (d - 1 > step) { query[v].eb(v, step); }
+      if (d - 1 <= step) {
+        ll k = step - (d - 1);
+        int bottom = TO[r];
+        int c = tree.depth[bottom];
+        k %= c;
+        if (k == 0) {
+          res[v] = r;
+          continue;
+        }
+        query[bottom].eb(v, k - 1);
+      }
+    }
+
+    vc<int> path;
+    auto dfs = [&](auto& dfs, int v) -> void {
+      path.eb(v);
+      for (auto&& [w, k]: query[v]) { res[w] = path[len(path) - 1 - k]; }
+      for (auto&& e: G[v]) dfs(dfs, e.to);
+      path.pop_back();
+    };
+    for (auto&& e: G[N]) { dfs(dfs, e.to); }
+    return res;
   }
 };
