cda4ec04584e52daeba1e6dc461283a5a85811d7
maspypy <maspypy.gmail.com>
2022-10-02 02:22:20 +0900

    use random

---
+++ test/library_checker/math/primitive_root.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/primitive_root"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/primitive_root.hpp"
+
+void solve() {
+  LL(p);
+  ll ANS = primitive_root_long(p);
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
