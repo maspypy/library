9df2122bcdb0cf94855b7c7e6e48f22f61804878
maspypy <maspypy.gmail.com>
2022-10-01 14:15:29 +0900

    mergesort

--- graph/tournament.hpp
+++ graph/tournament.hpp
@@ -1,9 +1,26 @@
-// https://official.contest.yandex.ru/opencupXXII/contest/30766/problems/K
-// a → b 辺があるかどうかを判定する bool check(int, int) を渡す。
+// https://yukicoder.me/problems/no/2085
 template <typename F>
 vc<int> hamiltonian_path_in_tournament(int n, F check) {
-  vc<int> P(n);
-  iota(all(P), 0);
-  sort(all(P), [&](auto& x, auto& y) { return check(x, y); });
-  return P;
+  auto dfs = [&](auto& dfs, int L, int R) -> vc<int> {
+    if (R == L + 1) return {L};
+    int M = (L + R) / 2;
+    vc<int> X = dfs(dfs, L, M);
+    vc<int> Y = dfs(dfs, M, R);
+    vc<int> P;
+    P.reserve(R - L);
+    int i = 0, j = 0;
+    while (len(P) < R - L) {
+      if (i == len(X)) { P.eb(Y[j++]); }
+      elif (j == len(Y)) { P.eb(X[i++]); }
+      else {
+        if (check(X[i], Y[j])) {
+          P.eb(X[i++]);
+        } else {
+          P.eb(Y[j++]);
+        }
+      }
+    }
+    return P;
+  };
+  return dfs(dfs, 0, n);
 }
