93163eb12d1e8e723fae3cc80cf12b888a169d04
maspypy <maspypy.gmail.com>
2022-10-12 08:06:00 +0900

    str/vec

--- string/is_substring.hpp
+++ string/is_substring.hpp
@@ -2,11 +2,13 @@
 
 // 連続部分列に含むかどうか。z-algo で線形時間
 template <typename STRING>
-bool is_substring(STRING S, STRING T) {
-  int n = len(S), m = len(T);
-  S += T;
-  auto Z = zalgorithm(S);
-  FOR3(i, n, n + m) {
+bool is_substring(STRING& S, STRING& T) {
+  int n = int(S.size()), m = int(T.size());
+  STRING ST;
+  for (auto&& x: S) ST.push_back(x);
+  for (auto&& x: T) ST.push_back(x);
+  auto Z = zalgorithm(ST);
+  for (int i = n; i < n + m; ++i) {
     if (Z[i] >= n) return true;
   }
   return false;
