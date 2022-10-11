e6015b7afad58f31f13c03ccf3013f5fcf508b80
maspypy <maspypy.gmail.com>
2022-10-12 08:05:03 +0900

    performance

--- linalg/mat_inv.hpp
+++ linalg/mat_inv.hpp
@@ -1,24 +1,28 @@
+// (det, invA) をかえす
 template <typename T>
-vc<vc<T>> mat_inv(vc<vc<T>> A) {
+pair<T, vc<vc<T>>> mat_inv(vc<vc<T>> A) {
+  T det = 1;
   int N = len(A);
   vv(T, B, N, N);
   FOR(n, N) B[n][n] = 1;
   FOR(i, N) {
-    FOR3(k, i, N) if (A[k][i] != 0) {
-      if (k != i) swap(A[i], A[k]), swap(B[i], B[k]);
+    FOR(k, i, N) if (A[k][i] != 0) {
+      if (k != i) {
+        swap(A[i], A[k]), swap(B[i], B[k]);
+        det = -det;
+      }
       break;
     }
-    if (A[i][i] == 0) return {};
+    if (A[i][i] == 0) return {T(0), {}};
     T c = T(1) / A[i][i];
-    FOR(j, N) {
-      A[i][j] *= c;
-      B[i][j] *= c;
-    }
+    det *= A[i][i];
+    FOR(j, i, N) A[i][j] *= c;
+    FOR(j, N) B[i][j] *= c;
     FOR(k, N) if (i != k) {
       T c = A[k][i];
-      FOR(j, N) A[k][j] -= A[i][j] * c;
+      FOR(j, i, N) A[k][j] -= A[i][j] * c;
       FOR(j, N) B[k][j] -= B[i][j] * c;
     }
   }
-  return B;
+  return {det, B};
 }
