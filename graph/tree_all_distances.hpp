826c7955596d91d010fe57f92e8e5600454ac854
maspypy <maspypy.gmail.com>
2022-10-13 22:34:59 +0900

    fix

--- graph/tree_all_distances.hpp
+++ graph/tree_all_distances.hpp
@@ -1,11 +1,11 @@
 #include "graph/centroid.hpp"
 #include "poly/convolution.hpp"
 
-template<typename Graph>
-vi tree_all_distances(Graph& G){
-  // frequency table of distance of all directed pairs. 
+template <typename Graph>
+vi tree_all_distances(Graph& G) {
+  // frequency table of distance of all directed pairs.
   // sum of result array = N^2
-  
+
   assert(G.is_prepared());
   assert(!G.is_directed());
 
@@ -14,7 +14,7 @@ vi tree_all_distances(Graph& G){
   ll N = G.N;
   vi ANS(N);
   FOR(root, N) {
-    auto data = CD.collect(root, 0);
+    auto data = CD.collect_dist(root);
     FOR(i, len(data)) {
       int n = 0;
       FOR(j, len(data[i])) chmax(n, data[i][j].se + 1);
