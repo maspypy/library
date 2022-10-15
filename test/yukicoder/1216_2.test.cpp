fe8c06f849e92c23dd84c31fd99eb26daaad9f4b
maspypy <maspypy.gmail.com>
2022-10-15 19:42:56 +0900

    test

---
+++ test/yukicoder/1216_2.test.cpp
@@ -0,0 +1,76 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1216"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/tree.hpp"
+#include "graph/statictreemonoid.hpp"
+// #include "graph/treemonoid.hpp"
+#include "ds/fenwick2d.hpp"
+
+void solve() {
+  LL(N, Q);
+  Graph<ll, 0> G(N);
+  G.read_tree(1);
+  TREE<decltype(G)> tree(G);
+  vi dat(N - 1);
+  FOR(i, N - 1) dat[i] = G.edges[i].cost;
+  // TreeMonoid<decltype(tree), Group_Add<ll>, 1> TM(tree, dat);
+  StaticTreeMonoid<decltype(tree), Group_Add<ll>, 1> TM(tree, dat);
+
+  auto& dist = tree.depth_weighted;
+  /*
+  ・頂点 v に、根に着くのが時刻 t であるようなものを追加
+  ・（消す）はじめて消えて到達するのが w であるとき、w に -1 個追加
+  euler tour をとって
+  */
+  using T = tuple<int, int, ll>;
+  vc<T> query;
+  auto& LID = tree.LID;
+
+  vi X, Y;
+  iota(all(X), 0);
+  FOR(Q) {
+    LL(tp, v, t, l);
+    --v;
+    if (tp == 0) {
+      // 追加クエリ
+      // 消えないで到達できる最大の頂点
+      auto check = [&](auto e) -> bool { return e <= l; };
+      auto to = TM.max_path(check, v, 0);
+      int w = tree.parent[to];
+
+      query.eb(1, LID[v], t + dist[v]);
+      X.eb(LID[v]);
+      Y.eb(t + dist[v]);
+      if (w != -1) {
+        X.eb(LID[w]);
+        Y.eb(t + dist[v]);
+        query.eb(-1, LID[w], t + dist[v]);
+      }
+    }
+    if (tp == 1) { query.eb(0, v, t); }
+  }
+
+  Fenwick2D<Group_Add<int>, ll, true> bit(X, Y);
+
+  for (auto&& [tp, x, t]: query) {
+    if (tp == 0) {
+      int v = x;
+      int l = tree.LID[v], r = tree.RID[v];
+      t += dist[v];
+      ll ANS = bit.sum(l, 0, r, t + 1);
+      print(ANS);
+    }
+    if (tp == 1) { bit.add(x, t, 1); }
+    if (tp == -1) { bit.add(x, t, -1); }
+  }
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
---
+++ test/yukicoder/1874.test.cpp
@@ -0,0 +1,79 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1874"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/fenwick.hpp"
+
+struct Grp {
+  using value_type = tuple<ll, ll, ll, ll>;
+  using X = value_type;
+  static X op(const X& x, const X& y) {
+    auto& [ax, bx, cx, dx] = x;
+    auto& [ay, by, cy, dy] = y;
+    return {ax + ay, bx + by, cx + cy, dx + dy};
+  }
+  static X inverse(const X& x) {
+    auto& [ax, bx, cx, dx] = x;
+    return {-ax, -bx, -cx, -dx};
+  }
+  // static X power(const X &x, ll n) { return n * x; }
+  static constexpr X unit() { return {0, 0, 0, 0}; }
+  static constexpr bool commute = 1;
+};
+
+void solve() {
+  LL(N);
+  ll LIM = 1'000'000;
+  vvc<int> Y_to_X(LIM + 1);
+  using T = typename Grp::value_type;
+  vc<T> bit_raw(LIM + 1);
+  vc<int> X;
+
+  FOR(N) {
+    LL(x, y);
+    bit_raw[x] = Grp::op(bit_raw[x], {1, x, y, x * y});
+    Y_to_X[y].eb(x);
+    X.eb(x);
+  }
+  UNIQUE(X);
+
+  FenwickTree<Grp> bit(bit_raw);
+
+  ll ANS = 1LL << 60;
+  unordered_map<int, ll> MP;
+  FOR(y, LIM + 1) {
+    if (len(Y_to_X[y]) == 0) continue;
+    for (auto&& x: Y_to_X[y]) { bit.add(x, {-2, -2 * x, -2 * y, -2 * x * y}); }
+    MP.clear();
+
+    auto f = [&](ll x) -> ll {
+      if (MP.count(x)) return MP[x];
+      auto [a, b, c, d] = bit.sum(x);
+      auto [a1, b1, c1, d1] = bit.sum_all();
+      a = a1 - 2 * a;
+      b = b1 - 2 * b;
+      c = c1 - 2 * c;
+      d = d1 - 2 * d;
+      ll res = a * x * y - b * y - c * x + d;
+      chmin(ANS, res);
+      return (MP[x] = res);
+    };
+
+    auto check = [&](int k) -> bool {
+      if (k == 0) return true;
+      return f(X[k - 1]) >= f(X[k]);
+    };
+    int k = binary_search(check, 0, len(X));
+    f(k);
+  }
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
