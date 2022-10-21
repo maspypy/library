1b70c66153b9832a1b62ebfe57438c24489ac985
maspypy <maspypy.gmail.com>
2022-10-21 19:08:39 +0900

    add test

---
+++ test/yukicoder/1838.test.cpp
@@ -0,0 +1,43 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1838"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/fenwick.hpp"
+#include "seq/inversion_rotate.hpp"
+
+void solve() {
+  LL(M, K);
+  ll N = M * K;
+  VEC(ll, A, N);
+  vvc<int> pos(M);
+  FOR(i, N) pos[A[i]].eb(i);
+  ll base = 0;
+  FenwickTree<Group_Add<int>> bit(N);
+  FOR(j, K) {
+    FOR(i, M) {
+      int x = pos[i][j];
+      base += bit.sum(x, N);
+    }
+    FOR(i, M) {
+      int x = pos[i][j];
+      bit.add(x, 1);
+    }
+  }
+  vi ANS(M, base);
+  FOR(j, K) {
+    vc<int> P(M);
+    FOR(i, M) P[i] = pos[i][j];
+    auto tmp = inversion_rotate(P, false);
+    FOR(i, M) ANS[i] += tmp[i];
+  }
+  print(MIN(ANS));
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
