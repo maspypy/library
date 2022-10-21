99a2084fbe36ab583b0ddd1c6126e9195a991fe6
maspypy <maspypy.gmail.com>
2022-10-21 18:52:33 +0900

    add inv rot

--- seq/inversion.hpp
+++ seq/inversion.hpp
@@ -1,14 +1,17 @@
 #include "ds/fenwick.hpp"
 
 template <typename T>
-ll inversion(vc<T>& A) {
-  auto key = A;
-  UNIQUE(key);
+ll inversion(vc<T>& A, bool SMALL = false) {
+  if (!SMALL) {
+    auto key = A;
+    UNIQUE(key);
+    for (auto&& x: A) x = LB(A, x);
+  }
   ll ANS = 0;
-  FenwickTree<Group_Add<int>> bit(len(key));
-  for (auto&& a: A) {
-    ll x = LB(key, a);
-    ANS += bit.prod_all() - bit.prod(x + 1);
+  ll K = MAX(A) + 1;
+  FenwickTree<Group_Add<int>> bit(K);
+  for (auto&& x: A) {
+    ANS += bit.sum(x + 1, K);
     bit.add(x, 1);
   }
   return ANS;
