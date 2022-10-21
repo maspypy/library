85d20fa2f7dd4dc2b6484876db025aedfbef0a02
maspypy <maspypy.gmail.com>
2022-10-21 13:43:47 +0900

    test

---
+++ test/yukicoder/1323.test.cpp
@@ -0,0 +1,81 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1323"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/grid_bfs.hpp"
+
+void solve() {
+  LL(H, W, sx, sy, gx, gy);
+  VEC(string, G, H);
+  --sx, --sy, --gx, --gy;
+  ll dx[] = {1, 0, -1, 0};
+  ll dy[] = {0, 1, 0, -1};
+  const int INF = 1LL << 30;
+  auto ng = [&]() -> void { print(-1); };
+
+  auto dist_1 = grid_bfs<string>(G, sx, sy, "#", INF, 4);
+  auto dist_2 = grid_bfs<string>(G, gx, gy, "#", INF, 4);
+
+  ll D = dist_1[gx][gy];
+  if (D == INF) return ng();
+
+  auto in_path = [&](int x, int y) -> bool {
+    ll d1 = dist_1[x][y], d2 = dist_2[x][y];
+    return d1 < INF && d2 < INF && d1 + d2 == D;
+  };
+
+  // unique path?
+  int cnt = 0;
+  FOR(x, H) FOR(y, W) if (in_path(x, y))++ cnt;
+  if (cnt > D + 1) return print(D + D);
+
+  ll ANS = INF;
+
+  // deg >= 3 の点を利用する場合
+  FOR(x, H) FOR(y, W) if (G[x][y] != '#') {
+    int deg = 0;
+    FOR(d, 4) if (G[x + dx[d]][y + dy[d]] != '#') deg++;
+    if (deg >= 3) {
+      bool mid = in_path(x, y);
+      if (x == sx && y == sy) mid = 0;
+      if (x == gx && y == gy) mid = 0;
+      if (mid) { chmin(ANS, 2 * D + 2); }
+      elif (dist_1[x][y] < INF) {
+        chmin(ANS, 2 * (dist_1[x][y] + dist_2[x][y]) + 4);
+      }
+    }
+  }
+
+  // two edge disjoint path
+  FOR(x, H) FOR(y, W) {
+    if (in_path(x, y)) G[x][y] = '#';
+  }
+  G[sx][sy] = G[gx][gy] = '.';
+
+  vv(int, dist, H, W, INF);
+  deque<pi> que;
+  dist[sx][sy] = 0;
+  que.eb(sx, sy);
+  while (len(que)) {
+    auto [x, y] = pick(que);
+    FOR(d, 4) {
+      int nx = x + dx[d], ny = y + dy[d];
+      if (G[nx][ny] == '#') continue;
+      if (x == sx && y == sy && nx == gx && ny == gy) continue;
+      if (chmin(dist[nx][ny], dist[x][y] + 1)) { que.eb(nx, ny); }
+    }
+  }
+  chmin(ANS, D + dist[gx][gy]);
+
+  if (ANS == INF) return ng();
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
---
+++ test/yukicoder/590.test.cpp
@@ -0,0 +1,93 @@
+#define PROBLEM "https://yukicoder.me/problems/no/590"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/functional.hpp"
+#include "nt/crt.hpp"
+#include "mod/modint.hpp"
+
+using mint = modint107;
+
+void solve() {
+  LL(N);
+  auto get = [&]() -> pair<vvc<int>, vc<pair<int, int>>> {
+    // サイクル
+    // どのサイクル、何番目
+    vvc<int> C;
+    vc<pair<int, int>> pos(N, {-1, -1});
+    VEC(ll, TO, N);
+    for (auto&& x: TO) --x;
+    FOR(r, N) {
+      if (pos[r].fi != -1) continue;
+      vc<int> cyc = {r};
+      while (1) {
+        ll nxt = TO[cyc.back()];
+        if (nxt == r) break;
+        cyc.eb(nxt);
+      }
+      FOR(j, len(cyc)) pos[cyc[j]] = {len(C), j};
+      C.eb(cyc);
+    }
+    return {C, pos};
+  };
+
+  auto [CA, posA] = get();
+  auto [CB, posB] = get();
+
+  /*
+  ・サイクル番号1、サイクル番号2、mod gcd(len) 1, 2
+  */
+  using T = tuple<int, int, int>;
+  map<T, vc<int>> MP;
+  FOR(v, N) {
+    auto [i1, j1] = posA[v];
+    auto [i2, j2] = posB[v];
+    ll n1 = len(CA[i1]);
+    ll n2 = len(CB[i2]);
+    ll g = gcd(n1, n2);
+    ll x = (j1 - j2) % g;
+    if (x < 0) x += g;
+    T t = {i1, i2, x};
+    MP[t].eb(v);
+  }
+
+  mint ANS = 0;
+
+  for (auto&& [key, I]: MP) {
+    auto r = I[0];
+    // t=0 で (r,r) にいるとする。(v,v) にいる時刻
+    vi X;
+    auto [i1, i2, ___] = key;
+    ll n1 = len(CA[i1]);
+    ll n2 = len(CB[i2]);
+    ll s1 = posA[r].se;
+    ll s2 = posB[r].se;
+    for (auto&& v: I) {
+      ll t1 = posA[v].se;
+      ll t2 = posB[v].se;
+      t1 -= s1;
+      t2 -= s2;
+      if (t1 < 0) t1 += n1;
+      if (t2 < 0) t2 += n2;
+      ll x = CRT({t1, t2}, {n1, n2});
+      X.eb(x);
+    }
+    X.eb(n1 / gcd(n1, n2) * n2);
+    sort(all(X));
+    FOR(k, len(X) - 1) {
+      mint dx = X[k + 1] - X[k];
+      ANS += (dx) * (dx - 1);
+    }
+  }
+  ANS /= mint(2);
+  print(ANS);
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
