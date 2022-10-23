eccbfdb636471b4749b5100bcc4e9075b7d35f5b
maspypy <maspypy.gmail.com>
2022-10-23 11:21:57 +0900

    RNG_64

--- random/base.hpp
+++ random/base.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
-ll RNG(ll a, ll b) {
-  static mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
-  uniform_int_distribution<ll> dist(a, b - 1);
-  return dist(mt);
+u64 RNG_64() {
+  static uint64_t x_
+      = uint64_t(chrono::duration_cast<chrono::nanoseconds>(
+                     chrono::high_resolution_clock::now().time_since_epoch())
+                     .count())
+        * 10150724397891781847ULL;
+  x_ ^= x_ << 7;
+  return x_ ^= x_ >> 9;
 }
 
-ll RNG(ll a) { return RNG(0, a); }
+u64 RNG(u64 lim) { return RNG_64() % lim; }
+
+ll RNG(ll l, ll r) { return l + RNG_64() % (r - l); }
