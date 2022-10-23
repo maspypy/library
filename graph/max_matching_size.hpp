8600a734c420dbded805da9b7f00541ebaca4834
maspypy <maspypy.gmail.com>
2022-10-23 11:37:28 +0900

    add tutte

---
+++ graph/max_matching_size.hpp
@@ -0,0 +1,18 @@
+#include "random/base.hpp"
+#include "mod/modint61.hpp"
+#include "linalg/matrix_rank.hpp"
+
+template <typename GT>
+int max_matching_size(GT& G) {
+  using mint = modint61;
+  assert(!G.is_directed());
+  int N = G.N;
+  vv(mint, tutte, N, N);
+  for (auto&& e: G.edges) {
+    mint x = RNG(mint::get_mod());
+    int i = e.frm, j = e.to;
+    tutte[i][j] += x;
+    tutte[j][i] -= x;
+  }
+  return matrix_rank(tutte) / 2;
+}
