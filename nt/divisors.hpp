ff88781df2b403bf071913228d530f96c2cff498
maspypy <maspypy.gmail.com>
2022-10-24 10:54:14 +0900

    add options

--- nt/divisors.hpp
+++ nt/divisors.hpp
@@ -1,8 +1,6 @@
 #include "nt/factor.hpp"
 
-// 約数をソートせずにかえす
-vc<ll> divisors(ll N) {
-  auto pf = factor(N);
+vc<ll> divisors_by_pf(const vc<pair<ll, int>>& pf, bool SORT) {
   vi div = {1};
   for (auto&& [p, e]: pf) {
     ll n = len(div);
@@ -12,5 +10,16 @@ vc<ll> divisors(ll N) {
       FOR(j, n) div.eb(div[j] * pp);
     }
   }
+  if (SORT) sort(all(div));
   return div;
+}
+
+vc<ll> divisors(ll N, bool SORT) {
+  auto pf = factor(N);
+  return divisors_by_pf(pf, SORT);
+}
+
+vc<ll> divisors_by_lpf(ll N, vc<int>& lpf, bool SORT) {
+  auto pf = factor_by_lpf(N, lpf);
+  return divisors_by_pf(pf, SORT);
 }
