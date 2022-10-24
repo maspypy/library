4d32f71e86989fb45b8cc59e5fc9c8b0930ae9c9
maspypy <maspypy.gmail.com>
2022-10-24 17:16:34 +0900

    hash vector

---
+++ random/hash_vector.hpp
@@ -0,0 +1,14 @@
+#include "random/base.hpp"
+#include "mod/modint61.hpp"
+
+template <typename T>
+ll hash_vector(vc<T>& X) {
+  using mint = modint61;
+  static vc<mint> hash_base;
+  int n = len(X);
+  while (len(hash_base) <= n) { hash_base.eb(RNG(mint::get_mod())); }
+  mint H = 0;
+  FOR(i, n) H += hash_base[i] * mint(X[i]);
+  H += hash_base[n];
+  return H.val;
+}
