23fc798d65019ac95e0bcb0c9d2a2032fd91b137
maspypy <maspypy.gmail.com>
2022-10-25 12:10:43 +0900

    template

--- alg/monoid/cntminmincnt.hpp
+++ alg/monoid/cntminmincnt.hpp
@@ -1,8 +1,11 @@
 #pragma once
+
 // 全体の個数、最小値、最小値の個数
+template <typename E>
 struct Monoid_CntMinMincnt {
-  using value_type = tuple<ll, ll, ll>;
+  using value_type = tuple<E, E, E>;
   using X = value_type;
+  static X from_element(E x) { return {1, x, 1}; }
   static X op(X x, X y) {
     auto [xcnt, xmin, xmincnt] = x;
     auto [ycnt, ymin, ymincnt] = y;
@@ -10,6 +13,6 @@ struct Monoid_CntMinMincnt {
     if (xmin == ymin) return {xcnt + ycnt, xmin, xmincnt + ymincnt};
     return {xcnt + ycnt, xmin, xmincnt};
   }
-  static constexpr X unit() { return {0, numeric_limits<ll>::max(), 0}; }
+  static constexpr X unit() { return {0, numeric_limits<E>::max(), 0}; }
   static constexpr bool commute = true;
 };
