4d7656485df65ba0ae40a422f190f1cd88a221f1
maspypy <maspypy.gmail.com>
2022-10-25 12:10:30 +0900

    add test

--- ds/query/rectangleunion.hpp
+++ ds/query/rectangleunion.hpp
@@ -1,21 +1,23 @@
 #include "ds/lazysegtree.hpp"
 #include "alg/lazy/cntminmincnt_add.hpp"
 
+template <typename XY = int>
 struct RectangleUnion {
-  using RECT = tuple<ll, ll, ll, ll>;
+  using RECT = tuple<XY, XY, XY, XY>;
   vc<RECT> rectangles;
-  vi X, Y;
+  vc<XY> X, Y;
 
-  void add_rect(ll xl, ll yl, ll xr, ll yr) {
+  void add_rect(int xl, int yl, int xr, int yr) {
     assert(xl < xr && yl < yr);
     X.eb(xl), X.eb(xr), Y.eb(yl), Y.eb(yr);
     rectangles.eb(xl, xr, yl, yr);
   }
 
-  ll calc() {
+  template <typename ANS_TYPE = ll>
+  ANS_TYPE calc() {
     UNIQUE(X), UNIQUE(Y);
-    ll N = len(X);
-    vc<vc<pi>> add(N), rm(N);
+    int N = len(X);
+    vc<vc<pair<int, int>>> add(N), rm(N);
     for (auto &&[xl, xr, yl, yr]: rectangles) {
       xl = LB(X, xl), xr = LB(X, xr);
       yl = LB(Y, yl), yr = LB(Y, yr);
@@ -23,18 +25,18 @@ struct RectangleUnion {
       rm[xr].eb(yl, yr);
     }
 
-    using Lazy = Lazy_CntMinMincnt_Add;
-
-    vc<typename Lazy::X> seg_raw(len(Y) - 1);
-    FOR(i, len(Y) - 1) seg_raw[i] = {Y[i + 1] - Y[i], 0, Y[i + 1] - Y[i]};
-    LazySegTree<Lazy> seg(seg_raw);
-    ll ANS = 0;
+    using Lazy = Lazy_CntMinMincnt_Add<XY>;
+    using T = typename Lazy::X_structure::value_type;
+    LazySegTree<Lazy> seg(len(Y) - 1, [&](int i) -> T {
+      return {Y[i + 1] - Y[i], 0, Y[i + 1] - Y[i]};
+    });
+    ANS_TYPE ANS = 0;
     FOR(i, len(X) - 1) {
-      ll dx = X[i + 1] - X[i];
+      ANS_TYPE dx = X[i + 1] - X[i];
       for (auto &&[yl, yr]: add[i]) seg.apply(yl, yr, 1);
       for (auto &&[yl, yr]: rm[i]) seg.apply(yl, yr, -1);
       auto [cnt, min, mincnt] = seg.prod_all();
-      ll n = cnt;
+      ANS_TYPE n = cnt;
       if (min == 0) n -= mincnt;
       ANS += n * dx;
     }
---
+++ test/library_checker/datastructure/area_of_union_of_rectangles.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/area_of_union_of_rectangles"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "ds/query/rectangleunion.hpp"
+
+void solve() {
+  LL(N);
+  RectangleUnion<int> X;
+  FOR(N) {
+    LL(a, b, c, d);
+    X.add_rect(a, b, c, d);
+  }
+  print(X.calc<ll>());
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  solve();
+
+  return 0;
+}
