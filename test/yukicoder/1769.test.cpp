29e8c341bfaf43af5eddb7d94576ffbb5ea11839
maspypy <maspypy.gmail.com>
2022-10-26 11:18:18 +0900

    add test

---
+++ test/yukicoder/1769.test.cpp
@@ -0,0 +1,88 @@
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "graph/base.hpp"
+#include "graph/centroid.hpp"
+#include "graph/tree.hpp"
+#include "ds/hashmap.hpp"
+
+void solve() {
+  LL(N);
+  Graph<int, 0> G(N);
+  G.read_tree(1);
+
+  vi ANS(N);
+  CentroidDecomposition<decltype(G)> CD(G);
+  HashMapLL<vc<int>> MP;
+
+  FOR(root, N) {
+    auto [conv, G] = CD.get_subgraph(root);
+    const int N = G.N;
+    // lca が 0 であるような path について解く
+    vc<int> A(N);
+    FOR(v, N) {
+      for (auto&& e: G[v]) A[e.to] = A[v] ^ e.cost;
+    }
+    TREE<decltype(G)> tree(G, 0, false);
+
+    // 根以外の点に対して、dep=1 の LA
+    vc<int> par_1(N, -1);
+    FOR(v, 1, N) {
+      int p = tree.parent[v];
+      par_1[v] = (p == 0 ? v : par_1[p]);
+    }
+
+    // 色 -> 頂点集合
+    MP.reset();
+    FOR(v, 1, N) if (A[v] != A[0]) MP[A[v]].eb(v);
+
+    // 根の次の点 -> 頂点の個数
+    vc<int> CNT(N);
+    FOR(v, N) if (par_1[v] == v) CNT[v] = tree.subtree_size(v);
+
+    for (auto&& [key, V]: MP.items()) {
+      // 根に到達できない点は V から除く
+      vc<int> W;
+      for (auto&& x: V) {
+        if (len(W) && tree.in_subtree(x, W.back())) { continue; }
+        W.eb(x);
+      }
+      V = W;
+      // 到達可能な点の個数を更新
+      ll s = N;
+      for (auto&& v: V) {
+        int x = tree.subtree_size(v);
+        CNT[par_1[v]] -= x;
+        s -= x;
+      }
+      // 各 v からの答を計算
+      for (auto&& v: V) { ANS[conv[v]] += s - CNT[par_1[v]]; }
+      for (auto&& v: V) {
+        int x = tree.subtree_size(v);
+        CNT[par_1[v]] += x;
+      }
+    }
+    // 根から進む場合
+    vc<int> OK(N);
+    OK[0] = 1;
+    FOR(v, N) {
+      if (!OK[v]) continue;
+      for (auto&& e: G[v]) {
+        if (A[e.to] == A[0]) continue;
+        OK[e.to] = 1;
+      }
+    }
+    ANS[conv[0]] += SUM<int>(OK) - 1;
+  }
+  print(SUM<ll>(ANS));
+}
+
+signed main() {
+  cout << fixed << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(T) solve();
+
+  return 0;
+}
