a42e1f021fc3c7c1735f8d2ee38e69eacfb55f7e
maspypy <maspypy.gmail.com>
2022-10-28 17:47:13 +0900

    add,chmax,chmin

--- alg/monoid/max_xplusa_b.hpp
+++ alg/monoid/max_xplusa_b.hpp
@@ -3,10 +3,14 @@
 // max(x+a, b) の合成
 template <typename T>
 struct Monoid_Max_xplusa_b {
-  using value_type = pi;
+  using value_type = pair<T, T>;
   using X = value_type;
   static const T LIM = numeric_limits<T>::lowest();
-  static T add(T x, T y) {
+
+  static X add(T x) { return {x, LIM}; }
+  static X chmax(T x) { return {0, x}; }
+
+  static T op_add(T x, T y) {
     if (x == LIM) return LIM;
     if (y == LIM) return LIM;
     return x + y;
@@ -19,7 +23,7 @@ struct Monoid_Max_xplusa_b {
   static X op(X A, X B) {
     auto [a, b] = A;
     auto [c, d] = B;
-    return {add(a, c), max(add(b, c), d)};
+    return {op_add(a, c), max(op_add(b, c), d)};
   }
   static constexpr X unit() { return {0, LIM}; }
   static constexpr bool commute = false;
