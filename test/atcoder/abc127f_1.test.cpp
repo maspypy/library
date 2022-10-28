723d9d5943441931a4749d85ae8fb04a8e67536c
maspypy <maspypy.gmail.com>
2022-10-29 02:49:05 +0900

    add test

---
+++ test/atcoder/abc127f_1.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://atcoder.jp/contests/abc127/tasks/abc127_f"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/waveletmatrix.hpp"
+
+void solve() {
+  LL(Q);
+  vi A, B, query;
+  FOR(Q) {
+    LL(t);
+    if (t == 1) {
+      LL(a, b);
+      A.eb(a);
+      B.eb(b);
+    }
+    if (t == 2) { query.eb(len(A)); }
+  }
+  WaveletMatrix<ll, 1> X(A);
+  auto Ac = cumsum<ll>(A);
+  auto Bc = cumsum<ll>(B);
+
+  const ll INF = 1LL << 60;
+
+  for (auto&& n: query) {
+    ll x = X.kth(0, n, (n - 1) / 2);
+    ll cnt_lo = X.freq(0, n, -INF, x);
+    ll sum_lo = X.sum(0, n, cnt_lo);
+    ll cnt_hi = n - cnt_lo;
+    ll sum_hi = Ac[n] - sum_lo;
+    ll ANS = Bc[n];
+    ANS += (cnt_lo * x - sum_lo);
+    ANS += (sum_hi - cnt_hi * x);
+    print(x, ANS);
+  }
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
---
+++ test/atcoder/abc217h.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://atcoder.jp/contests/abc217/tasks/abc217_h"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/slope.hpp"
+
+void solve() {
+  LL(N);
+  Slope_Trick_1<ll> f(vi(N, 0), vi(N, 0));
+
+  ll pt = 0;
+  FOR(N) {
+    LL(t, d, x);
+    ll dt = t - pt;
+    pt = t;
+    f.sliding_window_minimum(-dt, dt);
+    if (d == 0) f.add_a_minus_x(x);
+    if (d == 1) f.add_x_minus_a(x);
+  }
+  auto [xl, xr, min_f] = f.get_min();
+  print(min_f);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ll T = 1;
+  // LL(T);
+  FOR(_, T) solve();
+
+  return 0;
+}
