c517a6cba4a4152d9d012fb399284a909b1af2cf
maspypy <maspypy.gmail.com>
2022-10-28 09:10:40 +0900

    improve performance

--- ds/query/point_add_rectangle_sum.hpp
+++ ds/query/point_add_rectangle_sum.hpp
@@ -1,84 +1,72 @@
-#pragma once
 #include "ds/fenwick.hpp"
 
-
-template <typename AbelGroup, bool SMALL = false>
+template <typename AbelGroup, typename XY, bool SMALL_X = false>
 struct Point_Add_Rectangle_Sum {
   using G = typename AbelGroup::value_type;
-  int Q;
-  vector<ll> X, Y;
-  vector<G> WT;
-  vector<tuple<ll, ll, ll, ll>> rect;
+  using Point = tuple<XY, XY, G>;
+  vector<Point> point;
+  vector<tuple<XY, XY, XY, XY>> rect;
 
   Point_Add_Rectangle_Sum() {}
 
-  void add_query(ll x, ll y, G w) {
-    X.emplace_back(x), Y.emplace_back(y), WT.emplace_back(w);
-  }
-
-  void sum_query(ll xl, ll yl, ll xr, ll yr) {
-    rect.emplace_back(xl, yl, xr, yr);
-  }
+  void add_query(XY x, XY y, G w) { point.eb(x, y, w); }
+  void sum_query(XY xl, XY yl, XY xr, XY yr) { rect.eb(xl, yl, xr, yr); }
 
   vector<G> calc() {
-    int N = X.size(), Q = rect.size();
-    if (N == 0) return vector<G>(Q, AbelGroup::unit());
-    int NX = 0, NY = 0;
-    if (!SMALL) {
-      vector<ll> keyX = X, keyY = Y;
-      sort(all(keyX));
-      keyX.erase(unique(all(keyX)), keyX.end());
-      sort(all(keyY));
-      keyY.erase(unique(all(keyY)), keyY.end());
-      NX = keyX.size(), NY = keyY.size();
-      for (auto &&x: X) x = lower_bound(all(keyX), x) - keyX.begin();
-      for (auto &&y: Y) y = lower_bound(all(keyY), y) - keyY.begin();
-      for (auto &&[xl, yl, xr, yr]: rect) {
-        xl = lower_bound(all(keyX), xl) - keyX.begin();
-        xr = lower_bound(all(keyX), xr) - keyX.begin();
-        yl = lower_bound(all(keyY), yl) - keyY.begin();
-        yr = lower_bound(all(keyY), yr) - keyY.begin();
+    int N = point.size(), Q = rect.size();
+    if (N == 0 || Q == 0) return vector<G>(Q, AbelGroup::unit());
+    // X 方向の座圧
+    int NX = 0;
+    if (!SMALL_X) {
+      sort(all(point),
+           [&](auto &x, auto &y) -> bool { return get<0>(x) < get<0>(y); });
+      vc<XY> keyX;
+      keyX.reserve(N);
+      for (auto &&[a, b, c]: point) {
+        if (len(keyX) == 0 || keyX.back() != a) { keyX.eb(a); }
+        a = len(keyX) - 1;
       }
+      keyX.shrink_to_fit();
+      for (auto &&[xl, yl, xr, yr]: rect) {
+        xl = LB(keyX, xl);
+        xr = LB(keyX, xr);
+      }
+      NX = len(keyX);
     }
-    if (SMALL) {
-      ll mx = *(min_element(X.begin(), X.end()));
-      ll my = *(min_element(Y.begin(), Y.end()));
-      for (auto &&x: X) x -= mx;
-      for (auto &&y: Y) y -= my;
-      NX = *(max_element(X.begin(), X.end())) + 1;
-      NY = *(max_element(Y.begin(), Y.end())) + 1;
+    if (SMALL_X) {
+      XY mx = numeric_limits<XY>::max();
+      for (auto &&[x, y, g]: point) chmin(mx, x);
+      for (auto &&[x, y, g]: point) x -= mx, chmax(NX, x + 1);
       for (auto &&[xl, yl, xr, yr]: rect) {
         xl -= mx, xr -= mx;
-        yl -= my, yr -= my;
         xl = max(0, min<int>(xl, NX));
         xr = max(0, min<int>(xr, NX));
-        yl = max(0, min<int>(yl, NY));
-        yr = max(0, min<int>(yr, NY));
       }
     }
-    vector<vector<pair<int, G>>> ADD(NY + 1);
-    vector<vector<tuple<int, int, int>>> CALC_1(NY + 1);
-    vector<vector<tuple<int, int, int>>> CALC_2(NY + 1);
-    for (int n = 0; n < N; ++n) { ADD[Y[n]].emplace_back(X[n], WT[n]); }
-    for (int q = 0; q < Q; ++q) {
+
+    vc<tuple<XY, int, int, int>> event(Q + Q);
+    FOR(q, Q) {
       auto &[xl, yl, xr, yr] = rect[q];
-      CALC_1[yr].emplace_back(xl, xr, q);
-      CALC_2[yl].emplace_back(xl, xr, q);
-    };
+      event[2 * q] = {yl, xl, xr, 2 * q};
+      event[2 * q + 1] = {yr, xl, xr, 2 * q + 1};
+    }
+    sort(all(point),
+         [&](auto &x, auto &y) -> bool { return get<1>(x) < get<1>(y); });
+    sort(all(event),
+         [&](auto &x, auto &y) -> bool { return get<0>(x) < get<0>(y); });
 
-    vector<G> res(Q, AbelGroup::unit());
     FenwickTree<AbelGroup> bit(NX);
-
-    for (int y = 0; y <= NY; ++y) {
-      for (auto &&[xl, xr, q]: CALC_1[y]) {
-        auto x = bit.prod(xl, xr);
-        res[q] = AbelGroup::op(res[q], x);
+    vc<G> res(Q, AbelGroup::unit());
+    int j = 0;
+    for (auto &&[y, xl, xr, qq]: event) {
+      while (j < N && get<1>(point[j]) < y) {
+        bit.add(get<0>(point[j]), get<2>(point[j]));
+        ++j;
       }
-      for (auto &&[xl, xr, q]: CALC_2[y]) {
-        auto x = AbelGroup::inverse(bit.prod(xl, xr));
-        res[q] = AbelGroup::op(res[q], x);
-      }
-      for (auto &&[x, g]: ADD[y]) { bit.add(x, g); }
+      G g = bit.sum(xl, xr);
+      int q = qq / 2;
+      if (qq % 2 == 0) g = AbelGroup::inverse(g);
+      res[q] = AbelGroup::op(res[q], g);
     }
     return res;
   }
--- ds/query/rectangle_add_rectangle_sum.hpp
+++ ds/query/rectangle_add_rectangle_sum.hpp
@@ -2,7 +2,7 @@
 
 #include "ds/query/point_add_rectangle_sum.hpp"
 
-template <typename AbelGroup, bool SMALL = false>
+template <typename AbelGroup, typename XY, bool SMALL_X = false>
 struct Rectangle_Add_Rectangle_Sum {
   using WT = typename AbelGroup::value_type;
   using WT4 = tuple<WT, WT, WT, WT>;
@@ -33,11 +33,11 @@ struct Rectangle_Add_Rectangle_Sum {
     static constexpr bool commute = true;
   };
 
-  vector<tuple<ll, ll, ll, ll>> query;
-  Point_Add_Rectangle_Sum<G, SMALL> X;
-  ll min_x = 0, min_y = 0;
+  vector<tuple<XY, XY, XY, XY>> query;
+  Point_Add_Rectangle_Sum<G, XY, SMALL_X> X;
+  XY min_x = 0, min_y = 0;
 
-  void add_query(ll xl, ll yl, ll xr, ll yr, WT w) {
+  void add_query(XY xl, XY yl, XY xr, XY yr, WT w) {
     assert(xl <= xr && yl <= yr);
     min_x = min(min_x, xl);
     min_y = min(min_y, yl);
@@ -45,22 +45,22 @@ struct Rectangle_Add_Rectangle_Sum {
     auto nw = AbelGroup::inverse(w);
     X.add_query(xl, yl,
                 {w, AbelGroup::power(w, -yl), AbelGroup::power(w, -xl),
-                 AbelGroup::power(w, +xl * yl)});
+                 AbelGroup::power(w, +ll(xl) * yl)});
     // (xl,yr) に (x-xl)(y-yr) を減算
     X.add_query(xl, yr,
                 {nw, AbelGroup::power(w, +yr), AbelGroup::power(w, +xl),
-                 AbelGroup::power(w, -xl * yr)});
+                 AbelGroup::power(w, -ll(xl) * yr)});
     // (xr,yl) に (x-xr)(y-yl) を減算
     X.add_query(xr, yl,
                 {nw, AbelGroup::power(w, +yl), AbelGroup::power(w, +xr),
-                 AbelGroup::power(w, -xr * yl)});
+                 AbelGroup::power(w, -ll(xr) * yl)});
     // (xr,yr) に (x-xr)(y-yr) を加算
     X.add_query(xr, yr,
                 {w, AbelGroup::power(w, -yr), AbelGroup::power(w, -xr),
-                 AbelGroup::power(w, +xr * yr)});
+                 AbelGroup::power(w, +ll(xr) * yr)});
   }
 
-  void sum_query(ll xl, ll yl, ll xr, ll yr) {
+  void sum_query(XY xl, XY yl, XY xr, XY yr) {
     assert(xl <= xr && yl <= yr);
     query.emplace_back(xl, yl, xr, yr);
     X.sum_query(min_x, min_y, xl, yl);
@@ -70,7 +70,7 @@ struct Rectangle_Add_Rectangle_Sum {
   }
 
   vector<WT> calc() {
-    ll Q = query.size();
+    int Q = query.size();
     vector<WT> ANS(Q);
     auto tmp = X.calc();
 
@@ -79,28 +79,28 @@ struct Rectangle_Add_Rectangle_Sum {
       WT p = AbelGroup::unit(), m = AbelGroup::unit();
       {
         auto [a, b, c, d] = tmp[4 * q + 0];
-        p = AbelGroup::op(p, AbelGroup::power(a, xl * yl));
+        p = AbelGroup::op(p, AbelGroup::power(a, ll(xl) * yl));
         p = AbelGroup::op(p, AbelGroup::power(b, xl));
         p = AbelGroup::op(p, AbelGroup::power(c, yl));
         p = AbelGroup::op(p, d);
       }
       {
         auto [a, b, c, d] = tmp[4 * q + 1];
-        m = AbelGroup::op(m, AbelGroup::power(a, xl * yr));
+        m = AbelGroup::op(m, AbelGroup::power(a, ll(xl) * yr));
         m = AbelGroup::op(m, AbelGroup::power(b, xl));
         m = AbelGroup::op(m, AbelGroup::power(c, yr));
         m = AbelGroup::op(m, d);
       }
       {
         auto [a, b, c, d] = tmp[4 * q + 2];
-        m = AbelGroup::op(m, AbelGroup::power(a, xr * yl));
+        m = AbelGroup::op(m, AbelGroup::power(a, ll(xr) * yl));
         m = AbelGroup::op(m, AbelGroup::power(b, xr));
         m = AbelGroup::op(m, AbelGroup::power(c, yl));
         m = AbelGroup::op(m, d);
       }
       {
         auto [a, b, c, d] = tmp[4 * q + 3];
-        p = AbelGroup::op(p, AbelGroup::power(a, xr * yr));
+        p = AbelGroup::op(p, AbelGroup::power(a, ll(xr) * yr));
         p = AbelGroup::op(p, AbelGroup::power(b, xr));
         p = AbelGroup::op(p, AbelGroup::power(c, yr));
         p = AbelGroup::op(p, d);
