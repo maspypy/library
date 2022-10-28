6b39e95a7485f4b7cdd23f2e0c9fe52f60d81eae
maspypy <maspypy.gmail.com>
2022-10-29 03:23:02 +0900

    empty

---
+++ flow/min_cost_matching_on_line.hpp
@@ -0,0 +1,16 @@
+#include "ds/slope.hpp"
+
+// 座標 0, ..., N-1 に A_i 個ある
+// 座標 0, ..., N-1 で B_i 個まで受け入れられる
+ll min_cost_matching_on_line_1(vi A, vi B) {
+  assert(SUM<ll>(A) <= SUM<ll>(B));
+  const int N = len(A);
+  Slope_Trick_1<ll> f(vi(N + N + 1, 0), vi());
+  FOR(i, N) {
+    ll c = A[i] - B[i];
+    f.shift(c);
+    f.clear_inc();
+    f.add_abs(0);
+  }
+  return f.eval(0);
+}
