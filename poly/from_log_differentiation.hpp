91a5a0c7ccfd404d4a1c608f82faf20a26cdffaa
maspypy <maspypy.gmail.com>
2022-11-06 19:35:50 +0900

    upd

--- poly/from_log_differentiation.hpp
+++ poly/from_log_differentiation.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "seq/kth_term_of_p_recursive.hpp"
+
 // 対数微分 F'/F = a(x)/b(x) から F を復元する。
 // a, b が sparse であれば、O(N(K1+K2)) 時間でできる
 template <typename mint>
@@ -28,4 +30,22 @@ vc<mint> from_log_differentiation(int N, const vc<mint>& a, const vc<mint>& b) {
     f[n + 1] = df[n] * inv<mint>(n + 1);
   }
   return f;
+}
+
+// F'/F = a/b の解の、[x^K]F を求める。右辺は低次の有理式。
+template <typename mint>
+mint from_log_differentiation_kth(int K, vc<mint>& a, vc<mint>& b) {
+  assert(b[0] == mint(1));
+  int r = max(len(a), len(b) - 1);
+  vvc<mint> c(r + 1);
+  FOR(i, r + 1) {
+    mint c0 = 0, c1 = 0;
+    if (i < len(b)) c0 += mint(r - i) * b[i];
+    if (i < len(b)) c1 += b[i];
+    if (0 <= i - 1 && i - 1 < len(b)) c0 -= a[i - 1];
+    c[i] = {c0, c1};
+  }
+  auto f = from_log_differentiation(r - 1, a, b);
+  mint ANS = kth_term_of_p_recursive(f, c, K);
+  return ANS;
 }
