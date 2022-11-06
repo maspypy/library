f48107f8387a4fa8576ea734d35f911292a31474
maspypy <maspypy.gmail.com>
2022-11-06 13:29:34 +0900

    bug fix

--- poly/convolution_huge.hpp
+++ poly/convolution_huge.hpp
@@ -1,8 +1,8 @@
 #include "poly/convolution.hpp"
-template<typename mint>
+template <typename mint>
 vc<mint> convolution_huge(const vc<mint>& A, const vc<mint>& B) {
   int N = len(A), M = len(B);
-  if (N + M - 1 <= (1 << 23)) return convolution(A, B);
+  if (N + M - 1 <= (1 << 22)) return convolution(A, B);
   ll L = 22;
   vv(mint, C, 4, 2 << L);
   vv(mint, D, 4, 2 << L);
@@ -16,7 +16,7 @@ vc<mint> convolution_huge(const vc<mint>& A, const vc<mint>& B) {
 
   FOR(i, 7) {
     vc<mint> E(2 << L);
-    FOR(c, 4) FOR(d, 4) if(c + d == i) {
+    FOR(c, 4) FOR(d, 4) if (c + d == i) {
       FOR(k, 2 << L) E[k] += C[c][k] * D[d][k];
     }
     ntt(E, true);
