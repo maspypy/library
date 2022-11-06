65f7ad2dea80256a3d45c0069d286785a9d2f077
maspypy <maspypy.gmail.com>
2022-11-06 19:18:12 +0900

    solve log_diff

--- poly/sparse_exp_of_div.hpp
+++ poly/sparse_exp_of_div.hpp
@@ -1,27 +1,22 @@
-#include "poly/sparse_FODE.hpp"
+#include "poly/from_log_differentiation.hpp"
 
 // exp(f/g) で f,g が sparse
 template <typename mint>
-vc<mint> sparse_exp_of_div(const vc<mint>& f, const vc<mint>& g) {
-  const int N = len(f);
+vc<mint> sparse_exp_of_div(int N, const vc<mint>& f, const vc<mint>& g) {
   assert(f[0] == mint(0));
   assert(g[0] == mint(1));
   using P = pair<int, mint>;
   vc<P> dat_f, dat_g;
   FOR(i, len(f)) if (f[i] != mint(0)) dat_f.eb(i, f[i]);
   FOR(i, len(g)) if (g[i] != mint(0)) dat_g.eb(i, g[i]);
-  vc<mint> a(N), b(N);
-  // a = g^2, b = fg'-f'g
-  for (auto&& [i, x]: dat_g) {
-    for (auto&& [j, y]: dat_g) {
-      if (i + j < N) { a[i + j] += x * y; }
-    }
-  }
+
+  vc<mint> a(len(f) + len(g) - 2), b(2 * len(g) - 1);
+  // a = f'g-fg', b = g^2
   for (auto&& [i, x]: dat_f) {
-    for (auto&& [j, y]: dat_g) {
-      if (i + j - 1 >= N) continue;
-      b[i + j - 1] += x * y * mint(j - i);
-    }
+    for (auto&& [j, y]: dat_g) { a[i + j - 1] += x * y * mint(i - j); }
   }
-  return sparse_FODE(a, b);
+  for (auto&& [i, x]: dat_g) {
+    for (auto&& [j, y]: dat_g) { b[i + j] += x * y; }
+  }
+  return from_log_differentiation(N, a, b);
 }
