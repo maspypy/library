7ab3ab4b29c5e7bbca5ffe7159e4ad337b98daa4
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-12 16:39:46 +0900

    Update abc272_h.test.cpp

--- test/atcoder/abc272_h.test.cpp
+++ test/atcoder/abc272_h.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://atcoder.jp/contests/abc272/tasks/abc272_h"
+#define PROBLEM "https://atcoder.jp/contests/abc272/tasks/abc272_Ex"
 #include "my_template.hpp"
 #include "other/io.hpp"
 #include "mod/modint.hpp"
