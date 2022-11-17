9a1e20b67d460cb4284dfdb2e2b2ba582d718962
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-18 07:52:21 +0900

    Update simple_perm.test.cpp

--- test/mytest/simple_perm.test.cpp
+++ test/mytest/simple_perm.test.cpp
@@ -7,11 +7,15 @@
 
 using mint = modint998;
 
+
 void test() {
   vc<mint> I = indecomposable_permutations<mint>(10);
-  assert(I == vc<mint>({0, 1, 1, 3, 13, 71, 461, 3447, 29093, 273343}));
-  vc<mint> S = indecomposable_permutations<mint>(10);
-  assert(S == vc<mint>({0, 0, 0, 0, 2, 6, 46, 338, 2926, 28146}));
+  print(I);
+  flush();
+  assert(I
+         == vc<mint>({0, 1, 1, 3, 13, 71, 461, 3447, 29093, 273343, 2829325}));
+  vc<mint> S = simple_permutations<mint>(10);
+  assert(S == vc<mint>({0, 0, 0, 0, 2, 6, 46, 338, 2926, 28146, 298526}));
 }
 
 void solve() {
