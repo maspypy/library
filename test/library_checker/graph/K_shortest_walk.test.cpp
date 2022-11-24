a189957e5de3ae05aab019a0e4f3419a8fe601f0
maspypy <maspypy.gmail.com>
2022-11-24 17:14:47 +0900

    k_shortest walk

---
+++ test/library_checker/graph/K_shortest_walk.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/k_shortest_walk"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/base.hpp"
+#include "graph/shortest_path/K_shortest_walk.hpp"
+
+void solve() {
+  INT(N, M, s, t, K);
+  Graph<int, 1> G1(N);
+  G1.read_graph(M, 1, 0);
+  const ll INF = 1LL << 60;
+  auto ANS = K_shortest_walk<ll>(G1, s, t, K, INF);
+  for (auto &&x: ANS) {
+    if (x == INF) x = -1;
+    print(x);
+  }
+}
+
+signed main() {
+  solve();
+  return 0;
+}
