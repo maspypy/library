e4a7ee85473134f2f99f6a403846787888928b2a
maspypy <maspypy.gmail.com>
2022-11-24 17:32:18 +0900

    fix

--- ds/query/mo.hpp
+++ ds/query/mo.hpp
@@ -6,23 +6,23 @@ struct Mo {
   void calc(const AL &add_left, const AR &add_right, const EL &erase_left,
             const ER &erase_right, const O &query) {
     int n = 1;
-    for (auto &&[l, r]: lr) chmax(n, r);
-    int q = (int)lr.size();
+    for (auto &&[l, r]: LR) chmax(n, r);
+    int q = (int)LR.size();
     int bs = n / min<int>(n, sqrt(q));
     vector<int> ord(q);
     iota(begin(ord), end(ord), 0);
     sort(begin(ord), end(ord), [&](int a, int b) {
-      int ablock = lr[a].first / bs, bblock = lr[b].first / bs;
+      int ablock = LR[a].first / bs, bblock = LR[b].first / bs;
       if (ablock != bblock) return ablock < bblock;
-      return (ablock & 1) ? lr[a].second > lr[b].second
-                          : lr[a].second < lr[b].second;
+      return (ablock & 1) ? LR[a].second > LR[b].second
+                          : LR[a].second < LR[b].second;
     });
     int l = 0, r = 0;
     for (auto idx: ord) {
-      while (l > lr[idx].first) add_left(--l);
-      while (r < lr[idx].second) add_right(r++);
-      while (l < lr[idx].first) erase_left(l++);
-      while (r > lr[idx].second) erase_right(--r);
+      while (l > LR[idx].first) add_left(--l);
+      while (r < LR[idx].second) add_right(r++);
+      while (l < LR[idx].first) erase_left(l++);
+      while (r > LR[idx].second) erase_right(--r);
       query(idx);
     }
   }
---
+++ test/library_checker/datastructure/static_range_inversions_mo2.test.cpp
@@ -0,0 +1,69 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "ds/fenwick.hpp"
+#include "ds/query/rollback_mo.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  vi key = A;
+  UNIQUE(key);
+
+  for (auto&& x: A) x = LB(key, x);
+  ll K = len(key);
+  FenwickTree<Group_Add<int>> bit(K);
+
+  Rollback_Mo mo;
+  vi ANS(Q);
+  FOR(Q) {
+    LL(L, R);
+    mo.add(L, R);
+  }
+
+  ll inv = 0;
+  ll tinv = 0;
+  ll t = 0;
+  vc<int> history;
+  auto add_l = [&](int i) -> void {
+    int x = A[i];
+    inv += bit.sum(x);
+    bit.add(x, +1);
+    history.eb(x);
+  };
+  auto add_r = [&](int i) -> void {
+    int x = A[i];
+    inv += bit.sum_all() - bit.sum(x + 1);
+    bit.add(x, +1);
+    history.eb(x);
+  };
+  auto rb = [&](int t) -> void {
+    while (len(history) > t) { bit.add(pick(history), -1); }
+  };
+  auto reset = [&]() -> void {
+    rb(0);
+    inv = 0;
+  };
+  auto save = [&]() -> void {
+    t = len(history);
+    tinv = inv;
+  };
+  auto rollback = [&]() -> void {
+    rb(t);
+    inv = tinv;
+  };
+  auto calc = [&](int i) -> void { ANS[i] = inv; };
+  mo.calc(add_l, add_r, reset, save, rollback, calc);
+  for (auto&& x: ANS) print(x);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
