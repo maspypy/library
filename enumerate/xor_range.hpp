8ee3f1a495a7ffaf781651ed5245f432ec5dcc07
maspypy <maspypy.gmail.com>
2022-11-27 13:10:58 +0900

    change to enum

---
+++ enumerate/xor_range.hpp
@@ -0,0 +1,17 @@
+// lo <= a ^ x < hi となる x の区間 [L, R) の列挙
+template <typename F>
+void xor_range(u64 a, u64 lo, u64 hi, F query) {
+  FOR(k, 64) {
+    if (lo == hi) break;
+    ll b = 1LL << k;
+    if (lo & b) {
+      query((lo ^ a), (lo ^ a) + b);
+      lo += b;
+    }
+    if (hi & b) {
+      query((hi - b) ^ a, ((hi - b) ^ a) + b);
+      hi -= b;
+    }
+    if (a & b) a ^= b;
+  }
+}
