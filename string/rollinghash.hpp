4b69defe7e5bd3fdfb229ca1b3d6ec688a429b4d
maspypy <maspypy.gmail.com>
2022-11-28 00:06:01 +0900

    use m61

--- string/rollinghash.hpp
+++ string/rollinghash.hpp
@@ -1,68 +1,45 @@
 #pragma once
 #include "random/base.hpp"
+#include "mod/modint61.hpp"
 
 struct RollingHash {
-  static const uint64_t mod = (1ull << 61ull) - 1;
-  const uint64_t base;
-  vector<uint64_t> power;
+  using mint = modint61;
+  static constexpr u64 mod = mint::get_mod();
+  const mint base;
+  vc<mint> power;
 
-  static inline uint64_t add(uint64_t a, uint64_t b) {
-    if ((a += b) >= mod) a -= mod;
-    return a;
-  }
-
-  static inline uint64_t mul(uint64_t a, uint64_t b) {
-    const uint64_t MASK30 = (1LL << 30) - 1;
-    const uint64_t MASK31 = (1LL << 31) - 1;
-    const uint64_t MASK61 = (1LL << 61) - 1;
-    uint64_t au = a >> 31, ad = a & MASK31;
-    uint64_t bu = b >> 31, bd = b & MASK31;
-    uint64_t x = ad * bu + au * bd;
-    uint64_t xu = x >> 30, xd = x & MASK30;
-    x = au * bu * 2 + xu + (xd << 31) + ad * bd;
-    xu = x >> 61, xd = x & MASK61;
-    x = xu + xd;
-    if (x >= MASK61) x -= MASK61;
-    return x;
-  }
-
-  static inline uint64_t generate_base() { return RNG(mod); }
+  static inline mint generate_base() { return RNG(mod); }
 
   inline void expand(size_t sz) {
     if (power.size() < sz + 1) {
       int pre_sz = (int)power.size();
       power.resize(sz + 1);
-      for (int i = pre_sz - 1; i < sz; i++) {
-        power[i + 1] = mul(power[i], base);
-      }
+      FOR(i, pre_sz - 1, sz) power[i + 1] = power[i] * base;
     }
   }
 
-  explicit RollingHash(uint64_t base = generate_base())
-      : base(base), power{1} {}
+  explicit RollingHash(mint base = generate_base()) : base(base), power{1} {}
 
   template <typename STRING>
-  vector<uint64_t> build(const STRING& s) const {
+  vector<mint> build(const STRING& s) const {
     int sz = s.size();
-    vector<uint64_t> hashed(sz + 1);
-    for (int i = 0; i < sz; i++) {
-      hashed[i + 1] = add(mul(hashed[i], base), s[i]);
-    }
+    vector<mint> hashed(sz + 1);
+    for (int i = 0; i < sz; i++) { hashed[i + 1] = hashed[i] * base + s[i]; }
     return hashed;
   }
 
-  uint64_t query(const vector<uint64_t>& s, int l, int r) {
+  mint query(const vc<mint>& s, int l, int r) {
     expand(r - l);
-    return add(s[r], mod - mul(s[l], power[r - l]));
+    return (s[r] - s[l] * power[r - l]).val;
   }
 
-  uint64_t combine(uint64_t h1, uint64_t h2, size_t h2len) {
+  mint combine(mint h1, mint h2, int h2len) {
     expand(h2len);
-    return add(mul(h1, power[h2len]), h2);
+    return h1 * power[h2len] + h2;
   }
 
-  int lcp(const vector<uint64_t>& a, int l1, int r1, const vector<uint64_t>& b,
-          int l2, int r2) {
+  int lcp(const vc<mint>& a, int l1, int r1, const vc<mint>& b, int l2,
+          int r2) {
     int len = min(r1 - l1, r2 - l2);
     int low = 0, high = len + 1;
     while (high - low > 1) {
