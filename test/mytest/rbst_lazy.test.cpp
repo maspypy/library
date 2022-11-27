d1c6e0a38a08789d9808df012822e36cf986de92
maspypy <maspypy.gmail.com>
2022-11-28 03:22:53 +0900

    refactor

---
+++ test/mytest/rbst_lazy.test.cpp
@@ -0,0 +1,78 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "alg/lazy/min_set.hpp"
+#include "ds/bbst/rbst_lazy.hpp"
+#include "random/base.hpp"
+
+void test() {
+  using Lazy = Lazy_Min_Set<int, -1>;
+  using MonoX = typename Lazy::MX;
+
+  RBST_Lazy<Lazy, 100> X;
+  FOR(1000) {
+    X.reset();
+    int N = RNG(1, 20);
+    int Q = RNG(1, 1000);
+    vc<int> A(N);
+    FOR(i, N) A[i] = RNG(1, 100);
+    auto root = X.new_node(A);
+
+    FOR(Q) {
+      int t = RNG(0, 6);
+      if (t == 0) {
+        int i = RNG(0, N);
+        assert(A[i] == X.get(root, i));
+      }
+      if (t == 1) {
+        int i = RNG(0, N);
+        int x = RNG(1, 100);
+        root = X.set(root, i, x);
+        A[i] = x;
+      }
+      if (t == 2) {
+        int i = RNG(0, N);
+        int x = RNG(1, 100);
+        root = X.multiply(root, i, x);
+        A[i] = MonoX::op(A[i], x);
+      }
+      if (t == 3) {
+        int L = RNG(0, N);
+        int R = RNG(0, N);
+        if (L > R) swap(L, R);
+        ++R;
+        vc<int> B = {A.begin() + L, A.begin() + R};
+        assert(X.prod(root, L, R) == MIN(B));
+      }
+      if (t == 4) {
+        int L = RNG(0, N);
+        int R = RNG(0, N);
+        if (L > R) swap(L, R);
+        ++R;
+        root = X.reverse(root, L, R);
+        reverse(A.begin() + L, A.begin() + R);
+      }
+      if (t == 5) {
+        int L = RNG(0, N);
+        int R = RNG(0, N);
+        if (L > R) swap(L, R);
+        ++R;
+        int x = RNG(1, 100);
+        FOR(i, L, R) A[i] = x;
+        root = X.apply(root, L, R, x);
+      }
+    }
+  }
+}
+
+void solve() {
+  LL(a, b);
+  print(a + b);
+}
+
+signed main() {
+  test();
+  solve();
+
+  return 0;
+}
