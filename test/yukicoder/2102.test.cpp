560f1e0b89fdc09f61dd2dda6020ad613e0a2a65
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-28 00:54:01 +0900

    Update 2102.test.cpp

--- test/yukicoder/2102.test.cpp
+++ test/yukicoder/2102.test.cpp
@@ -15,25 +15,26 @@ void solve() {
     STR(S);
     ll n = len(S);
     auto SH = RH.build(S);
-    vi X;
+    using M61 = modint61;
+    vc<M61> X;
     X.eb(RH.query(SH, 0, n));
     FOR(i, n - 1) {
       string mi;
       mi += S[i + 1];
       mi += S[i];
-      ll m = RH.query(RH.build(mi), 0, 2);
-      ll l = RH.query(SH, 0, i);
-      ll r = RH.query(SH, i + 2, n);
+      M61 m = RH.query(RH.build(mi), 0, 2);
+      M61 l = RH.query(SH, 0, i);
+      M61 r = RH.query(SH, i + 2, n);
       l = RH.combine(l, m, 2);
       l = RH.combine(l, r, n - i - 2);
       X.eb(l);
     }
     bool ok = 0;
     for (auto&& x: X) {
-      if (MP.count(x)) ok = 1;
+      if (MP.count(x.val)) ok = 1;
     }
     Yes(ok);
-    MP.insert(X[0]);
+    MP.insert(X[0].val);
   }
 }
 
