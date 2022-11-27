f233901303cad06c89072fae1fbc6327b44d0177
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-27 16:21:57 +0900

    Update cntsum_affine.hpp

--- alg/lazy/cntsum_affine.hpp
+++ alg/lazy/cntsum_affine.hpp
@@ -1,10 +1,10 @@
-#include "alg/monoid/cntsum.hpp"
+#include "alg/monoid/add_pair.hpp"
 #include "alg/monoid/affine.hpp"
 
 template <typename E>
 struct Lazy_CntSum_Affine {
-  using X_structure = Group_CntSum<E>;
-  using A_structure = Group_Affine<E>;
+  using X_structure = Monoid_Add_Pair<E>;
+  using A_structure = Monoid_Affine<E>;
   using X = typename X_structure::value_type;
   using A = typename A_structure::value_type;
   static constexpr X act(const X &x, const A &a) {
