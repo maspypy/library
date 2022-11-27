d15432fa92d46c85b859fbb9df91bb73cd4c9edc
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-28 00:35:35 +0900

    Delete and.hpp

--- alg/monoid/and.hpp
+++
@@ -1,8 +0,0 @@
-template <typename E>
-struct Monoid_And {
-  using value_type = E;
-  using X = value_type;
-  static X op(X x, X y) { return x & y; }
-  static constexpr X unit() { return numeric_limits<E>::max(); }
-  static constexpr bool commute = true;
-};
