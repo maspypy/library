78cc17e47df69896486b00243c4308c913824813
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-27 15:01:54 +0900

    Update rectangle_sum_bit2d.test.cpp

--- test/library_checker/datastructure/rectangle_sum_bit2d.test.cpp
+++ test/library_checker/datastructure/rectangle_sum_bit2d.test.cpp
@@ -12,7 +12,7 @@ void solve() {
     LL(x, y, w);
     X[i] = x, Y[i] = y, W[i] = w;
   }
-  Fenwick2D<Group_Add<ll>, ll, false> bit(X, Y, W);
+  Fenwick2D<Monoid_Add<ll>, ll, false> bit(X, Y, W);
   FOR(_, Q) {
     LL(l, d, r, u);
     print(bit.sum(l, r, d, u));
