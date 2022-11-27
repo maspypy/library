699ca59a5ef9fd4f91d4bc732667166c512875df
maspypy <maspypy.gmail.com>
2022-11-27 22:15:30 +0900

    empty

--- alg/lazy/cntsummax_set.hpp
+++ alg/lazy/cntsummax_set.hpp
@@ -1,16 +1,17 @@
 #include "alg/monoid/cntsummax.hpp"
 #include "alg/monoid/set.hpp"
 
+template <typename E, ll none_val>
 struct Lazy_CntSumMax_Set {
   static constexpr ll INF = 1LL << 60;
-  using MX = Monoid_CntSumMax;
-  using MA = Monoid_Set<ll, -INF>;
+  using MX = Monoid_CntSumMax<E>;
+  using MA = Monoid_Set<ll, none_val>;
   using X_structure = MX;
   using A_structure = MA;
   using X = typename MX::value_type;
   using A = typename MA::value_type;
   static constexpr X act(const X& x, const A& a) {
-    if (a == -INF) return x;
+    if (a == MA::unit()) return x;
     auto [xc, xs, xm] = x;
     return {xc, xc * a, a};
   }
