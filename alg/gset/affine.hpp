8fa938b0e50dcd35fd4c9dd796ff90903670eda2
maspypy <maspypy.gmail.com>
2022-11-27 13:06:40 +0900

    change dir

--- alg/gset/affine.hpp
+++
@@ -1,8 +0,0 @@
-// 1 次元ベクトル空間に、アフィン変換が作用
-template <typename T>
-struct GSet_Affine {
-  using Group = Group_Affine<T>;
-  using G = typename Group::value_type;
-  using X = T;
-  static X act(const G &g, const X &x) { return g.fi * x + g.se; }
-};
