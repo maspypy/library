468d1bdcb6d272046dd004baa21000a4ee4f8874
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-28 00:35:50 +0900

    Delete or.hpp

--- alg/monoid/or.hpp
+++
@@ -1,8 +0,0 @@
-template <typename E>
-struct Monoid_Or {
-  using value_type = E;
-  using X = value_type;
-  static X op(X x, X y) { return x | y; }
-  static constexpr X unit() { return 0; }
-  static constexpr bool commute = true;
-};
