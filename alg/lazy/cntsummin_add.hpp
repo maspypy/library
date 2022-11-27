953f31bbb4a6e966360601b8bc483366783a1200
maspypy <maspypy.gmail.com>
2022-11-27 22:19:51 +0900

    empty

--- alg/lazy/cntsummin_add.hpp
+++ alg/lazy/cntsummin_add.hpp
@@ -1,16 +1,17 @@
 #include "alg/monoid/cntsummin.hpp"
 #include "alg/monoid/add.hpp"
 
+template <typename E>
 struct Lazy_CntSumMin_Add {
-  static constexpr ll INF = 1LL << 60;
-  using MX = Monoid_CntSumMin;
-  using MA = Monoid_Add<ll>;
+  using MX = Monoid_CntSumMin<E>;
+  using MA = Monoid_Add<E>;
   using X_structure = MX;
   using A_structure = MA;
   using X = typename MX::value_type;
   using A = typename MA::value_type;
   static constexpr X act(const X& x, const A& a) {
     auto [xc, xs, xm] = x;
-    return {xc, xs + xc * a, xm + a};
+    xm = (xm == numeric_limits<E>::max() ? xm : xm + a);
+    return {xc, xs + xc * a, xm};
   }
 };
