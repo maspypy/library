27a66ed76adf7134a511b18c8a76f3840c5ee5f7
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-27 13:59:01 +0900

    Update and rename power_sums.hpp to add_array.hpp

---
+++ alg/monoid/add_array.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+template <typename E, int K>
+struct Group_power_sums {
+  using value_type = array<E, K>;
+  using X = value_type;
+  static X op(X x, X y) {
+    X z;
+    FOR(i, K) z[i] = x[i] + y[i];
+    return z;
+  }
+  static X unit() { return X{}; }
+  static constexpr bool commute = 1;
+};
