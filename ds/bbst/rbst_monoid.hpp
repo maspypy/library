477a70979e0e92cf8d1a52630d532bd37257b653
maspypy <maspypy.gmail.com>
2022-11-28 02:17:35 +0900

    refactor

--- ds/bbst/rbst_monoid.hpp
+++ ds/bbst/rbst_monoid.hpp
@@ -1,4 +1,3 @@
-
 // reverse はとりあえず、Monoid の可換性を仮定している！
 template <typename Monoid, int NODES = 1'000'000>
 struct RBST_Monoid {
@@ -7,7 +6,7 @@ struct RBST_Monoid {
   struct Node {
     Node *l, *r;
     X x, prod;
-    int size;
+    u32 size;
     bool rev;
   };
 
@@ -26,10 +25,10 @@ struct RBST_Monoid {
   }
 
   Node *new_node(const vc<X> &dat) {
-    auto dfs = [&](auto &dfs, int l, int r) -> Node * {
+    auto dfs = [&](auto &dfs, u32 l, u32 r) -> Node * {
       if (l == r) return nullptr;
       if (r == l + 1) return new_node(dat[l]);
-      int m = (l + r) / 2;
+      u32 m = (l + r) / 2;
       Node *l_root = dfs(dfs, l, m);
       Node *r_root = dfs(dfs, m + 1, r);
       Node *root = new_node(dat[m]);
@@ -40,102 +39,64 @@ struct RBST_Monoid {
     return dfs(dfs, 0, len(dat));
   }
 
-  // 左に右をマージ
-  void merge(Node *&root, Node *r_root) {
-    root = merge_rec(root, r_root);
-    return;
+  Node *merge(Node *root, Node *r_root) { return merge_rec(root, r_root); }
+  Node *merge3(Node *a, Node *b, Node *c) { return merge(merge(a, b), c); }
+  pair<Node *, Node *> split(Node *root, u32 k) {
+    if (!root) {
+      assert(k == 0);
+      return {nullptr, nullptr};
+    }
+    assert(0 <= k && k <= root->size);
+    return split_rec(root, k);
+  }
+  tuple<Node *, Node *, Node *> split3(Node *root, u32 l, u32 r) {
+    Node *nm, *nr;
+    tie(root, nr) = split(root, r);
+    tie(root, nm) = split(root, l);
+    return {root, nm, nr};
   }
 
-  // 左右に分けて、root を左側に変更。右側を return する。
-  Node *split(Node *&root, int k) {
-    if (!root) assert(k == 0);
-    if (root) assert(0 <= k && k <= root->size);
-    auto [nl, nr] = split_rec(root, k);
-    root = nl;
-    return nr;
-  }
-
-  X prod(Node *&root, int l, int r) {
-    assert(0 <= l && l <= r && r <= root->size);
+  X prod(Node *root, u32 l, u32 r) {
     if (l == r) return Monoid::unit();
-    Node *r_root = split(root, r);
-    Node *m_root = split(root, l);
-    X res = m_root->prod;
-    merge(root, m_root);
-    merge(root, r_root);
+    return prod_rec(root, l, r);
+  }
+
+  Node *reverse(Node *root, u32 l, u32 r) {
+    assert(Monoid::commute);
+    assert(0 <= l && l <= r && r <= root->size);
+    if (r - l <= 1) return root;
+    auto [nl, nm, nr] = split3(root, l, r);
+    nm->rev ^= 1;
+    prop(nm), update(nm);
+    return merge3(nl, nm, nr);
+  }
+
+  Node *set(Node *root, u32 k, const X &x) { return set_rec(root, k, x); }
+  Node *multiply(Node *root, u32 k, const X &x) {
+    return multiply_rec(root, k, x);
+  }
+  X get(Node *root, u32 k) { return get_rec(root, k); }
+
+  vc<X> get_all(Node *root) {
+    vc<X> res;
+    auto dfs = [&](auto &dfs, Node *root) -> void {
+      if (!root) return;
+      prop(root);
+      dfs(dfs, root->l);
+      res.eb(root->x);
+      dfs(dfs, root->r);
+    };
+    dfs(dfs, root);
     return res;
   }
 
-  void reverse(Node *&root, int l, int r) {
-    assert(Monoid::commute);
-    assert(0 <= l && l <= r && r <= root->size);
-    if (r - l <= 1) return;
-    Node *r_root = split(root, r);
-    Node *m_root = split(root, l);
-    m_root->rev ^= 1;
-    prop(m_root);
-    update(m_root);
-    merge(root, m_root);
-    merge(root, r_root);
-  }
-
-  // root の k 番目に n を挿入する
-  void insert(Node *&root, int k, Node *n) {
-    if (!root) {
-      assert(k == 0);
-      root = n;
-      return;
-    }
-    assert(0 <= k && k <= root->size);
-    auto r_root = split(root, k);
-    merge(root, n);
-    merge(root, r_root);
-  }
-
-  void insert(Node *&root, int k, const X &x) { insert(root, k, new_node(x)); }
-
-  // root から k 番目を削除。削除したノードをかえす
-  Node *erase(Node *&root, int k) {
-    assert(0 <= k && k < root->size);
-    auto nr = split(root, k + 1);
-    auto nm = split(root, k);
-    merge(root, nr);
-    return nm;
-  }
-
-  void set(Node *&root, int k, const X &x) {
-    auto nr = split(root, 1 + k);
-    auto nm = split(root, k);
-    nm->x = nm->prod = x;
-    merge(nm, nr);
-    merge(root, nm);
-  }
-
-  void debug(Node *root) {
-    print("RBST");
-    string s;
-    auto dfs = [&](auto &dfs, Node *n) -> void {
-      if (!n) return;
-      s += "l";
-      dfs(dfs, n->l);
-      s.pop_back();
-      print(s, "size", n->size, "x", n->x, "prod", n->prod, "apply", n->a);
-      s += "r";
-      dfs(dfs, n->r);
-      s.pop_back();
-    };
-    dfs(dfs, root);
-  }
-
 private:
-  inline int xor128() {
-    static int x = 123456789;
-    static int y = 362436069;
-    static int z = 521288629;
-    static int w = 88675123;
-    int t;
-
-    t = x ^ (x << 11);
+  inline u32 xor128() {
+    static u32 x = 123456789;
+    static u32 y = 362436069;
+    static u32 z = 521288629;
+    static u32 w = 88675123;
+    u32 t = x ^ (x << 11);
     x = y;
     y = z;
     z = w;
@@ -167,7 +128,7 @@ private:
   Node *merge_rec(Node *l_root, Node *r_root) {
     if (!l_root) return r_root;
     if (!r_root) return l_root;
-    int sl = l_root->size, sr = r_root->size;
+    u32 sl = l_root->size, sr = r_root->size;
     if (xor128() % (sl + sr) < sl) {
       prop(l_root);
       l_root->r = merge_rec(l_root->r, r_root);
@@ -180,19 +141,76 @@ private:
     return r_root;
   }
 
-  pair<Node *, Node *> split_rec(Node *root, int k) {
+  pair<Node *, Node *> split_rec(Node *root, u32 k) {
     if (!root) return {nullptr, nullptr};
     prop(root);
-    int ls = (root->l ? root->l->size : 0);
-    if (k <= ls) {
+    u32 sl = (root->l ? root->l->size : 0);
+    if (k <= sl) {
       auto [nl, nr] = split_rec(root->l, k);
       root->l = nr;
       update(root);
       return {nl, root};
     }
-    auto [nl, nr] = split_rec(root->r, k - (1 + ls));
+    auto [nl, nr] = split_rec(root->r, k - (1 + sl));
     root->r = nl;
     update(root);
     return {root, nr};
+  }
+
+  Node *set_rec(Node *root, u32 k, const X &x) {
+    if (!root) return root;
+    prop(root);
+    u32 sl = (root->l ? root->l->size : 0);
+    if (k < sl) {
+      root->l = set_rec(root->l, k, x);
+      update(root);
+      return root;
+    }
+    if (k == sl) {
+      root->x = x;
+      update(root);
+      return root;
+    }
+    root->r = set_rec(root->r, k - (1 + sl), x);
+    update(root);
+    return root;
+  }
+
+  Node *multiply_rec(Node *root, u32 k, const X &x) {
+    if (!root) return root;
+    prop(root);
+    u32 sl = (root->l ? root->l->size : 0);
+    if (k < sl) {
+      root->l = multiply_rec(root->l, k, x);
+      update(root);
+      return root;
+    }
+    if (k == sl) {
+      root->x = Monoid::op(root->x, x);
+      update(root);
+      return root;
+    }
+    root->r = multiply_rec(root->r, k - (1 + sl), x);
+    update(root);
+    return root;
+  }
+
+  X prod_rec(Node *root, u32 l, u32 r) {
+    prop(root);
+    if (l == 0 && r == root->size) return root->prod;
+    u32 sl = (root->l ? root->l->size : 0);
+    X res = Monoid::unit();
+    if (l < sl) { res = Monoid::op(res, prod_rec(root->l, l, min(r, sl))); }
+    if (l <= sl && sl < r) res = Monoid::op(res, root->x);
+    u32 k = 1 + sl;
+    if (k < r) res = Monoid::op(res, prod_rec(root->r, max(k, l) - k, r - k));
+    return res;
+  }
+
+  X get_rec(Node *root, u32 k) {
+    u32 sl = (root->l ? root->l->size : 0);
+    if (k < sl) return get_rec(root->l, k);
+    if (k == sl) return root->x;
+    return get_rec(root->r, k - (1 + sl));
   }
 };
