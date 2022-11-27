a0c4f78e4eeb0c9adf1e5d142adb1537752f0794
maspypy <45988013+maspypy@users.noreply.github.com>
2022-11-27 14:28:36 +0900

    Update cntsum_set.hpp

--- alg/lazy/cntsum_set.hpp
+++ alg/lazy/cntsum_set.hpp
@@ -1,9 +1,9 @@
-#include "alg/monoid/cntsum.hpp"
+#include "alg/monoid/add_pair.hpp"
 #include "alg/monoid/set.hpp"
 
 template <typename E, E none_val>
 struct Lazy_CntSum_Set {
-  using X_structure = Group_CntSum<E>;
+  using X_structure = Monoid_Add_Pair<E>;
   using A_structure = Monoid_Set<E, none_val>;
   using X = typename X_structure::value_type;
   using A = typename A_structure::value_type;
