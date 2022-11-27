c8e5d18cfa83f3291be77b254855aaaaca90d056
maspypy <maspypy.gmail.com>
2022-11-28 00:42:11 +0900

    add test

--- test/mytest/datetime.test.cpp
+++ test/mytest/datetime.test.cpp
@@ -6,6 +6,14 @@
 
 using DT = DateTime;
 
+void test_ARC23_A() {
+  auto f = [&](int y, int m, int d) -> int {
+    return DateTime(2014, 5, 17).to_int() - DateTime(y, m, d).to_int();
+  };
+  assert(9449 == f(1988, 7, 3));
+  assert(735369 == f(1, 1, 1));
+}
+
 void test_from_int() {
   DT A(1, 1, 1);
   FOR(x, 1000000) {
@@ -26,6 +34,7 @@ signed main() {
   ios::sync_with_stdio(false);
   cout << setprecision(15);
 
+  test_ARC23_A();
   test_from_int();
   solve();
 
