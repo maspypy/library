e4af7ddc4ac35526280ac7b28158abaa7d248584
maspypy <maspypy.gmail.com>
2022-11-28 14:51:10 +0900

    refactor

--- alg/monoid/add.hpp
+++ alg/monoid/add.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-template <typename E>
+template <typename X>
 struct Monoid_Add {
-  using X = E;
   using value_type = X;
   static constexpr X op(const X &x, const X &y) noexcept { return x + y; }
   static constexpr X inverse(const X &x) noexcept { return -x; }
--- alg/monoid/assign.hpp
+++ alg/monoid/assign.hpp
@@ -1,7 +1,6 @@
-template <typename E, E none_val>
-struct Monoid_Set {
-  using value_type = E;
-  using X = value_type;
+template <typename X, X none_val>
+struct Monoid_Assign {
+  using value_type = X;
   static X op(X x, X y) { return (y == none_val ? x : y); }
   static constexpr X unit() { return none_val; }
   static constexpr bool commute = false;
