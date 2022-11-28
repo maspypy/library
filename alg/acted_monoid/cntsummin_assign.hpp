cb75d1752c8c5e10741edea73b72fbf73aa5b7d3
maspypy <maspypy.gmail.com>
2022-11-28 18:48:28 +0900

    fix

--- alg/acted_monoid/cntsummin_assign.hpp
+++ alg/acted_monoid/cntsummin_assign.hpp
@@ -3,13 +3,12 @@
 
 template <typename E, ll none_val>
 struct ActedMonoid_CntSumMin_Assign {
-  static constexpr ll INF = 1LL << 60;
   using Monoid_X = Monoid_CntSumMin<E>;
   using Monoid_A = Monoid_Assign<ll, none_val>;
   using X = typename Monoid_X::value_type;
   using A = typename Monoid_A::value_type;
   static constexpr X act(const X& x, const A& a) {
-    if (a == MA::unit()) return x;
+    if (a == Monoid_A::unit()) return x;
     auto [xc, xs, xm] = x;
     return {xc, xc * a, a};
   }
