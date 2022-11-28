7cf990fe6681343b6b2872ad88f97e3c73282e78
maspypy <maspypy.gmail.com>
2022-11-28 15:09:50 +0900

    commit

--- alg/acted_monoid/cntsum_add.hpp
+++ alg/acted_monoid/cntsum_add.hpp
@@ -2,13 +2,11 @@
 #include "alg/monoid/add_pair.hpp"
 
 template <typename E>
-struct Lazy_CntSum_Add {
-  using MX = Monoid_Add_Pair<E>;
-  using MA = Monoid_Add<E>;
-  using X_structure = MX;
-  using A_structure = MA;
-  using X = typename MX::value_type;
-  using A = typename MA::value_type;
+struct ActedMonoid_CntSum_Add {
+  using Monoid_X = Monoid_Add_Pair<E>;
+  using Monoid_A = Monoid_Add<E>;
+  using X = typename Monoid_X::value_type;
+  using A = typename Monoid_A::value_type;
   static constexpr X act(const X &x, const A &a) {
     return {x.fi, x.se + x.fi * a};
   }
--- alg/acted_monoid/cntsummin_add.hpp
+++ alg/acted_monoid/cntsummin_add.hpp
@@ -3,12 +3,10 @@
 
 template <typename E>
 struct Lazy_CntSumMin_Add {
-  using MX = Monoid_CntSumMin<E>;
-  using MA = Monoid_Add<E>;
-  using X_structure = MX;
-  using A_structure = MA;
-  using X = typename MX::value_type;
-  using A = typename MA::value_type;
+  using Monoid_X = Monoid_CntSumMin<E>;
+  using Monoid_A = Monoid_Add<E>;
+  using X = typename Monoid_X::value_type;
+  using A = typename Monoid_A::value_type;
   static constexpr X act(const X& x, const A& a) {
     auto [xc, xs, xm] = x;
     xm = (xm == numeric_limits<E>::max() ? xm : xm + a);
