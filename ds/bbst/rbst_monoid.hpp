9edff1022462063a33089a8b1398ab7be8e2022a
maspypy <maspypy.gmail.com>
2022-11-29 10:01:12 +0900

    persistent

--- ds/bbst/rbst_monoid.hpp
+++ ds/bbst/rbst_monoid.hpp
@@ -1,4 +1,4 @@
-template <typename Monoid, int NODES = 1'000'000>
+template <typename Monoid, bool PERSISTENT, int NODES>
 struct RBST_Monoid {
   using X = typename Monoid::value_type;
 
@@ -32,12 +32,25 @@ struct RBST_Monoid {
       if (l == r) return nullptr;
       if (r == l + 1) return new_node(dat[l]);
       u32 m = (l + r) / 2;
+      np l_root = dfs(dfs, l, m);
+      np r_root = dfs(dfs, m + 1, r);
       np root = new_node(dat[m]);
-      root->l = dfs(dfs, l, m), root->r = dfs(dfs, m + 1, r);
+      root->l = l_root, root->r = r_root;
       update(root);
       return root;
     };
     return dfs(dfs, 0, len(dat));
+  }
+
+  np copy_node(np &n) {
+    if (!n || !PERSISTENT) return n;
+    pool[pid].l = n->l, pool[pid].r = n->r;
+    pool[pid].x = n->x;
+    pool[pid].prod = n->prod;
+    pool[pid].rev_prod = n->rev_prod;
+    pool[pid].size = n->size;
+    pool[pid].rev = n->rev;
+    return &(pool[pid++]);
   }
 
   np merge(np l_root, np r_root) { return merge_rec(l_root, r_root); }
@@ -66,40 +79,41 @@ struct RBST_Monoid {
 
   X prod(np root, u32 l, u32 r) {
     if (l == r) return Monoid::unit();
-    return prod_rec(root, l, r);
+    return prod_rec(root, l, r, false);
   }
+  X prod(np root) { return (root ? root->prod : Monoid::unit()); }
 
   np reverse(np root, u32 l, u32 r) {
     assert(0 <= l && l <= r && r <= root->size);
     if (r - l <= 1) return root;
     auto [nl, nm, nr] = split3(root, l, r);
     nm->rev ^= 1;
-    swap(nm->prod, nm->rev_prod);
     swap(nm->l, nm->r);
+    swap(nm->prod, nm->rev_prod);
     return merge3(nl, nm, nr);
   }
 
   np set(np root, u32 k, const X &x) { return set_rec(root, k, x); }
   np multiply(np root, u32 k, const X &x) { return multiply_rec(root, k, x); }
-  X get(np root, u32 k) { return get_rec(root, k); }
+  X get(np root, u32 k) { return get_rec(root, k, false); }
 
   vc<X> get_all(np root) {
     vc<X> res;
     auto dfs = [&](auto &dfs, np root, bool rev) -> void {
       if (!root) return;
-      dfs(dfs, (rev ? root->r : root->l), root->rev ^ rev);
+      dfs(dfs, (rev ? root->r : root->l), rev ^ root->rev);
       res.eb(root->x);
-      dfs(dfs, (rev ? root->l : root->r), root->rev ^ rev);
+      dfs(dfs, (rev ? root->l : root->r), rev ^ root->rev);
     };
     dfs(dfs, root, 0);
     return res;
   }
 
   template <typename F>
-  u32 max_right(np root, const F check, u32 L) {
+  pair<np, np> split_max_right(np root, const F check) {
     assert(check(Monoid::unit()));
     X x = Monoid::unit();
-    return max_right_rec(root, check, L, x);
+    return split_max_right_rec(root, check, x);
   }
 
 private:
@@ -115,23 +129,28 @@ private:
     return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
   }
 
-  void push(np c) {
+  void prop(np c) {
+    // 自身をコピーする必要はない。
+    // 子をコピーする必要がある。複数の親を持つ可能性があるため。
     if (c->rev) {
       if (c->l) {
+        c->l = copy_node(c->l);
         c->l->rev ^= 1;
-        swap(c->l->prod, c->l->rev_prod);
         swap(c->l->l, c->l->r);
+        swap(c->l->prod, c->l->rev_prod);
       }
       if (c->r) {
+        c->r = copy_node(c->r);
         c->r->rev ^= 1;
-        swap(c->r->prod, c->r->rev_prod);
         swap(c->r->l, c->r->r);
+        swap(c->r->prod, c->r->rev_prod);
       }
       c->rev = 0;
     }
   }
 
   void update(np c) {
+    // データを保ったまま正常化するだけなので、コピー不要
     c->size = 1;
     c->prod = c->rev_prod = c->x;
     if (c->l) {
@@ -151,12 +170,14 @@ private:
     if (!r_root) return l_root;
     u32 sl = l_root->size, sr = r_root->size;
     if (xor128() % (sl + sr) < sl) {
-      push(l_root);
+      prop(l_root);
+      l_root = copy_node(l_root);
       l_root->r = merge_rec(l_root->r, r_root);
       update(l_root);
       return l_root;
     }
-    push(r_root);
+    prop(r_root);
+    r_root = copy_node(r_root);
     r_root->l = merge_rec(l_root, r_root->l);
     update(r_root);
     return r_root;
@@ -164,15 +185,17 @@ private:
 
   pair<np, np> split_rec(np root, u32 k) {
     if (!root) return {nullptr, nullptr};
-    push(root);
+    prop(root);
     u32 sl = (root->l ? root->l->size : 0);
     if (k <= sl) {
       auto [nl, nr] = split_rec(root->l, k);
+      root = copy_node(root);
       root->l = nr;
       update(root);
       return {nl, root};
     }
     auto [nl, nr] = split_rec(root->r, k - (1 + sl));
+    root = copy_node(root);
     root->r = nl;
     update(root);
     return {root, nr};
@@ -180,18 +203,21 @@ private:
 
   np set_rec(np root, u32 k, const X &x) {
     if (!root) return root;
-    push(root);
+    prop(root);
     u32 sl = (root->l ? root->l->size : 0);
     if (k < sl) {
+      root = copy_node(root);
       root->l = set_rec(root->l, k, x);
       update(root);
       return root;
     }
     if (k == sl) {
+      root = copy_node(root);
       root->x = x;
       update(root);
       return root;
     }
+    root = copy_node(root);
     root->r = set_rec(root->r, k - (1 + sl), x);
     update(root);
     return root;
@@ -199,65 +225,88 @@ private:
 
   np multiply_rec(np root, u32 k, const X &x) {
     if (!root) return root;
-    push(root);
+    prop(root);
     u32 sl = (root->l ? root->l->size : 0);
     if (k < sl) {
+      root = copy_node(root);
       root->l = multiply_rec(root->l, k, x);
       update(root);
       return root;
     }
     if (k == sl) {
+      root = copy_node(root);
       root->x = Monoid::op(root->x, x);
       update(root);
       return root;
     }
+    root = copy_node(root);
     root->r = multiply_rec(root->r, k - (1 + sl), x);
     update(root);
     return root;
   }
 
-  X prod_rec(np root, u32 l, u32 r) {
-    push(root);
-    if (l == 0 && r == root->size) return root->prod;
-    u32 sl = (root->l ? root->l->size : 0);
+  X prod_rec(np root, u32 l, u32 r, bool rev) {
+    if (l == 0 && r == root->size) {
+      return (rev ? root->rev_prod : root->prod);
+    }
+    np left = (rev ? root->r : root->l);
+    np right = (rev ? root->l : root->r);
+    u32 sl = (left ? left->size : 0);
     X res = Monoid::unit();
-    if (l < sl) { res = Monoid::op(res, prod_rec(root->l, l, min(r, sl))); }
+    if (l < sl) {
+      X y = prod_rec(left, l, min(r, sl), rev ^ root->rev);
+      res = Monoid::op(res, y);
+    }
     if (l <= sl && sl < r) res = Monoid::op(res, root->x);
     u32 k = 1 + sl;
-    if (k < r) res = Monoid::op(res, prod_rec(root->r, max(k, l) - k, r - k));
+    if (k < r) {
+      X y = prod_rec(right, max(k, l) - k, r - k, rev ^ root->rev);
+      res = Monoid::op(res, y);
+    }
     return res;
   }
 
-  X get_rec(np root, u32 k) {
-    push(root);
-    u32 sl = (root->l ? root->l->size : 0);
-    if (k < sl) return get_rec(root->l, k);
+  X get_rec(np root, u32 k, bool rev) {
+    np left = (rev ? root->r : root->l);
+    np right = (rev ? root->l : root->r);
+    u32 sl = (left ? left->size : 0);
     if (k == sl) return root->x;
-    return get_rec(root->r, k - (1 + sl));
+    rev ^= root->rev;
+    if (k < sl) return get_rec(left, k, rev);
+    return get_rec(right, k - (1 + sl), rev);
   }
 
   template <typename F>
-  u32 max_right_rec(np n, const F check, u32 L, X &x) {
-    if (!n) return 0;
-    push(n);
-    if (L == 0) {
-      X y = Monoid::op(x, n->prod);
-      if (check(y)) {
-        x = y;
-        return n->size;
+  pair<np, np> split_max_right_rec(np root, const F &check, X &x) {
+    if (!root) return {nullptr, nullptr};
+    prop(root);
+    root = copy_node(root);
+    X y = Monoid::op(x, root->prod);
+    if (check(y)) {
+      x = y;
+      return {root, nullptr};
+    }
+    np left = root->l, right = root->r;
+    if (left) {
+      X y = Monoid::op(x, root->l->prod);
+      if (!check(y)) {
+        auto [n1, n2] = split_max_right_rec(left, check, x);
+        root->l = n2;
+        update(root);
+        return {n1, root};
       }
-    }
-    u32 sl = (n->l ? n->l->size : 0);
-    if (L < sl) {
-      u32 k = max_right_rec(n->l, check, L, x);
-      if (k < sl) return k;
-    }
-    if (L <= sl) {
-      X y = Monoid::op(x, n->x);
-      if (!check(y)) { return sl; }
       x = y;
     }
-    L = (L > sl ? L - (1 + sl) : 0);
-    return (1 + sl) + max_right_rec(n->r, check, L, x);
+    y = Monoid::op(x, root->x);
+    if (!check(y)) {
+      root->l = nullptr;
+      update(root);
+      return {left, root};
+    }
+    x = y;
+    auto [n1, n2] = split_max_right_rec(right, check, x);
+    root->r = n1;
+    update(root);
+    return {root, n2};
   }
 };
