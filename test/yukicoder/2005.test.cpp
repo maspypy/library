7c5c33c66680947c28c093706e494de033760986
maspypy <maspypy.gmail.com>
2022-12-01 22:25:33 +0900

    empty

--- test/yukicoder/2005.test.cpp
+++ test/yukicoder/2005.test.cpp
@@ -3,7 +3,7 @@
 #include "other/io.hpp"
 
 #include "mod/modint.hpp"
-#include "ds/disjointsparse.hpp"
+#include "ds/disjoint_sparse/disjointsparse.hpp"
 #include "alg/monoid/mul.hpp"
 #include "seq/famous/eulerian_number.hpp"
 
