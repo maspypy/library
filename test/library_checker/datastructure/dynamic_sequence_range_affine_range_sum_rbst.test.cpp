e964116225833a5fe3ea5315c2b1c2ecf78775c2
maspypy <45988013+maspypy@users.noreply.github.com>
2022-12-01 13:08:00 +0900

    Update dynamic_sequence_range_affine_range_sum_rbst.test.cpp

--- test/library_checker/datastructure/dynamic_sequence_range_affine_range_sum_rbst.test.cpp
+++ test/library_checker/datastructure/dynamic_sequence_range_affine_range_sum_rbst.test.cpp
@@ -5,7 +5,7 @@
 
 #include "alg/acted_monoid/cntsum_affine.hpp"
 #include "mod/modint.hpp"
-#include "ds/bbst/rbst_acted_monoid.hpp"
+#include "ds/rbst/rbst_acted_monoid.hpp"
 
 using mint = modint998;
 
