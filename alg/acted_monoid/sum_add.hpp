c25d9f09e6470589d24213ae414a641828b7d20c
maspypy <maspypy.gmail.com>
2022-12-03 06:52:03 +0900

    add apply size

--- alg/acted_monoid/sum_add.hpp
+++ alg/acted_monoid/sum_add.hpp
@@ -1,13 +1,12 @@
 #include "alg/monoid/add.hpp"
-#include "alg/monoid/mul.hpp"
 
 template <typename E>
-struct ActedMonoid_Add_Mul {
+struct ActedMonoid_Sum_Add {
   using Monoid_X = Monoid_Add<E>;
-  using Monoid_A = Monoid_Mul<E>;
+  using Monoid_A = Monoid_Add<E>;
   using X = typename Monoid_X::value_type;
   using A = typename Monoid_A::value_type;
-  static constexpr X act(const X &x, const A &a, const ll &size = 1) {
-    return x * a;
+  static constexpr X act(const X &x, const A &a, const ll &size) {
+    return return x + a * E(size);
   }
 };
