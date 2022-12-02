86bd0b23d4c228f7fc03f4aeca356e8473b2e8fd
maspypy <maspypy.gmail.com>
2022-12-02 20:17:50 +0900

    empty

--- test/mytest/minidx_add.test.cpp
+++ test/mytest/minidx_add.test.cpp
@@ -3,7 +3,7 @@
 #include "other/io.hpp"
 #include "alg/acted_monoid/minidx_add.hpp"
 #include "random/base.hpp"
-#include "ds/lazysegtree.hpp"
+#include "ds/segtree/lazysegtree.hpp"
 
 void test() {
   int N = RNG(1, 100);
--- test/yukicoder/1573.test.cpp
+++ test/yukicoder/1573.test.cpp
@@ -1,19 +1,20 @@
 #define PROBLEM "https://yukicoder.me/problems/no/1573"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "nt/floor_range.hpp"
+#include "enumerate/floor_range.hpp"
 
 void solve() {
   LL(N, M);
   i128 ANS = 0;
-  floor_range(q, l, r, N) {
+  auto f = [&](ll q, ll l, ll r) -> void {
     ll lo = max(l, 1LL);
     ll hi = min(r - 1, M);
-    if (lo > hi) continue;
+    if (lo > hi) return;
     i128 x = (q * (q + 1) / 2 + q);
     i128 y = (lo + hi) * (hi - lo + 1) / 2;
     ANS += x * y;
-  }
+  };
+  floor_range(N, f);
   print(ANS % 998244353);
 }
 
