7e000c8a98cf53eb43a72ffecb93d4741dfeb125
maspypy <maspypy.gmail.com>
2022-12-03 09:22:09 +0900

    refactor

--- ds/fenwick/fenwick2d.hpp
+++ ds/fenwick/fenwick2d.hpp
@@ -1,7 +1,10 @@
 #include "alg/monoid/add.hpp"
-template <typename AbelGroup, typename XY, bool SMALL_X = false>
+
+template <typename Monoid, typename XY, bool SMALL_X = false>
 struct Fenwick2D {
-  using E = typename AbelGroup::value_type;
+  using G = Monoid;
+  using E = typename G::value_type;
+  static_assert(G::commute);
   int N;
   vc<XY> keyX;
   XY min_X;
@@ -9,55 +12,41 @@ struct Fenwick2D {
   vc<XY> keyY;
   vc<E> dat;
 
-  Fenwick2D(vc<XY>& X, vc<XY>& Y, vc<E>& wt) { build(X, Y, wt); }
-
+  Fenwick2D(vc<XY>& X, vc<XY>& Y, vc<E> wt) { build(X, Y, wt); }
   Fenwick2D(vc<XY>& X, vc<XY>& Y) {
-    vc<E> wt(len(X), AbelGroup::unit());
+    vc<E> wt(len(X), G::unit());
     build(X, Y, wt);
   }
-
-  void reset_value() { fill(all(dat), AbelGroup::unit()); }
 
   inline int xtoi(XY x) {
     return (SMALL_X ? clamp<int>(x - min_X, 0, N) : LB(keyX, x));
   }
+  inline int nxt(int i) { return i + ((i + 1) & -(i + 1)); }
+  inline int prev(int i) { return i - ((i + 1) & -(i + 1)); }
 
-  inline int nxt(int i) {
-    i += 1;
-    return i + (i & -i) - 1;
-  }
-
-  inline int prev(int i) {
-    i += 1;
-    return i - (i & -i) - 1;
-  }
-
-  void build(vc<XY>& X, vc<XY>& Y, vc<E>& wt) {
+  void build(vc<XY>& X, vc<XY>& Y, vc<E> wt) {
     if (!SMALL_X) {
       keyX = X;
       UNIQUE(keyX);
-      N = len(keyX);
+      N = len(keyX), keyX.shrink_to_fit();
     } else {
       min_X = (len(X) == 0 ? 0 : MIN(X));
       N = (len(X) == 0 ? 0 : MAX(X)) - min_X + 1;
       keyX.resize(N);
       FOR(i, N) keyX[i] = min_X + i;
     }
-
     vvc<XY> keyY_raw(N);
-    vc<vc<E>> dat_raw(N);
-
-    auto I = argsort(Y);
-    for (auto&& i: I) {
+    vvc<E> dat_raw(N);
+    for (auto&& i: argsort(Y)) {
       int ix = xtoi(X[i]);
-      ll y = Y[i];
+      XY y = Y[i];
       while (ix < N) {
         auto& KY = keyY_raw[ix];
         if (len(KY) == 0 || KY.back() < y) {
           KY.eb(y);
           dat_raw[ix].eb(wt[i]);
         } else {
-          dat_raw[ix].back() = AbelGroup::op(dat_raw[ix].back(), wt[i]);
+          dat_raw[ix].back() = G::op(dat_raw[ix].back(), wt[i]);
         }
         ix = nxt(ix);
       }
@@ -76,59 +65,33 @@ struct Fenwick2D {
       FOR(j, n - 1) {
         int k = nxt(j);
         if (k < n)
-          dat[indptr[i] + k]
-              = AbelGroup::op(dat[indptr[i] + k], dat[indptr[i] + j]);
+          dat[indptr[i] + k] = G::op(dat[indptr[i] + k], dat[indptr[i] + j]);
       }
     }
   }
 
+  void add(XY x, XY y, E val) { multiply(x, y, val); }
   void multiply(XY x, XY y, E val) {
     int i = xtoi(x);
     assert(keyX[i] == x);
-    while (i < N) {
-      multiply_i(i, y, val);
-      i = nxt(i);
-    }
-  }
-
-  void add(XY x, XY y, E val) { multiply(x, y, val); }
-
-  E prod(XY lx, XY rx, XY ly, XY ry) {
-    E pos = AbelGroup::unit();
-    E neg = AbelGroup::unit();
-    int L = xtoi(lx) - 1;
-    int R = xtoi(rx) - 1;
-    while (L < R) {
-      pos = AbelGroup::op(pos, prod_i(R, ly, ry));
-      R = prev(R);
-    }
-    while (R < L) {
-      neg = AbelGroup::op(neg, prod_i(L, ly, ry));
-      L = prev(L);
-    }
-    E ret = AbelGroup::op(pos, AbelGroup::inverse(neg));
-    return ret;
-  }
-
-  E prefix_prod(XY rx, XY ry) {
-    E pos = AbelGroup::unit();
-    int R = xtoi(rx) - 1;
-    while (R >= 0) {
-      pos = AbelGroup::op(pos, prefix_prod_i(R, ry));
-      R = prev(R);
-    }
-    return pos;
+    while (i < N) { multiply_i(i, y, val), i = nxt(i); }
   }
 
   E sum(XY lx, XY rx, XY ly, XY ry) { return prod(lx, rx, ly, ry); }
+  E prod(XY lx, XY rx, XY ly, XY ry) {
+    E pos = G::unit(), E neg = G::unit();
+    int L = xtoi(lx) - 1, int R = xtoi(rx) - 1;
+    while (L < R) { pos = G::op(pos, prod_i(R, ly, ry)), R = prev(R); }
+    while (R < L) { neg = G::op(neg, prod_i(L, ly, ry)), L = prev(L); }
+    return G::op(pos, G::inverse(neg));
+  }
 
   E prefix_sum(XY rx, XY ry) { return prefix_prod(rx, ry); }
-
-  void debug() {
-    print("keyX", keyX);
-    print("indptr", indptr);
-    print("keyY", keyY);
-    print("dat", dat);
+  E prefix_prod(XY rx, XY ry) {
+    E pos = G::unit();
+    int R = xtoi(rx) - 1;
+    while (R >= 0) { pos = G::op(pos, prefix_prod_i(R, ry)), R = prev(R); }
+    return pos;
   }
 
 private:
@@ -136,40 +99,26 @@ private:
     int LID = indptr[i], n = indptr[i + 1] - indptr[i];
     auto it = keyY.begin() + LID;
     int j = lower_bound(it, it + n, y) - it;
-    assert(keyY[LID + j] == y);
-    while (j < n) {
-      dat[LID + j] = AbelGroup::op(dat[LID + j], val);
-      j = nxt(j);
-    }
+    while (j < n) { dat[LID + j] = G::op(dat[LID + j], val), j = nxt(j); }
   }
 
   E prod_i(int i, XY ly, XY ry) {
-    E pos = AbelGroup::unit();
-    E neg = AbelGroup::unit();
+    E pos = G::unit(), E neg = G::unit();
     int LID = indptr[i], n = indptr[i + 1] - indptr[i];
     auto it = keyY.begin() + LID;
     int L = lower_bound(it, it + n, ly) - it - 1;
     int R = lower_bound(it, it + n, ry) - it - 1;
-    while (L < R) {
-      pos = AbelGroup::op(pos, dat[LID + R]);
-      R = prev(R);
-    }
-    while (R < L) {
-      neg = AbelGroup::op(neg, dat[LID + L]);
-      L = prev(L);
-    }
-    return AbelGroup::op(pos, AbelGroup::inverse(neg));
+    while (L < R) { pos = G::op(pos, dat[LID + R]), R = prev(R); }
+    while (R < L) { neg = G::op(neg, dat[LID + L]), L = prev(L); }
+    return G::op(pos, G::inverse(neg));
   }
 
   E prefix_prod_i(int i, XY ry) {
-    E pos = AbelGroup::unit();
+    E pos = G::unit();
     int LID = indptr[i], n = indptr[i + 1] - indptr[i];
     auto it = keyY.begin() + LID;
     int R = lower_bound(it, it + n, ry) - it - 1;
-    while (R >= 0) {
-      pos = AbelGroup::op(pos, dat[LID + R]);
-      R = prev(R);
-    }
+    while (R >= 0) { pos = G::op(pos, dat[LID + R]), R = prev(R); }
     return pos;
   }
 };
--- ds/segtree/lazysegtree.hpp
+++ ds/segtree/lazysegtree.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-template <typename ActedMonoid>
+template <typename AM>
 struct LazySegTree {
-  using Monoid_X = typename ActedMonoid::Monoid_X;
-  using Monoid_A = typename ActedMonoid::Monoid_A;
-  static_assert(Monoid_X::commute);
-  using X = typename Monoid_X::value_type;
-  using A = typename Monoid_A::value_type;
+  using AM = AM;
+  using MX = typename AM::MX;
+  using MA = typename AM::MA;
+  static_assert(MX::commute);
+  using X = typename MX::value_type;
+  using A = typename MA::value_type;
   int n, log, size;
   vc<X> dat;
   vc<A> laz;
@@ -20,7 +21,7 @@ struct LazySegTree {
   LazySegTree(const vc<X>& v) { build(v); }
 
   void build(int m) {
-    build(m, [](int i) -> X { return Monoid_X::unit(); });
+    build(m, [](int i) -> X { return MX::unit(); });
   }
   void build(const vc<X>& v) {
     build(len(v), [&](int i) -> X { return v[i]; });
@@ -30,22 +31,22 @@ struct LazySegTree {
     n = m, log = 1;
     while ((1 << log) < n) ++log;
     size = 1 << log;
-    dat.assign(size << 1, Monoid_X::unit());
-    laz.assign(size, Monoid_A::unit());
+    dat.assign(size << 1, MX::unit());
+    laz.assign(size, MA::unit());
     FOR(i, n) dat[size + i] = f(i);
     FOR_R(i, 1, size) update(i);
   }
 
-  void update(int k) { dat[k] = Monoid_X::op(dat[2 * k], dat[2 * k + 1]); }
+  void update(int k) { dat[k] = MX::op(dat[2 * k], dat[2 * k + 1]); }
   void apply_at(int k, A a) {
     int sz = 1 << (log - topbit(k));
-    dat[k] = ActedMonoid::act(dat[k], a, sz);
-    if (k < size) laz[k] = Monoid_A::op(laz[k], a);
+    dat[k] = AM::act(dat[k], a, sz);
+    if (k < size) laz[k] = MA::op(laz[k], a);
   }
   void push(int k) {
     apply_at(2 * k, laz[k]);
     apply_at(2 * k + 1, laz[k]);
-    laz[k] = Monoid_A::unit();
+    laz[k] = MA::unit();
   }
   void set(int p, X x) {
     assert(0 <= p && p < n);
@@ -71,7 +72,7 @@ struct LazySegTree {
 
   X prod(int l, int r) {
     assert(0 <= l && l <= r && r <= n);
-    if (l == r) return Monoid_X::unit();
+    if (l == r) return MX::unit();
 
     l += size;
     r += size;
@@ -81,10 +82,10 @@ struct LazySegTree {
       if (((r >> i) << i) != r) push((r - 1) >> i);
     }
 
-    X x = Monoid_X::unit();
+    X x = MX::unit();
     while (l < r) {
-      if (l & 1) x = Monoid_X::op(x, dat[l++]);
-      if (r & 1) x = Monoid_X::op(x, dat[--r]);
+      if (l & 1) x = MX::op(x, dat[l++]);
+      if (r & 1) x = MX::op(x, dat[--r]);
       l >>= 1;
       r >>= 1;
     }
@@ -101,7 +102,7 @@ struct LazySegTree {
     l += size;
     r += size;
 
-    if (!Monoid_A::commute) {
+    if (!MA::commute) {
       for (int i = log; i >= 1; i--) {
         if (((l >> i) << i) != l) push(l >> i);
         if (((r >> i) << i) != r) push((r - 1) >> i);
@@ -129,25 +130,25 @@ struct LazySegTree {
   template <typename F>
   int max_right(const F check, int l) {
     assert(0 <= l && l <= n);
-    assert(check(Monoid_X::unit()));
+    assert(check(MX::unit()));
     if (l == n) return n;
     l += size;
     for (int i = log; i >= 1; i--) push(l >> i);
-    X sm = Monoid_X::unit();
+    X sm = MX::unit();
     do {
       while (l % 2 == 0) l >>= 1;
-      if (!check(Monoid_X::op(sm, dat[l]))) {
+      if (!check(MX::op(sm, dat[l]))) {
         while (l < size) {
           push(l);
           l = (2 * l);
-          if (check(Monoid_X::op(sm, dat[l]))) {
-            sm = Monoid_X::op(sm, dat[l]);
+          if (check(MX::op(sm, dat[l]))) {
+            sm = MX::op(sm, dat[l]);
             l++;
           }
         }
         return l - size;
       }
-      sm = Monoid_X::op(sm, dat[l]);
+      sm = MX::op(sm, dat[l]);
       l++;
     } while ((l & -l) != l);
     return n;
@@ -156,26 +157,26 @@ struct LazySegTree {
   template <typename F>
   int min_left(const F check, int r) {
     assert(0 <= r && r <= n);
-    assert(check(Monoid_X::unit()));
+    assert(check(MX::unit()));
     if (r == 0) return 0;
     r += size;
     for (int i = log; i >= 1; i--) push((r - 1) >> i);
-    X sm = Monoid_X::unit();
+    X sm = MX::unit();
     do {
       r--;
       while (r > 1 && (r % 2)) r >>= 1;
-      if (!check(Monoid_X::op(dat[r], sm))) {
+      if (!check(MX::op(dat[r], sm))) {
         while (r < size) {
           push(r);
           r = (2 * r + 1);
-          if (check(Monoid_X::op(dat[r], sm))) {
-            sm = Monoid_X::op(dat[r], sm);
+          if (check(MX::op(dat[r], sm))) {
+            sm = MX::op(dat[r], sm);
             r--;
           }
         }
         return r + 1 - size;
       }
-      sm = Monoid_X::op(dat[r], sm);
+      sm = MX::op(dat[r], sm);
     } while ((r & -r) != r);
     return 0;
   }
