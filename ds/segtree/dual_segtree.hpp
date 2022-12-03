b206a1659f574d6d5dbb420548b76fa798911cc7
maspypy <maspypy.gmail.com>
2022-12-03 10:58:25 +0900

    empty

--- ds/segtree/dual_segtree.hpp
+++ ds/segtree/dual_segtree.hpp
@@ -1,24 +1,22 @@
 #pragma once
 
 template <typename Monoid>
-struct DualSegTree {
-  using A = typename Monoid::value_type;
+struct Dual_SegTree {
+  using MA = Monoid;
+  using A = typename MA::value_type;
   int n, log, size;
   vc<A> laz;
 
-  DualSegTree() : DualSegTree(0) {}
-  DualSegTree(int n) { resize(n); }
+  Dual_SegTree() : DualSegTree(0) {}
+  Dual_SegTree(int n) { build(n); }
 
-  void reset() { fill(all(laz), Monoid::unit()); }
-  void resize(int n_) {
-    n = n_;
+  void build(int m) {
+    n = m;
     log = 1;
     while ((1 << log) < n) ++log;
     size = 1 << log;
-    laz.assign(size << 1, Monoid::unit());
+    laz.assign(size << 1, MA::unit());
   }
-
-  void all_apply(int k, A a) { laz[k] = Monoid::op(laz[k], a); }
 
   A get(int p) {
     assert(0 <= p && p < n);
@@ -32,38 +30,28 @@ struct DualSegTree {
     return {laz.begin() + size, laz.begin() + size + n};
   }
 
-  void apply(int l, int r, A a) {
+  void apply(int l, int r, const A& a) {
     assert(0 <= l && l <= r && r <= n);
     if (l == r) return;
-
-    l += size;
-    r += size;
-
-    if (!Monoid::commute) {
+    l += size, r += size;
+    if (!MA::commute) {
       for (int i = log; i >= 1; i--) {
         if (((l >> i) << i) != l) push(l >> i);
         if (((r >> i) << i) != r) push((r - 1) >> i);
       }
     }
-
-    {
-      int l2 = l, r2 = r;
-      while (l < r) {
-        if (l & 1) all_apply(l++, a);
-        if (r & 1) all_apply(--r, a);
-        l >>= 1;
-        r >>= 1;
-      }
-      l = l2;
-      r = r2;
+    while (l < r) {
+      if (l & 1) all_apply(l++, a);
+      if (r & 1) all_apply(--r, a);
+      l >>= 1, r >>= 1;
     }
   }
-  void debug() { print("dualsegtree getall:", get_all()); }
 
 private:
   void push(int k) {
-    all_apply(2 * k, laz[k]);
-    all_apply(2 * k + 1, laz[k]);
-    laz[k] = Monoid::unit();
+    if (laz[k] == MA::unit()) return;
+    all_apply(2 * k, laz[k]), all_apply(2 * k + 1, laz[k]);
+    laz[k] = MA::unit();
   }
+  void all_apply(int k, A a) { laz[k] = MA::op(laz[k], a); }
 };
