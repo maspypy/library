7b0ac093168994140693679cd5293b90432da570
maspypy <maspypy.gmail.com>
2022-12-03 10:36:40 +0900

    empty

--- ds/segtree/segtree_2d.hpp
+++ ds/segtree/segtree_2d.hpp
@@ -1,28 +1,21 @@
 template <typename Monoid, typename XY, bool SMALL = false>
 struct SegTree2D {
-  using S = typename Monoid::value_type;
-  int N;
-  int full_N;
-  vc<XY> keyX;
-  int min_X;
+  using MX = Monoid;
+  using S = typename MX::value_type;
+  static_assert(MX::commute);
+  int N, min_X;
+  vc<XY> keyX, keyY;
   vc<int> indptr;
-  vc<XY> keyY;
   vc<S> dat;
 
-  inline int xtoi(XY x) {
-    if (SMALL) return clamp<XY>(x - min_X, 0, N);
-    return LB(keyX, x);
-  }
-
   SegTree2D(vc<XY>& X, vc<XY>& Y, vc<S> wt = vc<S>()) {
-    if (len(wt) == 0) wt = vc<S>(len(X), Monoid::unit());
-    assert(Monoid::commute); // 可換モノイドのみ
+    if (len(wt) == 0) wt = vc<S>(len(X), MX::unit());
     assert(len(X) == len(Y) && len(X) == len(wt));
-
     if (!SMALL) {
       keyX = X;
       UNIQUE(keyX);
       N = len(keyX);
+      keyX.shrink_to_fit();
     } else {
       min_X = (len(X) == 0 ? 0 : MIN(X));
       N = (len(X) == 0 ? 0 : MAX(X)) - min_X + 1;
@@ -30,11 +23,9 @@ struct SegTree2D {
       FOR(i, N) keyX[i] = min_X + i;
     }
 
-    vc<vc<XY>> keyY_raw(N + N);
-    vc<vc<S>> dat_raw(N + N);
-
-    auto I = argsort(Y);
-    for (auto&& i: I) {
+    vvc<XY> keyY_raw(N + N);
+    vvc<S> dat_raw(N + N);
+    for (auto&& i: argsort(Y)) {
       int ix = xtoi(X[i]), y = Y[i];
       ix += N;
       while (ix) {
@@ -43,7 +34,7 @@ struct SegTree2D {
           KY.eb(y);
           dat_raw[ix].eb(wt[i]);
         } else {
-          dat_raw[ix].back() = Monoid::op(dat_raw[ix].back(), wt[i]);
+          dat_raw[ix].back() = MX::op(dat_raw[ix].back(), wt[i]);
         }
         ix >>= 1;
       }
@@ -52,30 +43,16 @@ struct SegTree2D {
     indptr.assign(N + N + 1, 0);
     FOR(i, N + N) indptr[i + 1] = indptr[i] + len(keyY_raw[i]);
     int full_N = indptr.back();
-    keyY.resize(full_N);
-    dat.assign(2 * full_N, Monoid::unit());
+    keyY.resize(full_N), dat.assign(2 * full_N, MX::unit());
     FOR(i, N + N) {
       int off = 2 * indptr[i], n = indptr[i + 1] - indptr[i];
       FOR(j, n) {
         keyY[indptr[i] + j] = keyY_raw[i][j];
         dat[off + n + j] = dat_raw[i][j];
       }
-      FOR3_R(j, 1, n)
-      dat[off + j] = Monoid::op(dat[off + 2 * j + 0], dat[off + 2 * j + 1]);
-    }
-  }
-
-  void multiply_i(int i, XY y, S val) {
-    int LID = indptr[i], n = indptr[i + 1] - indptr[i];
-    auto it = keyY.begin() + LID;
-    int j = lower_bound(it, it + n, y) - it;
-    assert(keyY[LID + j] == y);
-    int off = 2 * LID;
-    j += n;
-
-    while (j) {
-      dat[off + j] = Monoid::op(dat[off + j], val);
-      j >>= 1;
+      FOR_R(j, 1, n) {
+        dat[off + j] = MX::op(dat[off + 2 * j], dat[off + 2 * j + 1]);
+      }
     }
   }
 
@@ -83,49 +60,48 @@ struct SegTree2D {
     int i = xtoi(x);
     assert(keyX[i] == x);
     i += N;
-    while (i) {
-      multiply_i(i, y, val);
-      i >>= 1;
-    }
-  }
-
-  S prod_i(int i, XY ly, XY ry) {
-    int LID = indptr[i], n = indptr[i + 1] - indptr[i];
-    auto it = keyY.begin() + LID;
-    int L = lower_bound(it, it + n, ly) - it;
-    int R = lower_bound(it, it + n, ry) - it;
-    int off = 2 * LID;
-    L += n;
-    R += n;
-    S val = Monoid::unit();
-    while (L < R) {
-      if (L & 1) val = Monoid::op(val, dat[off + (L++)]);
-      if (R & 1) val = Monoid::op(dat[off + (--R)], val);
-      L >>= 1;
-      R >>= 1;
-    }
-    return val;
+    while (i) { multiply_i(i, y, val), i >>= 1; }
   }
 
   S prod(XY lx, XY rx, XY ly, XY ry) {
-    int L = xtoi(lx);
-    int R = xtoi(rx);
-    L += N;
-    R += N;
-    S val = Monoid::unit();
+    int L = xtoi(lx) + N, R = xtoi(rx) + N;
+    S val = MX::unit();
     while (L < R) {
-      if (L & 1) val = Monoid::op(val, prod_i(L++, ly, ry));
-      if (R & 1) val = Monoid::op(prod_i(--R, ly, ry), val);
-      L >>= 1;
-      R >>= 1;
+      if (L & 1) val = MX::op(val, prod_i(L++, ly, ry));
+      if (R & 1) val = MX::op(prod_i(--R, ly, ry), val);
+      L >>= 1, R >>= 1;
     }
     return val;
   }
 
-  void debug() {
-    print("keyX", keyX);
-    print("indptr", indptr);
-    print("keyY", keyY);
-    print("dat", dat);
+private:
+  inline int xtoi(XY x) {
+    return (SMALL ? clamp<XY>(x - min_X, 0, N) : LB(keyX, x));
+  }
+  S prod_i(int i, XY ly, XY ry) {
+    int LID = indptr[i], n = indptr[i + 1] - indptr[i];
+    auto it = keyY.begin() + LID;
+    int L = lower_bound(it, it + n, ly) - it + n;
+    int R = lower_bound(it, it + n, ry) - it + n;
+    int off = 2 * LID;
+    S val = MX::unit();
+    while (L < R) {
+      if (L & 1) val = MX::op(val, dat[off + (L++)]);
+      if (R & 1) val = MX::op(dat[off + (--R)], val);
+      L >>= 1, R >>= 1;
+    }
+    return val;
+  }
+  void multiply_i(int i, XY y, S val) {
+    int LID = indptr[i], n = indptr[i + 1] - indptr[i];
+    auto it = keyY.begin() + LID;
+    int j = lower_bound(it, it + n, y) - it;
+    assert(keyY[LID + j] == y);
+    int off = 2 * LID;
+    j += n;
+    while (j) {
+      dat[off + j] = MX::op(dat[off + j], val);
+      j >>= 1;
+    }
   }
 };
--- ds/segtree/segtree_2d_dense.hpp
+++ ds/segtree/segtree_2d_dense.hpp
@@ -1,76 +1,68 @@
 #pragma once
 
-// https://codeforces.com/contest/1301/problem/E
 template <class Monoid>
-struct SegTree2D_dense {
-  using X = typename Monoid::value_type;
+struct SegTree2D_Dense {
+  using MX = Monoid;
+  using X = typename MX::value_type;
   using value_type = X;
+  static_assert(MX::commute);
   vc<X> dat;
   int H, W;
 
-  int get_idx(int x, int y) { return x * 2 * W + y; }
-
-  SegTree2D_dense() : SegTree2D_dense(0, 0) {}
-  SegTree2D_dense(int H, int W) : H(H), W(W), dat(4 * H * W, Monoid::unit()) {}
-  SegTree2D_dense(vc<vc<X>> &v) {
-    H = len(v);
-    W = (H == 0 ? 0 : len(v[0]));
-    dat.assign(4 * H * W, Monoid::unit());
-    FOR(x, H) FOR(y, W) { dat[get_idx(H + x, W + y)] = v[x][y]; }
-    FOR3(y, W, W + W) FOR_R(x, H) {
-      dat[get_idx(x, y)]
-          = Monoid::op(dat[get_idx(2 * x + 0, y)], dat[get_idx(2 * x + 1, y)]);
+  SegTree2D_Dense() : SegTree2D_Dense(0, 0) {}
+  SegTree2D_Dense(int H, int W) : H(H), W(W), dat(4 * H * W, MX::unit()) {}
+  SegTree2D_Dense(vc<vc<X>> &v) {
+    H = len(v), W = (H == 0 ? 0 : len(v[0]));
+    dat.assign(4 * H * W, MX::unit());
+    FOR(x, H) FOR(y, W) { dat[idx(H + x, W + y)] = v[x][y]; }
+    FOR(y, W, W + W) FOR_R(x, H) {
+      dat[idx(x, y)] = MX::op(dat[idx(2 * x + 0, y)], dat[idx(2 * x + 1, y)]);
     }
-    FOR(x, H + H) {
-      FOR_R(y, W) {
-        dat[get_idx(x, y)] = Monoid::op(dat[get_idx(x, 2 * y + 0)],
-                                        dat[get_idx(x, 2 * y + 1)]);
-      }
+    FOR(x, H + H) FOR(y, W) {
+      dat[idx(x, y)] = MX::op(dat[idx(x, 2 * y + 0)], dat[idx(x, 2 * y + 1)]);
     }
   }
 
   void set(int x, int y, X e) {
     x += H, y += W;
-    dat[get_idx(x, y)] = e;
+    dat[idx(x, y)] = e;
     int i = x;
     while (i >>= 1) {
-      dat[get_idx(i, y)]
-          = Monoid::op(dat[get_idx(2 * i + 0, y)], dat[get_idx(2 * i + 1, y)]);
+      dat[idx(i, y)] = MX::op(dat[idx(2 * i + 0, y)], dat[idx(2 * i + 1, y)]);
     }
     i = x;
     while (i) {
       int j = y;
       while (j >>= 1) {
-        dat[get_idx(i, j)] = Monoid::op(dat[get_idx(i, 2 * j + 0)],
-                                        dat[get_idx(i, 2 * j + 1)]);
+        dat[idx(i, j)] = MX::op(dat[idx(i, 2 * j + 0)], dat[idx(i, 2 * j + 1)]);
       }
       i >>= 1;
     }
   }
 
-  X prod_at(int x, int yl, int yr) {
-    X res = Monoid::unit();
-    yl += W, yr += W;
-    while (yl < yr) {
-      if (yl & 1) res = Monoid::op(res, dat[get_idx(x, yl++)]);
-      if (yr & 1) res = Monoid::op(res, dat[get_idx(x, --yr)]);
-      yl >>= 1, yr >>= 1;
-    }
-    return res;
-  }
-
   X prod(int xl, int xr, int yl, int yr) {
     assert(0 <= xl && xl <= xr && xr <= H);
     assert(0 <= yl && yl <= yr && yr <= W);
-    X res = Monoid::unit();
+    X res = MX::unit();
     xl += H, xr += H;
     while (xl < xr) {
-      if (xl & 1) res = Monoid::op(res, prod_at(xl++, yl, yr));
-      if (xr & 1) res = Monoid::op(res, prod_at(--xr, yl, yr));
+      if (xl & 1) res = MX::op(res, prod_x(xl++, yl, yr));
+      if (xr & 1) res = MX::op(res, prod_x(--xr, yl, yr));
       xl >>= 1, xr >>= 1;
     }
     return res;
   }
 
-  void debug() { print("segtree", dat); }
+private:
+  inline int idx(int x, int y) { return x * 2 * W + y; }
+  X prod_x(int x, int yl, int yr) {
+    X res = MX::unit();
+    yl += W, yr += W;
+    while (yl < yr) {
+      if (yl & 1) res = MX::op(res, dat[idx(x, yl++)]);
+      if (yr & 1) res = MX::op(res, dat[idx(x, --yr)]);
+      yl >>= 1, yr >>= 1;
+    }
+    return res;
+  }
 };
