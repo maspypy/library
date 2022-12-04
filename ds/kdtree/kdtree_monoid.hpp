dcdeb9986cf70f5e06ed1dc0b58cebe7515147af
maspypy <maspypy.gmail.com>
2022-12-05 02:37:00 +0900

    kdt mono

--- ds/kdtree/kdtree_monoid.hpp
+++ ds/kdtree/kdtree_monoid.hpp
@@ -3,11 +3,14 @@ struct KDTree_Monoid {
   using MX = Monoid;
   using X = typename MX::value_type;
   static_assert(MX::commute);
+
+  // 小数も考慮すると、閉で持つ設計方針になる。ただし、クエリはいつもの半開を使う
   vc<tuple<XY, XY, XY, XY>> closed_range;
   vc<X> dat;
   int n;
 
   KDTree_Monoid(vc<XY> xs, vc<XY> ys, vc<X> vs) : n(len(xs)) {
+    assert(n > 0);
     int log = 0;
     while ((1 << log) < n) ++log;
     dat.resize(1 << (log + 1));
@@ -26,14 +29,14 @@ struct KDTree_Monoid {
   X prod_all() { return dat[1]; }
 
 private:
-  void build(int idx, vc<XY>& xs, vc<XY>& ys, vc<X>& vs, bool divx = true) {
+  void build(int idx, vc<XY> xs, vc<XY> ys, vc<X> vs, bool divx = true) {
     int n = len(xs);
     auto& [xmin, xmax, ymin, ymax] = closed_range[idx];
     xmin = ymin = numeric_limits<XY>::max();
     xmax = ymax = numeric_limits<XY>::lowest();
 
     FOR(i, n) {
-      XY x = xs[i], y = ys[i];
+      auto x = xs[i], y = ys[i];
       chmin(xmin, x), chmax(xmax, x), chmin(ymin, y), chmax(ymax, y);
     }
     if (xmin == xmax && ymin == ymax) {
@@ -48,42 +51,47 @@ private:
     iota(all(I), 0);
     if (divx) {
       nth_element(I.begin(), I.begin() + m, I.end(),
-                  [&xs](int i, int j) { return xs[i] < xs[j]; });
+                  [xs](int i, int j) { return xs[i] < xs[j]; });
     } else {
       nth_element(I.begin(), I.begin() + m, I.end(),
-                  [&ys](int i, int j) { return ys[i] < ys[j]; });
+                  [ys](int i, int j) { return ys[i] < ys[j]; });
     }
-    vc<XY> lxs, rxs, lys, rys;
-    vc<X> lvs, rvs;
-    FOR(k, m) { lxs.eb(xs[I[k]]), lys.eb(ys[I[k]]), lvs.eb(vs[I[k]]); }
-    FOR(k, m, n) { rxs.eb(xs[I[k]]), rys.eb(ys[I[k]]), rvs.eb(vs[I[k]]); }
-    build(2 * idx + 0, lxs, lys, lvs, !divx);
-    build(2 * idx + 1, rxs, rys, rvs, !divx);
+    xs = rearrange(xs, I), ys = rearrange(ys, I), vs = rearrange(vs, I);
+    build(2 * idx + 0, {xs.begin(), xs.begin() + m},
+          {ys.begin(), ys.begin() + m}, {vs.begin(), vs.begin() + m}, !divx);
+    build(2 * idx + 1, {xs.begin() + m, xs.end()}, {ys.begin() + m, ys.end()},
+          {vs.begin() + m, vs.end()}, !divx);
     dat[idx] = MX::op(dat[2 * idx + 0], dat[2 * idx + 1]);
   }
 
-  bool isin(const int& idx, const XY& x, const XY& y) {
+  inline bool is_leaf(int idx) {
+    auto& [xmin, xmax, ymin, ymax] = closed_range[idx];
+    return xmin == xmax && ymin == ymax;
+  }
+
+  inline bool isin(XY x, XY y, int idx) {
     auto& [xmin, xmax, ymin, ymax] = closed_range[idx];
     return (xmin <= x && x <= xmax && ymin <= y && y <= ymax);
   }
 
-  void multiply_rec(const int& idx, const XY& x, const XY& y, const X& v) {
-    if (!isin(idx, x, y)) return;
-    auto& [xmin, xmax, ymin, ymax] = closed_range[idx];
-    if (xmin == xmax && ymin == ymax) {
+  bool multiply_rec(int idx, XY x, XY y, X v) {
+    if (!isin(x, y, idx)) return false;
+    if (is_leaf(idx)) {
       dat[idx] = MX::op(dat[idx], v);
-      return;
+      return true;
     }
-    multiply_rec(2 * idx + 0, x, y, v);
-    multiply_rec(2 * idx + 1, x, y, v);
-    dat[idx] = MX::op(dat[2 * idx + 0], dat[2 * idx + 1]);
+    bool done = 0;
+    if (multiply_rec(2 * idx + 0, x, y, v)) done = 1;
+    if (!done && multiply_rec(2 * idx + 1, x, y, v)) done = 1;
+    if (done) { dat[idx] = MX::op(dat[2 * idx + 0], dat[2 * idx + 1]); }
+    return done;
   }
 
   X prod_rec(int idx, XY x1, XY x2, XY y1, XY y2) {
     auto& [xmin, xmax, ymin, ymax] = closed_range[idx];
     if (x2 <= xmin || xmax < x1) return MX::unit();
     if (y2 <= ymin || ymax < y1) return MX::unit();
-    if (x1 <= xmin && xmax < x2 && y1 <= ymin && ymax < y2) return dat[idx];
+    if (x1 <= xmin && xmax < x2 && y1 <= ymin && ymax < y2) { return dat[idx]; }
     return MX::op(prod_rec(2 * idx + 0, x1, x2, y1, y2),
                   prod_rec(2 * idx + 1, x1, x2, y1, y2));
   }
