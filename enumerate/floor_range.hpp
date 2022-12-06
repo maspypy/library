43a623335ad5700a251718feaa4b0b9edb02df64
maspypy <maspypy.gmail.com>
2022-12-06 21:05:59 +0900

    empty

--- enumerate/floor_range.hpp
+++ enumerate/floor_range.hpp
@@ -1,13 +1,11 @@
 // f(q, l, r)：商が q になる範囲が [l, r)
 template <typename F>
 void floor_range(ll N, F f) {
-  ll q = 1;
-  ll r = N + 1;
+  ll q = 1, r = N + 1;
   while (1) {
     ll l = N / (q + 1) + 1;
     f(q, l, r);
     if (q == N) break;
-    r = l;
-    q = N / (l - 1);
+    r = l, q = N / (l - 1);
   }
 }
