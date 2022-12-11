83c8d7da3b1fd780006c8b3c33841f9181362b07
maspypy <maspypy.gmail.com>
2022-12-11 11:09:59 +0900

    binary trie

--- test/library_checker/datastructure/set_xor_min.test.cpp
+++ test/library_checker/datastructure/set_xor_min.test.cpp
@@ -1,21 +1,35 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/set_xor_min"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "ds/binarytrie/binarytrie.hpp"
+#include "ds/binarytrie.hpp"
+#include "ds/hashmap.hpp"
 
 void solve() {
-  LL(Q);
-  BinaryTrie<30> trie;
+  HashMapLL<bool, 20> MP;
+  Binary_Trie<30, false, 1'000'000, int, int> X;
+  using np = decltype(X)::np;
+  np root = nullptr;
+
+  INT(Q);
   FOR(Q) {
-    LL(t, x);
-    if (t == 0 && trie.count(x) == 0) trie.add(x, 1);
-    if (t == 1 && trie.count(x) == 1) trie.add(x, -1);
-    if (t == 2) { print(trie.min(x)); }
+    INT(t, x);
+    if (t == 0) {
+      if (MP[x] == 0) {
+        MP[x] = 1;
+        root = X.add(root, x, 1);
+      }
+    }
+    if (t == 1) {
+      if (MP[x] == 1) {
+        MP[x] = 0;
+        root = X.add(root, x, -1);
+      }
+    }
+    if (t == 2) { print(X.min(root, x)); }
   }
 }
 
 signed main() {
   solve();
-
   return 0;
 }
---
+++ test/mytest/binary_trie.test.cpp
@@ -0,0 +1,55 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/binarytrie.hpp"
+#include "random/base.hpp"
+
+void test() {
+  vector<int> A;
+  const int LOG = 5;
+  Binary_Trie<LOG, false, 100> X;
+  using np = decltype(X)::np;
+  np root = nullptr;
+
+  FOR(1000) {
+    int t = RNG(0, 4);
+    if (t == 0) { // add
+      int x = RNG(0, 1 << LOG);
+      A.eb(x);
+      root = X.add(root, x);
+    }
+    if (t == 1) { // get all
+      sort(all(A));
+      vc<int> B;
+      for (auto&& [k, cnt]: X.get_all(root)) { FOR(cnt) B.eb(k); }
+      assert(A == B);
+    }
+    if (t == 2 && len(A)) { // erase
+      int k = RNG(len(A));
+      int x = A[k];
+      swap(A[k], A.back());
+      A.pop_back();
+      X.add(root, x, -1);
+    }
+    if (t == 3 && len(A)) { // kth
+      int k = RNG(len(A));
+      int xor_val = RNG(0, 1 << LOG);
+      vc<int> B;
+      for (auto&& x: A) B.eb(x ^ xor_val);
+      sort(all(B));
+      assert(B[k] == int(X.kth(root, k, xor_val)));
+    }
+  }
+}
+
+void solve() {
+  INT(a, b);
+  print(a + b);
+}
+
+signed main() {
+  test();
+  solve();
+
+  return 0;
+}
