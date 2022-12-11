94c643d29b8de0fa88e15123083ae863798e08e8
maspypy <maspypy.gmail.com>
2022-12-12 00:25:50 +0900

    freq

--- test/mytest/binary_trie.test.cpp
+++ test/mytest/binary_trie.test.cpp
@@ -5,39 +5,54 @@
 #include "random/base.hpp"
 
 void test() {
-  vector<int> A;
   const int LOG = 5;
   Binary_Trie<LOG, false, 100> X;
-  using np = decltype(X)::np;
-  np root = nullptr;
 
-  FOR(1000) {
-    int t = RNG(0, 4);
-    if (t == 0) { // add
-      int x = RNG(0, 1 << LOG);
-      A.eb(x);
-      root = X.add(root, x);
-    }
-    if (t == 1) { // get all
-      sort(all(A));
-      vc<int> B;
-      for (auto&& [k, cnt]: X.get_all(root)) { FOR(cnt) B.eb(k); }
-      assert(A == B);
-    }
-    if (t == 2 && len(A)) { // erase
-      int k = RNG(len(A));
-      int x = A[k];
-      swap(A[k], A.back());
-      A.pop_back();
-      X.add(root, x, -1);
-    }
-    if (t == 3 && len(A)) { // kth
-      int k = RNG(len(A));
-      int xor_val = RNG(0, 1 << LOG);
-      vc<int> B;
-      for (auto&& x: A) B.eb(x ^ xor_val);
-      sort(all(B));
-      assert(B[k] == int(X.kth(root, k, xor_val)));
+  FOR(100) {
+    vector<int> A;
+    X.reset();
+    auto root = X.new_root();
+    FOR(1000) {
+      int t = RNG(0, 5);
+      if (t == 0) { // add
+        int x = RNG(0, 1 << LOG);
+        A.eb(x);
+        root = X.add(root, x);
+      }
+      if (t == 1) { // get all
+        sort(all(A));
+        vc<int> B;
+        for (auto&& [k, cnt]: X.get_all(root)) { FOR(cnt) B.eb(k); }
+        assert(A == B);
+      }
+      if (t == 2 && len(A)) { // erase
+        int k = RNG(len(A));
+        int x = A[k];
+        swap(A[k], A.back());
+        A.pop_back();
+        X.add(root, x, -1);
+      }
+      if (t == 3 && len(A)) { // kth
+        int k = RNG(len(A));
+        int xor_val = RNG(0, 1 << LOG);
+        vc<int> B;
+        for (auto&& x: A) B.eb(x ^ xor_val);
+        sort(all(B));
+        assert(B[k] == int(X.kth(root, k, xor_val)));
+      }
+      if (t == 4) { // freq
+        int lo = RNG(0, 1 << LOG);
+        int hi = RNG(0, 1 << LOG);
+        int xor_val = RNG(0, 1 << LOG);
+        if (lo > hi) swap(lo, hi);
+        ++hi;
+        int cnt = 0;
+        for (auto&& x: A) {
+          int y = x ^ xor_val;
+          if (lo <= y && y < hi) ++cnt;
+        }
+        assert(cnt == X.freq(root, lo, hi, xor_val));
+      }
     }
   }
 }
