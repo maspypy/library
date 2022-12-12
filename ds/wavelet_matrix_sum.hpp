95902587dc5750bfdc90409f5ff872b0c5bc0233
maspypy <maspypy.gmail.com>
2022-12-13 08:55:33 +0900

    wm

--- ds/wavelet_matrix_sum.hpp
+++ ds/wavelet_matrix_sum.hpp
@@ -1,142 +1,185 @@
+#include "ds/bit_vector.hpp"
 #include "alg/monoid/add.hpp"
 
-// Wavelet Matrix 上でさらに累積和を管理して、矩形和がとれるようにしたもの
-// 和は、変換関数 f を渡して、f(x) の可換群に関する和
-template <typename T, typename Monoid = Monoid_Add<T>>
+// 座圧するかどうかを COMPRESS で指定する
+// xor 的な使い方をする場合には、コンストラクタで log を渡すこと
+template <typename T, bool COMPRESS, typename Monoid = Monoid_Add<T>>
 struct Wavelet_Matrix_Sum {
-  using G = Monoid;
-  using GT = typename G::value_type;
-  static_assert(G::commute);
-  struct BitVector {
-    vector<u64> buf;
-    vector<int> sum;
-    BitVector(const vector<char>& a = {}) {
-      int n = a.size();
-      buf.assign((n + 63) >> 6, 0);
-      sum.assign(buf.size() + 1, 0);
-      FOR(i, n) if (a[i]) {
-        buf[i >> 6] |= 1ull << (i & 63);
-        sum[(i >> 6) + 1]++;
-      }
-      FOR(i, buf.size()) sum[i + 1] += sum[i];
-    }
-    int rank(int k, bool f = 1) {
-      int ret = sum[k >> 6]
-                + __builtin_popcountll(buf[k >> 6] & ((1ull << (k & 63)) - 1));
-      if (!f)
-        return k - ret;
-      else
-        return ret;
-    }
-  };
-
+  using MX = Monoid;
+  using X = typename MX::value_type;
   int N, lg;
   vector<int> mid;
-  vector<BitVector> bv;
-  vector<vector<GT>> cumsum;
+  vector<Bit_Vector> bv;
   vc<T> key;
+  const T INF;
+  const bool set_log;
+  vvc<X> cumsum;
 
-  template <typename F>
-  Wavelet_Matrix_Sum(
-      vector<T>& dat, F f = [](T x) -> GT { return x; })
-      : N(dat.size()) {
-    key = dat;
-    UNIQUE(key);
-    vc<int> A(N);
-    FOR(i, N) A[i] = LB(key, dat[i]);
-    lg = __lg(max(MAX(A), 1)) + 1;
+  Wavelet_Matrix_Sum(vc<T> A, int log = -1)
+      : Wavelet_Matrix_Sum(
+          A, [](int a) -> X { return a; }, log) {}
 
-    mid.resize(lg);
-    bv.resize(lg);
-    cumsum.resize(lg);
-    for (int d = lg - 1; d >= 0; d--) {
-      vector<char> add;
-      vector nxt(2, vector<int>());
-      for (auto& x: A) {
-        add.push_back(x >> d & 1);
-        nxt[x >> d & 1].push_back(x);
+  template <typename FUNC>
+  Wavelet_Matrix_Sum(FUNC F, vector<T> A, int log = -1)
+      : N(len(A)), lg(log), INF(numeric_limits<T>::max()), set_log(log != -1) {
+    if (COMPRESS) {
+      assert(!set_log);
+      key.reserve(N);
+      vc<int> I = argsort(A);
+      for (auto&& i: I) {
+        if (key.empty() || key.back() != A[i]) key.eb(A[i]);
+        A[i] = len(key) - 1;
       }
-      mid[d] = (int)nxt[0].size();
-      bv[d] = BitVector(add);
-      swap(A, nxt[0]);
-      A.insert(A.end(), all(nxt[1]));
-      vc<GT> cs(N + 1);
-      cs[0] = G::unit();
-      FOR(i, N) cs[i + 1] = G::op(cs[i], f(key[A[i]]));
-      cumsum[d] = cs;
+      key.shrink_to_fit();
+    }
+    if (lg == -1) lg = __lg(max(MAX(A), 1)) + 1;
+    mid.resize(lg);
+    bv.assign(lg, Bit_Vector(N));
+    cumsum.assign(1 + lg, vc<X>(N + 1, MX::unit()));
+    vc<T> A0(N), A1(N);
+    FOR_R(d, -1, lg) {
+      int p0 = 0, p1 = 0;
+      FOR(i, N) {
+        X x = F(COMPRESS ? key[A[i]] : A[i]);
+        cumsum[d + 1][i + 1] = MX::op(cumsum[d + 1][i], x);
+      }
+      if (d == -1) break;
+      FOR(i, N) {
+        bool f = (A[i] >> d & 1);
+        if (!f) A0[p0++] = A[i];
+        if (f) bv[d].set(i), A1[p1++] = A[i];
+      }
+      mid[d] = p0;
+      bv[d].build();
+      swap(A, A0);
+      FOR(i, p1) A[p0 + i] = A1[i];
     }
   }
 
-  // [L, R) 内にある [a, b) を数える
-  int freq(int L, int R, T a, T b) {
-    return freq_upper(L, R, b) - freq_upper(L, R, a);
+  // xor した結果が [a, b) に収まるものを数える
+  // 個数および和を返す
+  pair<int, X> count(int L, int R, T a, T b, T xor_val = 0) {
+    auto [c1, s1] = count_prefix(L, R, a, xor_val);
+    auto [c2, s2] = count_prefix(L, R, b, xor_val);
+    return {c2 - c1, MX::op(MX::inverse(s1), s2)};
   }
-  int freq_upper(int L, int R, T t) {
-    int x = LB(key, t);
-    if (x >= (1 << lg)) return R - L;
-    int ret = 0;
-    for (int h = lg - 1; h >= 0; --h) {
-      bool f = (x >> h) & 1;
-      if (f) ret += bv[h].rank(R, 0) - bv[h].rank(L, 0);
-      L = bv[h].rank(L, f) + (f ? mid[h] : 0);
-      R = bv[h].rank(R, f) + (f ? mid[h] : 0);
+
+  // xor した結果が [0, x) に収まるものを数える
+  // 個数および和を返す
+  pair<int, X> count_prefix(int L, int R, T x, T xor_val = 0) {
+    if (xor_val != 0) assert(set_log);
+    x = (COMPRESS ? LB(key, x) : x);
+    if (x >= (1 << lg)) return {R - L, get(lg, L, R)};
+    int cnt = 0;
+    X sm = MX::unit();
+    FOR_R(d, lg) {
+      bool add = (x >> d) & 1;
+      bool f = ((xor_val) >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int kf = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      if (add) {
+        cnt += kf;
+        if (f) {
+          sm = MX::op(sm, get(d, L + mid[d] - l0, R + mid[d] - r0));
+          L = l0, R = r0;
+        } else {
+          sm = MX::op(sm, get(d, l0, r0));
+          L = L + mid[d] - l0, R = R + mid[d] - r0;
+        }
+      } else {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      }
     }
+    return {cnt, sm};
+  }
+
+  // [L, R) の中で k 番目、および、下位 k 個の和
+  // k = R-L のときの first は、INF を返す
+  pair<T, X> kth(int L, int R, int k, T xor_val = 0) {
+    if (xor_val != 0) assert(set_log);
+    if (k == R - L) return {INF, get(lg, L, R)};
+    assert(0 <= k && k < R - L);
+    T ret = 0;
+    X sm = 0;
+    for (int d = lg - 1; d >= 0; --d) {
+      bool f = (xor_val >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int kf = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      if (k < kf) {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      } else {
+        k -= kf, ret |= T(1) << d;
+        if (f) {
+          sm = MX::op(sm, get(d, L + mid[d] - l0, R + mid[d] - r0));
+          L = l0, R = r0;
+        } else {
+          sm = MX::op(sm, get(d, l0, r0));
+          L = L + mid[d] - l0, R = R + mid[d] - r0;
+        }
+      }
+    }
+    if (k) sm = MX::op(sm, get(0, L, L + k));
+    return {(COMPRESS ? key[ret] : ret), sm};
+  }
+
+  // check(prefix sum) が true となる上限の最大値
+  template <typename F>
+  T max_right_value(F check, int L, int R, T xor_val = 0) {
+    assert(check(MX::unit()));
+    if (xor_val != 0) assert(set_log);
+    if (check(get(lg, L, R))) return INF;
+    T ret = 0;
+    X sm = MX::unit();
+    for (int d = lg - 1; d >= 0; --d) {
+      bool f = ((xor_val) >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      X lo_sm = (f ? get(d, L + mid[d] - l0, R + mid[d] - r0) : get(d, l0, r0));
+      int kf = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      if (check(MX::op(sm, lo_sm))) {
+        sm = MX::op(sm, lo_sm);
+        ret |= 1 << d;
+        if (f) L = l0, R = r0;
+        if (!f) L = L + mid[d] - l0, R = R + mid[d] - r0;
+      } else {
+        if (!f) L = l0, R = r0;
+        if (f) L = L + mid[d] - l0, R = R + mid[d] - r0;
+      }
+    }
+    return (COMPRESS ? key[ret] : ret);
+  }
+
+  // check(prefix sum) が true となる加算個数の最大値
+  template <typename F>
+  int max_right_count(F check, int L, int R, T xor_val = 0) {
+    assert(check(MX::unit()));
+    if (xor_val != 0) assert(set_log);
+    if (check(get(lg, L, R))) return R - L;
+    int ret = 0;
+    X sm = MX::unit();
+    for (int d = lg - 1; d >= 0; --d) {
+      bool f = (xor_val >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int kf = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      X lo_sm = (f ? get(d, L + mid[d] - l0, R + mid[d] - r0) : get(d, l0, r0));
+      if (check(MX::op(sm, lo_sm))) {
+        sm = MX::op(sm, lo_sm), ret += kf;
+        if (f) L = l0, R = r0;
+        if (!f) L += mid[d] - l0, R += mid[d] - r0;
+      } else {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      }
+    }
+    ret += binary_search(
+        [&](int k) -> bool { return check(MX::op(sm, get(0, L, L + k))); }, 0,
+        R - L);
     return ret;
   }
 
-  // [L, R) の中で k>=0 番目
-  T kth(int L, int R, int k) {
-    assert(0 <= k && k < R - L);
-    int ret = 0;
-    for (int h = lg - 1; h >= 0; h--) {
-      int l0 = bv[h].rank(L, 0), r0 = bv[h].rank(R, 0);
-      if (k < r0 - l0)
-        L = l0, R = r0;
-      else {
-        k -= r0 - l0;
-        ret |= 1 << h;
-        L += mid[h] - l0, R += mid[h] - r0;
-      }
-    }
-    return key[ret];
-  }
-
-  // [L, R) の中で小さい方から k 個の総和
-  T sum_by_count(int L, int R, int k) {
-    assert(0 <= k && k <= R - L);
-    T pos = G::unit(), neg = G::unit();
-    for (int h = lg - 1; h >= 0; h--) {
-      int l0 = bv[h].rank(L, 0), r0 = bv[h].rank(R, 0);
-      if (k < r0 - l0) {
-        L = l0, R = r0;
-      } else {
-        k -= r0 - l0;
-        pos = G::op(pos, cumsum[h][r0]);
-        neg = G::op(neg, cumsum[h][l0]);
-        L += mid[h] - l0, R += mid[h] - r0;
-      }
-    }
-    if (k) {
-      pos = G::op(pos, cumsum[0][L + k]);
-      neg = G::op(neg, cumsum[0][L]);
-    }
-    return G::op(pos, G::inverse(neg));
-  }
-
-  // [L, R) の中で value 未満のものの和
-  T sum_by_value(int L, int R, T value) {
-    int x = LB(key, value);
-    T pos = G::unit(), neg = G::unit();
-    for (int h = lg - 1; h >= 0; h--) {
-      bool f = (x >> h) & 1;
-      int l0 = bv[h].rank(L, 0), r0 = bv[h].rank(R, 0);
-      if (f) {
-        pos = G::op(pos, cumsum[h][r0]);
-        neg = G::op(neg, cumsum[h][l0]);
-      }
-      L = bv[h].rank(L, f) + (f ? mid[h] : 0);
-      R = bv[h].rank(R, f) + (f ? mid[h] : 0);
-    }
-    return G::op(pos, G::inverse(neg));
+private:
+  inline X get(int d, int L, int R) {
+    return MX::op(MX::inverse(cumsum[d][L]), cumsum[d][R]);
   }
 };
--- test/mytest/wavelet_matrix_sum.test.cpp
+++ test/mytest/wavelet_matrix_sum.test.cpp
@@ -1,20 +1,142 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 #include "my_template.hpp"
 #include "other/io.hpp"
+#include "ds/wavelet_matrix_sum.hpp"
 #include "random/base.hpp"
-#include "ds/wavelet_matrix.hpp"
 
-void test() {
-  FOR(N, 1, 50) {
-    vi A(N);
-    FOR(i, N) A[i] = RNG(0, 10);
-    Wavelet_Matrix<ll, true, Monoid_Add<ll>> X(A);
-    FOR(L, N) FOR(R, L, N + 1) {
-      vi B = {A.begin() + L, A.begin() + R};
+void test_compress() {
+  int N = RNG(1, 64);
+  int MAX = RNG(2, 1 << 10);
+  vc<int> A(N);
+  vc<int> add_val(MAX);
+  FOR(i, MAX) add_val[i] = RNG(0, MAX);
+  FOR(i, N) A[i] = RNG(MAX);
+  Wavelet_Matrix_Sum<int, true> WM([&](int x) -> int { return add_val[x]; }, A);
+  using P = pair<int, int>;
+
+  int Q = 100;
+  FOR(Q) {
+    int L = RNG(0, N);
+    int R = RNG(0, N);
+    if (L > R) swap(L, R);
+    ++R;
+    int lo = RNG(0, MAX);
+    int hi = RNG(0, MAX);
+    if (lo > hi) swap(lo, hi);
+    ++hi;
+    vc<int> B = {A.begin() + L, A.begin() + R};
+
+    int t = RNG(0, 5);
+    if (t == 0) { // count
+      int cnt = 0, sm = 0;
+      for (auto&& x: B)
+        if (lo <= x && x < hi) cnt += 1, sm += add_val[x];
+      assert(WM.count(L, R, lo, hi) == P({cnt, sm}));
+    }
+    if (t == 1) { // count_prefix
+      int cnt = 0, sm = 0;
+      for (auto&& x: B)
+        if (x < hi) ++cnt, sm += add_val[x];
+      assert(WM.count_prefix(L, R, hi) == P({cnt, sm}));
+    }
+    if (t == 2) { // kth
+      int k = RNG(R - L + 1);
       sort(all(B));
-      auto Bc = cumsum<ll>(B);
+      B.eb(WM.INF);
+      int sm = 0;
+      FOR(i, k) sm += add_val[B[i]];
+      assert(WM.kth(L, R, k) == P({B[k], sm}));
+    }
+    if (t == 3) { // max_right_value
+      int sm = RNG(0, MAX * (R - L) + 1);
+      int val = WM.max_right_value([&](int e) { return e <= sm; }, L, R);
+      int val2 = binary_search(
+          [&](int val) -> bool { return WM.count_prefix(L, R, val).se <= sm; },
+          0, MAX + 1);
+      if (val2 == MAX) val2 = WM.INF;
+      assert(val == val2);
+    }
 
-      FOR(k, R - L + 1) { assert(Bc[k] == X.sum(L, R, k)); }
+    if (t == 4) { // max_right_count
+      int sm = RNG(0, MAX * (R - L) + 1);
+      int k = WM.max_right_count([&](int e) { return e <= sm; }, L, R);
+      int k2 = binary_search(
+          [&](int k) -> bool { return WM.kth(L, R, k).se <= sm; }, 0,
+          R - L + 1);
+      assert(k == k2);
+    }
+  }
+}
+
+void test_not_compress() {
+  int N = RNG(1, 64);
+  int log = RNG(1, 7);
+  int MAX = 1 << log;
+  vc<int> A(N);
+  vc<int> add_val(MAX);
+  FOR(i, MAX) add_val[i] = RNG(0, MAX);
+  FOR(i, N) A[i] = RNG(MAX);
+  Wavelet_Matrix_Sum<int, false> WM([&](int x) { return add_val[x]; }, A, log);
+  using P = pair<int, int>;
+
+  int Q = 100;
+  FOR(Q) {
+    int L = RNG(0, N);
+    int R = RNG(0, N);
+    if (L > R) swap(L, R);
+    ++R;
+    int lo = RNG(0, MAX);
+    int hi = RNG(0, MAX);
+    int xor_val = RNG(0, MAX);
+    if (lo > hi) swap(lo, hi);
+    ++hi;
+    vc<int> B = {A.begin() + L, A.begin() + R};
+
+    int t = RNG(0, 5);
+    if (t == 0) { // count
+      int cnt = 0, sm = 0;
+      for (auto&& x: B) {
+        int y = x ^ xor_val;
+        if (lo <= y && y < hi) cnt += 1, sm += add_val[x];
+      }
+      assert(WM.count(L, R, lo, hi, xor_val) == P({cnt, sm}));
+    }
+    if (t == 1) { // count_prefix
+      int cnt = 0, sm = 0;
+      for (auto&& x: B) {
+        int y = x ^ xor_val;
+        if (y < hi) cnt += 1, sm += add_val[x];
+      }
+      assert(WM.count_prefix(L, R, hi, xor_val) == P({cnt, sm}));
+    }
+    if (t == 2) { // kth
+      int k = RNG(R - L + 1);
+      for (auto&& x: B) x ^= xor_val;
+      sort(all(B));
+      B.eb(WM.INF);
+      int sm = 0;
+      FOR(i, k) sm += add_val[B[i] ^ xor_val];
+      assert(WM.kth(L, R, k, xor_val) == P({B[k], sm}));
+    }
+    if (t == 3) { // max_right_value
+      int sm = RNG(0, MAX * (R - L) + 1);
+      int val
+          = WM.max_right_value([&](int e) { return e <= sm; }, L, R, xor_val);
+      int val2 = binary_search(
+          [&](int val) -> bool {
+            return WM.count_prefix(L, R, val, xor_val).se <= sm;
+          },
+          0, MAX + 1);
+      if (val2 == MAX) val2 = WM.INF;
+      assert(val == val2);
+    }
+    if (t == 4) { // max_right_count
+      int sm = RNG(0, MAX * (R - L) + 1);
+      int k = WM.max_right_count([&](int e) { return e <= sm; }, L, R, xor_val);
+      int k2 = binary_search(
+          [&](int k) -> bool { return WM.kth(L, R, k, xor_val).se <= sm; }, 0,
+          R - L + 1);
+      assert(k == k2);
     }
   }
 }
@@ -25,9 +147,8 @@ void solve() {
 }
 
 signed main() {
-  cout << fixed << setprecision(15);
-
-  test();
+  FOR(1000) test_compress();
+  FOR(1000) test_not_compress();
   solve();
 
   return 0;
