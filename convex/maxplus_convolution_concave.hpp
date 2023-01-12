890fc5fe604dc86254dddbaae7f0b27f754c2418
maspypy <maspypy.gmail.com>
2023-01-12 22:34:48 +0900

    change_dir, use smawk

---
+++ convex/maxplus_convolution_concave.hpp
@@ -0,0 +1,28 @@
+#include "convex/smawk.hpp"
+
+template <typename T, bool concaveA, bool concaveB>
+vc<T> maxplus_convolution_concave(vc<T> A, vc<T> B, const T INF) {
+  static_assert(concaveA || concaveB);
+  assert(INF < INF + INF);
+  if (!concaveB) swap(A, B);
+  int NA = len(A), NB = len(B);
+  int N = NA + NB - 1;
+  int L = 0, R = NB;
+  while (L < R && B[L] == -INF) ++L;
+  if (L == R) return vc<T>(N, -INF);
+  while (B[R - 1] == -INF) --R;
+  B = {B.begin() + L, B.begin() + R};
+  int nB = R - L;
+  int n = NA + nB - 1;
+
+  auto select = [&](int i, int j, int k) -> bool {
+    if (i < k) return false;
+    if (i - j >= nB) return true;
+    return A[j] + B[i - j] < A[k] + B[i - k];
+  };
+
+  vc<int> J = SMAWK(n, NA, select);
+  vc<T> C(N, -INF);
+  FOR(i, n) C[L + i] = (A[J[i]] == -INF ? -INF : A[J[i]] + B[i - J[i]]);
+  return C;
+}
