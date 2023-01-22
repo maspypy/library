0b81192154708f2c876dced20c2214b53564d69b
maspypy <maspypy.gmail.com>
2023-01-22 21:30:26 +0900

    empty

--- linalg/implicit_matrix/pascal.hpp
+++ linalg/implicit_matrix/pascal.hpp
@@ -1,31 +1,31 @@
 #include "poly/convolution.hpp"
 
-// g[i] = sum binom(i, j) f[j]
+// transpose = 0：g[i] = sum binom(i,j) f[j]
+// transpose = 1：g[i] = sum binom(j,i) f[j]
 template <typename mint>
-vc<mint> pascal_1(vc<mint> f, bool inverse) {
-  int n = len(f);
-  vc<mint> g(n);
-  FOR(i, n) g[i] = fact_inv<mint>(i);
-  if (inverse) FOR(i, n) if (i & 1) g[i] = -g[i];
-  FOR(i, n) f[i] *= fact_inv<mint>(i);
-  f = convolution(f, g);
-  f.resize(n);
-  FOR(i, n) f[i] *= fact<mint>(i);
-  return f;
-}
-
-// g[i] = sum binom(j, i) f[j]
-template <typename mint>
-vc<mint> pascal_2(vc<mint> f, bool inverse) {
-  int n = len(f);
-  FOR(i, n) f[i] *= fact<mint>(i);
-  reverse(all(f));
-  vc<mint> g(n);
-  FOR(i, n) g[i] = fact_inv<mint>(i);
-  if (inverse) FOR(i, n) if (i & 1) g[i] = -g[i];
-  f = convolution(f, g);
-  f.resize(n);
-  reverse(all(f));
-  FOR(i, n) f[i] *= fact_inv<mint>(i);
-  return f;
+vc<mint> pascal(vc<mint> f, bool transpose, bool inverse) {
+  if (!transpose) {
+    int n = len(f);
+    vc<mint> g(n);
+    FOR(i, n) g[i] = fact_inv<mint>(i);
+    if (inverse) FOR(i, n) if (i & 1) g[i] = -g[i];
+    FOR(i, n) f[i] *= fact_inv<mint>(i);
+    f = convolution(f, g);
+    f.resize(n);
+    FOR(i, n) f[i] *= fact<mint>(i);
+    return f;
+  }
+  if (transpose) {
+    int n = len(f);
+    FOR(i, n) f[i] *= fact<mint>(i);
+    reverse(all(f));
+    vc<mint> g(n);
+    FOR(i, n) g[i] = fact_inv<mint>(i);
+    if (inverse) FOR(i, n) if (i & 1) g[i] = -g[i];
+    f = convolution(f, g);
+    f.resize(n);
+    reverse(all(f));
+    FOR(i, n) f[i] *= fact_inv<mint>(i);
+    return f;
+  }
 }
--- test/mytest/pascal.test.cpp
+++ test/mytest/pascal.test.cpp
@@ -15,39 +15,27 @@ vc<mint> gen(int N) {
   return f;
 }
 
-vc<mint> pascal_1_naive(vc<mint> f) {
+vc<mint> pascal_naive(vc<mint> f, bool transpose) {
   int N = len(f);
   vc<mint> g(N);
-  FOR(i, N) FOR(j, N) if (i >= j) g[i] += C<mint>(i, j) * f[j];
-  return g;
-}
-
-vc<mint> pascal_2_naive(vc<mint> f) {
-  int N = len(f);
-  vc<mint> g(N);
-  FOR(i, N) FOR(j, N) if (i <= j) g[i] += C<mint>(j, i) * f[j];
-  return g;
-}
-
-void test_1() {
-  FOR(N, 1, 100) {
-    vc<mint> f = gen(N);
-    vc<mint> g1 = pascal_1<mint>(f, 0);
-    vc<mint> g2 = pascal_1_naive(f);
-    assert(g1 == g2);
-    vc<mint> h = pascal_1<mint>(g1, 1);
-    assert(f == h);
+  if (!transpose) {
+    FOR(i, N) FOR(j, N) if (i >= j) g[i] += C<mint>(i, j) * f[j];
   }
+  if (transpose) {
+    FOR(i, N) FOR(j, N) if (i <= j) g[i] += C<mint>(j, i) * f[j];
+  }
+  return g;
 }
 
-void test_2() {
-  FOR(N, 1, 100) {
-    vc<mint> f = gen(N);
-    vc<mint> g1 = pascal_2<mint>(f, 0);
-    vc<mint> g2 = pascal_2_naive(f);
-    assert(g1 == g2);
-    vc<mint> h = pascal_2<mint>(g1, 1);
-    assert(f == h);
+void test() {
+  FOR(t, 2) {
+    FOR(N, 1, 100) {
+      vc<mint> f = gen(N);
+      vc<mint> g1 = pascal<mint>(f, t, 0);
+      vc<mint> g2 = pascal_naive(f, t);
+      vc<mint> h = pascal<mint>(g1, t, 1);
+      assert(f == h);
+    }
   }
 }
 
@@ -57,8 +45,7 @@ void solve() {
 }
 
 signed main() {
-  test_1();
-  test_2();
+  test();
   solve();
 
   return 0;
