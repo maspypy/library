b93d92a80f336fac3ff349d8458d9742194fd1cc
maspypy <maspypy.gmail.com>
2023-01-22 22:24:51 +0900

    fps div

--- poly/fps_div.hpp
+++ poly/fps_div.hpp
@@ -1,3 +1,17 @@
+#include "poly/count_terms.hpp"
+#include "poly/fps_inv.hpp"
+
+// f/g. f の長さで出力される.
+template <typename mint, bool SPARSE = false>
+vc<mint> fps_div(vc<mint> f, vc<mint> g) {
+  if (SPARSE || count_terms(g) < 200) return fps_div_sparse(f, g);
+  int n = len(f);
+  g.resize(n);
+  g = fps_inv<mint>(g);
+  f = convolution(f, g);
+  f.resize(n);
+  return f;
+}
 
 // f/g ただし g は sparse
 template <typename mint>
