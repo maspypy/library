4477319eb7718b24839e335affa81a8189423dcf
maspypy <45988013+maspypy@users.noreply.github.com>
2023-01-23 21:38:53 +0900

    Update partition.hpp

--- enumerate/partition.hpp
+++ enumerate/partition.hpp
@@ -1,14 +1,12 @@
 /*
-N の partition の列挙して、partition p ごとに query(p) を行う
-partition は、減少列として辞書式の降順に列挙する
-生成にかかる時間
+partition は、「減少列」として辞書式の降順に列挙する。
 ・N = 50（204226）：12 ms
 ・N = 60（966467）：60 ms
 ・N = 70（4087968）：270 ms
 ・N = 80（15796476）：1100 ms
 */
 template <typename F>
-void enumerate_partitions(int N, F query, int LIM_len = -1, int LIM_val = -1) {
+void enumerate_partition(int N, F query, int LIM_len = -1, int LIM_val = -1) {
   assert(N >= 0);
   auto dfs = [&](auto self, vc<int>& p, int sum) -> void {
     if (sum == N) {
