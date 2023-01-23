1edb3a04904873d15d9ebc3b2975c087f4b339cc
maspypy <maspypy.gmail.com>
2023-01-23 16:19:24 +0900

    add test

--- ds/offline_query/range_xor_basis_query.hpp
+++ ds/offline_query/range_xor_basis_query.hpp
@@ -1,54 +1,42 @@
 #pragma once
 
-/*
-列もクエリも static な場合。計算量 O((N+Q)LOG)
-example
-https://atcoder.jp/contests/abc223/tasks/abc223_h
-https://codeforces.com/contest/1100/problem/F
-*/
+// 列もクエリも static
 template <typename INT_TYPE, int LOG>
 struct Range_Xor_Basis_Query {
   int n;
+  int Q;
   vc<INT_TYPE>& A;
-  vc<pair<int, int>> query;
-  vc<vi> QIDS;
+  vvc<pair<int, int>> QUERY;
 
-  Range_Xor_Basis_Query(vc<INT_TYPE>& A) : n(len(A)), A(A) {
-    QIDS.resize(n + 1);
+  Range_Xor_Basis_Query(vc<INT_TYPE>& A) : n(len(A)), Q(0), A(A) {
+    QUERY.resize(n + 1);
   }
 
-  void add(int l, int r) {
-    assert(0 <= l && l <= r && r <= n);
-    QIDS[r].eb(len(query));
-    query.eb(l, r);
+  // クエリ追加。
+  void query(int L, int R) {
+    assert(0 <= L && L <= R && R <= n);
+    QUERY[R].eb(Q++, L);
   }
 
-  // query_id, basis(vector) -> void という関数 F を渡す
+  // 関数 F(qid, basis) を渡す
   template <typename CALC>
   void calc(CALC F) {
     using P = pair<INT_TYPE, int>;
-    int Q = len(query);
-    FOR(q, Q) { QIDS[query[q].se].eb(q); }
-    array<P, LOG> dat;
-    FOR(k, LOG) dat[k] = {INT_TYPE(1) << k, -1};
+    array<P, LOG> d;
+    FOR(k, LOG) d[k] = {INT_TYPE(1) << k, -1};
     vc<INT_TYPE> basis;
-
     FOR(i, n + 1) {
-      for (auto&& q: QIDS[i]) {
-        auto& [l, r] = query[q];
+      for (auto&& [qid, L]: QUERY[i]) {
         basis.clear();
-        FOR_R(k, LOG) {
-          if (dat[k].se >= l) basis.eb(dat[k].fi);
-        }
-        F(q, basis);
+        FOR_R(k, LOG) if (d[k].se >= L) basis.eb(d[k].fi);
+        F(qid, basis);
       }
       if (i == n) break;
       P p = {A[i], i};
       FOR_R(k, LOG) {
-        if (p.fi >> k & 1) {
-          if (p.se > dat[k].se) swap(p, dat[k]);
-          p.fi ^= dat[k].fi;
-        }
+        if (!(p.fi >> k & 1)) continue;
+        if (p.se > d[k].se) swap(p, d[k]);
+        p.fi ^= d[k].fi;
       }
     }
   }
---
+++ test_atcoder/abc223h.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://atcoder.jp/contests/abc212/tasks/abc223_h"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "ds/offline_query/range_xor_basis_query.hpp"
+
+void solve() {
+  LL(N, Q);
+  VEC(ll, A, N);
+  Range_Xor_Basis_Query<ll, 60> S(A);
+  vi X(Q);
+  FOR(q, Q) {
+    LL(L, R);
+    read(X[q]);
+    S.query(--L, R);
+  }
+
+  vc<bool> ANS(Q);
+
+  S.calc([&](int q, vi basis) -> void {
+    ll x = X[q];
+    for (auto&& b: basis) x = min(x, x ^ b);
+    ANS[q] = x == 0;
+  });
+  for (auto&& a: ANS) Yes(a);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
