d595409af8bd5d474797ad90ee7b05b9e7fc8db9
maspypy <maspypy.gmail.com>
2023-01-25 06:10:02 +0900

    simple 2opt

--- ds/offline_query/mo.hpp
+++ ds/offline_query/mo.hpp
@@ -2,33 +2,47 @@ struct Mo {
   vc<pair<int, int>> LR;
   void add(int L, int R) { LR.emplace_back(L, R); }
 
-  template <typename AL, typename AR, typename EL, typename ER, typename O>
-  void calc(const AL &add_left, const AR &add_right, const EL &erase_left,
-            const ER &erase_right, const O &query) {
-    int n = 1;
-    for (auto &&[l, r]: LR) chmax(n, r);
-    int q = (int)LR.size();
-    int bs = n / min<int>(n, sqrt(q));
-    vector<int> ord(q);
-    iota(begin(ord), end(ord), 0);
-    sort(begin(ord), end(ord), [&](int a, int b) {
-      int ablock = LR[a].first / bs, bblock = LR[b].first / bs;
-      if (ablock != bblock) return ablock < bblock;
-      return (ablock & 1) ? LR[a].second > LR[b].second
-                          : LR[a].second < LR[b].second;
+  vc<int> get_mo_order() {
+    int N = 1;
+    for (auto &&[l, r]: LR) chmax(N, r);
+    int Q = len(LR);
+    int bs = N / min<int>(N, sqrt(Q));
+    vc<int> I(Q);
+    iota(all(I), 0);
+    sort(all(I), [&](int a, int b) {
+      int aa = LR[a].fi / bs, bb = LR[b].fi / bs;
+      if (aa != bb) return aa < bb;
+      return (aa & 1) ? LR[a].se > LR[b].se : LR[a].se < LR[b].se;
     });
-    int l = 0, r = 0;
-    for (auto idx: ord) {
-      while (l > LR[idx].first) add_left(--l);
-      while (r < LR[idx].second) add_right(r++);
-      while (l < LR[idx].first) erase_left(l++);
-      while (r > LR[idx].second) erase_right(--r);
-      query(idx);
+
+    auto cost = [&](int a, int b) -> int {
+      return abs(LR[I[a]].fi - LR[I[b]].fi) + abs(LR[I[a]].se - LR[I[b]].se);
+    };
+
+    // ランダムケースで数パーセント
+    FOR(k, Q - 5) {
+      if (cost(k, k + 2) + cost(k + 1, k + 3)
+          < cost(k, k + 1) + cost(k + 2, k + 3)) {
+        swap(I[k + 1], I[k + 2]);
+      }
+      if (cost(k, k + 3) + cost(k + 1, k + 4)
+          < cost(k, k + 1) + cost(k + 3, k + 4)) {
+        swap(I[k + 1], I[k + 3]);
+      }
     }
+    return I;
   }
 
-  template <typename A, typename E, typename O>
-  void calc(const A &add, const E &erase, const O &query) {
-    calc(add, add, erase, erase, query);
+  template <typename F1, typename F2, typename F3, typename F4, typename F5>
+  void calc(F1 add_l, F2 add_r, F3 rm_l, F4 rm_r, F5 query) {
+    auto I = get_mo_order();
+    int l = 0, r = 0;
+    for (auto idx: I) {
+      while (l > LR[idx].fi) add_l(--l);
+      while (r < LR[idx].se) add_r(r++);
+      while (l < LR[idx].fi) rm_l(l++);
+      while (r > LR[idx].se) rm_r(--r);
+      query(idx);
+    }
   }
 };
