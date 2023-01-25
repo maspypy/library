d3e9740dffdba90c19e993b37e79d43573babcf3
maspypy <maspypy.gmail.com>
2023-01-25 18:10:20 +0900

    a

--- ds/offline_query/mo_3d.hpp
+++ ds/offline_query/mo_3d.hpp
@@ -1,20 +1,9 @@
 // https://codeforces.com/contest/940/problem/F
-template <typename CHANGE>
+// https://codeforces.com/contest/1476/problem/G
 struct Mo_3d {
-  int n;
-  vc<CHANGE> change;
   vc<tuple<int, int, int>> query;
-  vector<pair<int, int>> lr;
 
-  explicit Mo_3d() {}
-  int size() { return query.size(); }
-
-  void add_query(int l, int r) {
-    int t = len(change) - 1;
-    query.eb(t, l, r);
-  }
-
-  void add_change(CHANGE x) { change.eb(x); }
+  void add_query(int t, int l, int r) { query.eb(t, l, r); }
 
   vc<int> get_mo_order(ll block_sz) {
     constexpr ll K = 1 << 20;
@@ -31,27 +20,45 @@ struct Mo_3d {
       x += t * K * K;
       key[q] = x;
     }
-    return argsort(key);
+    vc<int> I = argsort(key);
+
+    // ランダムケースで5パーセント程度
+    auto cost = [&](int a, int b) -> int {
+      auto [t1, x1, y1] = query[I[a]];
+      auto [t2, x2, y2] = query[I[b]];
+      return abs(t1 - t2) + abs(x1 - x2) + abs(y1 - y2);
+    };
+    FOR(k, Q - 5) {
+      if (cost(k, k + 2) + cost(k + 1, k + 3)
+          < cost(k, k + 1) + cost(k + 2, k + 3)) {
+        swap(I[k + 1], I[k + 2]);
+      }
+      if (cost(k, k + 3) + cost(k + 1, k + 4)
+          < cost(k, k + 1) + cost(k + 3, k + 4)) {
+        swap(I[k + 1], I[k + 3]);
+      }
+    }
+    return I;
   }
 
   template <typename F1, typename F2, typename F3, typename F4, typename F5,
             typename F6, typename F7>
-  void calc(F1 ADD_L, F2 ADD_R, F3 RM_L, F4 RM_R, F5 ADD_T, F6 RM_T, F7 CALC,
-            ll block_sz = -1) {
+  void calc(F1 ADD_L, F2 ADD_R, F3 RM_L, F4 RM_R, F5 ADD_CHANGE, F6 RM_CHANGE,
+            F7 CALC, ll block_sz = -1) {
     if (block_sz == -1) {
       ll Q = max(1LL, len(query));
       while (block_sz * block_sz * block_sz < Q * Q) { block_sz++; }
     }
     auto I = get_mo_order(block_sz);
-    ll t = -1, l = 0, r = 0;
+    ll t = 0, l = 0, r = 0;
     for (auto&& qid: I) {
       auto [nt, nl, nr] = query[qid];
       while (l > nl) ADD_L(--l);
       while (r < nr) ADD_R(r++);
       while (l < nl) RM_L(l++);
       while (r > nr) RM_R(--r);
-      while (t < nt) ADD_T(change[++t], l, r);
-      while (t > nt) RM_T(change[t--], l, r);
+      while (t < nt) ADD_CHANGE(t++, l, r);
+      while (t > nt) RM_CHANGE(--t, l, r);
       CALC(qid);
     }
   }
