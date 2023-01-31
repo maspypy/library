0debe7ee249d793b2127ba0c81be83f39a56a1e8
maspypy <maspypy.gmail.com>
2023-01-31 20:39:54 +0900

    int

--- enumerate/ceil_range.hpp
+++ enumerate/ceil_range.hpp
@@ -1,8 +1,8 @@
 // f(q, l, r)：ceil が q になる範囲が [l, r)
 // q == 1 のときには r == INF が入ることに注意
-template <typename F>
-void ceil_range(ll N, F f, ll INF) {
-  ll q = 1, l = N, r = INF;
+template <typename INT_TYPE, typename F>
+void ceil_range(INT_TYPE N, F f, INT_TYPE INF) {
+  INT_TYPE q = 1, l = N, r = INF;
   while (1) {
     f(q, l, r);
     if (q == N) break;
--- enumerate/floor_range.hpp
+++ enumerate/floor_range.hpp
@@ -1,9 +1,9 @@
 // f(q, l, r)：商が q になる範囲が [l, r)
-template <typename F>
-void floor_range(ll N, F f) {
-  ll q = 1, r = N + 1;
+template <typename INT_TYPE, typename F>
+void floor_range(INT_TYPE N, F f) {
+  INT_TYPE q = 1, r = N + 1;
   while (1) {
-    ll l = N / (q + 1) + 1;
+    INT_TYPE l = N / (q + 1) + 1;
     f(q, l, r);
     if (q == N) break;
     r = l, q = N / (l - 1);
