52ca461051d8f00483f281a5f6f9173045be1acd
maspypy <maspypy.gmail.com>
2023-01-31 23:39:57 +0900

    upd beats

--- ds/segtree/beats_summin_chmax.hpp
+++ ds/segtree/beats_summin_chmax.hpp
@@ -1,24 +1,23 @@
 #include "ds/segtree/segtree_beats.hpp"
-
-constexpr ll Beats_INF = 1LL << 40;
-
+template <typename T>
 struct Beats_SumMin_Chmax {
-  struct CntSumMin {
+  static constexpr T INF = numeric_limits<T>::max() / 2 - 1;
+  struct SumMin {
     struct X {
-      ll cnt, sum, min, minc, min2;
+      T sum, min, minc, min2;
       bool fail;
     };
     using value_type = X;
     static X op(const X& x, const X& y) {
-      if (x.cnt == 0) return y;
-      if (y.cnt == 0) return x;
+      if (x.min == INF) return y;
+      if (y.min == -INF) return x;
       X z;
-      z.cnt = x.cnt + y.cnt, z.sum = x.sum + y.sum;
+      z.sum = x.sum + y.sum;
 
       z.min = min(x.min, y.min);
       z.minc = (x.min == z.min ? x.minc : 0) + (y.min == z.min ? y.minc : 0);
 
-      z.min2 = Beats_INF;
+      z.min2 = INF;
       if (z.min < x.min && x.min < z.min2) z.min2 = x.min;
       if (z.min < x.min2 && x.min2 < z.min2) z.min2 = x.min2;
       if (z.min < y.min && y.min < z.min2) z.min2 = y.min;
@@ -27,73 +26,58 @@ struct Beats_SumMin_Chmax {
       z.fail = 0;
       return z;
     }
-    static constexpr X unit() { return {0, 0, 0, 0, 0, 0}; }
+    static constexpr X unit() { return {0, INF, 0, INF, 0}; }
     bool commute = true;
   };
   struct AddChmax {
-    using X = pi;
+    using X = pair<T, T>;
     using value_type = X;
     static constexpr X op(const X& x, const X& y) {
       auto [a, c] = x;
       auto [d, f] = y;
-      a += d, c += d;
-      c = max(c, f);
+      a += d, c += d, c = max(c, f);
       return {a, c};
     }
-    static constexpr X unit() { return {0, -Beats_INF}; }
+    static constexpr X unit() { return {0, -INF}; }
     bool commute = false;
   };
-  struct Lazy {
-    using MX = CntSumMin;
-    using MA = AddChmax;
-    using X_structure = MX;
-    using A_structure = MA;
-    using X = MX::value_type;
-    using A = MA::value_type;
-    static X act(X& x, const A& a) {
-      if (x.cnt == 0) return x;
+  struct Beats {
+    using Monoid_X = SumMin;
+    using Monoid_A = AddChmax;
+    using X = typename Monoid_X::value_type;
+    using A = typename Monoid_A::value_type;
+    static X act(X& x, const A& a, int cnt) {
       assert(!x.fail);
+      if (x.min == INF) return x;
       auto [add, ma] = a;
-      x.sum += x.cnt * add;
-      x.min += add, x.min2 += add;
-      if (ma == -Beats_INF) return x;
+      x.sum += cnt * add, x.min += add, x.min2 += add;
+      if (ma == -INF) return x;
 
-      ll before_min = x.min;
+      T before_min = x.min;
       x.min = max(x.min, ma);
-      if (x.minc == x.cnt) {
-        x.min2 = x.min;
-        x.sum = x.cnt * x.min;
-        return x;
-      }
-      if (x.min2 > x.min) {
-        x.sum += (x.min - before_min) * x.minc;
-      } else {
+      if (x.minc == cnt) { x.min2 = x.min, x.sum = cnt * x.min; }
+      elif (x.min2 > x.min) { x.sum += (x.min - before_min) * x.minc; }
+      else {
         x.fail = 1;
       }
       return x;
     }
   };
-  LazySegTreeBeats<Lazy> seg;
-  Beats_SumMin_Chmax(vc<ll>& A) {
-    using X = Lazy::MX::value_type;
-    vc<X> seg_raw(len(A));
-    FOR(i, len(A)) {
-      ll x = A[i];
-      seg_raw[i] = {1, x, x, 1, x};
-    }
-    seg = LazySegTreeBeats<Lazy>(seg_raw);
+  using X = typename SumMin::X;
+  SegTree_Beats<Beats> seg;
+  Beats_SumMin_Chmax(vc<T>& A) {
+    seg.build(len(A), [&](int i) -> X { return from_element(A[i]); });
   }
 
-  void set(int i, ll x) { seg.set(i, {1, x, x, 1, x}); }
+  void set(int i, ll x) { seg.set(i, from_element(x)); }
 
-  Lazy::MX::value_type prod(int l, int r) {
+  // (sum, min)
+  pair<T, T> prod(int l, int r) {
     auto e = seg.prod(l, r);
-    return e;
+    return {e.sum, e.min};
   }
+  static X from_element(T x) { return {x, x, 1, x}; }
 
-  void chmax(int l, int r, ll x) { seg.apply(l, r, {0, x}); }
-
-  void add(int l, int r, ll x) { seg.apply(l, r, {x, -Beats_INF}); }
-
-  void apply(int l, int r, pi addchmax) { seg.apply(l, r, addchmax); }
+  void chmax(int l, int r, T x) { seg.apply(l, r, {0, x}); }
+  void add(int l, int r, T x) { seg.apply(l, r, {x, -INF}); }
 };
