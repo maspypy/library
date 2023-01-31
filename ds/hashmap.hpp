648948f22956cef1ec45f4c4129ff918701380c4
maspypy <maspypy.gmail.com>
2023-01-31 21:39:16 +0900

    ll->u64

--- ds/hashmap.hpp
+++ ds/hashmap.hpp
@@ -1,43 +1,44 @@
 #pragma once
 #include "random/base.hpp"
 
-// long long -> Val
+// u64 -> Val
 template <typename Val, int LOG = 20>
 struct HashMap {
   int N;
-  ll* keys;
+  u64* keys;
   Val* vals;
   vc<int> IDS;
   bitset<1 << LOG> used;
   const int shift;
-  const uint64_t r = 11995408973635179863ULL;
+  const u64 r = 11995408973635179863ULL;
   HashMap()
-      : N(1 << LOG), keys(new ll[N]), vals(new Val[N]), shift(64 - __lg(N)) {}
+      : N(1 << LOG), keys(new u64[N]), vals(new Val[N]), shift(64 - __lg(N)) {}
   int hash(ll x) {
-    static const uint64_t FIXED_RANDOM
+    static const u64 FIXED_RANDOM
         = std::chrono::steady_clock::now().time_since_epoch().count();
-    return (uint64_t(x + FIXED_RANDOM) * r) >> shift;
+    return (u64(x + FIXED_RANDOM) * r) >> shift;
   }
 
-  int index(const ll& key) {
+  int index(const u64& key) {
     int i = 0;
     for (i = hash(key); used[i] && keys[i] != key; (i += 1) &= (N - 1)) {}
     return i;
   }
 
-  Val& operator[](const ll& key) {
+  // [] した時点で要素は作られる
+  Val& operator[](const u64& key) {
     int i = index(key);
     if (!used[i]) IDS.eb(i), used[i] = 1, keys[i] = key, vals[i] = Val{};
     return vals[i];
   }
 
-  Val get(const ll& key, Val default_value) {
+  Val get(const u64& key, Val default_value) {
     int i = index(key);
     if (!used[i]) return default_value;
     return vals[i];
   }
 
-  bool count(const ll& key) {
+  bool count(const u64& key) {
     int i = index(key);
     return used[i] && keys[i] == key;
   }
@@ -47,10 +48,10 @@ struct HashMap {
     IDS.clear();
   }
 
-  vc<pair<ll, Val>> items() {
-    vc<pair<ll, Val>> res;
-    res.reserve(len(IDS));
-    for (auto&& i: IDS) res.eb(keys[i], vals[i]);
+  // f(key, val)
+  template <typename F>
+  void enumerate_all(F f) {
+    for (auto&& i: IDS) f(keys[i], vals[i]);
     return res;
   }
 };
--- random/hash_vector.hpp
+++ random/hash_vector.hpp
@@ -4,7 +4,7 @@
 #include "mod/modint61.hpp"
 
 template <typename T>
-ll hash_vector(vc<T> X) {
+u64 hash_vector(vc<T> X) {
   using mint = modint61;
   static vc<mint> hash_base;
   int n = len(X);
@@ -13,11 +13,4 @@ ll hash_vector(vc<T> X) {
   FOR(i, n) H += hash_base[i] * mint(X[i]);
   H += hash_base[n];
   return H.val;
-}
-
-template <typename T>
-ll hash_pair(pair<T, T> X) {
-  static ll hash_base = 0;
-  if (hash_base == 0) hash_base = RNG_64();
-  return hash_base * X.fi + X.se;
 }
