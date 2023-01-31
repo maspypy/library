78b232ac4ad32408f6939569b961fd0a06fe854a
maspypy <maspypy.gmail.com>
2023-01-31 23:24:38 +0900

    upd test

--- test/yukicoder/1726.test.cpp
+++ test/yukicoder/1726.test.cpp
@@ -6,7 +6,6 @@
 void solve() {
   LL(N, M);
   ll ANS = 0;
-
   VEC(ll, P, N);
   VEC(pi, AB, N);
   VEC(pi, CD, M);
@@ -17,28 +16,20 @@ void solve() {
     for (auto&& [a, b]: AB) a = -a;
     FOR(2) {
       for (auto&& [a, b]: AB) b = -b;
-
       MP.reset();
-
       FOR(i, N) FOR(j, M) {
         auto [a, b] = AB[i];
         auto [c, d] = CD[j];
         ll key = ((a - c) << 32) + (b - d);
         MP[key] += P[i];
       }
-
-      for (auto&& [k, v]: MP.items()) { chmax(ANS, v); }
+      MP.enumerate_all([&](auto k, auto v) { chmax(ANS, v); });
     }
   }
   print(ANS);
 }
 
 signed main() {
-  cout << fixed << setprecision(15);
-
-  ll T = 1;
-  // LL(T);
-  FOR(T) solve();
-
+  solve();
   return 0;
 }
