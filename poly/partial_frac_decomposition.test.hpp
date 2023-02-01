4bb77b0d3317002701c3ca17b52d67b3e442ab21
maspypy <maspypy.gmail.com>
2023-02-02 02:54:18 +0900

    partial frac

---
+++ poly/partial_frac_decomposition.test.hpp
@@ -0,0 +1,20 @@
+#include "poly/multipoint.hpp"
+// O(Nlog^2N)
+// f / prod(x-a) = sum b/(x-a) という B を返す
+template <typename mint>
+vc<mint> partial_frac_decomposition(vc<mint> f, vc<mint> A) {
+  assert(len(f) <= len(A));
+  if (len(f) == 0) return vc<mint>(len(A));
+  SubproductTree<mint> X(A);
+  vc<mint> g = X.T[1]; // prod(1-ax)
+  g.resize(len(A) + 1);
+  reverse(all(g));
+  FOR(i, len(g) - 1) g[i] = g[i + 1] * mint(i + 1);
+  g.pop_back();
+
+  auto num = X.evaluation(f);
+  auto den = X.evaluation(g);
+  vc<mint> B(len(A));
+  FOR(i, len(A)) B[i] = num[i] / den[i];
+  return B;
+}
---
+++ test/mytest/partial_frac.test.cpp
@@ -0,0 +1,58 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "random/base.hpp"
+#include "poly/sum_of_rationals.hpp"
+#include "poly/partial_frac_decomposition.test.hpp"
+
+using mint = modint998;
+using poly = vc<mint>;
+
+vc<mint> gen(int n) {
+  vc<mint> f(n);
+  set<mint> ss;
+  FOR(i, n) {
+    while (1) {
+      mint x = RNG(-100, 100);
+      if (ss.count(x)) continue;
+      ss.insert(x);
+      f[i] = x;
+      break;
+    }
+  }
+  return f;
+}
+
+void check(poly f, vc<mint> A) {
+  int m = len(A);
+  auto B = partial_frac_decomposition<mint>(f, A);
+  assert(len(B) == m);
+  vc<pair<poly, poly>> dat;
+  FOR(i, m) {
+    poly F = {B[i]};
+    poly G = {-A[i], mint(1)};
+    dat.eb(F, G);
+  }
+  auto [p, q] = sum_of_rationals<mint>(dat);
+  while (len(p) && p.back() == mint(0)) p.pop_back();
+  while (len(f) && f.back() == mint(0)) f.pop_back();
+  assert(p == f);
+}
+
+void test() {
+  FOR(n, 50) FOR(m, n, 50) {
+    poly f = gen(n), A = gen(m);
+    check(f, A);
+  }
+}
+
+void solve() {
+  LL(a, b);
+  print(a + b);
+}
+
+signed main() {
+  test();
+  solve();
+  return 0;
+}
