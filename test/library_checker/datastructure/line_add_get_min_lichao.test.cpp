1220b66864c5fdbf737d38a68c2cd4fab5bdae7c
maspypy <45988013+maspypy@users.noreply.github.com>
2023-02-02 19:38:06 +0900

    Update line_add_get_min_lichao.test.cpp

--- test/library_checker/datastructure/line_add_get_min_lichao.test.cpp
+++ test/library_checker/datastructure/line_add_get_min_lichao.test.cpp
@@ -1,12 +1,12 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/line_add_get_min"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "convex/lichao.hpp"
+#include "convex/dynamic_lichao.hpp"
 
 void solve() {
   LL(N, Q);
   const int LIM = 1'000'000'000;
-  LiChao_Tree<ll, 0, 0, 400'000, 1> X(-LIM, LIM + 1);
+  Dynamic_LiChao_Tree<ll, 0, 0, 400'000, 1> X(-LIM, LIM + 1);
   using np = decltype(X)::np;
   np root = X.new_root();
   FOR(N) {
