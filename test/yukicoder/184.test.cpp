1ce011b9feccb73da6be4c350a8ec85cc5bf4cbf
maspypy <maspypy.gmail.com>
2023-02-03 02:55:53 +0900

    empty

--- test/yukicoder/184.test.cpp
+++ test/yukicoder/184.test.cpp
@@ -1,25 +1,19 @@
 #define PROBLEM "https://yukicoder.me/problems/no/184"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "alg/monoid/xor_basis.hpp"
+#include "alg/monoid/merge_vector_space.hpp"
 
 void solve() {
   LL(N);
   VEC(ll, A, N);
-  using Mono = Monoid_XorBasis<ll>;
-  auto sp = Mono::unit();
-  for (auto&& a: A) { Mono::add_element(sp, a); }
-  print(1LL << len(sp));
+  using Mono = Merge_Vector_Space<u64>;
+  using SP = typename Mono::value_type;
+  SP X;
+  for (auto&& a: A) { X.add_element(a); }
+  print(1LL << len(X));
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
-  ll T = 1;
-  // LL(T);
-  FOR(_, T) solve();
-
+  solve();
   return 0;
 }
