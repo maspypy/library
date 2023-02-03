a910d78ea74f658f03719debb2d0f1d405acdba6
maspypy <maspypy.gmail.com>
2023-02-04 02:48:46 +0900

    improve

--- mod/prefix_sum_of_binom.hpp
+++ mod/prefix_sum_of_binom.hpp
@@ -2,20 +2,22 @@ template <typename mint>
 struct Prefix_Sum_Of_Binom {
   const int MAX_N;
   const int B;
+  vc<mint> POW;
   vvc<mint> dat;
 
   Prefix_Sum_Of_Binom(int MAX_N) : MAX_N(MAX_N), B(sqrt(MAX_N + 1)) {
+    POW.resize(MAX_N + 1, mint(1));
+    FOR(i, MAX_N) POW[i + 1] = POW[i] + POW[i];
     int K = ceil(MAX_N, B + B) + 2;
     dat.resize(K);
     FOR(k, 0, K) {
       // [0, kB] での closed sum
       vc<mint>& f = dat[k];
-      f.resize(MAX_N + 1);
+      f.resize(MAX_N + 1 - k * B);
       int m = k * B;
-      f[0] = fact<mint>(m);
-      FOR(i, MAX_N) {
-        f[i + 1] = f[i] + f[i]
-                   - (i >= m ? fact<mint>(i) * fact_inv<mint>(i - m) : 0);
+      f[0] = POW[m] * fact<mint>(m);
+      FOR(i, MAX_N - m) {
+        f[i + 1] = f[i] + f[i] - fact<mint>(i + m) * fact_inv<mint>(i);
       }
     }
   }
@@ -24,21 +26,26 @@ struct Prefix_Sum_Of_Binom {
   mint query(int n, int m) {
     assert(0 <= m && m <= n + 1);
     if (m == 0) return mint(0);
-    if (m + m > n + 1) return mint(2).pow(n) - query(n, n + 1 - m);
+    if (m + m > n + 1) return POW[n] - query(n, n + 1 - m);
     --m;
     int a = m / B;
     if (m <= a * B + B / 2) {
-      mint x = dat[a][n] * fact_inv<mint>(a * B);
+      mint x = get(n, a);
       mint t = 0;
       FOR(i, a * B + 1, m + 1) t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
       return x + t * fact<mint>(n);
     } else {
-      mint x = dat[a + 1][n] * fact_inv<mint>((a + 1) * B);
+      mint x = get(n, a + 1);
       mint t = 0;
       FOR(i, m + 1, (a + 1) * B + 1)
       t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
       return x - t * fact<mint>(n);
     }
     return 0;
+  }
+
+  mint get(int n, int k) {
+    if (n <= k * B) return POW[n];
+    return dat[k][n - k * B] * fact_inv<mint>(k * B);
   }
 };
