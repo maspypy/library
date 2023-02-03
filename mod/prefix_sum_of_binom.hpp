15c04c66868609be4fcdeed9c1d6d7d32e3aa485
maspypy <maspypy.gmail.com>
2023-02-04 02:36:07 +0900

    improve performance

--- mod/prefix_sum_of_binom.hpp
+++ mod/prefix_sum_of_binom.hpp
@@ -1,4 +1,3 @@
-// \sum_{k=0}^m binom(n,k) を求めるクエリにオンラインで答える
 template <typename mint>
 struct Prefix_Sum_Of_Binom {
   const int MAX_N;
@@ -8,12 +7,16 @@ struct Prefix_Sum_Of_Binom {
   Prefix_Sum_Of_Binom(int MAX_N) : MAX_N(MAX_N), B(sqrt(MAX_N + 1)) {
     int K = ceil(MAX_N, B + B) + 2;
     dat.resize(K);
-    FOR(k, 1, K) {
-      // kB 個の和
+    FOR(k, 0, K) {
+      // [0, kB] での closed sum
       vc<mint>& f = dat[k];
       f.resize(MAX_N + 1);
-      f[0] = mint(1);
-      FOR(i, MAX_N) f[i + 1] = f[i] + f[i] - C<mint>(i, k * B - 1);
+      int m = k * B;
+      f[0] = fact<mint>(m);
+      FOR(i, MAX_N) {
+        f[i + 1] = f[i] + f[i]
+                   - (i >= m ? fact<mint>(i) * fact_inv<mint>(i - m) : 0);
+      }
     }
   }
 
@@ -22,14 +25,19 @@ struct Prefix_Sum_Of_Binom {
     assert(0 <= m && m <= n + 1);
     if (m == 0) return mint(0);
     if (m + m > n + 1) return mint(2).pow(n) - query(n, n + 1 - m);
+    --m;
     int a = m / B;
-    mint t = 0;
-    if (m <= a * B + (B / 2)) {
-      FOR(i, a * B, m) t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
-      return (a == 0 ? mint(0) : dat[a][n]) + t * fact<mint>(n);
+    if (m <= a * B + B / 2) {
+      mint x = dat[a][n] * fact_inv<mint>(a * B);
+      mint t = 0;
+      FOR(i, a * B + 1, m + 1) t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
+      return x + t * fact<mint>(n);
     } else {
-      FOR(i, m, (a + 1) * B) t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
-      return dat[a + 1][n] - t * fact<mint>(n);
+      mint x = dat[a + 1][n] * fact_inv<mint>((a + 1) * B);
+      mint t = 0;
+      FOR(i, m + 1, (a + 1) * B + 1)
+      t += fact_inv<mint>(i) * fact_inv<mint>(n - i);
+      return x - t * fact<mint>(n);
     }
     return 0;
   }
