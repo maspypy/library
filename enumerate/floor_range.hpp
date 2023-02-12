9e2d7b94b151399506d2c7f0ccf5cd74e3b0b538
maspypy <maspypy.gmail.com>
2023-02-12 20:18:55 +0900

    improve perf

--- enumerate/floor_range.hpp
+++ enumerate/floor_range.hpp
@@ -1,11 +1,21 @@
-// f(q, l, r)：商が q になる範囲が [l, r)
 template <typename T, typename F>
 void floor_range(T N, F f) {
-  T q = 1, r = N + 1;
-  while (1) {
-    T l = N / (q + 1) + 1;
-    f(q, l, r);
-    if (q == N) break;
-    r = l, q = N / (l - 1);
+  ll thresh = 1'000'000'000'000;
+  ll sq = sqrtl(N);
+  int n = (sq * sq + sq <= N ? sq : sq - 1);
+  if (N <= thresh) {
+    ll prev = N + 1;
+    for (int q = 1; q <= n; ++q) {
+      ll x = double(N) / (q + 1) + 1;
+      f(q, x, prev), prev = x;
+    }
+    for (int l = sq; l >= 1; --l) { f(double(N) / l, l, l + 1); }
+  } else {
+    ll prev = N + 1;
+    for (int q = 1; q <= n; ++q) {
+      ll x = N / (q + 1) + 1;
+      f(q, x, prev), prev = x;
+    }
+    for (int l = sq; l >= 1; --l) { f(N / l, l, l + 1); }
   }
 }
