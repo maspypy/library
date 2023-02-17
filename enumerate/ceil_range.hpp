fdc109f41ff387d190006efb2d698d03fc9d3067
maspypy <maspypy.gmail.com>
2023-02-17 11:47:17 +0900

    a

--- enumerate/ceil_range.hpp
+++ enumerate/ceil_range.hpp
@@ -2,12 +2,13 @@
 template <typename F>
 void ceil_range(ll N, F f) {
   assert(N <= (1LL << 50));
-  ll sq = sqrtl(N - 1);
-  int n = (N <= sq * sq + sq ? sq : sq + 1);
+  ll sq = sqrtl(N);
   ll prev = infty<ll>;
-  for (int q = 1; q <= sq + 1; ++q) {
+  for (int q = 1; q <= sq; ++q) {
     ll x = double(N + q - 1) / q;
     f(q, x, prev), prev = x;
   }
+  int n = (N <= sq * sq + sq ? sq : sq + 1);
+  if (N == sq * sq) --n;
   for (int l = n; l >= 1; --l) { f(double(N + l - 1) / l, l, l + 1); }
 }
