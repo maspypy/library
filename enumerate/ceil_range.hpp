c91fd7246fe34840ad19678ec5c31efccfdfacb1
maspypy <maspypy.gmail.com>
2023-02-17 10:21:36 +0900

    double div

--- enumerate/ceil_range.hpp
+++ enumerate/ceil_range.hpp
@@ -1,11 +1,13 @@
-// f(q, l, r)：ceil が q になる範囲が [l, r)
-// q == 1 のときには r == infty<T>
-template <typename T, typename F>
-void ceil_range(T N, F f) {
-  T q = 1, l = N, r = infty<T>;
-  while (1) {
-    f(q, l, r);
-    if (q == N) break;
-    r = l, q = ceil(N, l - 1), l = ceil(N, q);
+// 商が q の区間 [l,r) を q について昇順
+template <typename F>
+void ceil_range(ll N, F f) {
+  assert(N <= (1LL << 50));
+  ll sq = sqrtl(N - 1);
+  int n = (N <= sq * sq + sq ? sq : sq + 1);
+  ll prev = infty<ll>;
+  for (int q = 1; q <= sq + 1; ++q) {
+    ll x = double(N + q - 1) / q;
+    f(q, x, prev), prev = x;
   }
+  for (int l = n; l >= 1; --l) { f(double(N + l - 1) / l, l, l + 1); }
 }
