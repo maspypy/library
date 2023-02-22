ecff41dca9c2f3ab1135be0e5850ac5845708eb8
maspypy <maspypy.gmail.com>
2023-02-23 05:24:25 +0900

    ntt info

--- poly/ntt.hpp
+++ poly/ntt.hpp
@@ -1,72 +1,44 @@
 #pragma once
 
 template <class mint>
-struct ntt_info {
-  static constexpr int bsf_constexpr(unsigned int n) {
-    int x = 0;
-    while (!(n & (1 << x))) x++;
-    return x;
-  }
-
-  static constexpr int rank2 = bsf_constexpr(mint::get_mod() - 1);
-  array<mint, rank2 + 1> root;
-  array<mint, rank2 + 1> iroot;
-  array<mint, max(0, rank2 - 1)> rate2;
-  array<mint, max(0, rank2 - 1)> irate2;
-  array<mint, max(0, rank2 - 2)> rate3;
-  array<mint, max(0, rank2 - 2)> irate3;
-
-  ntt_info() {
-    int g = primitive_root(mint::get_mod());
-    assert(g != -1);
-    root[rank2] = mint(g).pow((mint::get_mod() - 1) >> rank2);
+void ntt(vector<mint>& a, bool inverse) {
+  static constexpr int rank2 = mint::ntt_info().fi;
+  static constexpr int mod = mint::get_mod();
+  static_assert(rank2 != -1);
+  static array<mint, rank2 + 1> root;
+  static array<mint, rank2 + 1> iroot;
+  static array<mint, max(0, rank2 - 1)> rate2, irate2;
+  static array<mint, max(0, rank2 - 2)> rate3, irate3;
+  static bool prepared = 0;
+  if (!prepared) {
+    prepared = 1;
+    root[rank2] = mint::ntt_info().se;
     iroot[rank2] = mint(1) / root[rank2];
     FOR_R(i, rank2) {
       root[i] = root[i + 1] * root[i + 1];
       iroot[i] = iroot[i + 1] * iroot[i + 1];
     }
-
-    {
-      mint prod = 1, iprod = 1;
-      for (int i = 0; i <= rank2 - 2; i++) {
-        rate2[i] = root[i + 2] * prod;
-        irate2[i] = iroot[i + 2] * iprod;
-        prod *= iroot[i + 2];
-        iprod *= root[i + 2];
-      }
+    mint prod = 1, iprod = 1;
+    for (int i = 0; i <= rank2 - 2; i++) {
+      rate2[i] = root[i + 2] * prod;
+      irate2[i] = iroot[i + 2] * iprod;
+      prod *= iroot[i + 2];
+      iprod *= root[i + 2];
     }
-    {
-      mint prod = 1, iprod = 1;
-      for (int i = 0; i <= rank2 - 3; i++) {
-        rate3[i] = root[i + 3] * prod;
-        irate3[i] = iroot[i + 3] * iprod;
-        prod *= iroot[i + 3];
-        iprod *= root[i + 3];
-      }
+    prod = 1, iprod = 1;
+    for (int i = 0; i <= rank2 - 3; i++) {
+      rate3[i] = root[i + 3] * prod;
+      irate3[i] = iroot[i + 3] * iprod;
+      prod *= iroot[i + 3];
+      iprod *= root[i + 3];
     }
   }
 
-  constexpr int primitive_root(int m) {
-    if (m == 167772161) return 3;
-    if (m == 469762049) return 3;
-    if (m == 754974721) return 11;
-    if (m == 880803841) return 26;
-    if (m == 998244353) return 3;
-    if (m == 1045430273) return 3;
-    if (m == 1051721729) return 6;
-    if (m == 1053818881) return 7;
-    return -1;
-  }
-};
-
-template <class mint>
-void ntt(vector<mint>& a, bool inverse) {
   int n = int(a.size());
   int h = topbit(n);
   assert(n == 1 << h);
-  static const ntt_info<mint> info;
   if (!inverse) {
-    int len = 0; // a[i, i+(n>>len), i+2*(n>>len), ..] is transformed
+    int len = 0;
     while (len < h) {
       if (h - len == 1) {
         int p = 1 << (h - len - 1);
@@ -79,30 +51,30 @@ void ntt(vector<mint>& a, bool inverse) {
             a[i + offset] = l + r;
             a[i + offset + p] = l - r;
           }
-          rot *= info.rate2[topbit(~s & -~s)];
+          rot *= rate2[topbit(~s & -~s)];
         }
         len++;
       } else {
         int p = 1 << (h - len - 2);
-        mint rot = 1, imag = info.root[2];
+        mint rot = 1, imag = root[2];
         for (int s = 0; s < (1 << len); s++) {
           mint rot2 = rot * rot;
           mint rot3 = rot2 * rot;
           int offset = s << (h - len);
           for (int i = 0; i < p; i++) {
-            auto mod2 = 1ULL * mint::get_mod() * mint::get_mod();
-            auto a0 = 1ULL * a[i + offset].val;
-            auto a1 = 1ULL * a[i + offset + p].val * rot.val;
-            auto a2 = 1ULL * a[i + offset + 2 * p].val * rot2.val;
-            auto a3 = 1ULL * a[i + offset + 3 * p].val * rot3.val;
-            auto a1na3imag = 1ULL * mint(a1 + mod2 - a3).val * imag.val;
-            auto na2 = mod2 - a2;
+            u64 mod2 = u64(mod) * mod;
+            u64 a0 = a[i + offset].val;
+            u64 a1 = u64(a[i + offset + p].val) * rot.val;
+            u64 a2 = u64(a[i + offset + 2 * p].val) * rot2.val;
+            u64 a3 = u64(a[i + offset + 3 * p].val) * rot3.val;
+            u64 a1na3imag = (a1 + mod2 - a3) % mod * imag.val;
+            u64 na2 = mod2 - a2;
             a[i + offset] = a0 + a2 + a1 + a3;
             a[i + offset + 1 * p] = a0 + a2 + (2 * mod2 - (a1 + a3));
             a[i + offset + 2 * p] = a0 + na2 + a1na3imag;
             a[i + offset + 3 * p] = a0 + na2 + (mod2 - a1na3imag);
           }
-          rot *= info.rate3[topbit(~s & -~s)];
+          rot *= rate3[topbit(~s & -~s)];
         }
         len += 2;
       }
@@ -118,44 +90,33 @@ void ntt(vector<mint>& a, bool inverse) {
         FOR(s, 1 << (len - 1)) {
           int offset = s << (h - len + 1);
           FOR(i, p) {
-            auto l = a[i + offset];
-            auto r = a[i + offset + p];
+            u64 l = a[i + offset].val;
+            u64 r = a[i + offset + p].val;
             a[i + offset] = l + r;
-            a[i + offset + p]
-                = (unsigned long long)(mint::get_mod() + l.val - r.val)
-                  * irot.val;
-            ;
+            a[i + offset + p] = (mod + l - r) * irot.val;
           }
-          irot *= info.irate2[topbit(~s & -~s)];
+          irot *= irate2[topbit(~s & -~s)];
         }
         len--;
       } else {
         int p = 1 << (h - len);
-        mint irot = 1, iimag = info.iroot[2];
+        mint irot = 1, iimag = iroot[2];
         FOR(s, (1 << (len - 2))) {
           mint irot2 = irot * irot;
           mint irot3 = irot2 * irot;
           int offset = s << (h - len + 2);
           for (int i = 0; i < p; i++) {
-            auto a0 = 1ULL * a[i + offset + 0 * p].val;
-            auto a1 = 1ULL * a[i + offset + 1 * p].val;
-            auto a2 = 1ULL * a[i + offset + 2 * p].val;
-            auto a3 = 1ULL * a[i + offset + 3 * p].val;
-
-            auto a2na3iimag
-                = 1ULL * mint((mint::get_mod() + a2 - a3) * iimag.val).val;
-
+            u64 a0 = a[i + offset + 0 * p].val;
+            u64 a1 = a[i + offset + 1 * p].val;
+            u64 a2 = a[i + offset + 2 * p].val;
+            u64 a3 = a[i + offset + 3 * p].val;
+            u64 x = (mod + a2 - a3) * iimag.val % mod;
             a[i + offset] = a0 + a1 + a2 + a3;
-            a[i + offset + 1 * p]
-                = (a0 + (mint::get_mod() - a1) + a2na3iimag) * irot.val;
-            a[i + offset + 2 * p]
-                = (a0 + a1 + (mint::get_mod() - a2) + (mint::get_mod() - a3))
-                  * irot2.val;
-            a[i + offset + 3 * p]
-                = (a0 + (mint::get_mod() - a1) + (mint::get_mod() - a2na3iimag))
-                  * irot3.val;
+            a[i + offset + 1 * p] = (a0 + mod - a1 + x) * irot.val;
+            a[i + offset + 2 * p] = (a0 + a1 + 2 * mod - a2 - a3) * irot2.val;
+            a[i + offset + 3 * p] = (a0 + 2 * mod - a1 - x) * irot3.val;
           }
-          irot *= info.irate3[topbit(~s & -~s)];
+          irot *= irate3[topbit(~s & -~s)];
         }
         len -= 2;
       }
