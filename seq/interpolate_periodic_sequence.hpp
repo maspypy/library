6a567792cbf97147696bbc0dc872148b20628e46
maspypy <maspypy.gmail.com>
2023-02-23 07:06:37 +0900

    change name

--- seq/interpolate_periodic_sequence.hpp
+++ seq/interpolate_periodic_sequence.hpp
@@ -2,16 +2,15 @@
 
 // 012[345][345][345] みたいなやつ
 template <typename T>
-struct Interpolate_Periodic_Sequence_Query {
+struct Interpolate_Periodic_Sequence {
   vc<T> dat;
   int p;
 
-  Interpolate_Periodic_Sequence_Query(vc<T> A) : dat(A) {
+  Interpolate_Periodic_Sequence(vc<T> A) : dat(A) {
     reverse(all(A));
     auto Z = zalgorithm(A);
     Z[0] = 0;
     p = max_element(all(Z)) - Z.begin();
-    FOR(i, p) assert(A[i] == A[p + i]);
   }
 
   T operator[](ll n) {
