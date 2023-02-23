e1af4d9c96bf9fd0101ac2bdfe242b0a99408324
maspypy <maspypy.gmail.com>
2023-02-23 16:42:59 +0900

    a

--- enumerate/bits.hpp
+++ enumerate/bits.hpp
@@ -1,5 +1,14 @@
+template <typename F>
+void enumerate_bits(int s, F f) {
+  while (s) {
+    int i = __builtin_ctz(s);
+    f(i);
+    s ^= 1 << i;
+  }
+}
+
 template <typename BS, typename F>
-void enumerate_bits(BS& b, int L, int R, F f) {
+void enumerate_bits_bitset(BS& b, int L, int R, F f) {
   int p = (b[L] ? L : b._Find_next(L));
   while (p < R) {
     f(p);
