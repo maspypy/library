18a942add5783a0cc98e28f80c4a08491b89f65b
maspypy <maspypy.gmail.com>
2023-02-25 00:04:38 +0900

    upd lis

--- seq/longest_increasing_subsequence.hpp
+++ seq/longest_increasing_subsequence.hpp
@@ -1,13 +1,15 @@
 // dp[i] := 第 i 項で終わる lis 長の最大値（[1, LIS]）
 template <typename T, bool strong = true>
-vc<int> longest_increasing_subsequence(vector<T> A) {
+pair<int, vc<int>> longest_increasing_subsequence(vector<T> A) {
   const int N = A.size();
   vc<T> dp(N, infty<T>);
+  int lis = 0;
   vc<int> lis_rank(N);
   FOR(i, N) {
     int j = (strong ? LB(dp, A[i]) : UB(dp, A[i]));
     dp[j] = A[i];
     lis_rank[i] = j + 1;
+    chmax(lis, j + 1);
   }
-  return lis_rank;
+  return {lis, lis_rank};
 }
