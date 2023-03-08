46fbde89916cd1e87621bc06024e22b17d7754b8
maspypy <maspypy.gmail.com>
2023-03-09 02:49:40 +0900

    change ui

--- ds/intervals.hpp
+++ ds/intervals.hpp
@@ -1,127 +1,3 @@
-// https://codeforces.com/contest/1638/problem/E
-// https://codeforces.com/contest/897/problem/E
-// 持つ値のタイプ T、座標タイプ X
-// コンストラクタでは T none_val を指定する
-template <typename T = ll, typename X = ll>
-struct Intervals {
-  static constexpr X LLIM = -infty<X>;
-  static constexpr X RLIM = infty<X>;
-  const T none_val;
-  // none_val でない区間の個数と長さ合計
-  int total_num;
-  X total_len;
-  map<X, T> dat;
-
-  Intervals(T none_val) : none_val(none_val), total_num(0), total_len(0) {
-    dat[LLIM] = none_val;
-    dat[RLIM] = none_val;
-  }
-
-  tuple<X, X, T> get(X x) {
-    auto it = dat.upper_bound(x);
-    X r = (*it).fi;
-    auto [l, t] = *prev(it);
-    return {l, r, t};
-  }
-
-  template <typename ADD, typename RM>
-  void set(X L, X R, T t, ADD& add_f, RM& rm_f) {
-    if (L == R) return;
-    assert(L < R);
-    // 区間 [l, r) を t に変更する
-    // まずは、重なるか隣り合う区間を全列挙
-    vc<tuple<X, X, T>> tmp;
-    auto it = prev(dat.lower_bound(L));
-    while (1) {
-      auto [l, t] = *it;
-      if (R < l) break;
-      it = next(it);
-      X r = (*it).fi;
-      tmp.eb(l, r, t);
-    }
-    auto [lx, _, lt] = tmp[0];
-    auto [__, rx, rt] = tmp.back();
-    // とりあえず全部削除
-    for (auto&& [l, r, t]: tmp) {
-      dat.erase(l);
-      if (t == none_val) continue;
-      total_num--;
-      total_len -= r - l;
-      rm_f(l, r, t);
-    }
-    if (lt == t) chmin(L, lx);
-    if (rt == t) chmax(R, rx);
-    if (lx < L) {
-      // [lx, L)
-      dat[lx] = lt;
-      if (lt != none_val) {
-        total_num++;
-        total_len += L - lx;
-        add_f(lx, L, lt);
-      }
-    }
-    if (R < rx) {
-      // [R, rx)
-      dat[R] = rt;
-      if (rt != none_val) {
-        total_num++;
-        total_len += rx - R;
-        add_f(R, rx, rt);
-      }
-    }
-    // [L, R)
-    dat[L] = t;
-    if (t != none_val) {
-      total_num++;
-      total_len += R - L;
-      add_f(L, R, t);
-    }
-  }
-
-  void set(X L, X R, T t = 1) {
-    auto f = [&](X L, X R, T t) -> void {};
-    set(L, R, t, f, f);
-  }
-
-  void erase(X L, X R) {
-    auto f = [&](X L, X R, T t) -> void {};
-    set(L, R, none_val, f, f);
-  }
-
-  // L, R 内のデータ (l, r, t) を全部取得する
-  vc<tuple<X, X, T>> get(X L, X R) {
-    vc<tuple<X, X, T>> res;
-    auto it = dat.lower_bound(L);
-    if (it != dat.begin()) it = prev(it);
-    while (1) {
-      auto [l, t] = *it;
-      if (R <= l) break;
-      it = next(it);
-      X r = (*it).fi;
-      X l0 = max(l, L);
-      X r0 = min(r, R);
-      if (l0 < r0) res.eb(l0, r0, t);
-    }
-    return res;
-  }
-
-  vc<tuple<X, X, T>> get_all() { return get(LLIM, RLIM); }
-
-  void debug() {
-    auto it = dat.begin();
-    print("Intervals");
-    print("total_num", total_num);
-    print("total_len", total_len);
-    while (1) {
-      auto [l, t] = *it;
-      ++it;
-      if (it == dat.end()) break;
-      X r = (*it).fi;
-      print("l, r, t", l, r, t);
-    }
-  }
-};
-
 #include "ds/fastset.hpp"
 
 // FastSet で高速化したもの
@@ -142,112 +18,182 @@ struct Intervals_Fast {
         total_num(0),
         total_len(0),
         dat(N, none_val),
-        ss(N + 1) {
+        ss(N) {
     ss.insert(0);
-    ss.insert(N);
   }
 
-  tuple<int, int, T> get(int x) {
-    auto l = ss.prev(x);
-    auto r = ss.next(x + 1);
-    return {l, r, dat[l]};
+  // x を含む区間の情報の取得
+  tuple<int, int, T> get(int x, bool ERASE) {
+    int l = ss.prev(x);
+    int r = ss.next(x + 1);
+    T t = dat[l];
+    if (x != none_val && ERASE) {
+      --total_num, total_len -= r - l;
+      dat[l] = none_val;
+      merge_at(l);
+      merge_at(r);
+    }
+    return {l, r, t};
   }
 
-  template <typename ADD, typename RM>
-  void set(int L, int R, T t, ADD& add_f, RM& rm_f) {
+  // [L, R) 内の全データの取得
+  template <typename F>
+  void enumerate_range(int L, int R, F f, bool ERASE) {
     assert(LLIM <= L && L <= R && R <= RLIM);
-    if (L == R) return;
-    assert(L < R);
-    // 区間 [l, r) を t に変更する
-    // まずは、重なるか隣り合う区間を全列挙
-    vc<tuple<int, int, T>> tmp;
-    auto l = ss.prev(L);
-    while (l < R) {
-      auto r = ss.next(l + 1);
-      auto t = dat[l];
-      tmp.eb(l, r, t);
-      l = r;
-    }
-    auto [lx, _, lt] = tmp[0];
-    auto [__, rx, rt] = tmp.back();
-    // とりあえず全部削除
-    for (auto&& [l, r, t]: tmp) {
-      ss.erase(l);
-      if (t == none_val) continue;
-      total_num--;
-      total_len -= r - l;
-      rm_f(l, r, t);
-    }
-    if (lt == t) chmin(L, lx);
-    if (rt == t) chmax(R, rx);
-    if (lx < L) {
-      // [lx, L)
-      ss.insert(lx);
-      dat[lx] = lt;
-      if (lt != none_val) {
-        total_num++;
-        total_len += L - lx;
-        add_f(lx, L, lt);
+    if (!ERASE) {
+      int l = ss.prev(L);
+      while (l < R) {
+        int r = ss.next(l + 1);
+        f(max(l, L), min(r, R), dat[l]);
+        l = r;
       }
+      return;
     }
-    if (R < rx) {
-      // [R, rx)
+    // 半端なところの分割
+    int p = ss.prev(L);
+    if (p < L) {
+      ss.insert(L);
+      dat[L] = dat[p];
+      if (dat[L] != none_val) ++total_num;
+    }
+    p = ss.next(R);
+    if (R < p) {
+      dat[R] = dat[ss.prev(R)];
       ss.insert(R);
-      dat[R] = rt;
-      if (rt != none_val) {
-        total_num++;
-        total_len += rx - R;
-        add_f(R, rx, rt);
-      }
+      if (dat[R] != none_val) ++total_num;
     }
-    // [L, R)
+    p = L;
+    while (p < R) {
+      int q = ss.next(p + 1);
+      T x = dat[p];
+      f(p, q, x);
+      if (dat[p] != none_val) --total_num, total_len -= q - p;
+      ss.erase(p);
+      p = q;
+    }
     ss.insert(L);
-    dat[L] = t;
-    if (t != none_val) {
-      total_num++;
-      total_len += R - L;
-      add_f(L, R, t);
-    }
+    dat[L] = none_val;
   }
 
   void set(int L, int R, T t) {
-    auto f = [&](int L, int R, T t) -> void {};
-    set(L, R, t, f, f);
+    enumerate_range(
+        L, R, [](int l, int r, T x) -> void {}, true);
+    if (t == none_val) return;
+    ss.insert(L);
+    dat[L] = t;
+    total_num++, total_len += R - L;
+    merge_at(L);
+    merge_at(R);
   }
 
-  void erase(int L, int R) {
-    auto f = [&](int L, int R, T t) -> void {};
-    set(L, R, none_val, f, f);
+  template <typename F>
+  void enumerate_all(F f) {
+    enumerate_range(0, RLIM, f, false);
   }
 
-  // L, R 内のデータ (l, r, t) を全部取得する
-  vc<tuple<int, int, T>> get(int L, int R) {
-    assert(LLIM <= L && L <= R && R <= RLIM);
-    vc<tuple<int, int, T>> res;
-    auto l = ss.prev(L);
-    while (l < R) {
-      auto t = dat[l];
-      auto r = ss.next(l + 1);
-      int l0 = max(l, L);
-      int r0 = min(r, R);
-      if (l0 < r0) res.eb(l0, r0, t);
-      l = r;
+  void merge_at(int p) {
+    if (p <= 0 || RLIM <= p) return;
+    int q = ss.prev(p - 1);
+    if (dat[p] == dat[q]) {
+      if (dat[p] != none_val) --total_num;
+      ss.erase(p);
     }
-    return res;
+  }
+};
+
+// https://codeforces.com/contest/1638/problem/E
+// 持つ値のタイプ T、座標タイプ X
+// コンストラクタでは T none_val を指定する
+template <typename T, typename X = ll>
+struct Intervals {
+  static constexpr X LLIM = -infty<X>;
+  static constexpr X RLIM = infty<X>;
+  const T none_val;
+  // none_val でない区間の個数と長さ合計
+  int total_num;
+  X total_len;
+  map<X, T> dat;
+
+  Intervals(T none_val) : none_val(none_val), total_num(0), total_len(0) {
+    dat[LLIM] = none_val;
+    dat[RLIM] = none_val;
   }
 
-  vc<tuple<int, int, T>> get_all() { return get(LLIM, RLIM); }
+  // x を含む区間の情報の取得
+  tuple<X, X, T> get(X x, bool ERASE) {
+    auto it2 = dat.upper_bound(x);
+    auto it1 = prev(it2);
+    auto [l, tl] = *it1;
+    auto [r, tr] = *it2;
+    if (x != none_val && ERASE) {
+      --total_num, total_len -= r - l;
+      dat[l] = none_val;
+      merge_at(l);
+      merge_at(r);
+    }
+    return {l, r, tl};
+  }
 
-  void debug() {
-    print("Intervals");
-    print("total_num", total_num);
-    print("total_len", total_len);
-    int l = 0;
-    while (l < RLIM) {
-      auto t = dat[l];
-      auto r = ss.next(l + 1);
-      print("l, r, t", l, r, t);
-      l = r;
+  // [L, R) 内の全データの取得
+  template <typename F>
+  void enumerate_range(X L, X R, F f, bool ERASE) {
+    assert(LLIM <= L && L <= R && R <= RLIM);
+    if (!ERASE) {
+      auto it = prev(dat.upper_bound(L));
+      while ((*it).fi < R) {
+        auto it2 = next(it);
+        f(max((*it).fi, L), min((*it2).fi, R), (*it).se);
+        it = it2;
+      }
+      return;
+    }
+    // 半端なところの分割
+    auto p = prev(dat.upper_bound(L));
+    if ((*p).fi < L) {
+      dat[L] = (*p).se;
+      if (dat[L] != none_val) ++total_num;
+    }
+    p = dat.lower_bound(R);
+    if (R < (*p).fi) {
+      T t = (*prev(p)).se;
+      dat[R] = t;
+      if (t != none_val) ++total_num;
+    }
+    while (1) {
+      auto p = dat.lower_bound(L);
+      if ((*p).fi >= R) break;
+      auto q = next(p);
+      T t = (*p).se;
+      f((*p).fi, (*q).fi, t);
+      if (t != none_val) --total_num, total_len -= (*q).fi - (*p).fi;
+      dat.erase(p);
+    }
+    dat[L] = none_val;
+  }
+
+  void set(X L, X R, T t) {
+    enumerate_range(
+        L, R, [](int l, int r, T x) -> void {}, true);
+    if (t == none_val) return;
+    dat[L] = t;
+    total_num++, total_len += R - L;
+    merge_at(L);
+    merge_at(R);
+  }
+
+  template <typename F>
+  void enumerate_all(F f) {
+    enumerate_range(LLIM, RLIM, f, false);
+  }
+
+  void merge_at(X p) {
+    if (p == LLIM || RLIM == p) return;
+    auto itp = dat.lower_bound(p);
+    assert((*itp).fi == p);
+    auto itq = prev(itp);
+    if ((*itp).se == (*itq).se) {
+      if ((*itp).se != none_val) --total_num;
+      dat.erase(itp);
     }
   }
 };
