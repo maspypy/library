ae693d011daed505ae8724a8be9b8681f6f86ce9
maspypy <45988013+maspypy@users.noreply.github.com>
2023-03-12 14:52:40 +0900

    Update 1339.test.cpp

--- test/yukicoder/1339.test.cpp
+++ test/yukicoder/1339.test.cpp
@@ -2,24 +2,20 @@
 #include "my_template.hpp"
 #include "other/io.hpp"
 #include "nt/discrete_log.hpp"
-#include "mod/modint.hpp"
+#include "mod/dynamic_modint.hpp"
 #include "alg/monoid/mul.hpp"
 
 void solve() {
   LL(N);
   while (N % 2 == 0) N /= 2;
   while (N % 5 == 0) N /= 5;
-  amint::set_mod(N);
-  auto H = [&](amint x) -> ll { return x.val; };
-  ll k = discrete_log_group<Monoid_Mul<amint>>(amint(10), amint(1), H, 1, N + 10);
+  dmint::set_mod(N);
+  auto H = [&](dmint x) -> ll { return x.val; };
+  ll k = discrete_log_group<Monoid_Mul<dmint>>(dmint(10), dmint(1), H, 1, N + 10);
   print(k);
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << fixed << setprecision(15);
-
   LL(T);
   FOR(T) solve();
 
