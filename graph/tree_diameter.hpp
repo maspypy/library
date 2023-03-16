bb35d920f429bcbf3365b5463c58f50f74de4790
maspypy <maspypy.gmail.com>
2023-03-17 00:39:46 +0900

    diam dp

--- graph/tree_diameter.hpp
+++ graph/tree_diameter.hpp
@@ -4,6 +4,11 @@
 template <typename T, typename GT>
 pair<T, vc<int>> tree_diameter(GT& G) {
   assert(G.is_prepared());
+  T sm = 0;
+  for (auto&& e: G.edges) {
+    sm += e.cost;
+    assert(sm < infty<T>);
+  }
   int A, B;
   auto [distA, parA] = bfs01<T>(G, 0);
   A = max_element(all(distA)) - distA.begin();
