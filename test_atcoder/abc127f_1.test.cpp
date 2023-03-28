b2221802241aaa1d99c058d2e631d8da6cf0077e
maspypy <maspypy.gmail.com>
2023-03-29 00:17:17 +0900

    sum_all

--- test_atcoder/abc127f_1.test.cpp
+++ test_atcoder/abc127f_1.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://atcoder.jp/contests/abc127/tasks/abc127_f"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "ds/wavelet_matrix_sum.hpp"
+#include "ds/wavelet_matrix.hpp"
 
 void solve() {
   LL(Q);
@@ -15,13 +15,13 @@ void solve() {
     }
     if (t == 2) { query.eb(len(A)); }
   }
-  Wavelet_Matrix_Sum<ll, 1> X(A);
+  Wavelet_Matrix<ll, 1> X(A, A);
   auto Bc = cumsum<ll>(B);
 
   for (auto&& n: query) {
     ll cnt_lo = (n - 1) / 2;
-    auto [med, sum_lo] = X.kth(0, n, cnt_lo);
-    auto total = X.kth(0, n, n).se;
+    auto [med, sum_lo] = X.kth_value_and_sum(0, n, cnt_lo);
+    auto total = X.sum(0, n, 0, n);
     ll cnt_hi = n - cnt_lo;
     ll sum_hi = total - sum_lo;
     ll ANS = Bc[n];
