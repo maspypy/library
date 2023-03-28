529cebb659562f1537faebfe0b1c25d34e470da8
maspypy <maspypy.gmail.com>
2023-03-28 23:05:41 +0900

    refine wavelet

--- ds/wavelet_matrix.hpp
+++ ds/wavelet_matrix.hpp
@@ -1,17 +1,24 @@
 #include "ds/bit_vector.hpp"
+#include "alg/monoid/add.hpp"
 
 // 座圧するかどうかを COMPRESS で指定する
 // xor 的な使い方をする場合には、コンストラクタで log を渡すこと
-template <typename T, bool COMPRESS>
+template <typename T, bool COMPRESS, typename Monoid = Monoid_Add<T>>
 struct Wavelet_Matrix {
+  using MX = Monoid;
+  using X = typename MX::value_type;
   int N, lg;
   vector<int> mid;
   vector<Bit_Vector> bv;
   vc<T> key;
   const bool set_log;
+  vvc<X> cumsum;
 
-  Wavelet_Matrix(vector<T> A, int log = -1)
+  // 和を使わないなら、SUM_data は空でよい
+  Wavelet_Matrix(vc<T> A, vc<X> SUM_data = {}, int log = -1)
       : N(len(A)), lg(log), set_log(log != -1) {
+    bool MAKE_SUM = !(SUM_data.empty());
+    vc<X>& S = SUM_data;
     if (COMPRESS) {
       assert(!set_log);
       key.reserve(N);
@@ -25,18 +32,30 @@ struct Wavelet_Matrix {
     if (lg == -1) lg = __lg(max<ll>(MAX(A), 1)) + 1;
     mid.resize(lg);
     bv.assign(lg, Bit_Vector(N));
+    if (MAKE_SUM) cumsum.assign(1 + lg, vc<X>(N + 1, MX::unit()));
     vc<T> A0(N), A1(N);
-    FOR_R(d, lg) {
+    vc<X> S0(N), S1(N);
+    FOR_R(d, -1, lg) {
       int p0 = 0, p1 = 0;
+      if (MAKE_SUM) {
+        FOR(i, N) { cumsum[d + 1][i + 1] = MX::op(cumsum[d + 1][i], S[i]); }
+      }
+      if (d == -1) break;
       FOR(i, N) {
         bool f = (A[i] >> d & 1);
-        if (!f) A0[p0++] = A[i];
-        if (f) bv[d].set(i), A1[p1++] = A[i];
+        if (!f) {
+          if (MAKE_SUM) S0[p0] = S[i];
+          A0[p0++] = A[i];
+        }
+        if (f) {
+          if (MAKE_SUM) S1[p1] = S[i];
+          bv[d].set(i), A1[p1++] = A[i];
+        }
       }
       mid[d] = p0;
       bv[d].build();
-      swap(A, A0);
-      FOR(i, p1) A[p0 + i] = A1[i];
+      swap(A, A0), swap(S, S0);
+      FOR(i, p1) A[p0 + i] = A1[i], S[p0 + i] = S1[i];
     }
   }
 
@@ -45,23 +64,7 @@ struct Wavelet_Matrix {
     return prefix_count(L, R, b, xor_val) - prefix_count(L, R, a, xor_val);
   }
 
-  // xor した結果で [0, x) に収まるものを数える
-  int prefix_count(int L, int R, T x, T xor_val = 0) {
-    if (xor_val != 0) assert(set_log);
-    x = (COMPRESS ? LB(key, x) : x);
-    if (x >= (1 << lg)) return R - L;
-    int ret = 0;
-    FOR_R(d, lg) {
-      bool add = (x >> d) & 1;
-      bool f = ((x ^ xor_val) >> d) & 1;
-      if (add) ret += bv[d].rank(R, !f) - bv[d].rank(L, !f);
-      L = bv[d].rank(L, f) + (f ? mid[d] : 0);
-      R = bv[d].rank(R, f) + (f ? mid[d] : 0);
-    }
-    return ret;
-  }
-
-  // [L, R) の中で k>=0 番目
+  // xor した結果で、[L, R) の中で k>=0 番目
   T kth(int L, int R, int k, T xor_val = 0) {
     if (xor_val != 0) assert(set_log);
     assert(0 <= k && k < R - L);
@@ -79,6 +82,97 @@ struct Wavelet_Matrix {
         if (f) L = l0, R = r0;
       }
     }
-    return (COMPRESS ? key[ret] : ret);
+    if (COMPRESS) ret = key[ret];
+    return ret;
+  }
+
+  // xor した結果で [k1, k2) 番目であるところの SUM_data の和
+  X sum(int L, int R, int k1, int k2, T xor_val = 0) {
+    return prefix_sum(L, R, k2, xor_val) - prefix_sum(L, R, k1, xor_val);
+  }
+
+  // check(cnt, prefix sum) が true となるような最大個数
+  template <typename F>
+  int max_right(F check, int L, int R, T xor_val = 0) {
+    assert(check(0, MX::unit()));
+    if (xor_val != 0) assert(set_log);
+    if (check(R - L, get(lg, L, R))) return R - L;
+    int cnt = 0;
+    X sm = MX::unit();
+    for (int d = lg - 1; d >= 0; --d) {
+      bool f = (xor_val >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int c = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      X s = (f ? get(d, L + mid[d] - l0, R + mid[d] - r0) : get(d, l0, r0));
+      if (check(cnt + c, MX::op(sm, s))) {
+        cnt += c, sm = MX::op(sm, s);
+        if (f) L = l0, R = r0;
+        if (!f) L += mid[d] - l0, R += mid[d] - r0;
+      } else {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      }
+    }
+    cnt += binary_search(
+        [&](int k) -> bool {
+          return check(cnt + k, MX::op(sm, get(0, L, L + k)));
+        },
+        0, R - L);
+    return cnt;
+  }
+
+private:
+  inline X get(int d, int L, int R) {
+    return MX::op(MX::inverse(cumsum[d][L]), cumsum[d][R]);
+  }
+
+  // xor した結果で [0, x) に収まるものを数える
+  int prefix_count(int L, int R, T x, T xor_val = 0) {
+    if (xor_val != 0) assert(set_log);
+    x = (COMPRESS ? LB(key, x) : x);
+    if (x == 0) return 0;
+    if (x >= (1 << lg)) return R - L;
+    int cnt = 0;
+    FOR_R(d, lg) {
+      bool add = (x >> d) & 1;
+      bool f = ((xor_val) >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int kf = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      if (add) {
+        cnt += kf;
+        if (f) { L = l0, R = r0; }
+        if (!f) { L += mid[d] - l0, R += mid[d] - r0; }
+      } else {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      }
+    }
+    return cnt;
+  }
+
+  // xor した結果で [0, k) 番目のものの和
+  X prefix_sum(int L, int R, int k, T xor_val = 0) {
+    if (xor_val != 0) assert(set_log);
+    assert(0 <= k && k <= R - L);
+    if (k == 0) return MX::unit();
+    assert(!cumsum.empty());
+
+    X sm = MX::unit();
+    for (int d = lg - 1; d >= 0; --d) {
+      bool f = (xor_val >> d) & 1;
+      int l0 = bv[d].rank(L, 0), r0 = bv[d].rank(R, 0);
+      int c = (f ? (R - L) - (r0 - l0) : (r0 - l0));
+      X s = (f ? get(d, L + mid[d] - l0, R + mid[d] - r0) : get(d, l0, r0));
+      if (k < c) {
+        if (!f) L = l0, R = r0;
+        if (f) L += mid[d] - l0, R += mid[d] - r0;
+      } else {
+        k -= c, sm = MX::op(sm, s);
+        if (f) { L = l0, R = r0; }
+        if (!f) { L += mid[d] - l0, R += mid[d] - r0; }
+      }
+    }
+    if (k) sm = MX::op(sm, get(0, L, L + k));
+    return sm;
   }
 };
--- test/mytest/wavelet_matrix.test.cpp
+++ test/mytest/wavelet_matrix.test.cpp
@@ -6,10 +6,13 @@
 
 void test_compress() {
   int N = RNG(1, 64);
-  int MAX = 1 << 10;
+  int MAX = RNG(2, 1 << 10);
   vc<int> A(N);
+  vc<int> X(N);
+  FOR(i, N) X[i] = RNG(MAX);
   FOR(i, N) A[i] = RNG(MAX);
-  Wavelet_Matrix<int, true> WM(A);
+
+  Wavelet_Matrix<int, true> WM(A, X);
 
   int Q = 100;
   FOR(Q) {
@@ -21,35 +24,57 @@ void test_compress() {
     int hi = RNG(0, MAX);
     if (lo > hi) swap(lo, hi);
     ++hi;
-    int k = RNG(R - L);
     vc<int> B = {A.begin() + L, A.begin() + R};
+    vc<int> Y = {X.begin() + L, X.begin() + R};
 
-    int t = RNG(0, 3);
+    int t = RNG(0, 4);
     if (t == 0) { // count
       int cnt = 0;
       for (auto&& x: B)
-        if (lo <= x && x < hi) ++cnt;
+        if (lo <= x && x < hi) cnt += 1;
       assert(WM.count(L, R, lo, hi) == cnt);
     }
-    if (t == 1) { // count_prefix
-      int cnt = 0;
-      for (auto&& x: B)
-        if (x < hi) ++cnt;
-      assert(WM.prefix_count(L, R, hi) == cnt);
+    if (t == 1) { // sm
+      int sm = 0;
+      int k1 = RNG(R - L + 1);
+      int k2 = RNG(R - L + 1);
+      if (k1 > k2) swap(k1, k2);
+      auto I = argsort(B);
+      FOR(i, k1, k2) sm += Y[I[i]];
+      assert(WM.sum(L, R, k1, k2) == sm);
     }
     if (t == 2) { // kth
+      int k = RNG(R - L);
       sort(all(B));
       assert(WM.kth(L, R, k) == B[k]);
+    }
+    if (t == 3) { // max_right
+      int a = RNG(0, 10);
+      int b = RNG(0, 10);
+      int c = RNG(0, a * (R - L) + b * MAX * (R - L) + 1);
+      auto check
+          = [&](int cnt, int sm) -> bool { return a * cnt + b * sm <= c; };
+      int k = WM.max_right(check, L, R);
+      int k2 = binary_search(
+          [&](int k) -> bool {
+            int sm = WM.sum(L, R, 0, k);
+            return check(k, sm);
+          },
+          0, R - L + 1);
+      assert(k == k2);
     }
   }
 }
 
 void test_not_compress() {
   int N = RNG(1, 64);
-  int MAX = 64;
+  int log = RNG(1, 7);
+  int MAX = 1 << log;
   vc<int> A(N);
+  vc<int> X(N);
+  FOR(i, N) X[i] = RNG(0, MAX);
   FOR(i, N) A[i] = RNG(MAX);
-  Wavelet_Matrix<int, false> WM(A, 6);
+  Wavelet_Matrix<int, false> WM(A, X, log);
 
   int Q = 100;
   FOR(Q) {
@@ -59,31 +84,49 @@ void test_not_compress() {
     ++R;
     int lo = RNG(0, MAX);
     int hi = RNG(0, MAX);
+    int xor_val = RNG(0, MAX);
     if (lo > hi) swap(lo, hi);
     ++hi;
-    int k = RNG(R - L);
-    int xor_val = RNG(MAX);
     vc<int> B = {A.begin() + L, A.begin() + R};
+    vc<int> Y = {X.begin() + L, X.begin() + R};
+    for (auto&& x: B) x ^= xor_val;
 
     int t = RNG(0, 3);
     if (t == 0) { // count
-      for (auto&& x: B) x ^= xor_val;
       int cnt = 0;
-      for (auto&& x: B)
-        if (lo <= x && x < hi) ++cnt;
+      for (auto&& x: B) {
+        if (lo <= x && x < hi) cnt += 1;
+      }
       assert(WM.count(L, R, lo, hi, xor_val) == cnt);
     }
-    if (t == 1) { // count_prefix
-      for (auto&& x: B) x ^= xor_val;
-      int cnt = 0;
-      for (auto&& x: B)
-        if (x < hi) ++cnt;
-      assert(WM.prefix_count(L, R, hi, xor_val) == cnt);
+    if (t == 1) { // sm
+      int sm = 0;
+      int k1 = RNG(R - L + 1);
+      int k2 = RNG(R - L + 1);
+      if (k1 > k2) swap(k1, k2);
+      auto I = argsort(B);
+      FOR(i, k1, k2) sm += Y[I[i]];
+      assert(WM.sum(L, R, k1, k2, xor_val) == sm);
     }
-    if (t == 2) { // xor_kth
-      for (auto&& x: B) x ^= xor_val;
+    if (t == 2) { // kth
+      int k = RNG(R - L);
       sort(all(B));
       assert(WM.kth(L, R, k, xor_val) == B[k]);
+    }
+    if (t == 3) { // max_right
+      int a = RNG(0, 10);
+      int b = RNG(0, 10);
+      int c = RNG(0, a * (R - L) + b * MAX * (R - L) + 1);
+      auto check
+          = [&](int cnt, int sm) -> bool { return a * cnt + b * sm <= c; };
+      int k = WM.max_right(check, L, R, xor_val);
+      int k2 = binary_search(
+          [&](int k) -> bool {
+            int sm = WM.sum(L, R, 0, k, xor_val);
+            return check(k, sm);
+          },
+          0, R - L + 1);
+      assert(k == k2);
     }
   }
 }
