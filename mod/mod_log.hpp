06bbe5313312409e69cbde2a37274041005a30d1
maspypy <maspypy.gmail.com>
2023-03-28 11:16:12 +0900

    monoid discrete log

--- mod/mod_log.hpp
+++ mod/mod_log.hpp
@@ -3,22 +3,7 @@
 #include "alg/monoid/mul.hpp"
 
 int mod_log(int mod, ll a, ll b) {
-  a = divmod(a, mod).se;
-  b = divmod(b, mod).se;
-  // まず群に帰着する。小さい場合は調べる
-  ll p = 1 % mod;
-  FOR(k, 32) {
-    if (p == b) return k;
-    p = p * a % mod;
-  }
-  if (a == 0 || b == 0) return -1;
-  ll g = gcd(mod, p);
-  if (b % g != 0) return -1;
-  mod /= g;
-  a %= mod, b %= mod;
-  if (gcd(b, mod) > 1) return -1;
-  // 群に帰着された
   dmint::set_mod(mod);
-  return discrete_log_group<Monoid_Mul<dmint>>(
-      dmint(a), dmint(b), [](auto x) { return x.val; }, 32, mod);
+  return discrete_log_monoid<Monoid_Mul<dmint>>(
+      dmint(a), dmint(b), [](auto x) { return x.val; }, 0, mod);
 }
--- test_atcoder/abc222g.test.cpp
+++ test_atcoder/abc222g.test.cpp
@@ -11,11 +11,8 @@ using AS = ActedSet_Affine<mint>;
 
 void solve() {
   LL(mod);
-  // repunit に帰着 → 10 が可逆元になるようにする
-  if (mod % 4 == 0 || mod % 5 == 0) return print(-1);
-  if (mod % 2 == 0) mod /= 2;
   mint::set_mod(mod);
-  pair<mint, mint> a = {mint(10), mint(1)};
+  pair<mint, mint> a = {mint(10), mint(2)};
   auto H = [&](mint x) -> int { return x.val; };
   ll ANS = discrete_log_acted<AS>(a, 0, 0, H, 1, mod + 100);
   print(ANS);
