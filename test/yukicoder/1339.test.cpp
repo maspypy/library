587c193282c0db91644acb4c2a6940f8e810406d
maspypy <45988013+maspypy@users.noreply.github.com>
2023-03-28 11:49:52 +0900

    Update 1339.test.cpp

--- test/yukicoder/1339.test.cpp
+++ test/yukicoder/1339.test.cpp
@@ -11,7 +11,7 @@ void solve() {
   while (N % 5 == 0) N /= 5;
   dmint::set_mod(N);
   auto H = [&](dmint x) -> ll { return x.val; };
-  ll k = discrete_log_group<Monoid_Mul<dmint>>(dmint(10), dmint(1), H, 1, N + 10);
+  ll k = discrete_log_monoid<Monoid_Mul<dmint>>(dmint(10), dmint(1), H, 1, N + 10);
   print(k);
 }
 
