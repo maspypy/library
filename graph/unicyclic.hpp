8248dd4b43d10e17f26e4c3e9c1c55699a454657
maspypy <maspypy.gmail.com>
2023-03-30 23:49:33 +0900

    make tree

--- graph/unicyclic.hpp
+++ graph/unicyclic.hpp
@@ -1,56 +1,61 @@
 #include "graph/base.hpp"
+#include "graph/tree.hpp"
+#include "ds/unionfind/unionfind.hpp"
 
+template <typename GT, bool DIRECTED = true>
 struct UnicyclicGraph {
+  using T = typename GT::cost_type;
+  GT& G0;
+  Graph<T, DIRECTED> G;
+  int N;
   int root;
-  Graph<int, 1> tree;
+  int out_eid;
   vc<int> TO;
   vc<int> cycle;     // 根に向かうような頂点列
   vc<bool> in_cycle; // vertex id -> bool
 
-  template <typename GT>
-  UnicyclicGraph(GT& G) : tree(G.N) {
-    int N = G.N;
-    assert(N == G.M);
+  UnicyclicGraph(GT& G) : G0(G), N(G.N) {
+    assert(!G.is_directed() && N == G.M);
+    UnionFind uf(N);
     TO.assign(N, -1);
+    FOR(eid, N) {
+      auto& e = G.edges[eid];
+      if (uf.merge(e.frm, e.to)) continue;
+      out_eid = eid;
+      root = e.frm;
+      TO[root] = e.to;
+      break;
+    }
     vc<bool> done(N);
-    vc<int> que;
-    auto deg = G.deg_array();
-    FOR(v, N) if (deg[v] == 1) que.eb(v);
+    vc<int> que = {root};
     while (len(que)) {
-      auto v = que.back();
-      que.pop_back();
+      int v = POP(que);
+      done[v] = 1;
       for (auto&& e: G[v]) {
-        if (done[e.id]) continue;
-        done[e.id] = 1;
-        int to = e.to;
-        TO[v] = to;
-        deg[to] -= 1;
-        if (deg[to] == 1) que.eb(to);
+        if (done[e.to] || e.id == out_eid) continue;
+        TO[e.to] = v;
+        que.eb(e.to);
       }
-      deg[v] = 0;
     }
-    root = -1;
-    FOR(v, N) if (deg[v] == 2) root = v;
-    assert(root != -1);
-    vc<int> P = {root};
-    while (1) {
-      int v = P.back();
-      bool upd = 0;
-      for (auto&& e: G[v]) {
-        if (done[e.id]) continue;
-        done[e.id] = 1;
-        P.eb(e.to);
-        upd = 1;
-        break;
-      }
-      if (!upd) break;
-    }
-    FOR(i, len(P) - 1) TO[P[i]] = P[i + 1];
-    cycle = {P.begin() + 1, P.end()};
-    reverse(all(cycle));
-    in_cycle.assign(N, false);
+    cycle = {TO[root]};
+    while (cycle.back() != root) cycle.eb(TO[cycle.back()]);
+    in_cycle.assign(N, 0);
     for (auto&& v: cycle) in_cycle[v] = 1;
-    FOR(v, N) if (v != root) tree.add(TO[v], v);
-    tree.build();
   }
+
+  // tree を作る
+  Tree<decltype(G)> build(bool keep_eid = false) {
+    G.resize(N);
+    FOR(eid, N) {
+      if (eid == out_eid) continue;
+      auto& e = G0.edges[eid];
+      int a = e.frm, b = e.to;
+      if (TO[a] == b) swap(a, b);
+      assert(TO[b] == a);
+      int k = (keep_eid ? eid : -1);
+      G.add(a, b, e.cost, k);
+    }
+    G.build();
+    return Tree<decltype(G)>(G, root);
+  };
 };
