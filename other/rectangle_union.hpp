05d0c5965d169d9f75b3139e86e44b7c59f73b5e
maspypy <maspypy.gmail.com>
2023-04-02 02:09:38 +0900

    improve perf

--- other/rectangle_union.hpp
+++ other/rectangle_union.hpp
@@ -7,7 +7,7 @@ struct Rectangle_Union {
   vc<RECT> rectangles;
   vc<XY> X, Y;
 
-  void add_rect(int xl, int xr, int yl, int yr) {
+  void add_rect(XY xl, XY xr, XY yl, XY yr) {
     assert(xl < xr && yl < yr);
     X.eb(xl), X.eb(xr), Y.eb(yl), Y.eb(yr);
     rectangles.eb(xl, xr, yl, yr);
@@ -15,30 +15,29 @@ struct Rectangle_Union {
 
   template <typename ANS_TYPE = ll>
   ANS_TYPE calc() {
-    UNIQUE(X), UNIQUE(Y);
     int N = len(X);
-    vc<vc<pair<int, int>>> add(N), rm(N);
-    for (auto &&[xl, xr, yl, yr]: rectangles) {
-      xl = LB(X, xl), xr = LB(X, xr);
-      yl = LB(Y, yl), yr = LB(Y, yr);
-      add[xl].eb(yl, yr);
-      rm[xr].eb(yl, yr);
-    }
+    vc<int> ord_x = argsort(X);
+    vc<int> ord_y = argsort(Y);
+    vc<int> rk_y(N);
+    FOR(i, N) rk_y[ord_y[i]] = i;
+    X = rearrange(X, ord_x);
+    Y = rearrange(Y, ord_y);
 
     using AM = ActedMonoid_MinMincnt_Add<XY>;
-    using T = typename AM::Monoid_X::value_type;
-    Lazy_SegTree<AM> seg(len(Y) - 1, [&](int i) -> T {
+    Lazy_SegTree<AM> seg(N - 1, [&](int i) -> pair<XY, XY> {
       return {0, Y[i + 1] - Y[i]};
     });
+
     ANS_TYPE ANS = 0;
-    FOR(i, len(X) - 1) {
-      ANS_TYPE dx = X[i + 1] - X[i];
-      for (auto &&[yl, yr]: add[i]) seg.apply(yl, yr, 1);
-      for (auto &&[yl, yr]: rm[i]) seg.apply(yl, yr, -1);
+    XY total = Y.back() - Y[0];
+    FOR(i, N - 1) {
+      int k = ord_x[i] / 2;
+      int a = (ord_x[i] & 1 ? -1 : 1);
+      seg.apply(rk_y[2 * k], rk_y[2 * k + 1], a);
       auto [min, mincnt] = seg.prod_all();
-      ANS_TYPE n = Y.back() - Y[0];
-      if (min == 0) n -= mincnt;
-      ANS += n * dx;
+      ANS_TYPE dy = total - (min == 0 ? mincnt : 0);
+      ANS_TYPE dx = X[i + 1] - X[i];
+      ANS += dx * dy;
     }
     return ANS;
   }
