dde5e28a100c020e832442ad9de8828ae0de799b
maspypy <maspypy.gmail.com>
2023-04-02 04:26:41 +0900

    use hashmap

--- string/count_subseq.hpp
+++ string/count_subseq.hpp
@@ -1,16 +1,16 @@
-template <typename mint, typename CHAR, typename STRING>
+#include "ds/hashmap.hpp"
+
+template <typename mint, typename CHAR, typename STRING, int LOG = 20>
 mint count_subseq(STRING S) {
-  auto key = S;
-  UNIQUE(key);
-  for (auto&& x: S) x = LB(key, x);
-  vc<int> IDX(len(key), -1);
+  static HashMap<int, LOG> IDX;
   ll N = len(S);
   vc<mint> DP(N + 1);
   DP[0] = 1;
   FOR(i, N) {
     CHAR c = S[i];
     DP[i + 1] = DP[i] + DP[i];
-    if (IDX[c] != -1) { DP[i + 1] -= DP[IDX[c]]; }
+    int k = IDX.get(c, -1);
+    if (k != -1) DP[i + 1] -= DP[k];
     IDX[c] = i;
   }
   return DP.back();
