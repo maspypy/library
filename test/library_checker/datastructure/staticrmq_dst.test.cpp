7cb44c0eb5e490ebc408d76f2bc7c331190d2f69
maspypy <maspypy.gmail.com>
2023-04-08 01:03:35 +0900

    st/dst

--- test/library_checker/datastructure/staticrmq_dst.test.cpp
+++ test/library_checker/datastructure/staticrmq_dst.test.cpp
@@ -1,14 +1,14 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "ds/sparse_table/sparse_table.hpp"
+#include "ds/sparse_table/disjoint_sparse_table.hpp"
 #include "alg/monoid/min.hpp"
 
 void solve() {
   LL(N, Q);
   VEC(int, A, N);
 
-  Sparse_Table<Monoid_Min<int>> seg(A);
+  Disjoint_Sparse_Table<Monoid_Min<int>> seg(A);
 
   FOR(Q) {
     LL(a, b);
