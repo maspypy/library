dd5be1559497b02cf89600362551fc616784758b
maspypy <maspypy.gmail.com>
2023-04-08 00:43:35 +0900

    u64

--- enumerate/bits.hpp
+++ enumerate/bits.hpp
@@ -7,6 +7,15 @@ void enumerate_bits(int s, F f) {
   }
 }
 
+template <typename F>
+void enumerate_bits(u64 s, F f) {
+  while (s) {
+    int i = __builtin_ctzll(s);
+    f(i);
+    s ^= 1 << i;
+  }
+}
+
 template <typename BS, typename F>
 void enumerate_bits_bitset(BS& b, int L, int R, F f) {
   int p = (b[L] ? L : b._Find_next(L));
