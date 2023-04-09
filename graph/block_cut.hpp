cd720149ad0e2247da07cb5737d7c0fae244663d
maspypy <maspypy.gmail.com>
2023-04-09 11:30:46 +0900

    a

--- graph/block_cut.hpp
+++ graph/block_cut.hpp
@@ -3,21 +3,20 @@
 /*
 block-cut tree を、block に通常の頂点を隣接させて拡張しておく
 https://twitter.com/noshi91/status/1529858538650374144?s=20&t=eznpFbuD9BDhfTb4PplFUg
-[0, n)：もとの頂点
-[n, n + n_block)：block
+[0, n)：もとの頂点 [n, n + n_block)：block
 関節点：[0, n) のうちで、degree >= 2 を満たすもの
 
 孤立点は、1 点だけからなる block
 */
-template <typename GRAPH>
-Graph<int, 0> block_cut(GRAPH& G) {
+template <typename GT>
+Graph<int, 0> block_cut(GT& G) {
   int n = G.N;
   vc<int> low(n), ord(n), st;
   vc<bool> used(n);
   st.reserve(n);
-  Graph<int, 0> BCT;
   int nxt = n;
   int k = 0;
+  vc<pair<int, int>> edges;
 
   auto dfs = [&](auto& dfs, int v, int p) -> void {
     st.eb(v);
@@ -32,10 +31,9 @@ Graph<int, 0> block_cut(GRAPH& G) {
         dfs(dfs, e.to, v);
         chmin(low[v], low[e.to]);
         if ((p == -1 && child > 1) || (p != -1 && low[e.to] >= ord[v])) {
-          BCT.resize(nxt + 1);
-          BCT.add(nxt, v);
+          edges.eb(nxt, v);
           while (len(st) > s) {
-            BCT.add(nxt, st.back());
+            edges.eb(nxt, st.back());
             st.pop_back();
           }
           ++nxt;
@@ -47,11 +45,12 @@ Graph<int, 0> block_cut(GRAPH& G) {
   };
   FOR(v, n) if (!used[v]) {
     dfs(dfs, v, -1);
-    BCT.resize(nxt + 1);
-    for (auto&& x: st) { BCT.add(nxt, x); }
+    for (auto&& x: st) { edges.eb(nxt, v); }
     ++nxt;
     st.clear();
   }
+  Graph<int, 0> BCT(nxt);
+  for (auto&& [a, b]: edges) BCT.add(a, b);
   BCT.build();
   return BCT;
 }
