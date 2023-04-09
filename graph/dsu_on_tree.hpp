726491d179b4f38c3f3baf6e37979bad1b3b76ef
maspypy <maspypy.gmail.com>
2023-04-09 11:30:46 +0900

    a

--- graph/dsu_on_tree.hpp
+++ graph/dsu_on_tree.hpp
@@ -6,12 +6,10 @@
 // データ構造によっては、履歴を使って高速に reset する。
 template <typename TREE, typename F1, typename F2, typename F3>
 void DSU_on_Tree(TREE& tree, F1& add, F2& query, F3& reset) {
-  auto& G = tree.G;
-
   auto dfs = [&](auto& dfs, int v) -> void {
     int heavy = -1;
     // solve light edge
-    for (auto&& e: G[v]) {
+    for (auto&& e: (*(tree.G_ptr))[v]) {
       if (e.to == tree.parent[v]) continue;
       if (tree.head[e.to] == e.to) {
         dfs(dfs, e.to);
@@ -23,7 +21,7 @@ void DSU_on_Tree(TREE& tree, F1& add, F2& query, F3& reset) {
     if (heavy != -1) { dfs(dfs, heavy); }
 
     // collect data in light subtree
-    for (auto&& e: G[v]) {
+    for (auto&& e: (*(tree.G_ptr)[v]) {
       if (e.to == tree.parent[v]) continue;
       if (e.to == heavy) continue;
       FOR(idx, tree.LID[e.to], tree.RID[e.to]) { add(tree.V[idx]); }
