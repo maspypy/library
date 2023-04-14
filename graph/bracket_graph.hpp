fcdb78b1546db2a1f1cdcb0cfe7917c9f412b848
maspypy <maspypy.gmail.com>
2023-04-14 22:08:33 +0900

    comment

--- graph/bracket_graph.hpp
+++ graph/bracket_graph.hpp
@@ -1,11 +1,13 @@
 #include "graph/base.hpp"
 
-// カッコ列をグラフにする。各頂点の範囲を表す配列 LR も作る。
-// 全体を表す根ノードも作って、N+1頂点。
+// {G, LR}.
+// regular bracket sequence をグラフにする。各頂点の範囲を表す配列 LR も作る。
+// 全体を表す根ノードも作って、N/2+1頂点。
 // ()() → [0,4), [0,2), [2,4)
-pair<Graph<int, 0>, vc<pair<int, int>>> bracket_graph(string& S) {
+// regular bracket sequence 以外が入力に来るときは、前後に()を補えば使える。
+pair<Graph<int, 1>, vc<pair<int, int>>> bracket_graph(string& S) {
   int N = len(S) / 2;
-  Graph<int, 0> G(N + 1);
+  Graph<int, 1> G(N + 1);
   vc<pair<int, int>> LR(N + 1);
   int now = 0;
   int nxt = 1;
