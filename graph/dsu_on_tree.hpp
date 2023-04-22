739c6acba0a89a8439cb3f0038c3aa765736a5f0
maspypy <maspypy.gmail.com>
2023-04-22 11:32:48 +0900

    a

--- graph/dsu_on_tree.hpp
+++ graph/dsu_on_tree.hpp
@@ -7,28 +7,15 @@
 template <typename TREE, typename F1, typename F2, typename F3>
 void DSU_on_Tree(TREE& tree, F1& add, F2& query, F3& reset) {
   int N = tree.N;
-  FOR_R(i, N) {
-    int v = tree.V[i];
-    int heavy = -1;
-    // solve light edge
-    for (auto&& e: (*(tree.G_ptr))[v]) {
-      if (e.to == tree.parent[v]) continue;
-      if (tree.head[e.to] == e.to) {
-        dfs(dfs, e.to);
-      } else {
-        heavy = e.to;
-      }
-    }
-    // solve heavy edge (keep subtree data)
-    if (heavy != -1) { dfs(dfs, heavy); }
-
+  FOR_R(vid, N) {
+    int v = tree.V[vid];
+    add(v);
     // collect data in light subtree
     for (auto&& e: (*tree.G_ptr)[v]) {
       if (e.to == tree.parent[v]) continue;
-      if (e.to == heavy) continue;
+      if (tree.head[e.to] != e.to) continue;
       FOR(idx, tree.LID[e.to], tree.RID[e.to]) { add(tree.V[idx]); }
     }
-    add(v);
     query(v);
 
     if (tree.head[v] == v) reset();
