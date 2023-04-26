bea18c7588d67d7f879337aa12999b93a15b8821
maspypy <maspypy.gmail.com>
2023-04-27 05:10:41 +0900

    test conv

---
+++ test/library_checker/convolution/convolution_mod_setntt.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "mod/dynamic_modint.hpp"
+#include "poly/convolution.hpp"
+
+using mint = dmint;
+
+void solve() {
+  mint::set_mod(998244353);
+  mint::set_ntt_info();
+
+  LL(N, M);
+  VEC(mint, A, N);
+  VEC(mint, B, M);
+  auto ANS = convolution(A, B);
+  print(ANS);
+}
+
+signed main() {
+  solve();
+
+  return 0;
+}
