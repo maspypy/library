9b2c38cb6b645ff39297d0d1ae260951c5fde5cf
maspypy <maspypy.gmail.com>
2023-05-03 03:23:47 +0900

    a

--- test/library_checker/datastructure/point_set_range_composite_monoid.test.cpp
+++ test/library_checker/datastructure/point_set_range_composite_monoid.test.cpp
@@ -1,45 +1,32 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
 #include "my_template.hpp"
 #include "other/io.hpp"
 
-#include "ds/segtree/segtree.hpp"
 #include "mod/modint.hpp"
 #include "alg/monoid/affine.hpp"
+#include "ds/segtree/segtree.hpp"
 
 using mint = modint998;
+using Mono = Monoid_Affine<mint>;
 
 void solve() {
   LL(N, Q);
-  using Mono = Monoid_Affine<mint>;
-  using F = Mono::value_type;
-
-  vc<F> seg_raw(N);
-  FOR(i, N) {
-    LL(a, b);
-    seg_raw[i] = {a, b};
-  }
-
-  SegTree<Mono> seg(seg_raw);
-
-  FOR(q, Q) {
-    LL(t);
+  SegTree<Mono> seg(N, [&](int i) -> pair<mint, mint> {
+    INT(a, b);
+    return {a, b};
+  });
+  FOR(Q) {
+    INT(t);
     if (t == 0) {
-      LL(i, a, b);
-      seg.set(i, F({a, b}));
+      INT(i, a, b);
+      seg.set(i, {a, b});
     } else {
-      LL(L, R, x);
-      auto f = seg.prod(L, R);
-      print(Mono::eval(f, x));
+      INT(L, R, x);
+      print(Mono::eval(seg.prod(L, R), mint(x)));
     }
   }
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
   solve();
-
   return 0;
 }
