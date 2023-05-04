f795c4be7e53e665a899026bdda672cbc2fc80d4
maspypy <maspypy.gmail.com>
2023-05-05 05:27:13 +0900

    a

--- setfunc/subset_convolution.hpp
+++ setfunc/subset_convolution.hpp
@@ -1,4 +1,21 @@
+#pragma once
+
 #include "setfunc/ranked_zeta.hpp"
+
+template <typename T, int LIM = 20>
+vc<T> subset_convolution_square(const vc<T>& A) {
+  auto RA = ranked_zeta<T, LIM>(A);
+  int n = topbit(len(RA));
+  FOR(s, len(RA)) {
+    auto& f = RA[s];
+    FOR_R(d, n + 1) {
+      T x = 0;
+      FOR(i, d + 1) x += f[i] * f[d - i];
+      f[d] = x;
+    }
+  }
+  return ranked_mobius<T, LIM>(RA);
+}
 
 template <typename T, int LIM = 20>
 vc<T> subset_convolution(const vc<T>& A, const vc<T>& B) {
@@ -11,21 +28,6 @@ vc<T> subset_convolution(const vc<T>& A, const vc<T>& B) {
     FOR_R(d, n + 1) {
       T x = 0;
       FOR(i, d + 1) x += f[i] * g[d - i];
-      f[d] = x;
-    }
-  }
-  return ranked_mobius<T, LIM>(RA);
-}
-
-template <typename T, int LIM = 20>
-vc<T> subset_convolution_square(const vc<T>& A) {
-  auto RA = ranked_zeta<T, LIM>(A);
-  int n = topbit(len(RA));
-  FOR(s, len(RA)) {
-    auto& f = RA[s];
-    FOR_R(d, n + 1) {
-      T x = 0;
-      FOR(i, d + 1) x += f[i] * f[d - i];
       f[d] = x;
     }
   }
