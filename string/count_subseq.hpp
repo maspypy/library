3089f53a5185d6fd337a826fda199dc39211709f
maspypy <maspypy.gmail.com>
2023-05-12 16:35:07 +0900

    change dp key

--- string/count_subseq.hpp
+++ string/count_subseq.hpp
@@ -1,17 +1,14 @@
 #include "ds/hashmap.hpp"
 
-template <typename mint, typename CHAR, typename STRING, int LOG = 20>
+// https://noshi91.hatenablog.com/entry/2023/02/26/135340
+template <typename mint, typename STRING, int LOG = 20>
 mint count_subseq(STRING S) {
-  static HashMap<int, LOG> IDX;
-  ll N = len(S);
-  vc<mint> DP(N + 1);
-  DP[0] = 1;
-  FOR(i, N) {
-    CHAR c = S[i];
-    DP[i + 1] = DP[i] + DP[i];
-    int k = IDX.get(c, -1);
-    if (k != -1) DP[i + 1] -= DP[k];
-    IDX[c] = i;
+  static HashMap<mint, LOG> dp;
+  mint sm = 1;
+  for (auto&& x: S) {
+    mint d = dp[x];
+    dp[x] = sm;
+    sm += sm - d;
   }
-  return DP.back();
+  return sm;
 }
