d7f2286e52f345222cfa7aad6de8f85ab436fd5c
maspypy <45988013+maspypy@users.noreply.github.com>
2023-05-12 16:30:27 +0900

    Update and rename poly_taylor_shift.hpp to polynomial_taylor_shift.hpp

--- poly/polynomial_taylor_shift.hpp
+++ poly/polynomial_taylor_shift.hpp
@@ -1,14 +1,15 @@
 #include "mod/powertable.hpp"
 #include "poly/convolution.hpp"
 
+// f(x) -> f(x+c)
 template <typename mint>
-vc<mint> poly_taylor_shift(vc<mint> a, mint c) {
-  ll N = len(a);
-  FOR(i, N) a[i] *= fact<mint>(i);
+vc<mint> polynomial_taylor_shift(vc<mint> f, mint c) {
+  ll N = len(f);
+  FOR(i, N) f[i] *= fact<mint>(i);
   auto b = powertable_1<mint>(c, N);
   FOR(i, N) b[i] *= fact_inv<mint>(i);
-  reverse(all(a));
-  auto f = convolution(a, b);
+  reverse(all(f));
+  f = convolution(f, b);
   f.resize(N);
   reverse(all(f));
   FOR(i, N) f[i] *= fact_inv<mint>(i);
