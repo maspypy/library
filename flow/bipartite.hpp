227d566f09b205ce4265ee22e10f282987886346
maspypy <maspypy.gmail.com>
2023-05-26 18:55:30 +0900

    bitset

--- flow/bipartite.hpp
+++ flow/bipartite.hpp
@@ -169,3 +169,55 @@ struct BipartiteMatching {
     print("min edge cover", edge_cover());
   }
 };
+
+// vc<bitset> で biadj matrix を渡す
+// N^3/w. (5000,5000) で 300ms 程度で動く場合がある
+// https://qoj.ac/problem/6308
+template <typename BS>
+struct BipartiteMatching_Dense {
+  int N1, N2;
+  vc<BS>& G;
+  vc<int> match_1, match_2;
+  vc<int> que;
+  vc<int> prev;
+  BS vis;
+
+  BipartiteMatching_Dense(vc<BS>& G, int N1, int N2)
+      : N1(N1), N2(N2), G(G), match_1(N1, -1), match_2(N2, -1) {
+    FOR(s, N1) bfs(s);
+  }
+
+  void bfs(int s) {
+    if (match_1[s] != -1) return;
+    que.resize(N1), prev.resize(N1);
+    int l = 0, r = 0;
+    vis.set(), prev[s] = -1;
+
+    que[r++] = s;
+    while (l < r) {
+      int u = que[l++];
+      BS cand = vis & G[u];
+      for (int v = cand._Find_first(); v < N2; v = cand._Find_next(v)) {
+        vis[v] = 0;
+        if (match_2[v] != -1) {
+          que[r++] = match_2[v];
+          prev[match_2[v]] = u;
+          continue;
+        }
+        int a = u, b = v;
+        while (a != -1) {
+          int t = match_1[a];
+          match_1[a] = b, match_2[b] = a, a = prev[a], b = t;
+        }
+        return;
+      }
+    }
+    return;
+  }
+
+  vc<pair<int, int>> matching() {
+    vc<pair<int, int>> res;
+    FOR(v, N1) if (match_1[v] != -1) res.eb(v, match_1[v]);
+    return res;
+  }
+};
