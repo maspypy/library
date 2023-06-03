c54ddc5040d3d8914a9a82ac05fba78897c85944
maspypy <45988013+maspypy@users.noreply.github.com>
2023-06-03 12:51:18 +0900

    Update 2342.test.cpp

--- test/yukicoder/2342.test.cpp
+++ test/yukicoder/2342.test.cpp
@@ -25,12 +25,12 @@ void solve() {
   FOR(Q) {
     LL(t);
     if (t == 1) {
-      LL(v);
+      INT(v);
       --v;
       print(KDT.prod(X[v], X[v] + 1, Y[v], Y[v] + 1));
     }
     if (t == 2) {
-      LL(v, K, C, D);
+      INT(v, K, C, D);
       --v;
       assert(K <= 10);
       pair<mint, mint> p = {C, D};
@@ -59,13 +59,13 @@ void solve() {
       }
     }
     if (t == 3) {
-      LL(v, C, D);
+      INT(v, C, D);
       --v;
       pair<mint, mint> p = {C, D};
       KDT.apply(tree.LID[v], tree.RID[v], 0, infty<int>, p);
     }
     if (t == 4) {
-      LL(u, v, C, D);
+      INT(u, v, C, D);
       --u, --v;
       pair<mint, mint> p = {C, D};
       for (auto&& [a, b]: tree.get_path_decomposition(u, v, false)) {
