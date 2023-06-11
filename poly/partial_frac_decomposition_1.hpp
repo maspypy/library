ff06ff1a9402c84ae24ea91f8c1a3b455b27a295
maspypy <maspypy.gmail.com>
2023-06-11 17:10:02 +0900

    rename

--- poly/partial_frac_decomposition_1.hpp
+++ poly/partial_frac_decomposition_1.hpp
@@ -1,18 +1,16 @@
 #include "poly/multipoint.hpp"
-// O(Nlog^2N)
-// 分母は相異なる 1 次式の積限定とする
-// f / prod(ax+b) = sum c/(ax+b) という C を返す
+
+// f(x) / prod(1-a_ix) = sum b_i/1-a_ix
 template <typename mint>
-vc<mint> partial_frac_decomposition(vc<mint> f, vc<mint> A) {
-  assert(len(f) <= len(A));
-  if (len(f) == 0) return vc<mint>(len(A));
+vc<mint> partial_frac_decomposition_1(vc<mint> f, vc<mint> A) {
+  int N = len(A);
   SubproductTree<mint> X(A);
   vc<mint> g = X.T[1]; // prod(1-ax)
-  g.resize(len(A) + 1);
+  g.resize(N + 1);
+  reverse(all(f));
   reverse(all(g));
   FOR(i, len(g) - 1) g[i] = g[i + 1] * mint(i + 1);
   g.pop_back();
-
   auto num = X.evaluation(f);
   auto den = X.evaluation(g);
   vc<mint> B(len(A));
