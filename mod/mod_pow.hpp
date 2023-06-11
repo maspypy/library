042e4783ba87c74f454610f8b247b1bf1a24feba
maspypy <maspypy.gmail.com>
2023-06-11 17:09:41 +0900

    mod=1 etc

--- mod/mod_pow.hpp
+++ mod/mod_pow.hpp
@@ -3,10 +3,11 @@
 
 // int
 ll mod_pow(ll a, ll n, int mod) {
+  assert(n >= 0);
   a %= mod;
+  if (a < 0) a += mod;
   Barrett bt(mod);
-  ll p = a;
-  ll v = bt.modulo(1);
+  ll p = a, v = bt.modulo(1);
   while (n) {
     if (n & 1) v = bt.mul(v, p);
     p = bt.mul(p, p);
@@ -16,9 +17,10 @@ ll mod_pow(ll a, ll n, int mod) {
 }
 
 ll mod_pow_long(ll a, ll n, ll mod) {
+  assert(n >= 0);
   a %= mod;
-  ll p = a;
-  ll v = 1;
+  if (a < 0) a += mod;
+  ll p = a, v = 1 % mod;
   while (n) {
     if (n & 1) v = i128(v) * p % mod;
     p = i128(p) * p % mod;
