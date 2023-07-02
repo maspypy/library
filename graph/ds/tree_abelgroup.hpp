9fb030724be66ad9916fbf95c5e0575dbc80c6c0
maspypy <maspypy.gmail.com>
2023-07-03 05:07:01 +0900

    a

--- graph/ds/tree_abelgroup.hpp
+++ graph/ds/tree_abelgroup.hpp
@@ -4,13 +4,14 @@
 template <typename TREE, typename AbelGroup, bool edge, bool path_query,
           bool subtree_query>
 struct Tree_AbelGroup {
-  using X = typename AbelGroup::value_type;
+  usnig MX = AbelGroup;
+  using X = typename MX::value_type;
   TREE &tree;
   int N;
-  FenwickTree<AbelGroup> bit, bit_subtree;
+  FenwickTree<MX> bit, bit_subtree;
 
   Tree_AbelGroup(TREE &tree) : tree(tree), N(tree.N) {
-    build([](int i) -> X { return AbelGroup::unit(); });
+    build([](int i) -> X { return MX::unit(); });
   }
 
   Tree_AbelGroup(TREE &tree, vc<X> &dat) : tree(tree), N(tree.N) {
@@ -26,48 +27,39 @@ struct Tree_AbelGroup {
   void build(F f) {
     vc<X> bit_raw_1(2 * N);
     vc<X> bit_raw_2(N);
-    if (!edge) {
-      FOR(v, N) {
-        X x = f(v);
-        bit_raw_1[tree.ELID(v)] = x;
-        bit_raw_1[tree.ERID(v)] = AbelGroup::inverse(x);
-        bit_raw_2[tree.LID[v]] = x;
-      }
-    } else {
-      FOR(e, N - 1) {
-        int v = tree.e_to_v(e);
-        X x = f(v);
-        bit_raw_1[tree.ELID(v)] = x;
-        bit_raw_1[tree.ERID(v)] = AbelGroup::inverse(x);
-        bit_raw_2[tree.LID[v]] = x;
-      }
+    FOR(v, N) {
+      X x = MX::unit();
+      if (!edge) x = f(v);
+      if (edge) x = (v == 0 ? MX::unit() : f(tree.v_to_e(v)));
+      bit_raw_1[tree.ELID(v)] = x;
+      bit_raw_1[tree.ERID(v)] = MX::inverse(x);
+      bit_raw_2[tree.LID[v]] = x;
     }
-    bit.build(bit_raw_1);
-    bit_subtree.build(bit_raw_2);
+    if constexpr (path_query) bit.build(bit_raw_1);
+    if constexpr (subtree_query) bit_subtree.build(bit_raw_2);
   }
 
   void add(int i, X x) {
     int v = (edge ? tree.e_to_v(i) : i);
-    if (path_query) {
-      X inv_x = AbelGroup::inverse(x);
+    if constexpr (path_query) {
       bit.add(tree.ELID(v), x);
-      bit.add(tree.ERID(v), inv_x);
+      bit.add(tree.ERID(v), MX::inverse(x));
     }
-    if (subtree_query) bit_subtree.add(tree.LID[v], x);
+    if constexpr (subtree_query) bit_subtree.add(tree.LID[v], x);
   }
 
   X prod_path(int frm, int to) {
-    assert(path_query);
+    static_assert(path_query);
     int lca = tree.LCA(frm, to);
     // [frm, lca)
     X x1 = bit.prod(tree.ELID(lca) + 1, tree.ELID(frm) + 1);
     // edge なら (lca, to]、vertex なら [lca, to]
     X x2 = bit.prod(tree.ELID(lca) + edge, tree.ELID(to) + 1);
-    return AbelGroup::op(x1, x2);
+    return MX::op(x1, x2);
   }
 
   X prod_subtree(int u) {
-    assert(subtree_query);
+    static_assert(subtree_query);
     int l = tree.LID[u], r = tree.RID[u];
     return bit_subtree.prod(l + edge, r);
   }
