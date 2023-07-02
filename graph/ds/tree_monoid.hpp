547eb96c57e7fba56deb3f1ab2a6ed9efb5dbec9
maspypy <maspypy.gmail.com>
2023-07-03 04:52:38 +0900

    refactor

--- graph/ds/tree_monoid.hpp
+++ graph/ds/tree_monoid.hpp
@@ -6,15 +6,15 @@
 
 template <typename TREE, typename Monoid, bool edge>
 struct Tree_Monoid {
-  using RevMonoid = Monoid_Reverse<Monoid>;
-  using X = typename Monoid::value_type;
+  using MX = Monoid;
+  using X = typename MX::value_type;
   TREE &tree;
   int N;
-  SegTree<Monoid> seg;
-  SegTree<RevMonoid> seg_r;
+  SegTree<MX> seg;
+  SegTree<Monoid_Reverse<MX>> seg_r;
 
   Tree_Monoid(TREE &tree) : tree(tree), N(tree.N) {
-    build([](int i) -> X { return Monoid::unit(); });
+    build([](int i) -> X { return MX::unit(); });
   }
 
   Tree_Monoid(TREE &tree, vc<X> &dat) : tree(tree), N(tree.N) {
@@ -28,76 +28,65 @@ struct Tree_Monoid {
 
   template <typename F>
   void build(F f) {
-    vc<X> seg_raw(N, Monoid::unit());
     if (!edge) {
-      seg.build(N, [&](int i) -> X { return f(tree.V[i]); });
-      if (!Monoid::commute) {
-        seg_r.build(N, [&](int i) -> X { return f(tree.V[i]); });
-      }
+      auto f_v = [&](int i) -> X { return f(tree.V[i]); };
+      seg.build(N, f_v);
+      if constexpr (!MX::commute) { seg_r.build(N, f_v); }
     } else {
-      seg.build(N, [&](int i) -> X {
-        return (i == 0 ? Monoid::unit() : f(tree.v_to_e(tree.V[i])));
-      });
-      if (!Monoid::commute) {
-        seg_r.build(N, [&](int i) -> X {
-          return (i == 0 ? Monoid::unit() : f(tree.v_to_e(tree.V[i])));
-        });
-      }
+      auto f_e = [&](int i) -> X {
+        return (i == 0 ? MX::unit() : f(tree.v_to_e(tree.V[i])));
+      };
+      seg.build(N, f_e);
+      if constexpr (!MX::commute) { seg_r.build(N, f_e); }
     }
   }
 
   void set(int i, X x) {
-    if (edge) i = tree.e_to_v(i);
+    if constexpr (edge) i = tree.e_to_v(i);
     i = tree.LID[i];
     seg.set(i, x);
-    if (!Monoid::commute) seg_r.set(i, x);
+    if constexpr (!MX::commute) seg_r.set(i, x);
   }
 
   void multiply(int i, X x) {
-    if (edge) i = tree.e_to_v(i);
+    if constexpr (edge) i = tree.e_to_v(i);
     i = tree.LID[i];
     seg.multiply(i, x);
-    if (!Monoid::commute) seg_r.multiply(i, x);
+    if constexpr (!MX::commute) seg_r.multiply(i, x);
   }
 
   X prod_path(int u, int v) {
     auto pd = tree.get_path_decomposition(u, v, edge);
-    X val = Monoid::unit();
-    for (auto &&[a, b]: pd) {
-      X x = (a <= b ? seg.prod(a, b + 1)
-                    : (Monoid::commute ? seg.prod(b, a + 1)
-                                       : seg_r.prod(b, a + 1)));
-      val = Monoid::op(val, x);
-    }
+    X val = MX::unit();
+    for (auto &&[a, b]: pd) { val = MX::op(val, get_prod(a, b)); }
     return val;
   }
 
   // uv path 上で prod_path(u, x) が check を満たす最後の x
-  // なければ -1
+  // なければ （つまり path(u,u) が ng ）-1
   template <class F>
   int max_path(F check, int u, int v) {
-    if (edge) return max_path_edge(check, u, v);
+    if constexpr (edge) return max_path_edge(check, u, v);
     if (!check(prod_path(u, u))) return -1;
     auto pd = tree.get_path_decomposition(u, v, edge);
-    X val = Monoid::unit();
+    X val = MX::unit();
     for (auto &&[a, b]: pd) {
-      X x = (a <= b ? seg.prod(a, b + 1)
-                    : (Monoid::commute ? seg.prod(b, a + 1)
-                                       : seg_r.prod(b, a + 1)));
-      if (check(Monoid::op(val, x))) {
-        val = Monoid::op(val, x);
+      X x = get_prod(a, b);
+      if (check(MX::op(val, x))) {
+        val = MX::op(val, x);
         u = (tree.V[b]);
         continue;
       }
-      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
+      auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
       if (a <= b) {
         // 下り
         auto i = seg.max_right(check_tmp, a);
         return (i == a ? u : tree.V[i - 1]);
       } else {
         // 上り
-        auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
-                                  : seg_r.min_left(check_tmp, a + 1));
+        int i = 0;
+        if constexpr (MX::commute) i = seg.min_left(check_tmp, a + 1);
+        if constexpr (!MX::commute) i = seg_r.min_left(check_tmp, a + 1);
         if (i == a + 1) return u;
         return tree.V[i];
       }
@@ -113,26 +102,34 @@ struct Tree_Monoid {
   X prod_all() { return prod_subtree(tree.V[0]); }
 
 private:
+  inline X get_prod(int a, int b) {
+    if constexpr (MX::commute) {
+      return (a <= b) ? seg.prod(a, b + 1) : seg.prod(b, a + 1);
+    }
+    return (a <= b) ? seg.prod(a, b + 1) : seg_r.prod(b, a + 1);
+  }
+
   template <class F>
   int max_path_edge(F check, int u, int v) {
-    assert(edge);
-    if (!check(Monoid::unit())) return -1;
+    static_assert(edge);
+    if (!check(MX::unit())) return -1;
     int lca = tree.lca(u, v);
     auto pd = tree.get_path_decomposition(u, lca, edge);
-    X val = Monoid::unit();
+    X val = MX::unit();
 
     // climb
     for (auto &&[a, b]: pd) {
       assert(a >= b);
-      X x = (Monoid::commute ? seg.prod(b, a + 1) : seg_r.prod(b, a + 1));
-      if (check(Monoid::op(val, x))) {
-        val = Monoid::op(val, x);
+      X x = get_prod(a, b);
+      if (check(MX::op(val, x))) {
+        val = MX::op(val, x);
         u = (tree.parent[tree.V[b]]);
         continue;
       }
-      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
-      auto i = (Monoid::commute ? seg.min_left(check_tmp, a + 1)
-                                : seg_r.min_left(check_tmp, a + 1));
+      auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
+      int i = 0;
+      if constexpr (MX::commute) i = seg.min_left(check_tmp, a + 1);
+      if constexpr (!MX::commute) i = seg_r.min_left(check_tmp, a + 1);
       if (i == a + 1) return u;
       return tree.parent[tree.V[i]];
     }
@@ -140,13 +137,13 @@ private:
     pd = tree.get_path_decomposition(lca, v, edge);
     for (auto &&[a, b]: pd) {
       assert(a <= b);
-      X x = seg.prod(a, b + 1);
-      if (check(Monoid::op(val, x))) {
-        val = Monoid::op(val, x);
+      X x = get_prod(a, b);
+      if (check(MX::op(val, x))) {
+        val = MX::op(val, x);
         u = (tree.V[b]);
         continue;
       }
-      auto check_tmp = [&](X x) -> bool { return check(Monoid::op(val, x)); };
+      auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
       auto i = seg.max_right(check_tmp, a);
       return (i == a ? u : tree.V[i - 1]);
     }
