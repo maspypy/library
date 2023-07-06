02cb4a5ec3f967a5849e6e262b98b28be5a052e8
maspypy <maspypy.gmail.com>
2023-07-06 13:22:49 +0900

    negative mod

--- mod/mod_inv.hpp
+++ mod/mod_inv.hpp
@@ -1,6 +1,10 @@
 #pragma once
 // long でも大丈夫
+// (val * x - 1) が mod の倍数になるようにする
+// 特に mod=0 なら x=0 が満たす
 ll mod_inv(ll val, ll mod) {
+  if (mod == 0) return 0;
+  mod = abs(mod);
   val %= mod;
   if (val < 0) val += mod;
   ll a = val, b = mod, u = 1, v = 0, t;
