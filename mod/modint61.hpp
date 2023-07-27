c28f0bbcd88d06ebf256372a3269500fb29d2c55
maspypy <maspypy.gmail.com>
2023-07-27 19:05:58 +0900

    a

--- mod/modint61.hpp
+++ mod/modint61.hpp
@@ -1,71 +1,53 @@
 #pragma once
+
 struct modint61 {
-  static constexpr bool is_modint = true;
-  static constexpr ll mod = (1LL << 61) - 1;
-  ll val;
-  constexpr modint61(const ll x = 0) : val(x) {
-    while (val < 0) val += mod;
-    while (val >= mod) val -= mod;
-  }
-  bool operator<(const modint61 &other) const {
-    return val < other.val;
-  } // To use std::map
-  bool operator==(const modint61 &p) const { return val == p.val; }
-  bool operator!=(const modint61 &p) const { return val != p.val; }
-  modint61 &operator+=(const modint61 &p) {
-    if ((val += p.val) >= mod) val -= mod;
+  static constexpr u64 mod = (1ULL << 61) - 1;
+  u64 val;
+  constexpr modint61() : val(0ULL) {}
+  constexpr modint61(u32 x) : val(x) {}
+  constexpr modint61(u64 x) : val(x % mod) {}
+  constexpr modint61(int x)
+      : val((x < 0) ? (x + static_cast<long long>(mod)) : x) {}
+  constexpr modint61(ll x)
+      : val(((x %= static_cast<long long>(mod)) < 0)
+                ? (x + static_cast<long long>(mod))
+                : x) {}
+  modint61 &operator+=(const modint61 &a) {
+    val = ((val += a.val) >= mod) ? (val - mod) : val;
     return *this;
   }
-  modint61 &operator-=(const modint61 &p) {
-    if ((val += mod - p.val) >= mod) val -= mod;
+  modint61 &operator-=(const modint61 &a) {
+    val = ((val -= a.val) >= mod) ? (val + mod) : val;
     return *this;
   }
-  modint61 &operator*=(const modint61 &p) {
-    ll a = val, b = p.val;
-    const ll MASK30 = (1LL << 30) - 1;
-    const ll MASK31 = (1LL << 31) - 1;
-    const ll MASK61 = (1LL << 61) - 1;
-    ll au = a >> 31, ad = a & MASK31;
-    ll bu = b >> 31, bd = b & MASK31;
-    ll x = ad * bu + au * bd;
-    ll xu = x >> 30, xd = x & MASK30;
-    x = au * bu * 2 + xu + (xd << 31) + ad * bd;
-    xu = x >> 61, xd = x & MASK61;
-    x = xu + xd;
-    if (x >= MASK61) x -= MASK61;
-    val = x;
+  modint61 &operator*=(const modint61 &a) {
+    const unsigned __int128 y = static_cast<unsigned __int128>(val) * a.val;
+    val = (y >> 61) + (y & mod);
+    val = (val >= mod) ? (val - mod) : val;
     return *this;
   }
-  modint61 operator-() const { return modint61(get_mod() - val); }
-  modint61 &operator/=(const modint61 &p) {
-    *this *= p.inverse();
-    return *this;
-  }
+  modint61 &operator/=(const modint61 &a) { return (*this *= a.inverse()); }
   modint61 operator+(const modint61 &p) const { return modint61(*this) += p; }
   modint61 operator-(const modint61 &p) const { return modint61(*this) -= p; }
   modint61 operator*(const modint61 &p) const { return modint61(*this) *= p; }
   modint61 operator/(const modint61 &p) const { return modint61(*this) /= p; }
-
+  bool operator==(const modint61 &p) const { return val == p.val; }
+  bool operator!=(const modint61 &p) const { return val != p.val; }
   modint61 inverse() const {
-    ll a = val, b = mod, u = 1, v = 0, t;
+    u64 a = val, b = mod, u = 1, v = 0, t;
     while (b > 0) {
       t = a / b;
       swap(a -= t * b, b), swap(u -= t * v, v);
     }
     return modint61(u);
   }
-  modint61 pow(int64_t n) const {
+  modint61 pow(ll n) const {
+    assert(n >= 0);
     modint61 ret(1), mul(val);
     while (n > 0) {
-      if (n & 1) ret = ret * mul;
-      mul = mul * mul;
-      n >>= 1;
+      if (n & 1) ret *= mul;
+      mul *= mul, n >>= 1;
     }
     return ret;
   }
-  static constexpr ll get_mod() { return mod; }
-#ifdef FASTIO
-  void write() { fastio::printer.write(val); }
-  void read() { fastio::scanner.read(val); }
-#endif
 };
