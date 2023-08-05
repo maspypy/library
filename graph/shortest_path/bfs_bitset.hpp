62fc965550e564c4dae4c35166b4076414661d7b
maspypy <maspypy.gmail.com>
2023-08-06 04:48:55 +0900

    a

--- graph/shortest_path/bfs_bitset.hpp
+++ graph/shortest_path/bfs_bitset.hpp
@@ -1,3 +1,4 @@
+#include "ds/my_bitset.hpp"
 
 // 密グラフの重みなし最短路問題
 // 01 行列を vc<bitset> の形で渡す
@@ -5,11 +6,15 @@
 // 参考：(4000,4000) を 4000 回で 2 秒以内？
 template <typename BITSET>
 vc<int> bfs_bitset(vc<BITSET>& G, int s) {
-  int N = len(G);
+  const int N = len(G);
+  assert(0 <= s && s < N);
   vc<int> dist(N, -1);
-  BITSET unused;
+  BITSET unused, que;
+  if constexpr (is_same_v<BITSET, My_Bitset>) {
+    unused = BITSET(N, 1);
+    que = BITSET(N, 0);
+  }
   FOR(v, N) unused[v] = 1;
-  BITSET que;
   que[s] = 1;
 
   int d = 0;
@@ -17,6 +22,7 @@ vc<int> bfs_bitset(vc<BITSET>& G, int s) {
     int p = que._Find_first();
     if (p >= N) break;
     BITSET nxt;
+    if constexpr (is_same_v<BITSET, My_Bitset>) { nxt = BITSET(N); }
     while (p < N) {
       dist[p] = d;
       unused[p] = 0;
