373720da99f5d9d78e32b5d1e9d0e1ebe5fe8c6c
maspypy <maspypy.gmail.com>
2023-08-06 03:59:34 +0900

    a

--- graph/decompose_complete.hpp
+++ graph/decompose_complete.hpp
@@ -1,50 +1,49 @@
-vc<vi> to_paths(ll N) {
-  // 偶数頂点の無向完全グラフをハミルトンパスに分解
+// 偶数頂点の無向完全グラフをハミルトンパスに分解
+vvc<int> to_paths(ll N) {
   assert(N >= 0 && N % 2 == 0);
   ll K = N / 2;
-  vv(ll, paths, K, N);
+  vv(int, paths, K, N);
   FOR(k, K) FOR(n, N) {
     auto &x = paths[k][n];
     x = k + (n & 1 ? (n + 1) / 2 : -(n / 2));
-    if (x < 0)
-      x += N;
-    if (x >= N)
-      x -= N;
+    if (x < 0) x += N;
+    if (x >= N) x -= N;
   }
   return paths;
 }
 
-vc<vi> to_cycles(ll N) {
-  // 奇数頂点の無向完全グラフをハミルトトンサイクルに分解
+// 奇数頂点の無向完全グラフをハミルトトンサイクルに分解
+vvc<int> to_cycles(ll N) {
   assert(N >= 0 && N % 2 == 1);
   auto paths = to_paths(N - 1);
-  FORIN(P, paths) P.eb(N - 1);
+  for (auto &&P: paths) P.eb(N - 1);
   return paths;
 }
 
-vc<vi> to_matchings(ll N) {
-  // 偶数頂点の無向完全グラフをマッチングに分解
-  // 辺彩色しているといってもよい
-  // このうちどの 2 個を選んでも、ハミルトンサイクルになる。
+// https://codeforces.com/contest/12/problem/E
+// 偶数頂点の無向完全グラフをマッチングに分解
+// 辺彩色しているといってもよい
+// このうちどの 2 個を選んでも、ハミルトンサイクルになる。
+vvc<pair<int, int>> to_matchings(ll N) {
   assert(N > 0 && N % 2 == 0);
-  vv(ll, match, N - 1, N);
-  ll mod = N - 1;
+  vvc<pair<int, int>> res(N - 1);
+  const int mod = N - 1;
   FOR(a, mod) {
-    match[a][0] = N - 1, match[a][1] = a;
-    FOR3(k, 1, N / 2) {
-      ll b = a - k, c = a + k;
-      if (b < 0)
-        b += mod;
-      if (c >= mod)
-        c -= mod;
-      match[a][2 * k] = b, match[a][2 * k + 1] = c;
+    res[a].reserve(N / 2);
+    res[a].eb(N - 1, a);
+    int x = a, y = a;
+    FOR(N / 2 - 1) {
+      --x, ++y;
+      if (x < 0) x += N - 1;
+      if (y >= N - 1) y -= N - 1;
+      res[a].eb(x, y);
     }
   }
-  return match;
+  return res;
 }
 
+// 偶数頂点の無向完全グラフを、ハミルトンサイクルと 1 つのマッチングに分解
 pair<vc<vi>, vi> to_cycles_and_matching(ll N) {
-  // 偶数頂点の無向完全グラフを、ハミルトンサイクルと 1 つのマッチングに分解
   assert(N > 0 && N % 2 == 0);
   vv(ll, cycles, N / 2 - 1, N);
   ll mod = N - 1;
@@ -54,10 +53,8 @@ pair<vc<vi>, vi> to_cycles_and_matching(ll N) {
     C[0] = a;
     FOR(i, N - 2) {
       ll nxt = (i % 2 == 0 ? 2 * a + 2 - C[i] : 2 * a - C[i]);
-      if (nxt < 0)
-        nxt += mod;
-      if (nxt >= mod)
-        nxt -= mod;
+      if (nxt < 0) nxt += mod;
+      if (nxt >= mod) nxt -= mod;
       C[i + 1] = nxt;
     }
     C[N - 1] = mod;
