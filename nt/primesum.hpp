ef2c5b725d69432a479b6cfe393535b55ba6431b
maspypy <45988013+maspypy@users.noreply.github.com>
2023-08-06 05:48:33 +0900

    Update primesum.hpp

--- nt/primesum.hpp
+++ nt/primesum.hpp
@@ -23,21 +23,21 @@ struct PrimeSum {
 
   template <typename F>
   void calc(const F f) {
-    auto primes = primetable(sqN);
+    auto primes = primetable<int>(sqN);
     sum_lo.resize(sqN + 1);
     sum_hi.resize(sqN + 1);
     FOR3(i, 1, sqN + 1) sum_lo[i] = f(i) - 1;
     FOR3(i, 1, sqN + 1) sum_hi[i] = f(double(N) / i) - 1;
-    for (auto&& p: primes) {
-      ll pp = p * p;
+    for (int p: primes) {
+      ll pp = ll(p) * p;
       if (pp > N) break;
       ll R = min(sqN, N / pp);
       ll M = sqN / p;
       T x = sum_lo[p - 1];
       T fp = sum_lo[p] - sum_lo[p - 1];
-      FOR3(i, 1, M + 1) sum_hi[i] -= fp * (sum_hi[i * p] - x);
-      FOR3(i, M + 1, R + 1) sum_hi[i] -= fp * (sum_lo[double(N) / (i * p)] - x);
-      FOR3_R(n, pp, sqN + 1) sum_lo[n] -= fp * (sum_lo[double(n / p)] - x);
+      FOR(i, 1, M + 1) sum_hi[i] -= fp * (sum_hi[i * p] - x);
+      FOR(i, M + 1, R + 1) sum_hi[i] -= fp * (sum_lo[N / (double(i) * p)] - x);
+      FOR_R(n, pp, sqN + 1) sum_lo[n] -= fp * (sum_lo[double(n) / p] - x);
     }
     calculated = 1;
   }
