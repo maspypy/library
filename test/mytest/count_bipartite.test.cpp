d7e0cd7289b9979fa99ba12664665749a74ed42f
maspypy <maspypy.gmail.com>
2023-08-07 12:35:48 +0900

    a

--- test/mytest/count_bipartite.test.cpp
+++ test/mytest/count_bipartite.test.cpp
@@ -9,11 +9,12 @@ using mint = modint998;
 
 void test() {
   vc<mint> A = count_bipartite<mint>(10, true);
-  vc<mint> B
-      = {0, 1, 1, 3, 19, 195, 3031, 67263, 2086099, 89224635, 5254054111};
+  vc<mint> B = {0LL,    1LL,     1LL,       3LL,        19LL,        195LL,
+                3031LL, 67263LL, 2086099LL, 89224635LL, 5254054111LL};
   assert(A == B);
   A = count_bipartite<mint>(10, false);
-  B = {1, 1, 2, 7, 41, 376, 5177, 103237, 2922446, 116011231, 6433447397};
+  B = {1LL,    1LL,      2LL,       7LL,         41LL,        376LL,
+       5177LL, 103237LL, 2922446LL, 116011231LL, 6433447397LL};
   assert(A == B);
 }
 
