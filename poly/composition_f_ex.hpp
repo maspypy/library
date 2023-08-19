d608e1b599c0dc6c8b2e05ab42c6685bac33d9c4
maspypy <maspypy.gmail.com>
2023-08-20 02:09:51 +0900

    a

--- poly/composition_f_ex.hpp
+++ poly/composition_f_ex.hpp
@@ -3,6 +3,7 @@
 // N 次多項式 f に対して、f(e^x) を N 次まで。O(Nlog^2N)
 template <typename mint>
 vc<mint> composition_f_ex(vc<mint> f) {
+  int N = len(f) - 1;
   vc<pair<mint, mint>> AB;
   FOR(k, len(f)) AB.eb(f[k], mint(k));
   return sum_of_exp_bx(N, AB);
