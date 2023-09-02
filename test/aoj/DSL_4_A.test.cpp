ca97e5d2bf7723a6a8c5be7885b5fede1f19ea31
maspypy <45988013+maspypy@users.noreply.github.com>
2023-09-03 01:41:05 +0900

    Update DSL_4_A.test.cpp

--- test/aoj/DSL_4_A.test.cpp
+++ test/aoj/DSL_4_A.test.cpp
@@ -2,7 +2,7 @@
   "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_4_A"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "other/rectangle_union.hpp"
+#include "ds/rectangle_union.hpp"
 
 void solve() {
   LL(N);
