04a43bcdf7c342ed8c8f68bf22611ec43a7f297f
maspypy <maspypy.gmail.com>
2023-09-04 21:37:24 +0900

    a

--- knapsack/subset_sum.hpp
+++ knapsack/subset_sum.hpp
@@ -1,8 +1,9 @@
-// 作れるならば復元、そうでないならば {} を返す
-// O(N max(vals)) 時間
-template <typename INT>
-vc<int> subset_sum(vc<INT>& vals, int target) {
-  if (target <= 0) return {};
+#include "ds/my_bitset.hpp"
+#include "enumerate/bits.hpp"
+
+// O(N MAX(vals))
+template <typename T>
+vc<int> subset_sum_solution_1(vc<T>& vals, int target) {
   int n = len(vals);
   int mx = MAX(vals);
   int b = 0, sb = 0;
@@ -54,4 +55,50 @@ vc<int> subset_sum(vc<INT>& vals, int target) {
   assert(sm == target);
 
   return I;
+}
+
+// O(N target / w)
+template <typename T>
+vc<int> subset_sum_solution_2(vc<T>& vals, int target) {
+  int n = len(vals);
+  auto I = argsort(vals);
+  My_Bitset dp(1, 1);
+  vc<int> last(target + 1, -1);
+  FOR(k, n) {
+    int v = vals[I[k]];
+    if (v > target) continue;
+    My_Bitset newdp = dp;
+    int new_size = min<int>(len(dp) + v, target + 1);
+    newdp.resize(new_size);
+    dp.resize(new_size - v);
+    newdp.or_to_range(v, new_size, dp);
+    // update したところをメモ
+    FOR(i, len(newdp.dat)) {
+      u64 upd = (i < len(dp.dat) ? dp.dat[i] : u64(0)) ^ newdp.dat[i];
+      enumerate_bits_64(upd, [&](int p) -> void { last[(i << 6) | p] = I[k]; });
+    }
+    swap(dp, newdp);
+  }
+  if (target >= len(dp) || !dp[target]) return {};
+  vc<int> ANS;
+  while (target > 0) {
+    int i = last[target];
+    ANS.eb(i);
+    target -= vals[i];
+  }
+  return ANS;
+}
+
+template <typename T>
+vc<int> subset_sum(vc<T>& vals, int target) {
+  if (target <= 0) return {};
+  int n = len(vals);
+  if (n == 0) return {};
+  int mx = MAX(vals);
+
+  // しきい値の調整をしていない
+  // solution 1: O(N mx))
+  // solution 2: O(N target / w)
+  return mx <= target / 500 ? subset_sum_solution_1(vals, target)
+                            : subset_sum_solution_2(vals, target);
 }
