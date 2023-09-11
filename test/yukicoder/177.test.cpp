d02a91c039c8530cabe86e3ed14afe8cf979389f
maspypy <45988013+maspypy@users.noreply.github.com>
2023-09-11 23:26:41 +0900

    Update 177.test.cpp

--- test/yukicoder/177.test.cpp
+++ test/yukicoder/177.test.cpp
@@ -9,9 +9,9 @@ void solve() {
   VEC(ll, A, N);
   LL(M);
   VEC(ll, C, M);
-  MaxFlowGraph<ll> G(N + M + 2);
   ll source = N + M;
   ll sink = N + M + 1;
+  MaxFlow<ll> G(N + M + 2, source, sink);
   auto left = [&](int i) -> int { return i; };
   auto right = [&](int i) -> int { return N + i; };
   FOR(i, N) G.add(source, left(i), A[i]);
@@ -26,7 +26,7 @@ void solve() {
     FOR(i, N) if (ok[i]) { G.add(left(i), right(j), infty<ll>); }
   }
   G.build();
-  ll f = G.flow(source, sink);
+  ll f = G.flow();
   if (f < W)
     print("BANSAKUTSUKITA");
   else
@@ -34,13 +34,6 @@ void solve() {
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
-  ll T = 1;
-  // LL(T);
-  FOR(_, T) solve();
-
+  solve();
   return 0;
 }
