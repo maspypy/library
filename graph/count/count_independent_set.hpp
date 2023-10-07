9dd6e017601db7f533db83357a8b55c3ef203d6a
maspypy <maspypy.gmail.com>
2023-10-07 13:07:07 +0900

    a

--- graph/count/count_independent_set.hpp
+++ graph/count/count_independent_set.hpp
@@ -124,3 +124,104 @@ vc<u64> count_independent_set_by_size(GT& G) {
   res.resize(N + 1);
   return res;
 }
+
+// 重みは頂点重みの積
+// https://codeforces.com/contest/468/problem/E
+template <typename T, typename GT>
+vc<T> count_independent_set_by_size_weighted(GT& G, vc<T> wt) {
+  using U = u64;
+  const int N = G.N;
+  assert(N < 64);
+  if (N == 0) return {1};
+  vc<U> nbd(N);
+  FOR(v, N) for (auto&& e: G[v]) nbd[v] |= U(1) << e.to;
+
+  auto solve_path = [&](const vc<T>& A) -> vc<T> {
+    int N = len(A);
+    vv(T, dp, 2, ceil(N, 2) + 2);
+    dp[0][0] = 1;
+    FOR(i, N) {
+      FOR_R(j, ceil(i, 2) + 1) {
+        T a = dp[0][j];
+        T b = dp[1][j];
+        dp[0][j] = a + b, dp[1][j] = 0;
+        dp[1][j + 1] += a * A[i];
+      }
+    }
+    vc<T> f(ceil(N, 2) + 1);
+    FOR(j, len(f)) f[j] = dp[0][j] + dp[1][j];
+    return f;
+  };
+  auto solve_cycle = [&](const vc<T>& A) -> vc<T> {
+    int N = len(A);
+    vvv(T, dp, 2, 2, ceil(N, 2) + 2);
+    dp[0][0][0] = 1;
+    dp[1][1][1] = A[0];
+    FOR(i, 1, N) {
+      FOR(k, 2) {
+        FOR_R(j, ceil(i, 2) + 1) {
+          T a = dp[k][0][j];
+          T b = dp[k][1][j];
+          dp[k][0][j] = a + b, dp[k][1][j] = 0;
+          dp[k][1][j + 1] += a * A[i];
+        }
+      }
+    }
+    vc<T> f(N / 2 + 1);
+    FOR(k, N / 2 + 1) { f[k] = dp[0][0][k] + dp[0][1][k] + dp[1][0][k]; }
+    return f;
+  };
+
+  auto dfs = [&](auto& dfs, U s) -> vc<T> {
+    vc<T> res = {1};
+    pair<int, int> p = {-1, -1}; // (v, d)
+    FOR(v, N) if (s >> v & 1) {
+      int d = popcnt(nbd[v] & s);
+      if (chmax(p.se, d)) p.fi = v;
+      if (d == 0) {
+        res.eb(0);
+        FOR_R(i, len(res) - 1) res[i + 1] += res[i] * wt[v];
+        s &= ~(U(1) << v);
+      }
+    }
+    if (s == 0) return res;
+    int v = p.fi;
+    if (p.se >= 3) {
+      s &= ~(U(1) << v);
+      auto f = dfs(dfs, s), g = dfs(dfs, s & ~nbd[v]);
+      if (len(f) < len(g) + 1) f.resize(len(g) + 1);
+      FOR(i, len(g)) f[i + 1] += g[i] * wt[v];
+      return convolution<T>(f, res);
+    }
+    // d <= 2, path と cycle のみ
+    vc<int> V;
+    FOR(v, N) if (s >> v & 1) V.eb(v);
+    int n = len(V);
+    Graph<bool, 0> G(n);
+    FOR(i, n) {
+      U x = nbd[V[i]] & s;
+      while (x) {
+        int v = topbit(x);
+        x ^= U(1) << v;
+        int j = LB(V, v);
+        if (i < j) G.add(i, j);
+      }
+    }
+    G.build();
+    auto [paths, cycs] = path_cycle(G);
+    for (auto&& P: paths) {
+      vc<T> A;
+      for (auto& i: P) A.eb(wt[V[i]]);
+      res = convolution<T>(res, solve_path(A));
+    }
+    for (auto&& P: cycs) {
+      vc<T> A;
+      for (auto& i: P) A.eb(wt[V[i]]);
+      res = convolution(res, solve_cycle(A));
+    }
+    return res;
+  };
+  auto res = dfs(dfs, (U(1) << N) - 1);
+  res.resize(N + 1);
+  return res;
+}
--- linalg/bitset/solve_linear.hpp
+++ linalg/bitset/solve_linear.hpp
@@ -1,3 +1,5 @@
+#include "ds/my_bitset.hpp"
+
 // 行ベクトルを bitset にする
 // (2000, 8000) で 300ms 程度（ABC276H）
 template <typename BS, typename T>
@@ -6,19 +8,21 @@ vc<BS> solve_linear(int n, int m, vc<BS> A, vc<T> b) {
   int rk = 0;
   FOR(j, m) {
     if (rk == n) break;
-    FOR(i, rk, n) if (A[i][j]) {
+    FOR(i, rk + 1, n) if (A[i][j]) {
       swap(A[rk], A[i]);
       if (b[rk] != b[i]) b[rk] = !b[rk], b[i] = !b[i];
       break;
     }
     if (!A[rk][j]) continue;
     FOR(i, n) if (i != rk) {
-      if (A[i][j]) { b[i] = b[i] ^ b[rk], A[i] ^= A[rk]; }
+      if (A[i][j]) { b[i] = b[i] ^ b[rk], A[i] = A[i] ^ A[rk]; }
     }
     ++rk;
   }
   FOR(i, rk, n) if (b[i]) return {};
   vc<BS> res(1);
+  if constexpr (is_same_v<BS, My_Bitset>) { res[0].resize(m); }
+
   vc<int> pivot(m, -1);
   int p = 0;
   FOR(i, rk) {
@@ -27,8 +31,10 @@ vc<BS> solve_linear(int n, int m, vc<BS> A, vc<T> b) {
   }
   FOR(j, m) if (pivot[j] == -1) {
     BS x;
+    if constexpr (is_same_v<BS, My_Bitset>) { x.resize(m); }
+
     x[j] = 1;
-    FOR(k, j) if (pivot[k] != -1) x[k] = A[pivot[k]][j];
+    FOR(k, j) if (pivot[k] != -1 && A[pivot[k]][j]) x[k] = 1;
     res.eb(x);
   }
   return res;
