a747a3262b3c631e1c73de38004305bd743ba0e5
maspypy <maspypy.gmail.com>
2023-10-14 01:43:46 +0900

    a

--- alg/monoid/add_array.hpp
+++ alg/monoid/add_array.hpp
@@ -9,5 +9,13 @@ struct Monoid_Add_Array {
     return x;
   }
   static constexpr X unit() { return X{}; }
+  static constexpr X inverse(X x) {
+    for (auto& v: x) v = -v;
+    return x;
+  }
+  static constexpr X power(X x, ll n) {
+    for (auto& v: x) v *= E(n);
+    return x;
+  }
   static constexpr bool commute = 1;
 };
