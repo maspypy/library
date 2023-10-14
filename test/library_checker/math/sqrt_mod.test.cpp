f0291b7afdbcab7d1533bc09aa861e2df73c2466
maspypy <maspypy.gmail.com>
2023-10-14 15:49:24 +0900

    a

--- test/library_checker/math/sqrt_mod.test.cpp
+++ test/library_checker/math/sqrt_mod.test.cpp
@@ -1,26 +1,16 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/sqrt_mod"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "mod/dynamic_modint.hpp"
+
 #include "mod/mod_sqrt.hpp"
 
-using mint = dmint;
-
 void solve() {
-  LL(T);
-  FOR(T) {
-    LL(y0, p);
-    mint::set_mod(p);
-    mint y = y0;
-    mint r = mod_sqrt(y);
-    if (r * r == y)
-      print(r);
-    else
-      print(-1);
-  }
+  INT(y, p);
+  print(mod_sqrt(y, p));
 }
 
 signed main() {
-  solve();
+  INT(T);
+  FOR(T) solve();
   return 0;
 }
