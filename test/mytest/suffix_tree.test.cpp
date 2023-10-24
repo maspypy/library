b26d4ca24c073ac040928ce7a37d6037a22b84e1
maspypy <maspypy.gmail.com>
2023-10-25 01:14:51 +0900

    a

--- test/mytest/suffix_tree.test.cpp
+++ test/mytest/suffix_tree.test.cpp
@@ -1,9 +1,62 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
-
 #include "my_template.hpp"
 #include "other/io.hpp"
 
+#include "random/base.hpp"
 #include "string/suffix_tree.hpp"
+#include "alg/monoid/min_idx.hpp"
+#include "ds/segtree/segtree.hpp"
+#include "graph/base.hpp"
+
+// https://twitter.com/maspy_stars/status/1565901414236205057?s=20&t=S2Tu6ayozHcakxai8dmh4g
+// 各ノードは、suffix array での長方形領域と見なして、
+// グラフおよび、領域データを作る。
+// sample: test/my_test/suffix_tree.test.cpp
+template <typename SUFFIX>
+pair<Graph<int, 1>, vc<tuple<int, int, int, int>>> suffix_tree_old(SUFFIX& X) {
+  auto SA = X.SA;
+  auto ISA = X.ISA;
+  auto LCP = X.LCP;
+
+  int N = len(SA);
+
+  using Mono = Monoid_Min_Idx<int, 1>;
+
+  SegTree<Mono> seg(N - 1, [&](int i) -> Mono::X { return {LCP[i], i}; });
+
+  using T = tuple<int, int, int, int>;
+  vc<T> dat;
+  dat.eb(0, N, 0, 0);
+  vc<pair<int, int>> edges;
+
+  auto dfs = [&](auto& dfs, int p, int l, int r, int h) -> void {
+    if (r == l + 1) {
+      int i = SA[l];
+      int sz = N - i;
+      if (h == sz) return;
+      int k = len(dat);
+      dat.eb(l, l + 1, h, sz);
+      edges.eb(p, k);
+      return;
+    }
+    auto [lcp, i] = seg.prod(l, r - 1);
+    if (lcp == h) {
+      dfs(dfs, p, l, i + 1, h);
+      dfs(dfs, p, i + 1, r, h);
+      return;
+    }
+    int k = len(dat);
+    dat.eb(l, r, h, lcp);
+    edges.eb(p, k);
+    dfs(dfs, k, l, r, lcp);
+  };
+  dfs(dfs, 0, 0, N, 0);
+
+  Graph<int, 1> G(len(dat));
+  for (auto&& [a, b]: edges) G.add(a, b);
+  G.build();
+  return {G, dat};
+}
 
 /*
 S = aabbabbaa
@@ -30,7 +83,8 @@ suffix tree の node はこの長方形領域を表す
 7112-----
 7113-----
 */
-void test() {
+
+void test1() {
   string S = "aabbabbaa";
   Suffix_Array X(S);
   auto [G, dat] = suffix_tree(X);
@@ -71,20 +125,34 @@ void test() {
   check_edge(G.edges[12], 11, 13);
 }
 
+void test2() {
+  FOR(N, 1, 10) {
+    FOR(100) {
+      int K = RNG(1, 5);
+      string S;
+      FOR(N) { S += 'a' + (RNG(0, K)); }
+      Suffix_Array SA(S);
+      auto [G1, dat1] = suffix_tree_old(SA);
+      auto [G2, dat2] = suffix_tree(SA);
+      assert(dat1 == dat2);
+      assert(G1.N == G2.N);
+      assert(G1.M == G2.M);
+      FOR(i, G1.M) {
+        assert(G1.edges[i].frm == G2.edges[i].frm);
+        assert(G1.edges[i].to == G2.edges[i].to);
+      }
+    }
+  }
+}
+
 void solve() {
-  LL(a, b);
+  INT(a, b);
   print(a + b);
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
-  test();
-
-  ll T = 1;
-  FOR(T) solve();
-
+  test1();
+  test2();
+  solve();
   return 0;
 }
