2ee5b85ed2eb21830253c83c6670393f09de45e2
maspypy <maspypy.gmail.com>
2023-10-25 02:01:34 +0900

    a

--- mod/mod_pow.hpp
+++ mod/mod_pow.hpp
@@ -17,7 +17,7 @@ int mod_pow(int a, ll n, int mod) {
 ll mod_pow_64(ll a, ll n, ll mod) {
   assert(n >= 0);
   a = ((a %= mod) < 0 ? a + mod : a);
-  Barrett bt(mod);
+  Barrett_64 bt(mod);
   ll p = a, v = bt.modulo(1);
   while (n) {
     if (n & 1) v = bt.mul(v, p);
