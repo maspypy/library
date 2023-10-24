824092c6e740129301042d68c8cbdea6dbdd63cc
maspypy <maspypy.gmail.com>
2023-10-24 13:35:52 +0900

    a

--- geo3d/base.hpp
+++ geo3d/base.hpp
@@ -1,26 +1,28 @@
 
 template <typename T>
-struct Point {
+struct Point_3d {
   T x, y, z;
 
-  Point() = default;
+  Point_3d() = default;
 
   template <typename A, typename B, typename C>
-  Point(A x, B y, C z) : x(x), y(y), z(z) {}
+  Point_3d(A x, B y, C z) : x(x), y(y), z(z) {}
 
-  Point operator+(Point p) const { return {x + p.x, y + p.y, z + p.z}; }
-  Point operator-(Point p) const { return {x - p.x, y - p.y, z - p.z}; }
-  bool operator==(Point p) const { return x == p.x && y == p.y && z == p.z; }
-  bool operator!=(Point p) const { return x != p.x || y != p.y || z == p.z; }
-  Point operator-() const { return {-x, -y, -z}; }
+  Point_3d operator+(Point_3d p) const { return {x + p.x, y + p.y, z + p.z}; }
+  Point_3d operator-(Point_3d p) const { return {x - p.x, y - p.y, z - p.z}; }
+  Point_3d operator*(T t) const { return {x * t, y * t, z * t}; }
+  bool operator==(Point_3d p) const { return x == p.x && y == p.y && z == p.z; }
+  bool operator!=(Point_3d p) const { return x != p.x || y != p.y || z == p.z; }
+  Point_3d operator-() const { return {-x, -y, -z}; }
 
-  bool is_parallel(Point p) const {
+  bool is_parallel(Point_3d p) const {
     return x * p.y == y * p.x && y * p.z == z * p.y && z * p.x == x * p.z;
   }
 
-  T dot(Point other) { return x * other.x + y * other.y + z * other.z; }
-  Point cross(Point other) {
-    return Point(y * other.z - z * other.y, z * other.x - x * other.z,
-                 x * other.y - y * other.x);
+  T dot(Point_3d other) { return x * other.x + y * other.y + z * other.z; }
+  double norm() { return sqrt(x * x + y * y + z * z); }
+  Point_3d cross(Point_3d other) {
+    return Point_3d(y * other.z - z * other.y, z * other.x - x * other.z,
+                    x * other.y - y * other.x);
   }
 };
