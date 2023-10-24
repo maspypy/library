8ac649abefb3a3fd01c74ed6f61190e9d768d8ae
maspypy <maspypy.gmail.com>
2023-10-24 15:43:21 +0900

    a

--- linalg/det_A_plus_xB.hpp
+++ linalg/det_A_plus_xB.hpp
@@ -1,6 +1,6 @@
 #include "random/base.hpp"
-#include "linalg/mat_mul.hpp"
-#include "linalg/mat_inv.hpp"
+#include "linalg/matrix_mul.hpp"
+#include "linalg/matrix_inv.hpp"
 #include "linalg/characteristic_poly.hpp"
 #include "poly/poly_taylor_shift.hpp"
 
@@ -13,9 +13,9 @@ vc<mint> det_A_plus_xB(vvc<mint> A, vvc<mint> B) {
   vc<mint> f(N + 1);
   mint a = RNG(mint::get_mod());
   FOR(i, N) FOR(j, N) A[i][j] += a * B[i][j];
-  auto [det_A, inv_A] = mat_inv(A);
+  auto [det_A, inv_A] = matrix_inv(A);
   if (det_A == 0) { return f; }
-  B = mat_mul(B, inv_A);
+  B = matrix_mul(B, inv_A);
   FOR(i, N) FOR(j, N) B[i][j] = -B[i][j];
   f = characteristic_poly(B);
   reverse(all(f));
