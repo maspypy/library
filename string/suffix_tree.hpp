f65998efc1f67dad1af1a837440bf84c568bde1e
maspypy <maspypy.gmail.com>
2023-10-25 01:14:51 +0900

    a

--- string/suffix_tree.hpp
+++ string/suffix_tree.hpp
@@ -1,7 +1,6 @@
 
 #include "string/suffix_array.hpp"
-#include "alg/monoid/min_idx.hpp"
-#include "ds/segtree/segtree.hpp"
+#include "seq/cartesian_tree.hpp"
 #include "graph/base.hpp"
 
 // https://twitter.com/maspy_stars/status/1565901414236205057?s=20&t=S2Tu6ayozHcakxai8dmh4g
@@ -14,39 +13,55 @@ pair<Graph<int, 1>, vc<tuple<int, int, int, int>>> suffix_tree(SUFFIX& X) {
   auto ISA = X.ISA;
   auto LCP = X.LCP;
 
-  int N = len(SA);
-
-  using Mono = Monoid_Min_Idx<int, 1>;
-
-  SegTree<Mono> seg(N - 1, [&](int i) -> Mono::X { return {LCP[i], i}; });
-
-  using T = tuple<int, int, int, int>;
-  vc<T> dat;
-  dat.eb(0, N, 0, 0);
+  vc<tuple<int, int, int, int>> dat;
   vc<pair<int, int>> edges;
 
-  auto dfs = [&](auto& dfs, int p, int l, int r, int h) -> void {
-    if (r == l + 1) {
-      int i = SA[l];
-      int sz = N - i;
-      if (h == sz) return;
-      int k = len(dat);
-      dat.eb(l, l + 1, h, sz);
-      edges.eb(p, k);
-      return;
+  int N = len(SA);
+  if (N == 1) {
+    Graph<int, 1> G(2);
+    G.add(0, 1);
+    dat.eb(0, 1, 0, 0);
+    dat.eb(0, 1, 0, 1);
+    return {G, dat};
+  }
+
+  dat.eb(0, N, 0, 0);
+  CartesianTree<int, true> CT(LCP);
+
+  auto dfs = [&](auto& dfs, int p, int idx, int h) -> void {
+    int L = CT.range[idx].fi;
+    int R = CT.range[idx].se + 1;
+    int hh = LCP[idx];
+    if (h < hh) {
+      edges.eb(p, len(dat));
+      p = len(dat);
+      dat.eb(L, R, h, hh);
     }
-    auto [lcp, i] = seg.prod(l, r - 1);
-    if (lcp == h) {
-      dfs(dfs, p, l, i + 1, h);
-      dfs(dfs, p, i + 1, r, h);
-      return;
+    if (CT.lch[idx] == -1) {
+      if (hh < N - SA[idx]) {
+        edges.eb(p, len(dat));
+        dat.eb(idx, idx + 1, hh, N - SA[idx]);
+      }
+    } else {
+      dfs(dfs, p, CT.lch[idx], hh);
     }
-    int k = len(dat);
-    dat.eb(l, r, h, lcp);
-    edges.eb(p, k);
-    dfs(dfs, k, l, r, lcp);
+    if (CT.rch[idx] == -1) {
+      if (hh < N - SA[idx + 1]) {
+        edges.eb(p, len(dat));
+        dat.eb(idx + 1, idx + 2, hh, N - SA[idx + 1]);
+      }
+    } else {
+      dfs(dfs, p, CT.rch[idx], hh);
+    }
   };
-  dfs(dfs, 0, 0, N, 0);
+  int r = CT.root;
+  if (LCP[r] > 0) {
+    edges.eb(0, 1);
+    dat.eb(0, N, 0, LCP[r]);
+    dfs(dfs, 1, r, LCP[r]);
+  } else {
+    dfs(dfs, 0, r, 0);
+  }
 
   Graph<int, 1> G(len(dat));
   for (auto&& [a, b]: edges) G.add(a, b);
