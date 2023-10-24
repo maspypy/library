37fb0df8de33cac4b75d04ba147ee5735ddf7a7c
maspypy <maspypy.gmail.com>
2023-10-24 14:07:48 +0900

    a

--- linalg/transpose.hpp
+++ linalg/transpose.hpp
@@ -1,5 +1,6 @@
 template <typename VC>
-vc<VC> transpose(const int n, const int m, const vc<VC>& A) {
+vc<VC> transpose(const vc<VC>& A, int H = -1, int W = -1) {
+  if (H == -1) { H = len(A), W = len(A[0]); }
   vc<VC> B(m, VC(n, A[0][0]));
   FOR(x, n) FOR(y, m) B[y][x] = A[x][y];
   return B;
