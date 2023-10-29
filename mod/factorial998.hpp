86082b7703b7f541e92caf26e76a24e127af0d87
maspypy <45988013+maspypy@users.noreply.github.com>
2023-10-30 01:33:52 +0900

    Update factorial998.hpp

--- mod/factorial998.hpp
+++ mod/factorial998.hpp
@@ -4,7 +4,7 @@ int factorial998table[1024] = {1,467742124,703158536,849331177,183632821,7867875
 int factorial998(ll n) {
   constexpr int mod = 998244353;
   if (n >= mod) return 0;
-  auto [q, r] = divmod(n, 1 << 20);
+  auto [q, r] = divmod<int>(n, 1 << 20);
   ll x = factorial998table[q];
   int s = q << 20;
   FOR(i, r) x = x * (s + i + 1) % mod;
