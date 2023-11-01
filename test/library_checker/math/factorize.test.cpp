cd375e04c8d28aab20c4797d77de5426b1dfd85f
maspypy <maspypy.gmail.com>
2023-11-02 04:28:32 +0900

    a

--- test/library_checker/math/factorize.test.cpp
+++ test/library_checker/math/factorize.test.cpp
@@ -5,25 +5,19 @@
 #include "nt/factor.hpp"
 
 void solve() {
-  LL(Q);
-  FOR(_, Q) {
-    LL(x);
-    auto pf = factor(x);
-    vi ANS;
-    for (auto&& [p, e]: pf) { FOR(_, e) ANS.eb(p); }
-    if (len(ANS))
-      print(len(ANS), ANS);
-    else
-      print(0);
-  }
+  LL(x);
+  auto pf = factor(x);
+  vi ANS;
+  for (auto&& [p, e]: pf) { FOR(_, e) ANS.eb(p); }
+  if (len(ANS))
+    print(len(ANS), ANS);
+  else
+    print(0);
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
-  solve();
+  INT(T);
+  FOR(T) solve();
 
   return 0;
 }
