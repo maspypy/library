db4d61c19e8cadb7e87ae0edc29a87630c84ed4c
maspypy <maspypy.gmail.com>
2023-11-02 19:21:51 +0900

    a

--- test/yukicoder/1867.test.cpp
+++ test/yukicoder/1867.test.cpp
@@ -1,78 +1,35 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1867"
 
-// https://cyanmond.hatenablog.com/entry/2023/08/07/163326
-// F f: f(fid,x)
-template <typename T, bool MINIMIZE, typename F>
-struct Monge_LiChao_Tree {
-  int n, log, size;
-  F f;
-  vc<int> FID;
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "convex/monge_lichao_tree.hpp"
 
-  Monge_LiChao_Tree(int n, F f) : n(n), f(f) {
-    log = 1;
-    while ((1 << log) < n) ++log;
-    size = 1 << log;
-    FID.assign(size << 1, -1);
+void solve() {
+  LL(N);
+  VEC(int, A, N);
+  vv(int, inv, N + 1, N + 1);
+  FOR_R(L, N + 1) FOR(R, L, N + 1) {
+    if (R - L <= 1) continue;
+    inv[L][R]
+        = inv[L + 1][R] + inv[L][R - 1] - inv[L + 1][R - 1] + (A[L] > A[R - 1]);
   }
 
-  void add_line(int fid) { return add_line_at(1, fid); }
-  void add_segment(int L, int R, int fid) {
-    L += size, R += size;
-    while (L < R) {
-      if (L & 1) add_line_at(L++, fid);
-      if (R & 1) add_line_at(--R, fid);
-      L >>= 1, R >>= 1;
+  vc<int> dp(N + 1);
+  FOR(n, 1, N + 1) {
+    vc<int> newdp(N + 1, -infty<int>);
+    auto f = [&](int i, int j) -> int { return dp[i] + inv[i][j]; };
+    Monge_LiChao_Tree<int, false, decltype(f)> LCT(N + 1, f);
+    FOR(i, N + 1) {
+      chmax(newdp[i], LCT.query(i).fi);
+      LCT.add_line(i);
     }
+    swap(dp, newdp);
+    int ans = inv[0][N] - dp[N];
+    print(ans);
   }
+}
 
-  // [fx, fid]
-  pair<T, int> query(int x) {
-    int i = x + size;
-    pair<T, int> res;
-    if (!MINIMIZE) res = {-infty<T>, -1};
-    if (MINIMIZE) res = {infty<T>, -1};
-    while (i) {
-      pair<T, int> res1 = {evaluate(FID[i], x), FID[i]};
-      res = (MINIMIZE ? min(res, res1) : max(res, res1));
-      i >>= 1;
-    }
-    return res;
-  }
-
-private:
-  void add_line_at(int i, int fid) {
-    int upper_bit = 31 - __builtin_clz(i);
-    int l = (size >> upper_bit) * (i - (1 << upper_bit));
-    int r = l + (size >> upper_bit);
-    while (l < r) {
-      int gid = FID[i];
-      T fl = evaluate(fid, l), fr = evaluate(fid, r - 1);
-      T gl = evaluate(gid, l), gr = evaluate(gid, r - 1);
-      bool bl = (MINIMIZE ? fl < gl : fl > gl);
-      bool br = (MINIMIZE ? fr < gr : fr > gr);
-      if (bl && br) {
-        FID[i] = fid;
-        return;
-      }
-      if (!bl && !br) return;
-      int m = (l + r) / 2;
-      T fm = evaluate(fid, m), gm = evaluate(gid, m);
-      bool bm = (MINIMIZE ? fm < gm : fm > gm);
-      if (bm) {
-        FID[i] = fid;
-        fid = gid;
-        if (!bl) { i = 2 * i + 0, r = m; }
-        if (bl) { i = 2 * i + 1, l = m; }
-      }
-      if (!bm) {
-        if (bl) { i = 2 * i + 0, r = m; }
-        if (!bl) { i = 2 * i + 1, l = m; }
-      }
-    }
-  }
-
-  T evaluate(int fid, int x) {
-    if (fid == -1) return (MINIMIZE ? infty<T> : -infty<T>);
-    chmin(x, n - 1);
-    return f(fid, x);
-  }
-};
+signed main() {
+  solve();
+  return 0;
+}
