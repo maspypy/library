7af17daa7dddfbd19f4858ac2e141052fe875718
maspypy <maspypy.gmail.com>
2023-11-03 16:29:39 +0900

    a

--- graph/count/count_forest.hpp
+++ graph/count/count_forest.hpp
@@ -4,8 +4,8 @@
 template <typename mint>
 vc<mint> count_forest(ll nmax) {
   vc<mint> f = count_tree<mint>(nmax);
-  FOR(i, nmax) f[i] *= fact_inv<mint>(i);
+  FOR(i, len(f)) f[i] *= fact_inv<mint>(i);
   f = fps_exp(f);
-  FOR(i, nmax) f[i] *= fact<mint>(i);
+  FOR(i, len(f)) f[i] *= fact<mint>(i);
   return f;
 }
