d5b6e88bd0290c4be90f35488852605930e398ed
maspypy <maspypy.gmail.com>
2023-11-04 06:12:21 +0900

    a

---
+++ test/library_checker/tree/frequency_table_of_tree_distance_2.test.cpp
@@ -0,0 +1,39 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/frequency_table_of_tree_distance"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "graph/centroid_decomposition.hpp"
+#include "poly/convolution.hpp"
+
+void solve() {
+  LL(N);
+  Graph<int> G(N);
+  G.read_tree(0, 0);
+  vi ANS(N);
+  auto f = [&](Graph<int, 1>& G, vc<int>& V, vc<int>& color) -> void {
+    int N = G.N;
+    vc<int> dist(N);
+    FOR(i, N) {
+      for (auto& e: G[i]) dist[e.to] = dist[i] + 1;
+    }
+    vi f(N), g(N);
+    FOR(i, N) {
+      if (color[i] == 0) f[dist[i]]++;
+      if (color[i] == 1) g[dist[i]]++;
+    }
+    while (len(f) && f.back() == 0) POP(f);
+    while (len(g) && g.back() == 0) POP(g);
+    f = convolution(f, g);
+    FOR(i, len(f)) ANS[i] += f[i];
+  };
+  centroid_decomposition_2(G, f);
+  ANS.erase(ANS.begin());
+  print(ANS);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
