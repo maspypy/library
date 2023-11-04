a0865dc97844289fd71cad5961e6c0f31629f1e9
maspypy <maspypy.gmail.com>
2023-11-04 15:56:55 +0900

    a

--- test/library_checker/tree/frequency_table_of_tree_distance_0.test.cpp
+++ test/library_checker/tree/frequency_table_of_tree_distance_0.test.cpp
@@ -12,21 +12,22 @@ void solve() {
   Graph<int> G(N);
   G.read_tree(0, 0);
   vi ANS(N);
-  auto f = [&](vc<int>& par, vc<int>& V, vc<int>& color) -> void {
+  auto f = [&](vc<int>& par, vc<int>& V, vc<int>& indptr) -> void {
     int N = len(par);
     vc<int> dist(N);
     FOR(i, 1, N) { dist[i] = dist[par[i]] + 1; }
-    vi f(N), g(N);
-    FOR(i, N) {
-      if (color[i] == 0) f[dist[i]]++;
-      if (color[i] == 1) g[dist[i]]++;
-    }
-    while (len(f) && f.back() == 0) POP(f);
-    while (len(g) && g.back() == 0) POP(g);
-    f = convolution(f, g);
-    FOR(i, len(f)) ANS[i] += f[i];
+    auto calc = [&](int L, int R, int sgn) -> void {
+      int mx = *max_element(dist.begin() + L, dist.begin() + R);
+      vi f(mx + 1);
+      FOR(i, L, R) f[dist[i]]++;
+      f = convolution(f, f);
+      FOR(i, len(f)) ANS[i] += sgn * f[i];
+    };
+    calc(0, N, +1);
+    FOR(k, 1, len(indptr) - 1) { calc(indptr[k], indptr[k + 1], -1); }
   };
-  centroid_decomposition<2>(G, f);
+  centroid_decomposition<0>(G, f);
+  for (auto& x: ANS) x /= 2;
   ANS.erase(ANS.begin());
   print(ANS);
 }
