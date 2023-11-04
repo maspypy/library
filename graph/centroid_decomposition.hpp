7889895e9aefff847dcc745dc4789382bda18b0f
maspypy <maspypy.gmail.com>
2023-11-04 15:56:55 +0900

    a

--- graph/centroid_decomposition.hpp
+++ graph/centroid_decomposition.hpp
@@ -1,12 +1,68 @@
 #include "graph/base.hpp"
 #include "graph/shortest_path/bfs01.hpp"
 
+template <typename F>
+void centroid_decomposition_0_dfs(vc<int>& par, vc<int>& vs, F f) {
+  const int N = len(par);
+  assert(N >= 1);
+  int c = -1;
+  vc<int> sz(N, 1);
+  FOR_R(i, N) {
+    if (sz[i] >= ceil<int>(N, 2)) {
+      c = i;
+      break;
+    }
+    sz[par[i]] += sz[i];
+  }
+  vc<int> color(N);
+  vc<int> V = {c};
+  int nc = 1;
+  FOR(v, 1, N) {
+    if (par[v] == c) { V.eb(v), color[v] = nc++; }
+  }
+  if (c > 0) {
+    for (int a = par[c]; a != -1; a = par[a]) { color[a] = nc, V.eb(a); }
+    ++nc;
+  }
+  FOR(i, N) {
+    if (i != c && color[i] == 0) color[i] = color[par[i]], V.eb(i);
+  }
+  vc<int> indptr(nc + 1);
+  FOR(i, N) indptr[1 + color[i]]++;
+  FOR(i, nc) indptr[i + 1] += indptr[i];
+  vc<int> counter = indptr;
+  vc<int> ord(N);
+  for (auto& v: V) { ord[counter[color[v]]++] = v; }
+  vc<int> new_idx(N);
+  FOR(i, N) new_idx[ord[i]] = i;
+  vc<int> name(N);
+  FOR(i, N) name[new_idx[i]] = vs[i];
+  {
+    vc<int> tmp(N, -1);
+    FOR(i, 1, N) {
+      int a = new_idx[i], b = new_idx[par[i]];
+      if (a > b) swap(a, b);
+      tmp[b] = a;
+    }
+    swap(par, tmp);
+  }
+  f(par, name, indptr);
+  FOR(k, 1, nc) {
+    int L = indptr[k], R = indptr[k + 1];
+    vc<int> par1(R - L, -1);
+    vc<int> name1(R - L, -1);
+    name1[0] = name[0];
+    FOR(i, L, R) name1[i - L] = name[i];
+    FOR(i, L, R) { par1[i - L] = max(par[i] - L, -1); }
+    centroid_decomposition_0_dfs(par1, name1, f);
+  }
+}
+
 /*
 https://maspypy.com/%e9%87%8d%e5%bf%83%e5%88%86%e8%a7%a3%e3%83%bb1-3%e9%87%8d%e5%bf%83%e5%88%86%e8%a7%a3%e3%81%ae%e3%81%8a%e7%b5%b5%e6%8f%8f%e3%81%8d
 1/3 CD のみ扱う
 centroid_decomposition_1：長さ 2 以上のパス全体
 */
-
 template <typename F>
 void centroid_decomposition_1_dfs(vc<int>& par, vc<int> vs, F f) {
   const int N = len(par);
@@ -35,12 +91,7 @@ void centroid_decomposition_1_dfs(vc<int>& par, vc<int> vs, F f) {
     if (color[par[i]] == 0) color[i] = 0, ord[i] = p++;
   }
   int n0 = p - 1;
-  int a = c;
-  while (1) {
-    a = par[a];
-    if (a == -1) break;
-    color[a] = 1, ord[a] = p++;
-  }
+  for (int a = par[c]; a != -1; a = par[a]) { color[a] = 1, ord[a] = p++; }
   FOR(i, N) {
     if (i != c && color[i] == -1) color[i] = 1, ord[i] = p++;
   }
@@ -65,28 +116,6 @@ void centroid_decomposition_1_dfs(vc<int>& par, vc<int> vs, F f) {
   f(par2, V2, n0, n1);
   centroid_decomposition_1_dfs(par0, V0, f);
   centroid_decomposition_1_dfs(par1, V1, f);
-}
-
-// 長さ 1 のパスは別処理することに注意
-// 長さ 2 以上のパスすべてに対する計算をしたい
-// f(G, V, n1, n2)
-// G: oriented
-// V: label in original tree, dfs order
-// color=1: V[1:1+n1]
-// color=2: V[1+n1:1+n1+n2]
-template <typename GT, typename F>
-void centroid_decomposition_1(GT& G, F f) {
-  if (G.N == 1) return;
-  if constexpr (GT::is_directed) {
-    vc<int> V = toposort(G);
-    G = G.rearrange(V, true);
-    centroid_decomposition_1_dfs(G, V, f);
-  } else {
-    Graph<typename GT::cost_type, true> H = to_directed_tree(G);
-    vc<int> V = toposort(H);
-    H = H.rearrange(V, true);
-    centroid_decomposition_1_dfs(H, V, f);
-  }
 }
 
 /*
@@ -130,12 +159,7 @@ void centroid_decomposition_2_dfs(vc<int>& par, vc<int>& vs, vc<int>& real,
     if (color[par[i]] == 0) color[i] = 0, ord[i] = p++;
   }
   int n0 = p - 1;
-  int a = c;
-  while (1) {
-    a = par[a];
-    if (a == -1) break;
-    color[a] = 1, ord[a] = p++;
-  }
+  for (int a = par[c]; a != -1; a = par[a]) { color[a] = 1, ord[a] = p++; }
   FOR(i, N) {
     if (i != c && color[i] == -1) color[i] = 1, ord[i] = p++;
   }
@@ -199,10 +223,10 @@ void centroid_decomposition(GT& G, F f) {
     tmp[new_idx[i]] = new_idx[j];
   }
   swap(par, tmp);
-  static_assert(MODE == 1 || MODE == 2);
-  if constexpr (MODE == 1) {
-    centroid_decomposition_1_dfs(par, V, f);
-  } else {
+  static_assert(MODE == 0 || MODE == 1 || MODE == 2);
+  if constexpr (MODE == 0) { centroid_decomposition_0_dfs(par, V, f); }
+  elif constexpr(MODE == 1) { centroid_decomposition_1_dfs(par, V, f); }
+  else {
     vc<int> real(N, 1);
     centroid_decomposition_2_dfs(par, V, real, f);
   }
