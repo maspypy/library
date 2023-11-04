1fa14a43dab644e55bc9d57ec75dff41ccae7520
maspypy <maspypy.gmail.com>
2023-11-04 16:22:34 +0900

    a

---
+++ test/yukicoder/1796_1.test.cpp
@@ -0,0 +1,55 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1796"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "graph/centroid_decomposition.hpp"
+#include "mod/modint.hpp"
+#include "poly/middle_product.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N);
+  VEC(mint, A, N);
+  Graph<int, 0> G(N);
+  G.read_tree();
+
+  vc<mint> ANS(N);
+  // 距離 0
+  FOR(v, N) ANS[v] += A[v];
+  // 距離 1
+  for (auto& e: G.edges) {
+    ANS[e.frm] += A[e.to] * inv<mint>(4);
+    ANS[e.to] += A[e.frm] * inv<mint>(4);
+  }
+
+  auto F = [&](vc<int>& par, vc<int> vs, int n1, int n2) -> void {
+    int n = 1 + n1 + n2;
+    vc<int> dist(n);
+    FOR(i, 1, n) dist[i] = dist[par[i]] + 1;
+    auto f = [&](int L1, int R1, int L2, int R2) -> void {
+      int n1 = *max_element(dist.begin() + L1, dist.begin() + R1);
+      int n2 = *max_element(dist.begin() + L2, dist.begin() + R2);
+      vc<mint> a(n1 + n2 + 1), b(n2 + 1);
+      FOR(i, n1 + n2 + 1) a[i] = inv<mint>(1 + i) * inv<mint>(1 + i);
+      FOR(i, L2, R2) { b[dist[i]] += A[vs[i]]; }
+      vc<mint> c = middle_product(a, b);
+      assert(len(c) == n1 + 1);
+      FOR(i, L1, R1) ANS[vs[i]] += c[dist[i]];
+    };
+    f(1, 1 + n1, 1 + n1, n);
+    f(1 + n1, n, 1, 1 + n1);
+  };
+  centroid_decomposition<1>(G, F);
+
+  mint k0 = fact<mint>(N);
+  k0 *= k0;
+  for (auto&& x: ANS) x *= k0;
+  for (auto&& x: ANS) print(x);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
