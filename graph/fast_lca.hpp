ef5d6bae29435885ed8e23462669d174a5f26496
maspypy <maspypy.gmail.com>
2023-11-05 01:22:54 +0900

    a

--- graph/fast_lca.hpp
+++ graph/fast_lca.hpp
@@ -3,68 +3,35 @@
 #include "alg/monoid/min.hpp"
 #include "ds/static_range_product.hpp"
 
-// sparse table を使う <O(NlogN),O(1)>
-// dist_only の方が 2 割くらい高速か
-template <typename TREE, bool dist_only>
+template <typename TREE>
 struct Fast_Lca {
-  using Mono1 = Monoid_Min<int>;
-  using Mono2 = Monoid_Min_Idx<int>;
   TREE& tree;
-  Static_Range_Produc<Mono1, Sparse_Table<Mono1>> seg_mi;
-  Static_Range_Produc<Mono2, Sparse_Table<Mono2>> seg_mi_idx;
+  using Mono = Monoid_Min<int>;
+  Sparse_Table<Monoid_Min<int>> seg;
   vc<int> pos;
 
   Fast_Lca(TREE& tree) : tree(tree) {
     int N = tree.N;
     pos.resize(N);
-    if constexpr (dist_only) {
-      vc<int> dat(2 * N - 1);
-      FOR(v, N) {
-        int a = tree.ELID(v);
-        int b = tree.ERID(v);
-        int d = tree.depth[v];
-        dat[a] = d;
-        pos[v] = a;
-        if (b < 2 * N - 1) dat[b] = d - 1;
-      }
-      seg_mi.build(dat);
-    } else {
-      vc<pair<int, int>> dat(2 * N - 1);
-      FOR(v, N) {
-        int a = tree.ELID(v);
-        int b = tree.ERID(v);
-        int d = tree.depth[v];
-        pos[v] = a;
-        dat[a] = {d, v};
-        if (b < 2 * N - 1) dat[b] = {d - 1, tree.parent[v]};
-      }
-      seg_mi_idx.build(dat);
+    vc<int> dat(2 * N);
+    FOR(v, N) {
+      int a = tree.ELID(v);
+      int b = tree.ERID(v);
+      pos[v] = a;
+      dat[a] = tree.LID[v];
+      dat[b] = (v == tree.V[0] ? -1 : tree.LID[tree.parent[v]]);
     }
-  }
-
-  int lca(int a, int b) {
-    static_assert(!dist_only);
-    int p = pos[a], q = pos[b];
-    if (p > q) swap(p, q);
-    return seg_mi_idx.prod(p, q + 1).se;
+    seg.build(dat);
   }
 
   int dist(int a, int b) {
-    if constexpr (dist_only) {
-      int da = tree.depth[a], db = tree.depth[b];
-      int p = pos[a], q = pos[b];
-      if (p > q) swap(p, q);
-      return da + db - 2 * seg_mi.prod(p, q + 1);
-    } else {
-      return lca_and_dist(a, b).se;
-    }
+    int c = lca(a, b);
+    return tree.depth[a] + tree.depth[b] - 2 * tree.depth[c];
   }
 
-  pair<int, int> lca_and_dist(int a, int b) {
-    int da = tree.depth[a], db = tree.depth[b];
+  int lca(int a, int b) {
     int p = pos[a], q = pos[b];
     if (p > q) swap(p, q);
-    auto [dc, c] = seg_mi_idx.prod(p, q + 1);
-    return {c, da + db - dc - dc};
+    return tree.V[seg.prod(p, q + 1)];
   }
 };
