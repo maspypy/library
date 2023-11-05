a431230f33175102acf15ca2ee3e15a0fb626714
maspypy <maspypy.gmail.com>
2023-11-05 21:26:20 +0900

    a

---
+++ string/prefix_substring_LCS.hpp
@@ -0,0 +1,30 @@
+#include "ds/wavelet_matrix.hpp"
+
+// https://codeforces.com/blog/entry/111625
+struct Prefix_Substring_LCS {
+  int N, M;
+  vc<Wavelet_Matrix<int, 0>> WM;
+
+  template <typename STRING>
+  Prefix_Substring_LCS(STRING S, STRING T) {
+    build(S, T);
+  }
+
+  template <typename STRING>
+  void build(STRING S, STRING T) {
+    N = len(S), M = len(T);
+    vv(int, dph, N + 1, M + 1);
+    vv(int, dpv, N + 1, M + 1);
+    FOR(j, M + 1) dph[0][j] = j;
+    FOR(i, 1, N + 1) FOR(j, 1, M + 1) {
+      bool same = S[i - 1] == T[j - 1];
+      int a = dph[i - 1][j], b = dpv[i][j - 1];
+      dph[i][j] = (same ? b : max(a, b));
+      dpv[i][j] = (same ? a : min(a, b));
+    }
+    FOR(i, N + 1) { WM.eb(Wavelet_Matrix<int, 0>(dph[i])); }
+  }
+
+  // LCS(S[0:n], T[L:R])
+  int query(int n, int L, int R) { return WM[n].count(L + 1, R + 1, 0, L + 1); }
+};
---
+++ test/library_checker/string/prefix_substring_lcs.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/prefix_substring_lcs"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "prefix_substring_LCS.hpp"
+
+void solve() {
+  INT(Q);
+  STR(S, T);
+  Prefix_Substring_LCS X(S, T);
+  FOR(Q) {
+    INT(a, b, c);
+    print(X.query(a, b, c));
+  }
+}
+
+signed main() {
+  solve();
+  return 0;
+}
