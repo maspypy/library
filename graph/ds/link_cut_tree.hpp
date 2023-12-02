5e9f245eb4258978202e6af4b1b0936109ad34a5
maspypy <maspypy.gmail.com>
2023-12-02 22:12:23 +0900

    a

---
+++ graph/ds/link_cut_tree.hpp
@@ -0,0 +1,245 @@
+/*
+各 heavy path を head が左, tail が右となるように splay tree で持つ.
+ユーザーが直接呼ぶ可能性があるものだけ int でも実装.
+LCT 外で探索するときなど，push を忘れないように注意.
+*/
+
+template <typename Node>
+struct Link_Cut_Tree {
+  using VX = typename Node::VX;
+  using np = Node *;
+  int n;
+  vc<Node> nodes;
+
+  Link_Cut_Tree(int n = 0) : n(n), nodes(n) { FOR(i, n) nodes[i] = Node(i); }
+
+  Node *operator[](int v) { return &nodes[v]; }
+
+  // パスを表す splay tree の根になっているかどうか
+  // underlying tree ではない
+  bool is_root(Node *c) { return state(c) == 0; }
+
+  // underlying tree の根
+  Node *get_root(Node *c) {
+    expose(c);
+    while (c->l) {
+      c->push();
+      c = c->l;
+    }
+    splay(c);
+    return c;
+  }
+
+  // underlying tree の根
+  int get_root(int c) { return get_root(&nodes[c])->idx; }
+
+  // parent(c)==p となるように link.
+  void link(Node *c, Node *p) {
+    evert(c);
+    evert(p);
+    // no edge -> heavy edge
+    c->p = p;
+    p->r = c;
+    p->update();
+  }
+
+  // parent(c)==p となるように link.
+  void link(int c, int p) { return link(&nodes[c], &nodes[p]); }
+
+  void cut(Node *a, Node *b) {
+    evert(a);
+    expose(b);
+    assert(!b->p);
+    assert((b->l) == a);
+    // heavy edge -> no edge
+    b->l->p = nullptr;
+    b->l = nullptr;
+    b->update();
+  }
+
+  void cut(int a, int b) { return cut(&nodes[a], &nodes[b]); }
+
+  // c を underlying tree の根とする.
+  // c は splay tree の根にもなる.
+  void evert(Node *c) {
+    expose(c);
+    c->reverse();
+    c->push();
+  }
+
+  // c を underlying tree の根とする.
+  // c は splay tree の根にもなる.
+  void evert(int c) { evert(&nodes[c]); }
+
+  Node *lca(Node *u, Node *v) {
+    assert(get_root(u) == get_root(v));
+    expose(u);
+    return expose(v);
+  }
+
+  int lca(int u, int v) { return lca(&nodes[u], &nodes[v])->idx; }
+
+  Node *jump(Node *u, Node *v, int k) {
+    evert(v);
+    expose(u);
+    assert(0 <= k && k < (u->size));
+    while (1) {
+      u->push();
+      int rs = (u->r ? u->r->size : 0);
+      if (k < rs) {
+        u = u->r;
+        continue;
+      }
+      if (k == rs) { break; }
+      k -= rs + 1;
+      u = u->l;
+    }
+    splay(u);
+    return u;
+  }
+
+  int jump(int u, int v, int k) {
+    auto c = jump((*this)[u], (*this)[v], k);
+    return c->idx;
+  }
+
+  // [root, c] がひとつの splay tree になるように変更する.
+  // c が右端で splay tree の根という状態になる.
+  // path query はこの状態で c の data を見る.
+  virtual Node *expose(Node *c) {
+    Node *now = c;
+    Node *rp = nullptr; // 今まで作ったパス
+
+    while (now) {
+      splay(now);
+      // heavy -> light, light -> heavy.
+      if (now->r) { now->add_light(now->r, now->r->x); }
+      if (rp) { now->erase_light(rp, rp->x); }
+      now->r = rp;
+      now->update();
+      rp = now;
+      now = now->p;
+    }
+    splay(c);
+    return rp;
+  }
+
+  // [root, c] がひとつの splay tree になるように変更する.
+  // c が右端で splay tree の根という状態になる.
+  // path query はこの状態で c の data を見る.
+  int expose(int c) {
+    Node *x = expose(&nodes[c]);
+    if (!x) return -1;
+    return x->idx;
+  }
+
+  Node *get_parent(Node *x) {
+    expose(x);
+    if (!x->l) return nullptr;
+    x = x->l;
+    while (x->r) x = x->r;
+    return x;
+  }
+
+  int get_parent(int x) {
+    Node *p = get_parent((*this)[x]);
+    return (p ? p->idx : -1);
+  }
+
+  // splay tree 内で完結する操作. 特に heavy, light 構造は変わらない.
+  // light edge のポインタは変更されうる
+  void rotate(Node *n) {
+    // n を根に近づける
+    Node *pp, *p, *c;
+    p = n->p;
+    pp = p->p;
+    if (p->l == n) {
+      c = n->r;
+      n->r = p;
+      p->l = c;
+    } else {
+      c = n->l;
+      n->l = p;
+      p->r = c;
+    }
+    p->update(), n->update();
+
+    if (pp) {
+      if (pp->l == p) pp->l = n;
+      elif (pp->r == p) pp->r = n;
+      else {
+        // light edge pointer が p から n に変わる
+        // 集約値は変わらないので場合によっては省略可能
+        pp->erase_light(p, n->x);
+        pp->add_light(n, n->x);
+      }
+    }
+    n->p = pp;
+    p->p = n;
+    if (c) c->p = p;
+  }
+
+  inline int state(Node *n) {
+    if (!n->p) return 0;
+    if (n->p->l == n) return 1;
+    if (n->p->r == n) return -1;
+    return 0;
+  }
+
+  // splay tree 内で完結する操作. 特に heavy, light 構造は変わらない.
+  // light pointer は rotate 内でケア
+  void splay(Node *c) {
+    c->push();
+    while (!is_root(c)) {
+      Node *p = c->p;
+      Node *pp = (p ? p->p : nullptr);
+      if (state(p) == 0) {
+        p->push(), c->push();
+        rotate(c);
+      }
+      elif (state(c) == state(p)) {
+        pp->push(), p->push(), c->push();
+        rotate(p);
+        rotate(c);
+      }
+      else {
+        pp->push(), p->push(), c->push();
+        rotate(c);
+        rotate(c);
+      }
+    }
+  }
+
+  void set_vdata(Node *c, VX x) {
+    evert(c);
+    c->set_vdata(x);
+  }
+  void set_vdata(int c, VX x) { set_vdata((*this)[c], x); }
+
+  vc<int> collect_heavy_path(int v) {
+    np c = (*this)[v];
+    while (!is_root(c)) c = c->p;
+    vc<int> res;
+    auto dfs = [&](auto &dfs, np c, bool rev) -> void {
+      if (!rev) {
+        if (c->l) dfs(dfs, c->l, rev ^ c->rev);
+        res.eb(c->idx);
+        if (c->r) dfs(dfs, c->r, rev ^ c->rev);
+      } else {
+        if (c->r) dfs(dfs, c->r, rev ^ c->rev);
+        res.eb(c->idx);
+        if (c->l) dfs(dfs, c->l, rev ^ c->rev);
+      }
+    };
+    dfs(dfs, c, false);
+    return res;
+  }
+  void debug(int N) {
+    print("idx,p,lch,rch,rev");
+    auto f = [&](np c) -> int { return (c ? c->idx : -1); };
+    FOR(i, N) {
+      print(i, f((*this)[i]->p), f((*this)[i]->l), f((*this)[i]->r),
+            (*this)[i]->rev);
+    }
+  }
+};
---
+++ test/yukicoder/902.test.cpp
@@ -0,0 +1,115 @@
+#define PROBLEM "https://yukicoder.me/problems/no/902"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "graph/ds/link_cut_tree.hpp"
+#include "ds/removable_queue.hpp"
+
+// X: cluster data
+// VX: vertex data
+// MX: light child data
+struct Node {
+  using VX = ll;
+  using X = ll;
+
+  Node *l, *r, *p;
+  int idx, size; // size は heavy path の頂点数
+  bool rev;
+  VX vx = 0;
+  X x = 0;
+
+  Node(int i = 0)
+      : l(nullptr), r(nullptr), p(nullptr), idx(i), size(1), rev(0) {}
+
+  void update() {
+    size = 1;
+    x = vx;
+    if (l) { size += l->size, x += l->x; }
+    if (r) { size += r->size, x += r->x; }
+  }
+
+  void push() {
+    if (rev) {
+      if (l) l->reverse();
+      if (r) r->reverse();
+      rev = 0;
+    }
+  }
+
+  // data の reverse も行う
+  void reverse() {
+    rev ^= 1;
+    swap(l, r);
+  }
+
+  // LCT 内で expose, update を行うのでここは変更だけ
+  void set_vdata(VX x) { vx = x; }
+
+  // c がこの時点では update されていないかもしれないが, x は正常なものが入る
+  // c->x 等は使わないように注意する
+  // c->idx を持っておいて探索で利用することがある（重心の探索）
+  void add_light(Node *c, X x) {}
+  void erase_light(Node *c, X x) {}
+};
+
+void solve() {
+  LL(N);
+  Link_Cut_Tree<Node> LCT(2 * N - 1);
+
+  FOR(e, N - 1) {
+    LL(a, b, c);
+    int idx = N + e;
+    LCT.set_vdata(idx, c);
+    LCT.link(a, idx), LCT.link(b, idx);
+  }
+
+  auto dist = [&](int a, int b) -> ll {
+    LCT.evert(a);
+    LCT.expose(b);
+    return LCT[b]->x;
+  };
+
+  auto solve_1 = [&]() -> void {
+    LL(u, v, w, x);
+    // uv -> vw(x)
+    ll e = LCT.jump(u, v, 1);
+    LCT.cut(u, e), LCT.cut(e, v);
+    LCT.set_vdata(e, x);
+    LCT.link(v, e), LCT.link(e, w);
+  };
+
+  auto solve_2 = [&]() -> void {
+    LL(K);
+    VEC(int, X, K);
+    // 頂点インデックス順で dfs したときの euler 順にソートできる
+    // jump の計算のときに evert しているので LCA 取得前に evert しなおすこと
+    int r = X[0];
+    sort(all(X), [&](auto a, auto b) -> bool {
+      if (a == b) return false;
+      LCT.evert(r);
+      int c = LCT.lca(a, b);
+      if (a == c) return true;
+      if (b == c) return false;
+      int x = LCT.jump(c, a, 1);
+      int y = LCT.jump(c, b, 1);
+      return x < y;
+    });
+    X.eb(X[0]);
+    ll ANS = 0;
+    FOR(k, K) { ANS += dist(X[k], X[k + 1]); }
+    ANS /= 2;
+    print(ANS);
+  };
+
+  LL(Q);
+  FOR(Q) {
+    LL(t);
+    if (t == 1) solve_1();
+    if (t == 2) solve_2();
+  }
+}
+
+signed main() {
+  solve();
+  return 0;
+}
