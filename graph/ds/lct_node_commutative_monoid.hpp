243f05cc1d26c7c04e7b0472c1e1d27ba106167d
maspypy <maspypy.gmail.com>
2023-12-03 03:27:33 +0900

    a

--- graph/ds/lct_node_commutative_monoid.hpp
+++ graph/ds/lct_node_commutative_monoid.hpp
@@ -1,7 +1,9 @@
 
-template <typename Monoid>
+// SUBTREE : cluster が subtree 情報を持つ場合
+template <typename Monoid, bool SUBTREE = false>
 struct LCT_Node_Commutative_Monoid {
   static_assert(Monoid::commute);
+  static constexpr bool NODE_FOR_SUBTREE = SUBTREE;
   using np = LCT_Node_Commutative_Monoid *;
   // デフォルト
   np l, r, p;
@@ -11,7 +13,7 @@ struct LCT_Node_Commutative_Monoid {
   using MX = Monoid;
   using X = MX::value_type;
   using VX = X;
-  X x, vx;
+  X x, vx, mid;
 
   LCT_Node_Commutative_Monoid(int i = 0)
       : l(nullptr),
@@ -21,11 +23,13 @@ struct LCT_Node_Commutative_Monoid {
         size(1),
         rev(0),
         x(MX::unit()),
-        vx(MX::unit()) {}
+        vx(MX::unit()),
+        mid(MX::unit()) {}
 
   void update() {
     size = 1;
     x = vx;
+    if constexpr (SUBTREE) x = MX::op(x, mid);
     if (l) { size += l->size, x = Monoid::op(l->x, x); }
     if (r) { size += r->size, x = Monoid::op(x, r->x); }
   }
@@ -50,6 +54,10 @@ struct LCT_Node_Commutative_Monoid {
   // c がこの時点では update されていないかもしれないが, x は正常なものが入る
   // c->x 等は使わないように注意する
   // c->idx を持っておくと探索できることがある
-  void add_light(np c, X x) {}
-  void erase_light(np c, X x) {}
+  void add_light(np c, X x) {
+    if constexpr (SUBTREE) mid = MX::op(mid, x);
+  }
+  void erase_light(np c, X x) {
+    if constexpr (SUBTREE) mid = MX::op(mid, MX::inverse(x));
+  }
 };
