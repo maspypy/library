18ee004257d7612f6baa9a7d9e8ccf813ff856c3
maspypy <45988013+maspypy@users.noreply.github.com>
2023-12-09 17:19:00 +0900

    Update abc212g.test.cpp

--- test_atcoder/abc212g.test.cpp
+++ test_atcoder/abc212g.test.cpp
@@ -1,11 +1,11 @@
 #define PROBLEM "https://atcoder.jp/contests/abc212/tasks/abc212_g"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "nt/function_on_divisors.hpp"
+#include "nt/array_on_divisors.hpp"
 
 void solve() {
   LL(P);
-  Function_on_Divisors<ll> X(P - 1);
+  Array_On_Divisors<ll> X(P - 1);
   X.set_euler_phi();
   i128 ANS = 1;
   X.enumerate([&](ll a, ll b) -> void { ANS += i128(a) * b; });
