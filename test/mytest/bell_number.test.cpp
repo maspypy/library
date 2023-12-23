8b7d8ff3a9c059f1da6f117d1cd38db259157dc2
maspypy <45988013+maspypy@users.noreply.github.com>
2023-12-23 11:49:46 +0900

    Update bell_number.test.cpp

--- test/mytest/bell_number.test.cpp
+++ test/mytest/bell_number.test.cpp
@@ -1,6 +1,8 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 #include "my_template.hpp"
 
+#include "other/io.hpp"
+
 #include "seq/famous/bell_number.hpp"
 #include "mod/modint.hpp"
 
@@ -9,7 +11,7 @@ using mint = modint998;
 void test() {
   vc<mint> A = bell_number<mint>(10);
   vc<mint> B
-      = {mint(1),   mint(1),   mint(2),    mint(5),     mint(52),    mint(15),
+      = {mint(1),   mint(1),   mint(2),    mint(5),     mint(15),    mint(52),
          mint(203), mint(877), mint(4140), mint(21147), mint(115975)};
   assert(A == B);
 }
