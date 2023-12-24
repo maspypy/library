c608558ceb03c0635293e4cfeecae47c9d537574
maspypy <maspypy.gmail.com>
2023-12-24 13:20:10 +0900

    a

---
+++ string/palindrome_decomposition_dp.hpp
@@ -0,0 +1,49 @@
+#include "string/palindromic_tree.hpp"
+
+// dp[i] := すべての n 個への分割に対する x^n の総和
+// dp[j] = sum_i dp[i]*x if [i,j) palindrome
+// https://arxiv.org/pdf/1403.2431.pdf
+template <typename T, typename F1, typename F2>
+vc<T> palindrome_decomposition_dp(string S, T add_unit, T mul_unit, F1 add,
+                                  F2 mul_x) {
+  int N = len(S);
+  Palindromic_Tree<26> X(S, 'a');
+  int n = len(X.nodes);
+  /*
+  各ノードに対して
+  suffix との長さの差分
+  同じ差分で何ステップ遡れるか？
+  遡った先の node
+  */
+  vc<int> diff(n, infty<int>);
+  vc<int> step(n);
+  vc<int> up(n);
+  FOR(v, 2, n) {
+    int w = X.nodes[v].link;
+    int d = X.nodes[v].length - X.nodes[w].length;
+    diff[v] = d;
+    step[v] = (diff[v] == diff[w] ? step[w] : 0) + 1;
+    up[v] = (diff[v] == diff[w] ? up[w] : w);
+  }
+
+  vc<T> dp(N + 1, add_unit);
+  vc<T> gdp(N + 1, add_unit);
+  dp[0] = mul_unit;
+
+  auto& path = X.path;
+  FOR(j, 1, N + 1) {
+    int v = path[j];
+    int i = j - X.nodes[v].length;
+    while (v >= 2) {
+      if (step[v] == 1) {
+        // 1 項だけからなる等差数列の集約で初期化
+        gdp[i] = dp[i];
+      } else {
+        // 等差数列の末尾を追加
+        gdp[i] = add(gdp[i], dp[i + diff[v] * (step[v] - 1)]);
+      }
+      dp[j] = add(dp[j], mul_x(gdp[i])), i += diff[v] * step[v], v = up[v];
+    }
+  }
+  return dp;
+}
---
+++ test/mytest/palindrome_decomposition_dp.test.cpp
@@ -0,0 +1,88 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "random/base.hpp"
+#include "string/palindrome_decomposition_dp.hpp"
+
+void test_min_palindrome_decomposition() {
+  FOR(N, 30) {
+    FOR(K, 1, 5) {
+      FOR(10) {
+        string S;
+        FOR(N) {
+          char c = 'a' + RNG(0, K);
+          S += c;
+        }
+        vv(int, is_pal, N + 1, N + 1);
+        FOR_R(L, N + 1) {
+          FOR(R, N + 1) {
+            if (R - L <= 1) {
+              is_pal[L][R] = 1;
+              continue;
+            }
+            is_pal[L][R] = (S[L] == S[R - 1]) && is_pal[L + 1][R - 1];
+          }
+        }
+        vc<int> DP(N + 1, infty<int>);
+        DP[0] = 0;
+        FOR(R, 1, N + 1) {
+          FOR(L, R) {
+            if (is_pal[L][R]) chmin(DP[R], DP[L] + 1);
+          }
+        }
+        vc<int> dp = palindrome_decomposition_dp<int>(
+            S, infty<int>, 0, [&](int x, int y) -> int { return min(x, y); },
+            [&](int x) -> int { return x + 1; });
+        assert(dp == DP);
+      }
+    }
+  }
+}
+
+void test_cnt_palindrome_decomposition() {
+  FOR(N, 30) {
+    FOR(K, 1, 5) {
+      FOR(10) {
+        string S;
+        FOR(N) {
+          char c = 'a' + RNG(0, K);
+          S += c;
+        }
+        vv(int, is_pal, N + 1, N + 1);
+        FOR_R(L, N + 1) {
+          FOR(R, N + 1) {
+            if (R - L <= 1) {
+              is_pal[L][R] = 1;
+              continue;
+            }
+            is_pal[L][R] = (S[L] == S[R - 1]) && is_pal[L + 1][R - 1];
+          }
+        }
+        vc<int> DP(N + 1, 0);
+        DP[0] = 1;
+        FOR(R, 1, N + 1) {
+          FOR(L, R) {
+            if (is_pal[L][R]) DP[R] += DP[L];
+          }
+        }
+        vc<int> dp = palindrome_decomposition_dp<int>(
+            S, 0, 1, [&](int x, int y) -> int { return x + y; },
+            [&](int x) -> int { return x; });
+        assert(dp == DP);
+      }
+    }
+  }
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << a << b << "\n";
+}
+
+signed main() {
+  test_min_palindrome_decomposition();
+  test_cnt_palindrome_decomposition();
+  return 0;
+}
