52c9dc5de52526bfbc480181c23fcf53b460d946
maspypy <maspypy.gmail.com>
2023-12-24 13:20:10 +0900

    a

--- test/yukicoder/465.test.cpp
+++ test/yukicoder/465.test.cpp
@@ -3,72 +3,30 @@
 #include "my_template.hpp"
 #include "other/io.hpp"
 
-#include "string/palindromic_tree.hpp"
+#include "string/palindrome_decomposition_dp.hpp"
 
-/*
-解説にある論文
-https://arxiv.org/pdf/1403.2431.pdf
-P[r] := {l | S[l:r) is a non-empty palindrome}
-これは O(log) 個の等差数列として書ける.
-gdp[i]: 等差数列の左端となる点 i に i,i+d,i+2d,... を始点とする
-palindrome の集約値を入れる.
-*/
-vc<pair<int, int>> calc(string S) {
-  int N = len(S);
-  Palindromic_Tree<26> X(S, 'a');
-  int n = len(X.nodes);
-  /*
-  各ノードに対して
-  suffix との長さの差分
-  同じ差分で何ステップ遡れるか？
-  遡った先の node
-  */
-  vc<int> diff(n, infty<int>);
-  vc<int> step(n);
-  vc<int> up(n);
-  FOR(v, 2, n) {
-    int w = X.nodes[v].link;
-    int d = X.nodes[v].length - X.nodes[w].length;
-    diff[v] = d;
-    step[v] = (diff[v] == diff[w] ? step[w] : 0) + 1;
-    up[v] = (diff[v] == diff[w] ? up[w] : w);
-  }
-
-  // P, PP
-  vc<pair<int, int>> dp(N + 1);
-  vc<int> gdp(N + 1);
-
-  auto& path = X.path;
-  FOR(j, 1, N + 1) {
-    int v = path[j];
-    int i = j - X.nodes[v].length;
-    dp[j].fi = (i == 0);
-    while (v >= 2) {
-      if (step[v] == 1) {
-        // 1 項だけからなる等差数列の集約で初期化
-        gdp[i] = dp[i].fi;
-      } else {
-        // 等差数列の末尾を追加
-        gdp[i] += dp[i + diff[v] * (step[v] - 1)].fi;
-      }
-      dp[j].se += gdp[i], i += diff[v] * step[v], v = up[v];
-    }
-    // print(j, dp[j]);
-  }
-  return dp;
-}
+using ARR = array<int, 3>;
 
 void solve() {
   STR(S);
   int N = len(S);
-  auto dp1 = calc(S);
+
+  auto add = [&](ARR& x, ARR y) -> ARR {
+    return {x[0] + y[0], x[1] + y[1], x[2] + y[2]};
+  };
+  auto mul_x = [&](ARR& x) -> ARR { return {0, x[0], x[1]}; };
+
+  auto dp1
+      = palindrome_decomposition_dp<ARR>(S, {0, 0, 0}, {1, 0, 0}, add, mul_x);
   reverse(all(S));
-  auto dp2 = calc(S);
+  auto dp2
+      = palindrome_decomposition_dp<ARR>(S, {0, 0, 0}, {1, 0, 0}, add, mul_x);
+  reverse(all(S));
   vc<int> PA(N);
-  FOR(n, N) { PA[n] = dp2[n].fi; }
+  FOR(n, N) { PA[n] = dp2[n][1]; }
   PA = cumsum<int>(PA);
   ll ANS = 0;
-  FOR(i, N + 1) ANS += dp1[i].se * ll(PA[N - i]);
+  FOR(i, N + 1) ANS += dp1[i][2] * ll(PA[N - i]);
   print(ANS);
 }
 
