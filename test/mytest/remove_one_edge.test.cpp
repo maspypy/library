2296c869aea5d821faaaf6292f389956224761de
maspypy <maspypy.gmail.com>
2024-01-01 08:39:54 +0900

    a

---
+++ test/mytest/remove_one_edge.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+
+#include "random/random_graph.hpp"
+#include "ds/unionfind/unionfind.hpp"
+#include "graph/ds/remove_one_edge_connectivity.hpp"
+
+void test() {
+  FOR(N, 20) {
+    FOR(1000) {
+      Graph<int, 0> G(N);
+      for (auto& [a, b]: random_graph<false>(N, false)) G.add(a, b);
+      G.build();
+      Remove_One_Edge_Connectivity<decltype(G)> X(G);
+      FOR(e, G.M) {
+        UnionFind uf(N);
+        FOR(i, G.M) {
+          if (e != i) uf.merge(G.edges[i].frm, G.edges[i].to);
+        }
+        assert(X.n_comp(e) == (uf.n_comp));
+        FOR(u, N) FOR(v, N) {
+          assert(X.is_connected(e, u, v) == (uf[u] == uf[v]));
+        }
+        FOR(v, N) assert(X.size(e, v) == uf.size(v));
+      }
+    }
+  }
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << "\n";
+}
+
+signed main() {
+  test();
+  solve();
+  return 0;
+}
