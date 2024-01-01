6165c532a979d052e9633ae8939284d1ef9133ea
maspypy <maspypy.gmail.com>
2024-01-01 16:51:09 +0900

    a

--- graph/ds/remove_one_edge_connectivity.hpp
+++ graph/ds/remove_one_edge_connectivity.hpp
@@ -2,19 +2,24 @@
 
 // 1 辺消したときに
 // u,v が連結か / 連結成分数 / v の連結成分サイズ
-template <typename GT>
 struct Remove_One_Edge_Connectivity {
-  GT& G;
   int N, M;
   int n_comp_base;
+  vc<pair<int, int>> edge;
   vc<int> root;
   vc<int> LID, RID;
   vc<bool> bridge;
 
-  Remove_One_Edge_Connectivity(GT& G) : G(G) { build(); }
+  template <typename GT>
+  Remove_One_Edge_Connectivity(GT& G) {
+    build(G);
+  }
 
-  void build() {
+  template <typename GT>
+  void build(GT& G) {
     N = G.N, M = G.M;
+    edge.resize(M);
+    for (auto& e: G.edges) edge[e.id] = {e.frm, e.to};
     root.assign(N, -1);
     LID.assign(N, -1), RID.assign(N, -1);
     bridge.assign(M, 0);
@@ -39,6 +44,9 @@ struct Remove_One_Edge_Connectivity {
     FOR(v, N) {
       if (root[v] == -1) { n_comp_base++, root[v] = v, dfs(dfs, v, -1); }
     }
+    for (auto& [a, b]: edge) {
+      if (in_subtree(a, b)) swap(a, b);
+    }
   }
 
   int n_comp(int eid) { return n_comp_base + (bridge[eid] ? 1 : 0); }
@@ -46,17 +54,15 @@ struct Remove_One_Edge_Connectivity {
   bool is_connected(int eid, int u, int v) {
     if (root[u] != root[v]) return false;
     if (!bridge[eid]) return true;
-    int a = G.edges[eid].frm, b = G.edges[eid].to;
-    if (in_subtree(a, b)) swap(a, b);
+    auto [a, b] = edge[eid];
     return (in_subtree(u, b) == in_subtree(v, b));
   }
 
   int size(int eid, int v) {
     int r = root[v];
     if (!bridge[eid]) { return subtree_size(r); }
-    int a = G.edges[eid].frm, b = G.edges[eid].to;
+    auto [a, b] = edge[eid];
     if (root[a] != r) { return subtree_size(r); }
-    if (in_subtree(a, b)) swap(a, b);
     if (in_subtree(v, b)) return subtree_size(b);
     return subtree_size(r) - subtree_size(b);
   }
