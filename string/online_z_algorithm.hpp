0a701b14a0d611e3f83a9aaa78cefd503a8b4f3d
maspypy <maspypy.gmail.com>
2024-01-03 07:53:36 +0900

    a

---
+++ string/online_z_algorithm.hpp
@@ -0,0 +1,40 @@
+
+// https://heno239.hatenablog.com/entry/2020/07/07/140651
+// S[i] を追加すると，S[j:i] が lcp であるような j 全体の vector がかえる.
+template <typename STRING = string, typename CHAR = char>
+struct Online_Z_Algorithm {
+  string S;
+  vc<int> Z;
+  queue<int> cur;
+  vvc<int> memo;
+  int p;
+
+  Online_Z_Algorithm() : p(1){};
+
+  vc<int> add(int i, CHAR c) {
+    assert(i == len(S));
+    S.push_back(c);
+    int len = S.size();
+    Z.eb(-1);
+    memo.resize(1 + i);
+    vc<int> end;
+
+    if (len == 1) return end;
+    if (S[0] != c) Z[i] = 0, end.eb(i);
+
+    auto del = [&](int j) -> void { Z[j] = i - j, memo[i].eb(j), end.eb(j); };
+
+    while (p <= i) {
+      if (Z[p] != -1) { ++p; }
+      elif (S[i - p] != S[i]) { del(p++); }
+      else {
+        break;
+      }
+    }
+    if (p < i) {
+      for (int j: memo[i - p]) { del(j + p); }
+    }
+    return end;
+  }
+  int query(int i) { return (Z[i] == -1 ? len(S) - i : Z[i]); }
+};
---
+++ test/library_checker/string/online_z.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "string/online_z_algorithm.hpp"
+
+void solve() {
+  STR(S);
+  ll N = len(S);
+  Online_Z_Algorithm<string, char> X;
+  vc<int> ANS(N, -1);
+
+  FOR(i, N) {
+    auto end = X.add(i, S[i]);
+    for (auto& j: end) { ANS[j] = i - j; }
+  }
+  FOR(i, N) if (ANS[i] == -1) ANS[i] = N - i;
+  print(ANS);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
