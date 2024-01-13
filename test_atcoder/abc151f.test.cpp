e6fc99c624ad8039edd05ef31d5d513a08ef4b27
maspypy <maspypy.gmail.com>
2024-01-13 13:39:55 +0900

    a

--- test_atcoder/abc151f.test.cpp
+++ test_atcoder/abc151f.test.cpp
@@ -1,5 +1,6 @@
 #define PROBLEM "https://atcoder.jp/contests/abc151/tasks/abc151_f"
 #define ERROR 0.000001
+
 #include "my_template.hpp"
 #include "other/io.hpp"
 #include "geo/minimum_enclosing_circle.hpp"
@@ -9,8 +10,14 @@ void solve() {
   using Re = double;
   using P = Point<Re>;
   VEC(P, points, N);
-  auto c = minimum_enclosing_circle<Re, Re>(points);
-  print(c.r);
+  auto [i, j, k] = minimum_enclosing_circle(points);
+  if (k == -1) {
+    Re r = dist<Re>(points[i], points[j]);
+    print(r / 2);
+  } else {
+    auto c = outcircle<Re>(points[i], points[j], points[k]);
+    print(c.r);
+  }
 }
 
 signed main() {
