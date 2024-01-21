d81c42489a143169e7a3f3bac974f19e5c98c6a7
maspypy <maspypy.gmail.com>
2024-01-21 17:01:49 +0900

    a

---
+++ test/library_checker/graph/eulerwalk_d.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/eulerian_trail_directed"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+#include "graph/eulerwalk.hpp"
+
+void solve() {
+  LL(N, M);
+  Graph<int, 1> G(N);
+  G.read_graph(M, 0, 0);
+  auto [vs, es] = euler_walk(G);
+  if (vs.empty()) return No();
+  Yes();
+  print(vs);
+  print(es);
+}
+
+signed main() {
+  INT(T);
+  FOR(T) solve();
+  return 0;
+}
