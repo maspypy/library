71f80327af4476bf1a40048dc5f54bb1d9cd24c6
maspypy <maspypy.gmail.com>
2024-01-22 23:22:27 +0900

    a

--- test/library_checker/geometry/count_points_in_triangles_naive.test.cpp
+++ test/library_checker/geometry/count_points_in_triangles_naive.test.cpp
@@ -1,31 +1,20 @@
 #include "my_template.hpp"
 #include "other/io.hpp"
 
-#include "geo/count_points_in_triangles.hpp"
+#include "geo/convex_polygon.hpp"
 
 using P = Point<ll>;
-
 void solve() {
-  INT(N);
+  LL(N);
   VEC(P, A, N);
-  INT(M);
+  LL(M);
   VEC(P, B, M);
-
-  INT(Q);
+  LL(Q);
   FOR(Q) {
-    INT(a, b, c);
-    P A = points_1[a], B = points_1[b], C = points_1[c];
-    // counter-clockwise
-    if ((B - A).det(C - A) < 0) swap(B, C);
-
+    LL(a, b, c);
+    ConvexPolygon<ll> X({A[a], A[b], A[c]});
     int ans = 0;
-    FOR(i, M) {
-      P p = points_2[i];
-      if ((B - A).det(p - A) <= 0) continue;
-      if ((C - B).det(p - B) <= 0) continue;
-      if ((A - C).det(p - C) <= 0) continue;
-      ++ans;
-    }
+    FOR(i, M) { ans += (X.side(B[i]) == 1); }
     print(ans);
   }
 }
