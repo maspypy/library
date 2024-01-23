a2d33753b37e4d07b1d42e6a5a28f362348956f7
maspypy <maspypy.gmail.com>
2024-01-23 16:01:42 +0900

    a

--- test/library_checker/matrix/sparse_matrix_det.test.cpp
+++ test/library_checker/matrix/sparse_matrix_det.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/sparse_matrix_det"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "linalg/spmat_det.hpp"
+#include "linalg/blackbox/det.hpp"
 
 #include "mod/modint.hpp"
 using mint = modint998;
@@ -10,15 +10,16 @@ void solve() {
   LL(N, K);
   using T = tuple<int, int, mint>;
   VEC(T, dat, K);
-  print(spmat_det(N, dat));
+  auto A = [&](vc<mint> X) -> vc<mint> {
+    vc<mint> Y(N);
+    for (auto& [a, b, c]: dat) Y[b] += X[a] * c;
+    return Y;
+  };
+  mint d = blackbox_det<mint>(N, A);
+  print(d);
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
   solve();
-
   return 0;
 }
--- test/mytest/min_poly.test.cpp
+++ test/mytest/min_poly.test.cpp
@@ -2,7 +2,6 @@
 #include "my_template.hpp"
 
 #include "mod/modint.hpp"
-#include "linalg/spmat_min_poly.hpp"
 #include "linalg/blackbox/min_poly.hpp"
 #include "linalg/matrix_mul.hpp"
 
@@ -14,67 +13,56 @@ void test() {
   A.eb(0, 1, 2);
   A.eb(1, 0, 3);
   A.eb(1, 1, 4);
-  vc<mint> f = spmat_min_poly<mint>(2, A);
+  vc<mint> f = blackbox_min_poly<mint>(2, [&](vc<mint> f) -> vc<mint> {
+    vc<mint> g(2);
+    for (auto& [a, b, c]: A) g[b] += f[a] * c;
+    return g;
+  });
   assert(f == vc<mint>({mint(-2), mint(-5), mint(1)}));
 
   A.clear();
   A.eb(0, 1, 1);
-  f = spmat_min_poly<mint>(2, A);
+  f = blackbox_min_poly<mint>(2, [&](vc<mint> f) -> vc<mint> {
+    vc<mint> g(2);
+    for (auto& [a, b, c]: A) g[b] += f[a] * c;
+    return g;
+  });
   assert(f == vc<mint>({mint(0), mint(0), mint(1)}));
-  assert(f == blackbox_min_poly<mint>(2, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(2);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
 
   A.clear();
-  f = spmat_min_poly<mint>(2, A);
+  f = blackbox_min_poly<mint>(2, [&](vc<mint> f) -> vc<mint> {
+    vc<mint> g(2);
+    for (auto& [a, b, c]: A) g[b] += f[a] * c;
+    return g;
+  });
   assert(f == vc<mint>({mint(0), mint(1)}));
-  assert(f == blackbox_min_poly<mint>(2, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(2);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
 
   A.clear();
-  f = spmat_min_poly<mint>(0, A);
+  f = blackbox_min_poly<mint>(0, [&](vc<mint> f) -> vc<mint> {
+    vc<mint> g(0);
+    for (auto& [a, b, c]: A) g[b] += f[a] * c;
+    return g;
+  });
   assert(f == vc<mint>({mint(1)}));
-  assert(f == blackbox_min_poly<mint>(0, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(0);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
 
   A.clear();
   A.eb(0, 1, 1);
   A.eb(0, 2, 1);
   A.eb(1, 2, 1);
-  f = spmat_min_poly<mint>(3, A);
+  f = blackbox_min_poly<mint>(3, [&](vc<mint> f) -> vc<mint> {
+    vc<mint> g(3);
+    for (auto& [a, b, c]: A) g[b] += f[a] * c;
+    return g;
+  });
   assert(f == vc<mint>({mint(0), mint(0), mint(0), mint(1)}));
-  assert(f == blackbox_min_poly<mint>(3, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(3);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
 
   A.clear();
-  A.eb(0, 2, 1);
-  f = spmat_min_poly<mint>(3, A);
-  assert(f == vc<mint>({mint(0), mint(0), mint(1)}));
-  assert(f == blackbox_min_poly<mint>(3, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(3);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
-
-  A.clear();
-  f = spmat_min_poly<mint>(3, A);
+  f = blackbox_min_poly<mint>(3, [&](vc<mint> a) -> vc<mint> {
+    vc<mint> b(3);
+    for (auto&& [i, j, x]: A) b[j] += a[i] * x;
+    return b;
+  });
   assert(f == vc<mint>({mint(0), mint(1)}));
-  assert(f == blackbox_min_poly<mint>(3, [&](vc<mint> a) -> vc<mint> {
-           vc<mint> b(3);
-           for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-           return b;
-         }));
 
   // random matrix
   FOR(N, 1, 20) {
@@ -82,12 +70,11 @@ void test() {
     FOR(i, N) FOR(j, N) mat[i][j] = RNG(0, 998244353);
     A.clear();
     FOR(i, N) FOR(j, N) A.eb(i, j, mat[i][j]);
-    f = spmat_min_poly(N, A);
-    assert(f == blackbox_min_poly<mint>(N, [&](vc<mint> a) -> vc<mint> {
-             vc<mint> b(N);
-             for (auto&& [i, j, x]: A) b[j] += a[i] * x;
-             return b;
-           }));
+    f = blackbox_min_poly<mint>(N, [&](vc<mint> a) -> vc<mint> {
+      vc<mint> b(N);
+      for (auto&& [i, j, x]: A) b[j] += a[i] * x;
+      return b;
+    });
     vv(mint, B, N, N);
     FOR(i, N) B[i][i] = 1;
     vv(mint, C, N, N);
