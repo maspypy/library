388da6a6aed9f5a3d331ca33a13946fe8b47693a
maspypy <maspypy.gmail.com>
2024-01-26 20:17:35 +0900

    a

--- test/yukicoder/2280.test.cpp
+++ test/yukicoder/2280.test.cpp
@@ -9,14 +9,16 @@ void solve() {
   ll ANS = 0;
 
   ll xAA = 0, xAB = 0, xBA = 0;
-  if (K == A) { xAA = range_freq_of_linear(1, NA, A, 0, B, 1, B - A); }
+  if (K == A) {
+    xAA = range_freq_of_linear<i128, ll>(1, NA, A, 0, B, 1, B - A);
+  }
 
   auto f = [&](ll A, ll B) -> ll {
     if (K > A || K > B) return 0;
     // A の倍数 -> B の倍数
     ll res = 0;
     ll ub = floor(M - K, A);
-    res = range_freq_of_linear(1, ub + 1, A, K, B, 0, 1);
+    res = range_freq_of_linear<i128, ll>(1, ub + 1, A, K, B, 0, 1);
     return res;
   };
 
