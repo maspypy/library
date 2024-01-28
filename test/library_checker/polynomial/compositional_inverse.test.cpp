6375ba29bc6230e99c2dbcab270e154b792e6d13
maspypy <maspypy.gmail.com>
2024-01-28 22:40:04 +0900

    a

---
+++ test/library_checker/polynomial/compositional_inverse.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/compositional_inverse_of_formal_power_series"
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "mod/modint.hpp"
+#include "poly/compositional_inverse.hpp"
+
+void solve() {
+  LL(N);
+  VEC(mint, F, N);
+  vc<mint> G = compositional_inverse(F);
+  print(G);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
