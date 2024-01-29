b2e1450b46ee2fb03e04477a8f8b311dcd2e5fb7
maspypy <maspypy.gmail.com>
2024-01-29 22:33:16 +0900

    a

--- graph/count/count_labeled_connected.hpp
+++ graph/count/count_labeled_connected.hpp
@@ -1,6 +1,7 @@
 #include "graph/count/count_labeled_undirected.hpp"
 #include "poly/fps_log.hpp"
 
+// https://oeis.org/A001187
 template <typename mint>
 vc<mint> count_labeled_connected(int N) {
   vc<mint> F = count_labeled_undirected<mint>(N);
