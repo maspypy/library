6efb94554e53db793f862fae2b9975a898df1f8d
maspypy <maspypy.gmail.com>
2024-01-30 02:03:40 +0900

    a

--- graph/count/count_unlabeled_tree.hpp
+++ graph/count/count_unlabeled_tree.hpp
@@ -3,9 +3,9 @@
 #include "poly/convolution.hpp"
 
 // https://oeis.org/A000055
-// 0, 1, 1, 1, 2, 3, 6, 11, ...
+// https://oeis.org/A000081
 template <typename mint>
-vc<mint> count_unlabeled_tree(int N) {
+vc<mint> count_unlabeled_tree(int N, bool rooted) {
   // f: rooted tree
   // f(x) = x prod (1+x^n+x^{2n}+...)^f_n
   // g(x) = f(x)/x
@@ -19,9 +19,9 @@ vc<mint> count_unlabeled_tree(int N) {
     // -log(1-x)=x+xx/2+...
     FOR(k, 1, (N - 1) / n + 1) { log_g[n * k] += f[n] * inv<mint>(k); }
   }
+  if (rooted) return f;
   // この時点で f は unlabeled rooted tree (OEIS A000081)
   // unlabeled tree の母関数は f(x) - 1/2(f(x)^2-f(x^2))
-  // となるのだが，証明はかなりテクニカル.
   vc<mint> ff = convolution<mint>(f, f);
   vc<mint> ANS = f;
   FOR(i, N + 1) ANS[i] -= inv<mint>(2) * ff[i];
--- test/mytest/graph_count.test.cpp
+++ test/mytest/graph_count.test.cpp
@@ -1,24 +1,24 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 #include "my_template.hpp"
 
-#include "graph/count/count_unicyclic.hpp"
-#include "graph/count/count_tree.hpp"
-#include "graph/count/count_forest.hpp"
+#include "graph/count/count_labeled_unicyclic.hpp"
+#include "graph/count/count_labeled_tree.hpp"
+#include "graph/count/count_labeled_forest.hpp"
 #include "mod/modint.hpp"
 
 using mint = modint998;
 
 void test() {
   int N = 10;
-  auto uni = count_unicyclic<mint>(N);
+  auto uni = count_labeled_unicyclic<mint>(N);
   assert(uni
          == vc<mint>(
              {0, 0, 0, 1, 15, 222, 3660, 68295, 1436568, 33779340, 880107840}));
-  auto tree = count_tree<mint>(N);
+  auto tree = count_labeled_tree<mint>(N);
   assert(tree
          == vc<mint>(
              {0, 1, 1, 3, 16, 125, 1296, 16807, 262144, 4782969, 100000000}));
-  auto forest = count_forest<mint>(N);
+  auto forest = count_labeled_forest<mint>(N);
   assert(forest
          == vc<mint>(
              {1, 1, 2, 7, 38, 291, 2932, 36961, 561948, 10026505, 205608536}));
