1514fe833c81cdc064e1055469ddbb76a1527f1d
maspypy <maspypy.gmail.com>
2024-01-30 02:52:32 +0900

    a

--- graph/count/count_labeled_bipartite.hpp
+++ graph/count/count_labeled_bipartite.hpp
@@ -10,14 +10,13 @@ vc<mint> count_labeled_bipartite(int N, bool connected) {
   vc<mint> F(N + 1);
   mint ipow = 1;
   F[0] = 1;
-  FOR(i, 1, N + 1) F[i] = F[i] * ipow, ipow *= inv<mint>(2);
+  FOR(i, 1, N + 1) F[i] = F[i - 1] * ipow, ipow *= inv<mint>(2);
   FOR(i, N + 1) F[i] *= fact_inv<mint>(i);
   F = convolution(F, F);
   F.resize(N + 1);
   mint pow = 1, c = 1;
-  FOR(i, 1, N + 1) F[i] *= c, c *= pow, pow += pow;
+  FOR(i, N + 1) F[i] *= c, c *= pow, pow += pow, print(i, c);
 
-  // colored bipartite connected
   if (connected) {
     F = fps_log(F);
     FOR(i, N + 1) F[i] *= inv<mint>(2);
