c9fc7776f5d061d1b18f56c903ef0ec885fa5fa1
maspypy <maspypy.gmail.com>
2024-01-29 22:12:20 +0900

    a

--- graph/count/count_labeled_connected.hpp
+++ graph/count/count_labeled_connected.hpp
@@ -1,12 +1,9 @@
+#include "graph/count/count_labeled_undirected.hpp"
 #include "poly/fps_log.hpp"
 
 template <typename mint>
 vc<mint> count_labeled_connected(int N) {
-  vc<mint> F(N + 1);
-  mint pow = 1;
-  F[0] = 1;
-  FOR(n, 1, N + 1) { F[n] = F[n - 1] * pow, pow += pow; }
-  // F: 任意の labeled graph
+  vc<mint> F = count_labeled_undirected<mint>(N);
   FOR(i, N + 1) F[i] *= fact_inv<mint>(i);
   F = fps_log(F);
   FOR(i, N + 1) F[i] *= fact<mint>(i);
