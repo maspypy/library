dca11a6c90be8d3e337d407faec7053088775c08
maspypy <maspypy.gmail.com>
2024-02-01 17:05:40 +0900

    a

---
+++ graph/count/count_labeled_strong_digraph.hpp
@@ -0,0 +1,18 @@
+#include "poly/fps_inv.hpp"
+#include "poly/fps_log.hpp"
+
+template <typename mint>
+vc<mint> count_labeled_strong_digraph(int N) {
+  vc<mint> F(N + 1, 1);
+  // 2^n(n-1)倍したあと 2^nC2 で割る
+  mint pow = 1;
+  FOR(i, 1, N + 1) F[i] = F[i - 1] * pow, pow += pow;
+  FOR(i, N + 1) F[i] *= fact_inv<mint>(i);
+  F = fps_inv<mint>(F);
+  pow = 1;
+  mint c = 1;
+  FOR(i, N + 1) F[i] *= c, c *= pow, pow += pow;
+  F = fps_log(F);
+  FOR(i, N + 1) F[i] = -F[i] * fact<mint>(i);
+  return F;
+}
