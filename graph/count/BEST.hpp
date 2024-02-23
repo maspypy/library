1f0d1c6ad0e6fc53e0dc18d0fda44cadb031bf2e
maspypy <maspypy.gmail.com>
2024-02-23 19:59:00 +0900

    a

--- graph/count/BEST.hpp
+++ graph/count/BEST.hpp
@@ -3,41 +3,43 @@
 #include "linalg/blackbox/det.hpp"
 
 /*
-ひとつ選んだ辺から始めて全ての点・辺を通る closed walk を数える.
+ひとつ選んだ辺から始めて全ての辺を通る closed walk を数える.
 多重辺は vc<int>(eid) で渡す，なければすべて 1. e.cost は参照しない.
 辺はラベル付きで考える. 多重辺を同一視する場合などは後で階乗で割ること.
 O(N^2+NM) （ + 最後に重複度の階乗をかける）．
 */
 template <typename mint, typename GT>
-mint BEST_theorem(GT& G, vc<int> edge_multiplicity = {}) {
+mint BEST_theorem(GT G, vc<int> edge_multiplicity = {}) {
   static_assert(GT::is_directed);
   int N = G.N, M = G.M;
   if (M == 0) return 0;
   if (edge_multiplicity.empty()) edge_multiplicity.assign(M, 1);
-
-  // やらなくても 0 になるはずだが一応
-  UnionFind uf(N);
-  vc<int> indeg(N), outdeg(N);
-  FOR(i, M) {
-    int a = G.edges[i].frm, b = G.edges[i].to, x = edge_multiplicity[i];
-    if (!x) continue;
-    outdeg[a] += x, indeg[b] += x;
-    uf.merge(a, b);
+  vc<int> vs;
+  for (auto& e: G.edges) {
+    if (edge_multiplicity[e.id] == 0) continue;
+    vs.eb(e.frm), vs.eb(e.to);
   }
-  if (uf.n_comp != 1) return 0;
-  FOR(v, N) if (indeg[v] != outdeg[v]) return 0;
 
+  UNIQUE(vs);
+  G = G.rearrange(vs, true);
+  N = G.N;
+
+  vc<int> indeg(N), outdeg(N);
   vc<tuple<int, int, mint>> mat;
-  FOR(i, M) {
-    int a = G.edges[i].frm, b = G.edges[i].to, x = edge_multiplicity[i];
+  for (auto& e: G.edges) {
+    int a = e.frm, b = e.to, x = edge_multiplicity[e.id];
+    outdeg[a] += x, indeg[b] += x;
     if (a < N - 1 && b < N - 1) mat.eb(a, b, -x);
     if (a < N - 1) mat.eb(a, a, x);
   }
+  FOR(v, N) if (indeg[v] != outdeg[v]) return 0;
+
   auto apply = [&](vc<mint> A) -> vc<mint> {
     vc<mint> B(N - 1);
     for (auto& [a, b, c]: mat) B[b] += A[a] * c;
+    return B;
   };
-  mint d = blackbox_det(N - 1, apply);
+  mint d = blackbox_det<mint>(N - 1, apply);
   for (auto& x: outdeg) { d *= fact<mint>(x - 1); }
   return d;
 }
