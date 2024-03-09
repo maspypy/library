9895b76a48fefd551fa546b0b5f5124bef2c28e5
maspypy <maspypy.gmail.com>
2024-03-10 03:27:25 +0900

    a

--- test/mytest/enumerate_multisets.test.cpp
+++ test/mytest/enumerate_multisets.test.cpp
@@ -2,27 +2,24 @@
 #include "my_template.hpp"
 
 #include "enumerate/multiset.hpp"
-#include "mod/modint.hpp"
-
-using mint = modint998;
 
 void test() {
   {
     vvc<int> res;
     auto f = [&](vc<int> A) -> void { res.eb(A); };
-    enumerate_multiset(3, 2, f);
+    enumerate_multiset(2, 3, f);
     assert(len(res) == 6);
-    assert(res[0] == vc<int>({0, 0}));
-    assert(res[1] == vc<int>({0, 1}));
-    assert(res[2] == vc<int>({0, 2}));
-    assert(res[3] == vc<int>({1, 1}));
-    assert(res[4] == vc<int>({1, 2}));
-    assert(res[5] == vc<int>({2, 2}));
+    assert(res[0] == vc<int>({0, 0, 2}));
+    assert(res[1] == vc<int>({0, 1, 1}));
+    assert(res[2] == vc<int>({0, 2, 0}));
+    assert(res[3] == vc<int>({1, 0, 1}));
+    assert(res[4] == vc<int>({1, 1, 0}));
+    assert(res[5] == vc<int>({2, 0, 0}));
   }
   {
     int cnt = 0;
     auto f = [&](vc<int> A) -> void { ++cnt; };
-    enumerate_multiset(3, 4, f);
+    enumerate_multiset(4, 3, f);
     assert(cnt == 15);
   }
 }
