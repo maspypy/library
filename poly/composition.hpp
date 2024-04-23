c9121a8803e2c3dcb762815449850bb94dd69e8e
maspypy <maspypy.gmail.com>
2024-04-23 20:14:14 +0900

    a

--- poly/composition.hpp
+++ poly/composition.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include "poly/convolution.hpp"
-#include "poly/convolution2d.hpp"
 #include "poly/poly_taylor_shift.hpp"
-#include "poly/middle_product.hpp"
+#include "poly/transposed_ntt.hpp"
 
 template <typename mint>
 vc<mint> composition_old(vc<mint>& Q, vc<mint>& P) {
@@ -42,107 +41,126 @@ vc<mint> composition_old(vc<mint>& Q, vc<mint>& P) {
   return ANS;
 }
 
-// https://noshi91.hatenablog.com/entry/2024/03/16/224034
-// O(Nlog^2N)
+// f(g(x)), O(Nlog^2N)
 template <typename mint>
 vc<mint> composition(vc<mint> f, vc<mint> g) {
-  // 必要ではないが簡略化のために，[x^0]g=0 に帰着しておく.
-  const int N = len(f) - 1;
-  assert(len(f) == N + 1 && len(g) == N + 1);
-  if (N == -1) return {};
-  mint c = g[0];
-  g[0] = 0;
-  f = poly_taylor_shift<mint>(f, c);
+  assert(len(f) == len(g));
+  if (f.empty()) return {};
 
-  auto degree = [&](vvc<mint>& F) -> pair<int, int> {
-    return {len(F) - 1, len(F[0]) - 1};
+  // [x^0]g=0 に帰着しておく
+  if (g[0] != mint(0)) {
+    f = poly_taylor_shift<mint>(f, g[0]);
+    g[0] = 0;
+  }
+
+  int n0 = len(f);
+  int n = 1;
+  while (n < len(f)) n *= 2;
+  f.resize(n), g.resize(n);
+
+  vc<mint> W(n);
+  {
+    // bit reverse order
+    vc<int> btr(n);
+    int log = topbit(n);
+    FOR(i, n) { btr[i] = (btr[i >> 1] >> 1) + ((i & 1) << (log - 1)); }
+    int t = mint::ntt_info().fi;
+    mint r = mint::ntt_info().se;
+    mint dw = r.inverse().pow((1 << t) / (2 * n));
+    mint w = 1;
+    for (auto& i: btr) { W[i] = w, w *= dw; }
+  }
+
+  auto rec = [&](auto& rec, int n, int k, vc<mint>& Q) -> vc<mint> {
+    if (n == 1) {
+      reverse(all(f));
+      transposed_ntt(f, 1);
+      mint c = mint(1) / mint(k);
+      for (auto& x: f) x *= c;
+      vc<mint> p(4 * k);
+      FOR(i, k) p[2 * i] = f[i];
+      return p;
+    }
+    auto doubling_y = [&](vc<mint>& A, int l, int r, bool t) -> void {
+      mint z = W[k / 2].inverse();
+      vc<mint> f(k);
+      if (!t) {
+        FOR(i, l, r) {
+          FOR(j, k) f[j] = A[2 * n * j + i];
+          ntt(f, 1);
+          mint r = 1;
+          FOR(j, 1, k) r *= z, f[j] *= r;
+          ntt(f, 0);
+          FOR(j, k) A[2 * n * (k + j) + i] = f[j];
+        }
+      } else {
+        FOR(i, l, r) {
+          FOR(j, k) f[j] = A[2 * n * (k + j) + i];
+          transposed_ntt(f, 0);
+          mint r = 1;
+          FOR(j, 1, k) r *= z, f[j] *= r;
+          transposed_ntt(f, 1);
+          FOR(j, k) A[2 * n * j + i] += f[j];
+        }
+      }
+    };
+
+    auto FFT_x = [&](vc<mint>& A, int l, int r, bool t) -> void {
+      vc<mint> f(2 * n);
+      if (!t) {
+        FOR(j, l, r) {
+          move(A.begin() + 2 * n * j, A.begin() + 2 * n * (j + 1), f.begin());
+          ntt(f, 0);
+          move(all(f), A.begin() + 2 * n * j);
+        }
+      } else {
+        FOR(j, l, r) {
+          move(A.begin() + 2 * n * j, A.begin() + 2 * n * (j + 1), f.begin());
+          transposed_ntt(f, 0);
+          move(all(f), A.begin() + 2 * n * j);
+        }
+      }
+    };
+
+    if (n <= k) doubling_y(Q, 1, n, 0), FFT_x(Q, 0, 2 * k, 0);
+    if (n > k) FFT_x(Q, 0, k, 0), doubling_y(Q, 0, 2 * n, 0);
+
+    FOR(i, 2 * n * k) Q[i] += 1;
+    FOR(i, 2 * n * k, 4 * n * k) Q[i] -= 1;
+
+    vc<mint> nxt_Q(4 * n * k);
+    vc<mint> F(2 * n), G(2 * n), f(n), g(n);
+    FOR(j, 2 * k) {
+      move(Q.begin() + 2 * n * j, Q.begin() + 2 * n * j + 2 * n, G.begin());
+      FOR(i, n) { g[i] = G[2 * i] * G[2 * i + 1]; }
+      ntt(g, 1);
+      move(g.begin(), g.begin() + n / 2, nxt_Q.begin() + n * j);
+    }
+    FOR(j, 4 * k) nxt_Q[n * j] = 0;
+
+    vc<mint> p = rec(rec, n / 2, k * 2, nxt_Q);
+    FOR_R(j, 2 * k) {
+      move(p.begin() + n * j, p.begin() + n * j + n / 2, f.begin());
+      move(Q.begin() + 2 * n * j, Q.begin() + 2 * n * j + 2 * n, G.begin());
+      fill(f.begin() + n / 2, f.end(), mint(0));
+      transposed_ntt(f, 1);
+      FOR(i, n) {
+        f[i] *= W[i];
+        F[2 * i] = G[2 * i + 1] * f[i], F[2 * i + 1] = -G[2 * i] * f[i];
+      }
+      move(F.begin(), F.end(), p.begin() + 2 * n * j);
+    }
+    if (n <= k) FFT_x(p, 0, 2 * k, 1), doubling_y(p, 0, n, 1);
+    if (n > k) doubling_y(p, 0, 2 * n, 1), FFT_x(p, 0, k, 1);
+    return p;
   };
 
-  /*
-  f, g は N 次とする.
-  合成は (f_i) -> sum_i f_ig(x)^i である. これの転置を考える.
-  (h_j) に対して [x^0]h(x^-1)sum_if_ig(x)^i = sum_if_i[x^0]h(x^{-1})g(x)^i
-  であるから [x^N]rev_h(x)g(x)^i を計算するというのが転置である．
-  したがって合成は [x^N]rev_h(x)g(x)^i 列挙の転置をとれば計算できる.
+  vc<mint> Q(4 * n);
+  FOR(i, n) Q[i] = -g[i];
 
-  転置問題は, 次のような反復により解ける：
-  ・入力を reverse する.
-  ・次を k=0,1,2,...,K で繰り返す（K=lg n 程度）.
-  　・g だけから定まる何らかの G_k(x,y) と畳み込む
-  　・適切な部分を抜き出す
-
-  よって合成は次の計算による
-  ・次を k=K,...,2,1,0 で繰り返す
-  　・適切な部分に入れる
-  　・G_k(x,y) との畳み込みの転置でうつす
-  ・reverse して出力する.
-
-  G_k(x,y) は k の昇順に計算できて，これを逆順に処理するため，
-  ここでは再帰関数により実装する．
-  */
-  auto dfs = [&](auto& dfs, vvc<mint> G) -> vvc<mint> {
-    auto [n, m] = degree(G);
-    if (n == 0) {
-      // [x^0]g=0 としていたので G=1 である.
-      // 転置問題ではこの時点での多項式がそのまま出力となる.
-      // よって入力をそのまま多項式に入れて返す.
-      vv(mint, F, n + 1, m + 1);
-      FOR(j, len(f)) F[0][j] = f[j];
-      return F;
-    }
-    vvc<mint> G1 = G;
-    FOR(i, n + 1) {
-      if (i % 2 == 0) continue;
-      FOR(j, m + 1) G1[i][j] = -G[i][j];
-    }
-    vvc<mint> G2 = convolution2d(G, G1);
-    int n2 = n / 2;
-    FOR(i, n2 + 1) G2[i] = G2[2 * i];
-    G2.resize(n2 + 1);
-    vvc<mint> F = dfs(dfs, G2);
-    assert(degree(F) == degree(G2));
-    // 転置問題では G1 と畳み込んだあと n の parity に応じた部分を抜き出す.
-    // よってまず dfs の出力を抜き出す前の位置に入れて, 畳み込みの転置をとる.
-
-    // 畳み込みの転置をとるとは何なのか.
-    // (n,m) 次の多項式積 H(x,y) G(x,y)
-    // H, G を適切に配置した (n+1)(2m+1)-1 次多項式を用意して
-    // 畳み込んで 2(n+1)(2m+1)-2 次多項式を得る.
-    // この適切な (2n,2m) 次多項式を抜き出す.
-    // 転置をとると次のようになる.
-    // F を適切に配置した (2n,2m) 次多項式を作る.
-    // ここから 2(n+1)(2m+1)-2 次多項式を作る.
-    // これと適切な多項式の middle product をとって
-    // (n+1)(2m+1)-1 次多項式を得る.
-    vc<mint> A(2 * (n + 1) * (2 * m + 1) - 1);
-    FOR(i, len(F)) FOR(j, len(F[0])) {
-      int i1 = 2 * i + (n % 2);
-      int idx = i1 * (2 * m + 1) + j;
-      A[idx] = F[i][j];
-    }
-    vc<mint> B((n + 1) * (2 * m + 1));
-    FOR(i, n + 1) FOR(j, m + 1) {
-      int idx = i * (2 * m + 1) + j;
-      B[idx] = G1[i][j];
-    }
-
-    A = middle_product<mint>(A, B);
-    assert(len(A) == (n + 1) * (2 * m + 1));
-    vv(mint, res, n + 1, m + 1);
-    FOR(i, n + 1) FOR(j, m + 1) {
-      int idx = (2 * m + 1) * i + j;
-      res[i][j] = A[idx];
-    }
-    return res;
-  };
-  vv(mint, G, N + 1, 2);
-  G[0][0] = 1;
-  FOR(i, N + 1) G[i][1] = -g[i];
-  vvc<mint> F = dfs(dfs, G);
-  assert(degree(F) == (pair<int, int>{N, 1}));
-  vc<mint> ANS(N + 1);
-  FOR(i, N + 1) ANS[i] = F[i][0];
-  // 最後に reverse する
-  reverse(all(ANS));
-  return ANS;
+  vc<mint> p = rec(rec, n, 1, Q);
+  p.resize(n);
+  reverse(all(p));
+  p.resize(n0);
+  return p;
 }
