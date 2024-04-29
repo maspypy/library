4f3143cc0d8fe93db4c8af13f48fb44811610bee
maspypy <maspypy.gmail.com>
2024-04-29 17:51:30 +0900

    a

--- test/library_checker/math/powproj_sps.test.cpp
+++ test/library_checker/math/powproj_sps.test.cpp
@@ -1,18 +1,19 @@
 #define PROBLEM \
   "https://judge.yosupo.jp/problem/power_projection_of_set_power_series"
+
 #include "my_template.hpp"
 #include "other/io.hpp"
 
+#include "setfunc/power_projection_of_sps.hpp"
 #include "mod/modint.hpp"
-#include "setfunc/transposed_sps_composition.hpp"
 
 using mint = modint998;
 
 void solve() {
   LL(N, M);
-  VEC(mint, A, 1 << N);
-  VEC(mint, B, 1 << N);
-  vc<mint> ANS = transposed_sps_composition_poly<mint, 20>(A, B, M);
+  VEC(mint, s, 1 << N);
+  VEC(mint, wt, 1 << N);
+  vc<mint> ANS = power_projection_of_sps<mint, 20>(wt, s, M);
   print(ANS);
 }
 
