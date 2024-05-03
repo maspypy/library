977ea0a2d2a5760ce714517ecb8857a5ec854434
maspypy <45988013+maspypy@users.noreply.github.com>
2024-05-03 18:47:31 +0900

    Update abc127f_1.test.cpp

--- test_atcoder/abc127f_1.test.cpp
+++ test_atcoder/abc127f_1.test.cpp
@@ -20,7 +20,7 @@ void solve() {
 
   for (auto&& n: query) {
     ll cnt_lo = (n - 1) / 2;
-    auto [med, sum_lo] = X.kth_value_and_sum(0, n, cnt_lo);
+    auto [med, sum_lo] = X.kth_value_sum(0, n, cnt_lo);
     auto total = X.sum(0, n, 0, n);
     ll cnt_hi = n - cnt_lo;
     ll sum_hi = total - sum_lo;
