56c0660753f92b8d6e569d5a5233e69744130c72
maspypy <maspypy.gmail.com>
2024-05-29 22:30:57 +0900

    only <

--- ds/double_end_queue.hpp
+++ ds/double_end_queue.hpp
@@ -1,4 +1,4 @@
-
+// T は operator< を持つ
 template <typename T>
 struct Double_End_Queue {
   // interval heap
@@ -41,8 +41,8 @@ struct Double_End_Queue {
   }
 
   void debug() {
-    print("debug dat=", dat), flush();
-    FOR(i, len(dat) - 1) if (i % 2 == 0) assert(dat[i] <= dat[i + 1]);
+    print("debug dat=", dat);
+    FOR(i, len(dat) - 1) if (i % 2 == 0) assert(!(dat[i + 1] < dat[i]));
     FOR(i, 1, ceil<int>(len(dat), 2)) {
       int p = (i - 1) / 2;
       int a = dat[2 * p + 0], b = dat[2 * p + 1];
@@ -60,7 +60,7 @@ private:
     int n = len(dat);
     if (i % 2 == 0) {
       while (1) {
-        if (i + 1 < n && dat[i] > dat[i + 1]) swap(dat[i], dat[i + 1]);
+        if (i + 1 < n && dat[i + 1] < dat[i]) swap(dat[i], dat[i + 1]);
         int j = i, l = 2 * i + 2, r = 2 * i + 4;
         if (l < n && dat[l] < dat[j]) j = l;
         if (r < n && dat[r] < dat[j]) j = r;
@@ -69,12 +69,12 @@ private:
       }
     } else {
       while (1) {
-        if (dat[i - 1] > dat[i]) swap(dat[i - 1], dat[i]);
+        if (dat[i] < dat[i - 1]) swap(dat[i - 1], dat[i]);
         int j = i, l = 2 * i + 1, r = 2 * i + 3;
         if (r >= n) --r;
         if (l >= n) --l;
-        if (l < n && dat[l] > dat[j]) j = l;
-        if (r < n && dat[r] > dat[j]) j = r;
+        if (l < n && dat[j] < dat[l]) j = l;
+        if (r < n && dat[j] < dat[r]) j = r;
         if (i == j) break;
         swap(dat[i], dat[j]), i = j;
         if (i % 2 == 0) break;
@@ -88,18 +88,18 @@ private:
       int p = parent(i) ^ 1;
       if (dat[p] < dat[i]) { swap(dat[i], dat[p]), i = p; }
     }
-    if (i % 2 == 1 && dat[i - 1] > dat[i]) { swap(dat[i - 1], dat[i]), --i; }
+    if (i % 2 == 1 && dat[i] < dat[i - 1]) { swap(dat[i - 1], dat[i]), --i; }
     if (i % 2 == 0) {
       while (i >= 2) {
         int p = parent(i);
-        if (dat[p] <= dat[i]) break;
+        if (!(dat[i] < dat[p])) break;
         swap(dat[p], dat[i]), i = p;
       }
       return;
     }
     while (i >= 3) {
       int p = parent(i);
-      if (dat[p] >= dat[i]) break;
+      if (!(dat[p] < dat[i])) break;
       swap(dat[p], dat[i]), i = p;
     }
   }
