84b7a05672c39b32ad702ed9234f592185887996
maspypy <maspypy.gmail.com>
2024-06-11 16:34:43 +0900

    a

--- graph/count/BEST.hpp
+++ graph/count/BEST.hpp
@@ -1,5 +1,6 @@
 #include "graph/base.hpp"
 #include "linalg/blackbox/det.hpp"
+#include "linalg/det.hpp"
 
 /*
 ひとつ選んだ辺から始めて全ての辺を通る closed walk を数える.
@@ -7,7 +8,7 @@
 辺はラベル付きで考える. 多重辺を同一視する場合などは後で階乗で割ること.
 O(N^2+NM) （ + 最後に重複度の階乗をかける）．
 */
-template <typename mint, typename GT>
+template <typename mint, bool sparse, typename GT>
 mint BEST_theorem(GT G, vc<int> edge_multiplicity = {}) {
   static_assert(GT::is_directed);
   int N = G.N, M = G.M;
@@ -24,21 +25,36 @@ mint BEST_theorem(GT G, vc<int> edge_multiplicity = {}) {
   N = G.N;
 
   vc<int> indeg(N), outdeg(N);
-  vc<tuple<int, int, mint>> mat;
-  for (auto& e: G.edges) {
-    int a = e.frm, b = e.to, x = edge_multiplicity[e.id];
-    outdeg[a] += x, indeg[b] += x;
-    if (a < N - 1 && b < N - 1) mat.eb(a, b, -x);
-    if (a < N - 1) mat.eb(a, a, x);
-  }
-  FOR(v, N) if (indeg[v] != outdeg[v]) return 0;
+  if constexpr (sparse) {
+    vc<tuple<int, int, mint>> mat;
+    for (auto& e: G.edges) {
+      int a = e.frm, b = e.to, x = edge_multiplicity[e.id];
+      outdeg[a] += x, indeg[b] += x;
+      if (a < N - 1 && b < N - 1) mat.eb(a, b, -x);
+      if (a < N - 1) mat.eb(a, a, x);
+    }
+    FOR(v, N) if (indeg[v] != outdeg[v]) return 0;
 
-  auto apply = [&](vc<mint> A) -> vc<mint> {
-    vc<mint> B(N - 1);
-    for (auto& [a, b, c]: mat) B[b] += A[a] * c;
-    return B;
-  };
-  mint d = blackbox_det<mint>(N - 1, apply);
-  for (auto& x: outdeg) { d *= fact<mint>(x - 1); }
-  return d;
+    auto apply = [&](vc<mint> A) -> vc<mint> {
+      vc<mint> B(N - 1);
+      for (auto& [a, b, c]: mat) B[b] += A[a] * c;
+      return B;
+    };
+    mint d = blackbox_det<mint>(N - 1, apply);
+    for (auto& x: outdeg) { d *= fact<mint>(x - 1); }
+    return d;
+  } else {
+    // dense
+    vv(mint, mat, N - 1, N - 1);
+    for (auto& e: G.edges) {
+      int a = e.frm, b = e.to, x = edge_multiplicity[e.id];
+      outdeg[a] += x, indeg[b] += x;
+      if (a < N - 1 && b < N - 1) mat[a][b] -= x;
+      if (a < N - 1) mat[a][a] += x;
+    }
+    FOR(v, N) if (indeg[v] != outdeg[v]) return 0;
+    mint d = det(mat);
+    for (auto& x: outdeg) { d *= fact<mint>(x - 1); }
+    return d;
+  }
 }
