2b5a50abb5646d10210f8e2dced67e93e44610c7
maspypy <maspypy.gmail.com>
2024-06-11 16:34:43 +0900

    a

---
+++ test/library_checker/graph/counting_eulerian_dense.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/counting_eulerian_circuits"
+
+#include "my_template.hpp"
+#include "other/io.hpp"
+
+#include "mod/modint.hpp"
+#include "graph/count/BEST.hpp"
+
+using mint = modint998;
+
+void solve() {
+  LL(N, M);
+  Graph<int, 1> G(N);
+  G.read_graph(M, 0, 0);
+  mint ANS = BEST_theorem<mint, false>(G);
+  print(ANS);
+}
+
+signed main() {
+  solve();
+  return 0;
+}
