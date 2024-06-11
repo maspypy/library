dac2bfd47fa3c1c9acb6d3c49a8a74bc03eaa185
maspypy <maspypy.gmail.com>
2024-06-11 19:43:00 +0900

    a

--- string/count_subseq.hpp
+++ string/count_subseq.hpp
@@ -1,4 +1,6 @@
 // https://noshi91.hatenablog.com/entry/2023/02/26/135340
+// 行列にするときは、(mp[x]+1) の列を持つと小さい行列になることがある.
+// https://yukicoder.me/problems/no/2762
 template <typename mint, typename STRING>
 mint count_subseq(STRING& S) {
   map<int, mint> dp;
