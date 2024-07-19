8e16e976b6016defbb9d9b516d6b353c929bd223
maspypy <maspypy.gmail.com>
2024-07-20 03:35:44 +0900

    a

--- test/yukicoder/2243.test.cpp
+++ test/yukicoder/2243.test.cpp
@@ -11,7 +11,6 @@ using mint = modint998;
 void solve() {
   LL(M, N);
   // M つかわない
-
   VEC(int, A, N);
   map<int, int> MP;
   for (auto&& x: A) MP[x]++;
@@ -19,21 +18,15 @@ void solve() {
   for (auto&& [a, b]: MP) A.eb(b);
 
   // binom(x,a) の総積を求める
-  vvc<mint> polys;
+  vc<mint> polys;
   for (auto&& a: A) {
-    FOR(i, a) { polys.eb(vc<mint>({-i, 1})); }
+    FOR(i, a) { polys.eb(i); }
   }
-  vc<mint> F = convolution_all(polys);
+  vc<mint> F = convolution_all_1(polys);
+  reverse(all(F));
   vc<mint> X(N + 1);
   FOR(i, N + 1) X[i] = i;
   F = multipoint_eval(F, X);
-
-  /*
-  FOR(n, N + 1) {
-    F[n] = 1;
-    for (auto&& a: A) { F[n] *= C<mint>(n, a) * fact<mint>(a); }
-  }
-  */
   F = pascal<mint>(F, 0, 1);
   print(SUM<mint>(F));
 }
