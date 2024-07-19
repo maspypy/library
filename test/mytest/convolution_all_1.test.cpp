2fc32444cca6f1cb5b20af072f1ba0927d6dd6b5
maspypy <maspypy.gmail.com>
2024-07-20 03:03:46 +0900

    a

---
+++ test/mytest/convolution_all_1.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+
+#include "mod/modint.hpp"
+#include "random/base.hpp"
+#include "poly/convolution_all.hpp"
+
+using mint = modint998;
+
+void test() {
+  FOR(10000) {
+    int N = RNG(0, 1000);
+    vc<mint> A(N);
+    FOR(i, N) A[i] = RNG(0, mint::get_mod());
+    vvc<mint> polys;
+    for (auto& a: A) polys.eb(vc<mint>({mint(1), -a}));
+    vc<mint> F = convolution_all_1<mint>(A);
+    vc<mint> G = convolution_all<mint>(polys);
+    assert(F == G);
+  }
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << "\n";
+}
+
+signed main() {
+  test();
+  solve();
+  return 0;
+}
