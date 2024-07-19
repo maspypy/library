e35c1656cfa228f547803182d28da1edc44b8515
maspypy <maspypy.gmail.com>
2024-07-19 19:22:24 +0900

    a

--- test/mytest/wavelet_matrix.test.cpp
+++ test/mytest/wavelet_matrix.test.cpp
@@ -3,24 +3,11 @@
 
 #include "random/base.hpp"
 #include "ds/wavelet_matrix/wavelet_matrix.hpp"
+#include "ds/static_range_product_group.hpp"
+#include "ds/segtree/segtree.hpp"
+#include "alg/monoid/min.hpp"
 
-template <typename Monoid>
-struct Static_Range_Sum {
-  using MX = Monoid;
-  using X = Monoid::value_type;
-  int n;
-  vc<X> dat;
-  Static_Range_Sum() {}
-  void build(vc<X>& A) {
-    n = len(A);
-    dat.assign(n + 1, MX::unit());
-    for (int i = 0; i < n; ++i) dat[i + 1] = MX::op(dat[i], A[i]);
-  }
-  X prod(int l, int r) { return MX::op(MX::inverse(dat[l]), dat[r]); }
-  X sum(int l, int r) { return MX::op(MX::inverse(dat[l]), dat[r]); }
-};
-
-template <bool SMALL_Y>
+template <bool SMALL_Y, typename SEGTREE>
 void test(int N) {
   int MAX = RNG(2, 1 << 10);
   vc<int> A(N);
@@ -28,7 +15,8 @@ void test(int N) {
   FOR(i, N) X[i] = RNG(MAX);
   FOR(i, N) A[i] = RNG(MAX);
 
-  Wavelet_Matrix<int, false, Static_Range_Sum<Monoid_Add<int>>> WM(A, X);
+  Wavelet_Matrix<int, false, SEGTREE> WM(A, X);
+  using Mono = SEGTREE::MX;
 
   int Q = 100;
   FOR(Q) {
@@ -43,24 +31,23 @@ void test(int N) {
     vc<int> Y = {X.begin() + L, X.begin() + R};
     int t = RNG(0, 7);
     if (t == 0) { // count
-      SHOW(A);
-      SHOW(L, R, lo, hi);
       int cnt = 0;
       for (auto&& x: B)
         if (lo <= x && x < hi) cnt += 1;
       assert(WM.count(L, R, lo, hi) == cnt);
     }
-    // if (t == 1) { // sm
-    //   int sm = 0;
-    //   FOR(i, L, R) if (lo <= A[i] && A[i] < hi) sm += X[i];
-    //   assert(WM.sum(L, R, lo, hi) == sm);
-    // }
-    // if (t == 2) { // kth
-    //   if (L == R) continue;
-    //   int k = RNG(R - L);
-    //   sort(all(B));
-    //   assert(WM.kth(L, R, k) == B[k]);
-    // }
+    if (t == 1) { // sm
+      int sm = Mono::unit();
+      FOR(i, L, R) if (lo <= A[i] && A[i] < hi) sm = Mono::op(sm, X[i]);
+      assert(WM.prod(L, R, lo, hi) == sm);
+    }
+    if (t == 2) { // kth
+      if (L == R) continue;
+      int k = RNG(R - L);
+      sort(all(B));
+      SHOW(B[k], WM.kth(L, R, k));
+      assert(WM.kth(L, R, k) == B[k]);
+    }
     // if (t == 3) { // max_right
     //   int a = RNG(0, 10);
     //   int b = RNG(0, 10);
@@ -87,22 +74,22 @@ void test(int N) {
     //   auto p = WM.kth_value_sum(L, R, k);
     //   assert(p.fi == val && p.se == sm);
     // }
-    // if (t == 5) { // next
-    //   int x = RNG(-1, MAX + 2);
-    //   int ans = infty<int>;
-    //   for (auto& b: B) {
-    //     if (x <= b) chmin(ans, b);
-    //   }
-    //   assert(ans == WM.next(L, R, x));
-    // }
-    // if (t == 6) { // prev
-    //   int x = RNG(-1, MAX + 1);
-    //   int ans = -infty<int>;
-    //   for (auto& b: B) {
-    //     if (b <= x) chmax(ans, b);
-    //   }
-    //   assert(ans == WM.prev(L, R, x));
-    // }
+    if (t == 5) { // next
+      int x = RNG(-1, MAX + 2);
+      int ans = infty<int>;
+      for (auto& b: B) {
+        if (x <= b) chmin(ans, b);
+      }
+      assert(ans == WM.next(L, R, x));
+    }
+    if (t == 6) { // prev
+      int x = RNG(-1, MAX + 1);
+      int ans = -infty<int>;
+      for (auto& b: B) {
+        if (b <= x) chmax(ans, b);
+      }
+      assert(ans == WM.prev(L, R, x));
+    }
   }
 }
 
@@ -113,8 +100,14 @@ void solve() {
 }
 
 signed main() {
-  FOR(N, 64) { FOR(50) test<true>(N); }
-  FOR(N, 64) { FOR(50) test<false>(N); }
+  FOR(N, 64) {
+    FOR(50) {
+      test<true, Static_Range_Product_Group<Monoid_Add<int>>>(N);
+      test<false, Static_Range_Product_Group<Monoid_Add<int>>>(N);
+      test<true, SegTree<Monoid_Min<int>>>(N);
+      test<false, SegTree<Monoid_Min<int>>>(N);
+    }
+  }
   solve();
 
   return 0;
