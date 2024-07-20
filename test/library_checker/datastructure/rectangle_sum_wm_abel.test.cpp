e53dd147af13000286811b91b5c7fabc09d72b96
maspypy <maspypy.gmail.com>
2024-07-20 11:26:06 +0900

    a

--- test/library_checker/datastructure/rectangle_sum_wm_abel.test.cpp
+++ test/library_checker/datastructure/rectangle_sum_wm_abel.test.cpp
@@ -2,15 +2,16 @@
 
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "ds/wavelet_matrix/wavelet_matrix_2d_range_static_abelgroup.hpp"
+#include "ds/wavelet_matrix/wavelet_matrix_2d_range.hpp"
+#include "ds/static_range_product_group.hpp"
 #include "alg/monoid/add.hpp"
 
 void solve() {
   LL(N, Q);
   vc<u32> X(N), Y(N), W(N);
   FOR(i, N) read(X[i], Y[i], W[i]);
-  Wavelet_Matrix_2D_Range_Static_AbelGroup<Monoid_Add<ll>, int, false, false>
-      WM(N, [&](int i) -> tuple<int, int, ll> {
+  Wavelet_Matrix_2D_Range<Prefix_Sum<ll>, int, false, false> WM(
+      N, [&](int i) -> tuple<int, int, ll> {
         return {X[i], Y[i], W[i]};
       });
   FOR(q, Q) {
