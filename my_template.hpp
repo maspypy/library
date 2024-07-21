a6fe962593698f68d63a95d7a19afc138afb919c
maspypy <maspypy.gmail.com>
2024-07-21 16:21:08 +0900

    a

--- my_template.hpp
+++ my_template.hpp
@@ -29,7 +29,7 @@ constexpr u32 infty<u32> = infty<int>;
 template <>
 constexpr u64 infty<u64> = infty<ll>;
 template <>
-constexpr i128 infty<i128> = i128(infty<ll>) * 2'020'000'000'000'000'000;
+constexpr i128 infty<i128> = i128(infty<ll>) * 2'000'000'000'000'000'000;
 template <>
 constexpr double infty<double> = infty<ll>;
 template <>
