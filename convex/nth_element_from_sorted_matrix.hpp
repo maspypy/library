311df2696d2ca1079654525b2c49bc06c03aa749
maspypy <maspypy.gmail.com>
2024-07-24 17:29:19 +0900

    a

--- convex/nth_element_from_sorted_matrix.hpp
+++ convex/nth_element_from_sorted_matrix.hpp
@@ -1,4 +1,5 @@
 
+// https://uoj.ac/problem/891
 template <typename T, typename F>
 vi nth_element_from_sorted_matrix(ll N, ll M, ll K, F f, int k1 = 0, int k2 = 0,
                                   bool tr = false) {
@@ -15,7 +16,7 @@ vi nth_element_from_sorted_matrix(ll N, ll M, ll K, F f, int k1 = 0, int k2 = 0,
   if (K > N) {
     A = nth_element_from_sorted_matrix<T>(N, M / 2, (K - N) / 2, f, k1, k2 + 1,
                                           tr);
-    for (auto& a: A) a *= 2;
+    for (auto &a: A) a *= 2;
     K = K - (K - N) / 2 * 2;
   }
   pqg<pair<T, int>> que;
@@ -24,12 +25,17 @@ vi nth_element_from_sorted_matrix(ll N, ll M, ll K, F f, int k1 = 0, int k2 = 0,
     j = ((j + 1) << k2) - 1;
     return (tr ? f(j, i) : f(i, j));
   };
-  FOR(i, N) if (A[i] < M) que.emplace(g(i, A[i]), i);
+  if (A[0] < M) que.emplace(g(0, A[0]), 0);
+  FOR(i, 1, N) if (A[i] < A[i - 1]) que.emplace(g(i, A[i]), i);
   while (K) {
     --K;
     auto [x, i] = POP(que);
     A[i]++;
-    if (K && A[i] < M) que.emplace(g(i, A[i]), i);
+    if (K == 0) break;
+    if (A[i] < M && (i == 0 || A[i - 1] > A[i])) { que.emplace(g(i, A[i]), i); }
+    if (i + 1 < N && A[i + 1] == A[i] - 1) {
+      que.emplace(g(i + 1, A[i + 1]), i + 1);
+    }
   }
   return A;
 }
