239b30d7d878c163f3f9034857fe6c78e3c3ffef
maspypy <maspypy.gmail.com>
2024-07-29 11:54:02 +0900

    a

--- string/run_length.hpp
+++ string/run_length.hpp
@@ -1,6 +1,6 @@
-template <typename STRING = string, typename CHAR = char>
-vc<pair<CHAR, int>> run_length(STRING& S) {
-  vc<pair<CHAR, int>> res;
+template <typename STRING = string>
+vc<pair<typename STRING::value_type, int>> run_length(STRING& S) {
+  vc<pair<typename STRING::value_type, int>> res;
   for (auto&& x: S) {
     if (res.empty() || res.back().fi != x) { res.emplace_back(x, 0); }
     res.back().se++;
