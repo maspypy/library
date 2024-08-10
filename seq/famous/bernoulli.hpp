e8ec6731fc7f6ccf2dda56c4bc383f578d805947
maspypy <maspypy.gmail.com>
2024-08-10 20:35:59 +0900

    a

--- seq/famous/bernoulli.hpp
+++ seq/famous/bernoulli.hpp
@@ -16,3 +16,33 @@ vc<mint> bernoulli_number(int N) {
   FOR(i, n + 1) B[2 * i] = F[i] * fact<mint>(2 * i);
   return B;
 }
+
+template <typename mint>
+mint single_bernoulli(int n) {
+  // https://atcoder.jp/contests/xmascon23/tasks/xmascon23_e
+  if (n == 0) return 1;
+  if (n == 1) return -inv<mint>(2);
+  /*
+  B_n = [x^n/n!] x / (exp(x)-1) = F(1-e^x)
+  F(x) = 1+(1/2)x+(1/3)x^2+...
+  これを x^n で打ち切る
+  F(x) = 1+(1/2)x+(1/3)x^2+...+(1/n+1)x^n, G(x) = F(1-x)
+  (xF(x)) d/dx = 1-x^{n+1}/1-x
+  ((1-x)G(x)) -d/dx = 1-(1-x)^{n+1}/x = H(x)
+  */
+  vc<mint> G(n + 2);
+  mint sm = 0;
+  FOR(i, 1, n + 2) {
+    mint c = C<mint>(n + 1, i);
+    mint h = (i % 2 == 0 ? c : -c);
+    // H(x) = ... gx^{i-1}
+    G[i] = h * inv<mint>(i);
+    sm += inv<mint>(i);
+  }
+  G[0] = sm;
+  FOR(i, n) G[i + 1] += G[i];
+  vc<mint> pow = powertable_2<mint>(n, n);
+  mint ans = 0;
+  FOR(i, n + 1) { ans += pow[i] * G[i]; }
+  return ans;
+}
