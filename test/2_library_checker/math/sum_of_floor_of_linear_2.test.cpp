a3240c8da24e9b0cc3ff52f8006633e9111d2f7b
maspypy <45988013+maspypy@users.noreply.github.com>
2024-08-26 02:37:11 +0900

    Update sum_of_floor_of_linear_2.test.cpp

--- test/2_library_checker/math/sum_of_floor_of_linear_2.test.cpp
+++ test/2_library_checker/math/sum_of_floor_of_linear_2.test.cpp
@@ -7,7 +7,8 @@
 void solve() {
   u32 n, m, a, b;
   read(n, m, a, b);
-  print(floor_sum_of_linear_polynomial_nonnegative<u64, 0, 0, u64>(n, a, b, m));
+  auto x = floor_sum_of_linear_polynomial_nonnegative<u64, 0, 1, u64>(n, a, b, m);
+  print(x[0][1]);
 }
 
 signed main() {
