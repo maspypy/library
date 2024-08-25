b1796ab95e5155dcb6ff8eb306226ebe9231acd4
maspypy <45988013+maspypy@users.noreply.github.com>
2024-08-26 04:16:15 +0900

    Update 2362.test.cpp

--- test/3_yukicoder/2362.test.cpp
+++ test/3_yukicoder/2362.test.cpp
@@ -9,12 +9,12 @@ void solve() {
   read(N, M, X, Y);
   u64 ANS = 0;
   {
-    auto a = floor_sum_of_linear_polynomial<u64, 1, u64>(N, X, 0, M);
-    ANS -= a[0] * N - a[1];
+    auto a = floor_sum_of_linear_polynomial_nonnegative<u64, 1, 1, u64>(N, X, 0, M);
+    ANS -= a[0][1] * N - a[1][1];
   }
   {
-    auto a = floor_sum_of_linear_polynomial<u64, 1, u64>(N, X, Y, M);
-    ANS -= a[0] * (N - 1) - 2 * a[1];
+    auto a = floor_sum_of_linear_polynomial_nonnegative<u64, 1, 1, u64>(N, X, Y, M);
+    ANS -= a[0][1] * (N - 1) - 2 * a[1][1];
   }
   print(ANS);
 }
