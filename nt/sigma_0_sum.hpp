f4deae6ee233a4cf1a51774d1b2b7a691edee80d
maspypy <maspypy.gmail.com>
2024-08-28 10:25:52 +0900

    a

--- nt/sigma_0_sum.hpp
+++ nt/sigma_0_sum.hpp
@@ -1,8 +1,27 @@
 // sum_[1,N] sigma_0(n)
-ll sigma_0_sum(ll N) {
-  assert(N <= (1LL << 50));
-  ll sq = sqrtl(N);
-  ll ANS = 0;
-  for (int d = 1; d <= sq; ++d) { ANS += ll(double(N) / d); }
-  return 2 * ANS - sq * sq;
+template <typename T = u64>
+T sigma_0_sum_small(u64 N) {
+  u32 sq = sqrtl(N);
+  T ANS = 0;
+  for (u32 d = 1; d <= sq; ++d) { ANS += N / d; }
+  return 2 * ANS - u64(sq) * sq;
+}
+
+// https://oeis.org/A006218
+// sigma0(1)+...+sigma0(N) = sum floor(N/i)
+template <typename T = u64>
+T sigma_0_sum_large(u64 N) {
+  u32 sq = sqrtl(N);
+  auto above = [&](u128 x, u128 y) -> bool { return y * (sq - x) > N; };
+  auto slope = [&](u128 x, u128 a, u128 b) -> bool {
+    x = sq - x;
+    return b * x * x <= N * a;
+  };
+  T ANS = convex_floor_sum<u64, T>(sq, above, slope);
+  return 2 * ANS - u64(sq) * sq;
+}
+
+template <typename T = u64>
+T sigma_0_sum(u64 N) {
+  return (N < (1e14) ? sigma_0_sum_small<T>(N) : sigma_0_sum_large<T>(N));
 }
---
+++ test/1_mytest/sigma_0_sum.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "my_template.hpp"
+
+#include "nt/convex_floor_sum.hpp"
+#include "nt/sigma_0_sum.hpp"
+
+void test() {
+  FOR(N, 1, 10000) {
+    u64 ans = 0;
+    FOR(x, 1, N + 1) ans += N / x;
+    assert(ans == sigma_0_sum_small(N));
+    assert(ans == sigma_0_sum_large(N));
+  }
+  u64 N = 1'000'000'000'000'000;
+  u64 ANS = 34'693'207'724'724'246; // https://oeis.org/A057494
+  u64 a = sigma_0_sum_small(N);
+  u64 b = sigma_0_sum_small(N);
+  assert(a == ANS && b == ANS);
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << "\n";
+}
+
+int main() {
+  test();
+  solve();
+}
