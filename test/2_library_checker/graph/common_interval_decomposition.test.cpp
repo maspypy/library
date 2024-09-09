939fa3e4378a94bf88b3496307ee09b14feab564
maspypy <maspypy.gmail.com>
2024-09-09 09:54:15 +0900

    a

--- test/2_library_checker/graph/common_interval_decomposition.test.cpp
+++ test/2_library_checker/graph/common_interval_decomposition.test.cpp
@@ -1,5 +1,4 @@
-#define PROBLEM \
-  "https://judge.yosupo.jp/problem/common_interval_decomposition_tree"
+#define PROBLEM "https://judge.yosupo.jp/problem/common_interval_decomposition_tree"
 #include "my_template.hpp"
 #include "other/io.hpp"
 #include "seq/common_interval_decomposition.hpp"
@@ -7,7 +6,7 @@
 void solve() {
   LL(N);
   VEC(int, P, N);
-  Common_Inverval_Decomposition<1000000> X(P);
+  Common_Inverval_Decomposition X(P);
   int nxt = 0;
   auto dfs = [&](auto& dfs, auto n, int p) -> void {
     string t = (n->inc || n->dec || (n->r - n->l == 1) ? "linear" : "prime");
