d6059126bd302952e46ec33a5910386e2f30d0f9
maspypy <maspypy.gmail.com>
2024-10-01 01:05:09 +0900

    a

--- test/2_library_checker/number_theory/nim_product.test.cpp
+++ test/2_library_checker/number_theory/nim_product.test.cpp
@@ -1,12 +1,14 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/nim_product_64"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "nt/nim_product.hpp"
+#include "nt/nimber/base.hpp"
 
+using F = Nimber64;
 void solve() {
   u64 a, b;
   read(a, b);
-  print(nim_product(a, b));
+  F ANS = F(a) * F(b);
+  print(ANS.val);
 }
 
 signed main() {
