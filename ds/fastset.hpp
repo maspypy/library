515ee2f675ef3bc196fdcb424583cfa63181798d
maspypy <maspypy.gmail.com>
2024-10-28 19:23:56 +0900

    a

--- ds/fastset.hpp
+++ ds/fastset.hpp
@@ -31,20 +31,18 @@ struct FastSet {
     build(n);
     FOR(i, n) { seg[0][i / B] |= u64(f(i)) << (i % B); }
     FOR(h, log - 1) {
-      FOR(i, len(seg[h])) {
-        seg[h + 1][i / B] |= u64(bool(seg[h][i])) << (i % B);
-      }
+      FOR(i, len(seg[h])) { seg[h + 1][i / B] |= u64(bool(seg[h][i])) << (i % B); }
     }
   }
 
   bool operator[](int i) const { return seg[0][i / B] >> (i % B) & 1; }
   void insert(int i) {
-    for (int h = 0; h < log; h++) {
-      seg[h][i / B] |= u64(1) << (i % B), i /= B;
-    }
+    assert(0 <= i && i < n);
+    for (int h = 0; h < log; h++) { seg[h][i / B] |= u64(1) << (i % B), i /= B; }
   }
   void add(int i) { insert(i); }
   void erase(int i) {
+    assert(0 <= i && i < n);
     u64 x = 0;
     for (int h = 0; h < log; h++) {
       seg[h][i / B] &= ~(u64(1) << (i % B));
