e0df3abfda449eb7fb100961a53bb9dfb642c407
maspypy <maspypy.gmail.com>
2024-11-18 03:41:33 +0900

    a

--- ds/segtree/beats_kinetic.hpp
+++ ds/segtree/beats_kinetic.hpp
@@ -1,6 +1,6 @@
 #include "ds/segtree/segtree_beats.hpp"
 
-// (x[i],y[i]) からなる列. a>=0 であるときに y[i] :- y[i] + ax[i] + b という作用ができる
+// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b という作用ができる
 // x には単調性は要らない. x,sum(a):T1, y,sum(b):T2, T1*T1<=T2.
 // https://codeforces.com/blog/entry/82094#comment-688448
 // https://atcoder.jp/contests/jsc2024-final/tasks/jsc2024_final_d
@@ -8,6 +8,7 @@ template <typename T1, typename T2>
 struct Beats_Kinetic_Max {
   struct Mono_X {
     struct X {
+      int idx;
       T1 x;
       T2 y;
       T1 nxt_change;
@@ -17,7 +18,7 @@ struct Beats_Kinetic_Max {
     static X op(X L, X R) {
       X M;
       if (L.y < R.y) { swap(L, R); }
-      M.x = L.x, M.y = L.y;
+      M.idx = L.idx, M.x = L.x, M.y = L.y;
       M.nxt_change = min(L.nxt_change, R.nxt_change);
       if (L.x < R.x) {
         T2 t = floor<T2>(L.y - R.y, R.x - L.x);
@@ -26,7 +27,7 @@ struct Beats_Kinetic_Max {
       M.fail = 0;
       return M;
     }
-    static constexpr X unit() { return {0, -infty<T2>, infty<T1>, 0}; }
+    static constexpr X unit() { return {-1, 0, -infty<T2>, infty<T1>, 0}; }
     bool commute = true;
   };
   struct Mono_A {
@@ -55,31 +56,33 @@ struct Beats_Kinetic_Max {
   using S = typename Mono_X::X;
   SegTree_Beats<Beats> seg;
   Beats_Kinetic_Max(vc<T1>& X, vc<T2>& Y) {
-    seg.build(len(X), [&](int i) -> S { return {X[i], Y[i], infty<T1>, 0}; });
+    seg.build(len(X), [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
   }
   template <typename F>
   Beats_Kinetic_Max(int n, F f) {
     seg.build(n, [&](int i) -> S {
       auto [x, y] = f(i);
-      return {x, y, infty<T1>, 0};
+      return {i, x, y, infty<T1>, 0};
     });
   }
 
-  void set(int i, T1 x, T2 y) { seg.set(i, {x, y, infty<T1>, 0}); }
+  void set(int i, T1 x, T2 y) { seg.set(i, {i, x, y, infty<T1>, 0}); }
 
-  // (x,y)
-  pair<T1, T2> prod(int l, int r) {
+  // (i,x,y)
+  tuple<int, T1, T2> prod(int l, int r) {
     auto e = seg.prod(l, r);
-    return {e.x, e.y};
+    return {e.idx, e.x, e.y};
   }
-  pair<T1, T2> prod_all() {
+  // (i,x,y)
+  tuple<int, T1, T2> prod_all() {
     auto e = seg.prod_all();
-    return {e.x, e.y};
+    return {e.idx, e.x, e.y};
   }
+  // y[i] := y[i] + ax[i] + b
   void apply(int l, int r, T1 a, T2 b) { seg.apply(l, r, {a, b}); }
 };
 
-// (x[i],y[i]) からなる列. a>=0 であるときに y[i] :- y[i] + ax[i] + b という作用ができる
+// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b という作用ができる
 // x には単調性は要らない. x,sum(a):T1, y,sum(b):T2, T1*T1<=T2.
 // https://codeforces.com/blog/entry/82094#comment-688448
 // https://atcoder.jp/contests/jsc2024-final/tasks/jsc2024_final_d
@@ -87,6 +90,7 @@ template <typename T1, typename T2>
 struct Beats_Kinetic_Min {
   struct Mono_X {
     struct X {
+      int idx;
       T1 x;
       T2 y;
       T1 nxt_change;
@@ -96,7 +100,7 @@ struct Beats_Kinetic_Min {
     static X op(X L, X R) {
       X M;
       if (L.y > R.y) { swap(L, R); }
-      M.x = L.x, M.y = L.y;
+      M.idx = L.idx, M.x = L.x, M.y = L.y;
       M.nxt_change = min(L.nxt_change, R.nxt_change);
       if (L.x > R.x) {
         T2 t = floor<T2>(R.y - L.y, L.x - R.x);
@@ -105,7 +109,7 @@ struct Beats_Kinetic_Min {
       M.fail = 0;
       return M;
     }
-    static constexpr X unit() { return {0, infty<T2>, infty<T1>, 0}; }
+    static constexpr X unit() { return {-1, 0, infty<T2>, infty<T1>, 0}; }
     bool commute = true;
   };
   struct Mono_A {
@@ -134,26 +138,28 @@ struct Beats_Kinetic_Min {
   using S = typename Mono_X::X;
   SegTree_Beats<Beats> seg;
   Beats_Kinetic_Min(vc<T1>& X, vc<T2>& Y) {
-    seg.build(len(X), [&](int i) -> S { return {X[i], Y[i], infty<T1>, 0}; });
+    seg.build(len(X), [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
   }
   template <typename F>
   Beats_Kinetic_Min(int n, F f) {
     seg.build(n, [&](int i) -> S {
       auto [x, y] = f(i);
-      return {x, y, infty<T1>, 0};
+      return {i, x, y, infty<T1>, 0};
     });
   }
 
-  void set(int i, T1 x, T2 y) { seg.set(i, {x, y, infty<T1>, 0}); }
+  void set(int i, T1 x, T2 y) { seg.set(i, {i, x, y, infty<T1>, 0}); }
 
-  // (x,y)
-  pair<T1, T2> prod(int l, int r) {
+  // (i,x,y)
+  tuple<int, T1, T2> prod(int l, int r) {
     auto e = seg.prod(l, r);
-    return {e.x, e.y};
+    return {e.idx, e.x, e.y};
   }
-  pair<T1, T2> prod_all() {
+  tuple<int, T1, T2> prod_all() {
     auto e = seg.prod_all();
-    return {e.x, e.y};
+    return {e.idx, e.x, e.y};
   }
+
+  // y[i] := y[i] + ax[i] + b
   void apply(int l, int r, T1 a, T2 b) { seg.apply(l, r, {a, b}); }
 };
