7fa5a9ca4e3fe7dc1551dae8fa5facd8af05a629
maspypy <maspypy.gmail.com>
2024-11-29 20:31:18 +0900

    a

--- graph/optimal_product_on_tree.hpp
+++ graph/optimal_product_on_tree.hpp
@@ -1,5 +1,6 @@
 #include "graph/tree.hpp"
 #include "ds/unionfind/unionfind.hpp"
+#include "ds/segtree/segtree.hpp"
 
 // 木の各頂点に全順序モノイドの元が置かれている（x<=y -> axb<=ayb）
 // topological 順序でかけるとき，結果を最大化する
@@ -7,8 +8,7 @@
 // https://qoj.ac/problem/6561
 // いや抽象化が足りていないな、x<=y iff xy<=yx がいるか？
 template <typename TREE, typename Monoid, bool MINIMIZE, typename F>
-pair<vc<int>, typename Monoid::value_type> optimal_product_on_tree(
-    TREE& tree, vc<typename Monoid::value_type> A, F compare) {
+pair<vc<int>, typename Monoid::value_type> optimal_product_on_tree(TREE& tree, vc<typename Monoid::value_type> A, F compare) {
   const int N = tree.N;
   const int root = tree.V[0];
   using X = typename Monoid::value_type;
@@ -17,31 +17,37 @@ pair<vc<int>, typename Monoid::value_type> optimal_product_on_tree(
   FOR(v, N) head[v] = v, tail[v] = v, nxt[v] = -1;
 
   UnionFind uf(N);
-  // (x, v, size v)
-  using T = tuple<X, int, int>;
-  auto my_compare = [&](T& t1, T& t2) -> bool {
-    X& x1 = get<0>(t1);
-    X& x2 = get<0>(t2);
-    if constexpr (MINIMIZE) return compare(x2, x1);
-    if constexpr (!MINIMIZE) return compare(x1, x2);
+
+  int size = 1;
+  while (size < N) size *= 2;
+  vc<int> seg(2 * size, -1);
+
+  // prique だと動的データ構造を持たせるときに混乱したのでセグ木で
+  auto upd = [&](int i) -> void {
+    int a = seg[2 * i + 0], b = seg[2 * i + 1];
+    if (a == -1) seg[i] = b;
+    elif (b == -1) seg[i] = a;
+    else seg[i] = (compare(A[a], A[b]) ? a : b);
   };
+  auto set = [&](int i, int x) -> void {
+    i += size;
+    seg[i] = x;
+    while (i > 1) { i /= 2, upd(i); }
+  };
+  FOR(i, N) if (i != root) seg[size + i] = i;
+  FOR_R(i, 1, size) upd(i);
 
-  priority_queue<T, vc<T>, decltype(my_compare)> que(my_compare);
-  FOR(v, N) if (v != root) que.emplace(A[v], v, 1);
-
-  while (len(que)) {
-    auto [_, v, sv] = que.top();
-    que.pop();
-    if (uf.size(v) != sv) continue;
+  FOR(N - 1) {
+    int v = seg[1];
     int a = head[uf[v]], b = tail[uf[v]];
     int p = uf[tree.parent[a]];
     int c = head[p], d = tail[p];
-    X x = Monoid::op(A[p], A[v]);
+    set(v, -1), set(p, -1);
+    X pv = Monoid::op(A[p], A[v]);
     uf.merge(p, v);
     v = uf[v];
-    A[v] = x, head[v] = c, tail[v] = b, nxt[d] = a;
-    if (uf[v] == uf[root]) continue;
-    que.emplace(A[v], v, uf.size(v));
+    A[v] = pv, head[v] = c, tail[v] = b, nxt[d] = a;
+    if (uf[v] != uf[root]) set(v, v);
   }
   vc<int> I = {root};
   while (nxt[I.back()] != -1) I.eb(nxt[I.back()]);
