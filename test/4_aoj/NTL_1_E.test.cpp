c4cac3ee6e02192517ff7d16b39bea007d6d8a21
maspypy <maspypy.gmail.com>
2024-11-29 17:01:27 +0900

    empty

--- test/4_aoj/NTL_1_E.test.cpp
+++ test/4_aoj/NTL_1_E.test.cpp
@@ -1,5 +1,4 @@
-#define PROBLEM \
-  "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_E"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_E"
 
 #include "my_template.hpp"
 #include "other/io.hpp"
@@ -7,18 +6,11 @@
 
 void solve() {
   LL(a, b);
-  auto [x, y, d] = extgcd(a, b);
+  auto [x, y, d] = extgcd2(a, b);
   print(x, y);
 }
 
 signed main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  cout << setprecision(15);
-
-  ll T = 1;
-  // LL(T);
-  FOR(_, T) solve();
-
+  solve();
   return 0;
 }
