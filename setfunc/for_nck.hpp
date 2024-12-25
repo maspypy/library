451096f407741a3e78c466657643cb8b29fe6c96
maspypy <maspypy.gmail.com>
2024-12-26 06:06:11 +0900

    a

--- setfunc/for_nck.hpp
+++
@@ -1,3 +0,0 @@
-#define FOR_nCk(s, n, k) \
-  for (ll s = (1LL << (k)) - 1, tmp_var = 0; s < (1LL << (n)); \
-       tmp_var = s | (s - 1), s = (tmp_var + 1) | (((~tmp_var & -~tmp_var) - 1) >> (__builtin_ctzll(s) + 1)))
