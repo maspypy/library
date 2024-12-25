3a5e3d7b2d19e7a3373acf5808f9961730cfe8c4
maspypy <maspypy.gmail.com>
2024-12-26 06:06:11 +0900

    a

--- enumerate/bits.hpp
+++ enumerate/bits.hpp
@@ -1,21 +1,3 @@
-template <typename F>
-void enumerate_bits_32(u32 s, F f) {
-  while (s) {
-    int i = __builtin_ctz(s);
-    f(i);
-    s ^= 1 << i;
-  }
-}
-
-template <typename F>
-void enumerate_bits_64(u64 s, F f) {
-  while (s) {
-    int i = __builtin_ctzll(s);
-    f(i);
-    s ^= u64(1) << i;
-  }
-}
-
 template <typename BS, typename F>
 void enumerate_bits_bitset(BS& b, int L, int R, F f) {
   int p = (b[L] ? L : b._Find_next(L));
---
+++ setfunc/all_k_subset.hpp
@@ -0,0 +1,30 @@
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+
+template <typename UINT>
+struct all_k_subset {
+  static_assert(is_unsigned<UINT>::value);
+  struct iter {
+    const UINT n, k, s;
+    UINT t;
+    iter(UINT s, UINT k) : n(UINT(1) << popcnt(s)), k(k), s(s), t((UINT(1) << k) - 1) {}
+    __attribute__((target("bmi2"))) auto operator*() const { return _pdep_u32(t, s); }
+    auto operator++() {
+      if (k == 0) {
+        t = UINT(-1);
+      } else {
+        UINT y = t + (-t & t);
+        t = y | ((y ^ t) >> lowbit(t << 2));
+      }
+    }
+    auto operator!=(const iter) const { return t < n; }
+  };
+  UINT s, k;
+  all_k_subset(UINT s, UINT k) : s(s), k(k) { assert(s != UINT(-1)); }
+  auto begin() { return iter(s, k); }
+  auto end() { return iter(0, 0); }
+};
