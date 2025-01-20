a7da60807f32e81e785002942b78f5fad042afb5
maspypy <maspypy.gmail.com>
2025-01-20 22:41:37 +0900

    a

--- mod/mod_pow.hpp
+++ mod/mod_pow.hpp
@@ -5,6 +5,7 @@
 
 u32 mod_pow(int a, ll n, int mod) {
   assert(n >= 0);
+  if (mod == 1) return 0;
   a = ((a %= mod) < 0 ? a + mod : a);
   if ((mod & 1) && (mod < (1 << 30))) {
     using mint = Mongomery_modint_32<202311021>;
@@ -22,6 +23,7 @@ u32 mod_pow(int a, ll n, int mod) {
 
 u64 mod_pow_64(ll a, ll n, u64 mod) {
   assert(n >= 0);
+  if (mod == 1) return 0;
   a = ((a %= mod) < 0 ? a + mod : a);
   if ((mod & 1) && (mod < (u64(1) << 62))) {
     using mint = Mongomery_modint_64<202311021>;
