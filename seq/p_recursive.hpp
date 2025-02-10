3a3086fdd9b9ac4d36427b99afa114f35e3faef6
maspypy <maspypy.gmail.com>
2025-02-11 04:20:31 +0900

    a

--- seq/p_recursive.hpp
+++ seq/p_recursive.hpp
@@ -1,4 +1,5 @@
 
+#include "poly/prefix_product_of_poly.hpp"
 #include "linalg/solve_linear.hpp"
 #include "mod/all_inverse.hpp"
 
@@ -66,4 +67,38 @@ vc<mint> extend_p_recursive(vc<mint> A, vvc<mint> F, int N) {
     A[n + M] = (-ans) * S[n];
   }
   return A;
+}
+
+// a0, ..., a_{r-1} および f_0, ..., f_r を与える
+// a_r f_0(0) + a_{r-1}f_1(0) + ... = 0
+// a_{r+1} f_0(1) + a_{r}f_1(1) + ... = 0
+template <typename T>
+T kth_term_of_p_recursive(vc<T> a, vc<vc<T>>& fs, ll k) {
+  if (k < len(a)) return a[k];
+  int r = len(fs) - 1;
+  assert(len(a) >= r);
+  a.resize(r);
+  assert(len(fs) == r + 1);
+
+  vc<vc<vc<T>>> A;
+  A.resize(r);
+  FOR(i, r) A[i].resize(r);
+  FOR(i, r) {
+    // A[0][i] = -fs[i + 1];
+    for (auto&& x: fs[i + 1]) A[0][i].eb(-x);
+  }
+  FOR3(i, 1, r) A[i][i - 1] = fs[0];
+  vc<T> den = fs[0];
+  auto res = prefix_product_of_poly_matrix(A, k - r + 1);
+  reverse(all(a));
+  T ANS = 0;
+  FOR(j, r) ANS += res[0][j] * a[j];
+  ANS /= prefix_product_of_poly(den, k - r + 1);
+  return ANS;
+}
+
+template <typename mint>
+vvc<mint> interpolate_p_recursive(vc<mint> A, int max_m, int max_deg, int k) {
+  auto fs = find_p_recursive<mint>(a, 1, 1);
+  return kth_term_of_p_recursive<mint>(a, fs, k);
 }
