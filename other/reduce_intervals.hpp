562392dbaf7c6c7060f610c79d71dd17aea1ba04
maspypy <maspypy@gmail.com>
2025-03-03 15:19:30 +0900

    a

--- other/reduce_intervals.hpp
+++ other/reduce_intervals.hpp
@@ -2,6 +2,7 @@
 // rm_included = true : I < J となる J が存在すれば I を消す
 // rm_included = false : I > J となる J が存在すれば I を消す
 // 残す区間のインデックスを区間の順序についてソートして返す
+// 完全に同じ区間は任意に選んだひとつだけ残す
 template <typename T>
 vc<int> reduce_intervals(vc<T> L, vc<T> R, bool rm_included) {
   int N = len(L);
@@ -9,26 +10,30 @@ vc<int> reduce_intervals(vc<T> L, vc<T> R, bool rm_included) {
   vc<int> I(N);
   FOR(i, N) I[i] = i;
   if (rm_included) {
-    sort(all(I), [&](auto& a, auto& b) -> bool {
-      if (L[a] != L[b]) return L[a] < L[b];
+    sort(all(I), [&](auto &a, auto &b) -> bool {
+      if (L[a] != L[b])
+        return L[a] < L[b];
       return R[a] > R[b];
     });
-    for (auto& j: I) {
+    for (auto &j : I) {
       if (!ANS.empty()) {
         int i = ANS.back();
-        if (R[j] <= R[i] && R[j] - L[j] < R[i] - L[i]) continue;
+        if (R[j] <= R[i] && R[j] - L[j] <= R[i] - L[i])
+          continue;
       }
       ANS.eb(j);
     }
   } else {
-    sort(all(I), [&](auto& a, auto& b) -> bool {
-      if (R[a] != R[b]) return R[a] < R[b];
+    sort(all(I), [&](auto &a, auto &b) -> bool {
+      if (R[a] != R[b])
+        return R[a] < R[b];
       return L[a] > L[b];
     });
-    for (auto& j: I) {
+    for (auto &j : I) {
       if (!ANS.empty()) {
         int i = ANS.back();
-        if (L[j] <= L[i] && R[j] - L[j] > R[i] - L[i]) continue;
+        if (R[j] <= R[i] && R[j] - L[j] == R[i] - L[i])
+          continue;
       }
       ANS.eb(j);
     }
