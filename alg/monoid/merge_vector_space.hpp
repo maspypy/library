17c9e9a1244cc85228a959e22bf353ca869cf0f9
maspypy <maspypy@gmail.com>
2025-09-19 11:50:34 +0900

    a

--- alg/monoid/merge_vector_space.hpp
+++ alg/monoid/merge_vector_space.hpp
@@ -1,10 +1,16 @@
 #include "linalg/xor/vector_space.hpp"
 
-template <typename UINT>
+template <typename UINT, int MAX_DIM>
 struct Merge_Vector_Space {
-  using value_type = Vector_Space<UINT>;
+  using value_type = Vector_Space<UINT, MAX_DIM>;
   using X = value_type;
-  static X op(X x, X y) { return Vector_Space<UINT>::merge(x, y); }
+  static X op(X x, X y) {
+    if (len(x) < len(y)) swap(x, y);
+    for (UINT v : y.dat) {
+      x.add_element(v);
+    }
+    return x;
+  }
   static constexpr X unit() { return {}; }
   static constexpr bool commute = 1;
 };
