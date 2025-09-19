7e6c1a045bd39293e266f91e79cd46d6910907d5
maspypy <45988013+maspypy@users.noreply.github.com>
2025-09-19 12:02:30 +0900

    Update 184.test.cpp

--- test/3_yukicoder/184.test.cpp
+++ test/3_yukicoder/184.test.cpp
@@ -1,16 +1,18 @@
 #define PROBLEM "https://yukicoder.me/problems/no/184"
 #include "my_template.hpp"
 #include "other/io.hpp"
-#include "alg/monoid/merge_vector_space.hpp"
+
+#include "linalg/xor/vector_space.hpp"
 
 void solve() {
   LL(N);
   VEC(ll, A, N);
-  using Mono = Merge_Vector_Space<u64>;
-  using SP = typename Mono::value_type;
+  using SP = Vector_Space<u64, 60>;
   SP X;
-  for (auto&& a: A) { X.add_element(a); }
-  print(1LL << len(X));
+  for (auto&& a : A) {
+    X.add_element(a);
+  }
+  print(u64(1) << len(X));
 }
 
 signed main() {
