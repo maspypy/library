37105af2a03c4c20fe09df5edc6b7c5ffc4a5868
maspypy <maspypy@gmail.com>
2025-11-21 13:28:31 +0900

    a

--- ds/segtree/beats_kinetic.hpp
+++ ds/segtree/beats_kinetic.hpp
@@ -1,8 +1,8 @@
 #include "ds/segtree/segtree_beats.hpp"
 
-// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b という作用ができる
-// x には単調性は要らない. x,sum(a):T1, y,sum(b):T2, T1*T1<=T2.
-// https://codeforces.com/blog/entry/82094#comment-688448
+// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b
+// という作用ができる x には単調性は要らない. x,sum(a):T1, y,sum(b):T2,
+// T1*T1<=T2. https://codeforces.com/blog/entry/82094#comment-688448
 // https://atcoder.jp/contests/jsc2024-final/tasks/jsc2024_final_d
 template <typename T1, typename T2>
 struct Beats_Kinetic_Max {
@@ -17,12 +17,14 @@ struct Beats_Kinetic_Max {
     using value_type = X;
     static X op(X L, X R) {
       X M;
-      if (L.y < R.y) { swap(L, R); }
+      if (L.y < R.y) {
+        swap(L, R);
+      }
       M.idx = L.idx, M.x = L.x, M.y = L.y;
       M.nxt_change = min(L.nxt_change, R.nxt_change);
       if (L.x < R.x) {
         T2 t = floor<T2>(L.y - R.y, R.x - L.x);
-        chmin(M.nxt_change, t + 1);
+        M.nxt_change = min<T2>(M.nxt_change, t + 1);
       }
       M.fail = 0;
       return M;
@@ -33,7 +35,9 @@ struct Beats_Kinetic_Max {
   struct Mono_A {
     using X = pair<T1, T2>;
     using value_type = X;
-    static constexpr X op(const X& x, const X& y) { return {x.fi + y.fi, x.se + y.se}; }
+    static constexpr X op(const X& x, const X& y) {
+      return {x.fi + y.fi, x.se + y.se};
+    }
     static constexpr X unit() { return {0, 0}; }
     bool commute = true;
   };
@@ -56,7 +60,8 @@ struct Beats_Kinetic_Max {
   using S = typename Mono_X::X;
   SegTree_Beats<Beats> seg;
   Beats_Kinetic_Max(vc<T1>& X, vc<T2>& Y) {
-    seg.build(len(X), [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
+    seg.build(len(X),
+              [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
   }
   template <typename F>
   Beats_Kinetic_Max(int n, F f) {
@@ -82,9 +87,9 @@ struct Beats_Kinetic_Max {
   void apply(int l, int r, T1 a, T2 b) { seg.apply(l, r, {a, b}); }
 };
 
-// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b という作用ができる
-// x には単調性は要らない. x,sum(a):T1, y,sum(b):T2, T1*T1<=T2.
-// https://codeforces.com/blog/entry/82094#comment-688448
+// (x[i],y[i]) からなる列. a>=0 であるときに y[i] := y[i] + ax[i] + b
+// という作用ができる x には単調性は要らない. x,sum(a):T1, y,sum(b):T2,
+// T1*T1<=T2. https://codeforces.com/blog/entry/82094#comment-688448
 // https://atcoder.jp/contests/jsc2024-final/tasks/jsc2024_final_d
 template <typename T1, typename T2>
 struct Beats_Kinetic_Min {
@@ -99,12 +104,14 @@ struct Beats_Kinetic_Min {
     using value_type = X;
     static X op(X L, X R) {
       X M;
-      if (L.y > R.y) { swap(L, R); }
+      if (L.y > R.y) {
+        swap(L, R);
+      }
       M.idx = L.idx, M.x = L.x, M.y = L.y;
       M.nxt_change = min(L.nxt_change, R.nxt_change);
       if (L.x > R.x) {
         T2 t = floor<T2>(R.y - L.y, L.x - R.x);
-        chmin(M.nxt_change, t + 1);
+        M.nxt_change = min<T2>(M.nxt_change, t + 1);
       }
       M.fail = 0;
       return M;
@@ -115,7 +122,9 @@ struct Beats_Kinetic_Min {
   struct Mono_A {
     using X = pair<T1, T2>;
     using value_type = X;
-    static constexpr X op(const X& x, const X& y) { return {x.fi + y.fi, x.se + y.se}; }
+    static constexpr X op(const X& x, const X& y) {
+      return {x.fi + y.fi, x.se + y.se};
+    }
     static constexpr X unit() { return {0, 0}; }
     bool commute = true;
   };
@@ -138,7 +147,8 @@ struct Beats_Kinetic_Min {
   using S = typename Mono_X::X;
   SegTree_Beats<Beats> seg;
   Beats_Kinetic_Min(vc<T1>& X, vc<T2>& Y) {
-    seg.build(len(X), [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
+    seg.build(len(X),
+              [&](int i) -> S { return {i, X[i], Y[i], infty<T1>, 0}; });
   }
   template <typename F>
   Beats_Kinetic_Min(int n, F f) {
