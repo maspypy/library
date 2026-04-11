f455f4df9a16a4a4d31eb5184041ab356954ac38
toyboot4e <toyboot4e@gmail.com>
2026-04-12 02:39:18 +0900

    fix: `check` in `FenwickTree.max_right`

--- ds/fenwicktree/fenwicktree.hpp
+++ ds/fenwicktree/fenwicktree.hpp
@@ -97,7 +97,7 @@ struct FenwickTree {
       --k;
       if (i + (1 << k) - 1 < len(dat)) {
         E t = G::op(s, dat[i + (1 << k) - 1]);
-        if (check(t)) { i += (1 << k), s = t; }
+        if (i + (1 << k) <= L || check(t)) { i += (1 << k), s = t; }
       }
     }
     return i;
