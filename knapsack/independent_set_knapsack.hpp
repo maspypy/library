2b283dfa1f2bbb2e602091feac0d88ae40901ef7
maspypy <maspypy@gmail.com>
2026-05-05 03:15:57 +0900

    a

---
+++ knapsack/independent_set_knapsack.hpp
@@ -0,0 +1,51 @@
+#include "graph/tree.hpp"
+
+// https://arxiv.org/pdf/1807.04942
+// Example 1.
+template <typename TREE, typename VAL>
+vc<VAL> independent_set_knapsack(TREE tree, vc<int> weight, vc<VAL> val,
+                                 int LIM) {
+  using V = vc<VAL>;
+  // return: ng, ok
+  auto dfs = [&](auto& dfs, int v, V X) -> array<V, 2> {
+    assert(len(X) == LIM + 1);
+    int heavy = tree.heavy_child(v);
+    array<V, 2> Y = {V(LIM + 1, -infty<VAL>), V(LIM + 1, -infty<VAL>)};
+    if (heavy == -1) {
+      FOR(i, LIM + 1) {
+        chmax(Y[0][i], X[i]);
+        chmax(Y[1][i], X[i]);
+      }
+      FOR(i, LIM - weight[v] + 1) chmax(Y[1][i + weight[v]], X[i] + val[v]);
+      return Y;
+    }
+    auto Z = dfs(dfs, heavy, X);
+    auto ch = tree.collect_light(v);
+
+    // not take v.
+    {
+      V cur = Z[1];
+      for (int w : ch) {
+        cur = dfs(dfs, w, cur)[1];
+      }
+      FOR(i, LIM + 1) chmax(Y[0][i], cur[i]), chmax(Y[1][i], cur[i]);
+    }
+
+    // take v.
+    {
+      V cur = Z[0];
+      for (int w : ch) {
+        cur = dfs(dfs, w, cur)[0];
+      }
+      FOR(i, LIM - weight[v] + 1) chmax(Y[1][i + weight[v]], cur[i] + val[v]);
+    }
+    return Y;
+  };
+
+  V X(LIM + 1, -infty<VAL>);
+  X[0] = 0;
+  V ANS = dfs(dfs, 0, X)[1];
+  for (auto& x : ANS)
+    if (x < 0) x = -infty<VAL>;
+  return ANS;
+}
---
+++ test/1_mytest/independent_set_knapsack.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "my_template.hpp"
+
+#include "random/random_graph.hpp"
+#include "knapsack/independent_set_knapsack.hpp"
+
+void test(int N) {
+  FOR(1000) {
+    auto E = random_tree(N);
+    Graph<int, 0> G(N);
+    for (auto& [a, b] : E) G.add(a, b);
+    G.build();
+    Tree<decltype(G)> tree(G);
+    vc<int> weight, val;
+    FOR(N) {
+      weight.eb(RNG(1, 10));
+      val.eb(RNG(1, 10));
+    }
+    int LIM = RNG(1, 100);
+    auto ANS = independent_set_knapsack(tree, weight, val, LIM);
+    vc<int> god(LIM + 1, -infty<int>);
+    FOR(s, 1 << N) {
+      bool ok = 1;
+      for (auto& [a, b] : E) {
+        if ((s >> a & 1) && (s >> b & 1)) ok = 0;
+      }
+      if (!ok) continue;
+      int a = 0, b = 0;
+      FOR(v, N) if (s >> v & 1) a += weight[v], b += val[v];
+      if (a <= LIM) chmax(god[a], b);
+    }
+    assert(god == ANS);
+  }
+}
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << "\n";
+}
+
+int main() {
+  FOR(N, 1, 11) test(N);
+  solve();
+  return 0;
+}
