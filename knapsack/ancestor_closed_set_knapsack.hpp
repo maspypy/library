3c4bc542ef1a7332edaffe245c8c8128467a0316
maspypy <maspypy@gmail.com>
2026-05-05 03:38:05 +0900

    a

---
+++ knapsack/ancestor_closed_set_knapsack.hpp
@@ -0,0 +1,42 @@
+
+#include "graph/tree.hpp"
+
+// https://arxiv.org/pdf/1807.04942
+// Example 2. v を選ぶならば par[v] も選ぶ
+// // O(LIM n})
+template <typename TREE, typename VAL>
+vc<VAL> ancestor_closed_set_knapsack(TREE& tree, vc<int> weight, vc<VAL> val,
+                                     int LIM) {
+  using V = vc<VAL>;
+
+  auto dfs = [&](auto& dfs, int v, const V& X) -> V {
+    assert(len(X) == LIM + 1);
+    int heavy = tree.heavy_child(v);
+    V Y(LIM + 1, -infty<VAL>);
+    if (heavy == -1) {
+      FOR(i, LIM + 1) { chmax(Y[i], X[i]); }
+      FOR(i, LIM - weight[v] + 1) chmax(Y[i + weight[v]], X[i] + val[v]);
+      return Y;
+    }
+    auto Z = dfs(dfs, heavy, X);
+    auto ch = tree.collect_light(v);
+
+    // not take v.
+    FOR(i, LIM + 1) chmax(Y[i], X[i]);
+
+    // take v.
+    V cur = move(Z);
+    for (int w : ch) {
+      cur = dfs(dfs, w, cur);
+    }
+    FOR(i, LIM - weight[v] + 1) chmax(Y[i + weight[v]], cur[i] + val[v]);
+    return Y;
+  };
+
+  V X(LIM + 1, -infty<VAL>);
+  X[0] = 0;
+  V ANS = dfs(dfs, 0, X);
+  for (auto& x : ANS)
+    if (x < 0) x = -infty<VAL>;
+  return ANS;
+}
--- knapsack/independent_set_knapsack.hpp
+++ knapsack/independent_set_knapsack.hpp
@@ -2,12 +2,13 @@
 
 // https://arxiv.org/pdf/1807.04942
 // Example 1.
+// O(LIM n^{1.59})
 template <typename TREE, typename VAL>
-vc<VAL> independent_set_knapsack(TREE tree, vc<int> weight, vc<VAL> val,
+vc<VAL> independent_set_knapsack(TREE& tree, vc<int> weight, vc<VAL> val,
                                  int LIM) {
   using V = vc<VAL>;
   // return: ng, ok
-  auto dfs = [&](auto& dfs, int v, V X) -> array<V, 2> {
+  auto dfs = [&](auto& dfs, int v, const V& X) -> array<V, 2> {
     assert(len(X) == LIM + 1);
     int heavy = tree.heavy_child(v);
     array<V, 2> Y = {V(LIM + 1, -infty<VAL>), V(LIM + 1, -infty<VAL>)};
@@ -26,7 +27,8 @@ vc<VAL> independent_set_knapsack(TREE tree, vc<int> weight, vc<VAL> val,
     {
       V cur = Z[1];
       for (int w : ch) {
-        cur = dfs(dfs, w, cur)[1];
+        array<V, 2> T = dfs(dfs, w, cur);
+        cur = move(T[1]);
       }
       FOR(i, LIM + 1) chmax(Y[0][i], cur[i]), chmax(Y[1][i], cur[i]);
     }
@@ -35,7 +37,8 @@ vc<VAL> independent_set_knapsack(TREE tree, vc<int> weight, vc<VAL> val,
     {
       V cur = Z[0];
       for (int w : ch) {
-        cur = dfs(dfs, w, cur)[0];
+        array<V, 2> T = dfs(dfs, w, cur);
+        cur = move(T[0]);
       }
       FOR(i, LIM - weight[v] + 1) chmax(Y[1][i + weight[v]], cur[i] + val[v]);
     }
