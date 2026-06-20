5e26498ee0ae769a4eee64e3d27f56d6382ed443
maspypy <maspypy@gmail.com>
2026-06-20 11:40:52 +0900

    empty

--- convex/monge/monge_shortest_path.hpp
+++ convex/monge/monge_shortest_path.hpp
@@ -1,46 +1,64 @@
 #pragma once
 
-// dp[0] = 0
-// dp[r] = min_{0 <= l < r} dp[l] + f(l, r)
-// return: {dp, frm}
-// minimize_cnt: Alien の復元に使う
-template <typename T, typename F>
-pair<vc<T>, vc<int>> monge_shortest_path(int N, F f, bool minimize_cnt = true) {
-  vc<T> dp(N + 1, infty<T>);
-  vc<int> frm(N + 1, 0);
-  vc<int> cnt(N + 1, infty<int>);
+template <typename T>
+struct Monge_Shortest_Path {
+  int N;
+  vc<T> dp;
+  vc<int> cnt, frm;
 
-  dp[0] = 0;
-  cnt[0] = 0;
+  Monge_Shortest_Path(int N) : N(N) {}
 
-  auto better_tie = [&](int new_cnt, int old_cnt) -> bool {
-    return minimize_cnt ? new_cnt < old_cnt : new_cnt > old_cnt;
-  };
-  auto check = [&](int r, int l) -> void {
-    T x = dp[l] + f(l, r);
-    int c = cnt[l] + 1;
-    if (dp[r] > x || (dp[r] == x && better_tie(c, cnt[r]))) {
-      dp[r] = x;
-      frm[r] = l;
-      cnt[r] = c;
+  template <typename F>
+  pair<T, int> solve(F f, bool minimize_cnt = true) {
+    dp.assign(N + 1, infty<T>);
+    cnt.assign(N + 1, infty<int>);
+    frm.assign(N + 1, 0);
+
+    dp[0] = 0;
+    cnt[0] = 0;
+
+    auto better_tie = [&](int new_cnt, int old_cnt) -> bool {
+      return minimize_cnt ? new_cnt < old_cnt : new_cnt > old_cnt;
+    };
+
+    auto check = [&](int r, int l) -> void {
+      T x = dp[l] + f(l, r);
+      int c = cnt[l] + 1;
+      if (dp[r] > x || (dp[r] == x && better_tie(c, cnt[r]))) {
+        dp[r] = x;
+        cnt[r] = c;
+        frm[r] = l;
+      }
+    };
+
+    // simple larsch, https://noshi91.hatenablog.com/entry/2023/02/18/005856
+    auto dfs = [&](auto& dfs, int l, int r) -> void {
+      if (r - l == 1) return;
+      int m = (l + r) / 2;
+
+      FOR(k, frm[l], frm[r] + 1) check(m, k);
+      dfs(dfs, l, m);
+
+      FOR(k, l + 1, m + 1) check(r, k);
+      dfs(dfs, m, r);
+    };
+
+    if (N > 0) {
+      check(N, 0);
+      dfs(dfs, 0, N);
     }
-  };
 
-  // simple larsch, https://noshi91.hatenablog.com/entry/2023/02/18/005856
-  auto dfs = [&](auto& dfs, int l, int r) -> void {
-    if (r - l == 1) return;
-    int m = (l + r) / 2;
-    FOR(k, frm[l], frm[r] + 1) check(m, k);
-    dfs(dfs, l, m);
-    FOR(k, l + 1, m + 1) check(r, k);
-    dfs(dfs, m, r);
-  };
-
-  if (N > 0) {
-    check(N, 0), dfs(dfs, 0, N);
+    return {dp[N], cnt[N]};
   }
-  return {dp, frm};
-}
+
+  vc<int> restore_path() {
+    vc<int> path;
+    for (int v = N; v > 0; v = frm[v]) path.eb(v);
+    path.eb(0);
+    reverse(all(path));
+    return path;
+  }
+};
 
 // yuki705 では simple larsch より低速だったし使わない想定でいく
 // #include "convex/larsch.hpp"
--- convex/monge/monge_shortest_path_d_edge.hpp
+++ convex/monge/monge_shortest_path_d_edge.hpp
@@ -1,50 +1,25 @@
 #pragma once
-#include "convex/larsch.hpp"
 
-// https://codeforces.com/contest/2183/problem/H
+#include "convex/alien.hpp"
+#include "convex/monge/monge_shortest_path.hpp"
+
+// lo: penalty = lo では cnt > d
+// hi: penalty = hi では cnt <= d
 template <typename T, typename F>
-T monge_shortest_path_d_edge(int N, int d, T flim, F f) {
-  assert(1 <= d && d <= N);
-  if (d == 1) return f(0, N);
-  if (d == N) {
-    T ans = 0;
-    FOR(i, N) ans += f(i, i + 1);
-    return ans;
-  }
-  if (d == 2) {
-    T ans = infty<T>;
-    FOR(i, 1, N) chmin(ans, f(0, i) + f(i, N));
-    return ans;
-  }
+T monge_shortest_path_d_edge(int N, int d, T lo, T hi, F f) {
+  Monge_Shortest_Path<T> solver(N);
+  auto solve = [&](T pena) -> pair<T, int> {
+    return solver.solve([&](int l, int r) -> T { return f(l, r) + penalty; });
+  };
+  return alien_trick<T>(N, d, lo, hi, solve);
+}
 
-  vc<pair<T, int>> dp(N + 1);
-  map<T, pair<T, int>> MP;
-  T ANS = -infty<T>;
-  auto calc = [&](T lambda) -> pair<T, int> {
-    if (MP.count(lambda)) return MP[lambda];
-    dp[0] = {0, 0};
-    auto eval = [&](int i, int j) -> T {
-      ++i;
-      if (i <= j) return infty<T>;
-      return dp[j].fi + f(j, i);
-    };
-    LARSCH<T, decltype(eval)> larsch(N, eval);
-
-    FOR(r, 1, N + 1) {
-      int l = larsch.get_argmin();
-      dp[r].fi = dp[l].fi + f(l, r) + lambda;
-      dp[r].se = dp[l].se + 1;
-    }
-    chmax(ANS, dp[N].fi - lambda * d);
-    return MP[lambda] = dp[N];
+template <typename T, typename F>
+T monge_shortest_path_d_edge_auto_search_range(int N, int d, T lo, T hi, F f) {
+  Monge_Shortest_Path<T> solver(N);
+  auto solve = [&](T pena) -> pair<T, int> {
+    return solver.solve([&](int l, int r) -> T { return f(l, r) + penalty; });
   };
 
-  T lo = -3 * flim - 10, hi = 3 * flim + 10;
-  while (lo + 1 < hi) {
-    T mi = (lo + hi) / 2;
-    int k = calc(mi).se;
-    if (k == d) break;
-    (k > d ? lo : hi) = mi;
-  }
-  return ANS;
+  return alien_trick_auto_search_range<T>(N, d, solve);
 }
