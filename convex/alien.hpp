d320202e21a1b9fa6714de721d1084c07ff3342f
maspypy <maspypy@gmail.com>
2026-06-20 11:11:08 +0900

    a

--- convex/alien.hpp
+++ convex/alien.hpp
@@ -1,68 +1,36 @@
 #pragma once
 
 // T は整数型を仮定
-// solve(penalty, minimize_cnt) -> {dp, frm}
+// solve(penalty) -> {val, cnt}
 // lo: cnt > K, hi: cnt <= K
+// return: ans, pena
 template <typename T, typename F>
-T alien_trick(int N, int K, T lo, T hi, F solve) {
+pair<T, T> alien_trick(int N, int K, T lo, T hi, F solve) {
   while (lo + 1 < hi) {
     T mid = lo + (hi - lo) / 2;
-
-    auto [dp, frm] = solve(mid, true);
-    int cnt = 0, v = N;
-    while (v) ++cnt, v = frm[v];
-
-    if (cnt <= K) {
-      hi = mid;
-    } else {
-      lo = mid;
-    }
+    auto [val, cnt] = solve(mid);
+    (cnt <= K ? hi : lo) = mid;
   }
-
-  auto [dp, frm] = solve(hi, true);
-  return dp[N] - hi * K;
+  auto [val, cnt] = solve(hi);
+  return {val - hi * K, hi};
 }
 
-// T は整数型を仮定
-// solve(penalty, minimize_cnt) -> {dp, frm}
-// lo: cnt > K, hi: cnt <= K
-template <typename T, typename F, typename G>
-pair<T, vc<int>> alien_trick_restore(int N, int K, type_identity_t<T> lo,
-                                     type_identity_t<T> hi, F solve, G cost) {
-  while (lo + 1 < hi) {
-    T mid = lo + (hi - lo) / 2;
-    auto [dp, frm] = solve(mid, true);
-    int cnt = 0, v = N;
-    while (v) ++cnt, v = frm[v];
-
-    if (cnt <= K) {
-      hi = mid;
-    } else {
-      lo = mid;
+template <typename T, typename F>
+pair<T, T> alien_trick_auto_search_range(int N, int K, F solve) {
+  T lo = 0, hi = 0;
+  int c0 = solve(0).se;
+  if (c0 <= K) {
+    lo = -1, hi = 0;
+    while (solve(lo).se < K) {
+      T d = hi - lo;
+      lo -= 2 * d, hi -= d;
+    }
+  } else {
+    lo = 0, hi = 1;
+    while (solve(hi).se > K) {
+      T d = hi - lo;
+      lo += d, hi += 2 * d;
     }
   }
-
-  auto [dp, frm1] = solve(hi, true);
-  auto [dp2, frm2] = solve(hi, false);
-  assert(dp2 == dp);
-  vc<int> cnt_min(N + 1), cnt_max(N + 1);
-  T ANS = dp[N] - hi * K;
-  FOR(i, 1, N + 1) cnt_min[i] = cnt_min[frm1[i]] + 1;
-  FOR(i, 1, N + 1) cnt_max[i] = cnt_max[frm2[i]] + 1;
-  assert(cnt_min[N] <= K && K <= cnt_max[N]);
-  vc<int> path;
-  path.eb(N);
-  int rest = K;
-  int j = N;
-  FOR_R(i, N) {
-    if (dp[j] == dp[i] + cost(i, j) + hi && cnt_min[i] <= rest - 1 &&
-        rest - 1 <= cnt_max[i]) {
-      path.eb(i);
-      --rest;
-      j = i;
-    }
-  }
-  assert(j == 0 && rest == 0);
-  reverse(all(path));
-  return {ANS, path};
+  return alien_trick(N, K, lo, hi, solve);
 }
