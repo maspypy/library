767470b647f1e410097312a164b6783ff6b82e2c
maspypy <maspypy@gmail.com>
2026-07-26 19:43:20 +0900

    a

---
+++ other/bit.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+int popcnt(int x) { return __builtin_popcount(x); }
+int popcnt(u32 x) { return __builtin_popcount(x); }
+int popcnt(ll x) { return __builtin_popcountll(x); }
+int popcnt(u64 x) { return __builtin_popcountll(x); }
+int popcnt_sgn(int x) { return (__builtin_parity(unsigned(x)) & 1 ? -1 : 1); }
+int popcnt_sgn(u32 x) { return (__builtin_parity(x) & 1 ? -1 : 1); }
+int popcnt_sgn(ll x) { return (__builtin_parityll(x) & 1 ? -1 : 1); }
+int popcnt_sgn(u64 x) { return (__builtin_parityll(x) & 1 ? -1 : 1); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 1, 2)
+int topbit(int x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(u32 x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
+int topbit(ll x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+int topbit(u64 x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
+// (0, 1, 2, 3, 4) -> (-1, 0, 1, 0, 2)
+int lowbit(int x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(u32 x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
+int lowbit(ll x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+int lowbit(u64 x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
+
+template <typename T>
+T kth_bit(int k) {
+  return T(1) << k;
+}
+template <typename T>
+bool has_kth_bit(T x, int k) {
+  return x >> k & 1;
+}
+
+template <typename UINT>
+struct all_bit {
+  UINT s;
+  struct iter {
+    UINT s;
+    int operator*() const { return lowbit(s); }
+    void operator++() { s &= s - 1; }
+    bool operator!=(nullptr_t) const { return s; }
+  };
+  iter begin() const { return {s}; }
+  nullptr_t end() const { return nullptr; }
+};
+
+template <typename UINT>
+struct all_subset {
+  UINT s;
+  struct iter {
+    UINT s, t;
+    bool done = false;
+    UINT operator*() const { return t; }
+    void operator++() {
+      done = (t == 0);
+      t = (t - 1) & s;
+    }
+    bool operator!=(nullptr_t) const { return !done; }
+  };
+  iter begin() const { return {s, s}; }
+  nullptr_t end() const { return nullptr; }
+};
+
+constexpr u64 full_mask(int n) { return n == 64 ? -1ULL : (1ULL << n) - 1; }
