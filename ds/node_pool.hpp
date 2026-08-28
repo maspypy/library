c784be966f29b8799a5c645cafec53e92cb3c86a
maspypy <maspypy@gmail.com>
2026-08-29 08:51:03 +0900

    a

--- ds/node_pool.hpp
+++ ds/node_pool.hpp
@@ -1,57 +1,52 @@
 // マルチテストケースに弱いので static で確保すること
 template <class Node>
 struct Node_Pool {
-  struct Slot {
-    union alignas(Node) {
-      Slot* next;
-      unsigned char storage[sizeof(Node)];
-    };
+  union Slot {
+    Node node;
+    Slot* next;
+
+    Slot() {}
+    ~Slot() {}
   };
   using np = Node*;
 
   static constexpr int CHUNK_SIZE = 1 << 12;
 
   vc<unique_ptr<Slot[]>> chunks;
-  Slot* cur = nullptr;
-  int cur_used = 0;
+  int chunk_id = 0;
+  int pos = 0;
   Slot* free_head = nullptr;
-
-  Node_Pool() { alloc_chunk(); }
 
   template <class... Args>
   np create(Args&&... args) {
     Slot* s = new_slot();
-    return ::new (s) Node(forward<Args>(args)...);
+    return ::new (&s->node) Node(forward<Args>(args)...);
   }
 
   np clone(const np x) {
     assert(x);
     Slot* s = new_slot();
-    return ::new (s) Node(*x);  // コピーコンストラクタ呼び出し
+    return ::new (&s->node) Node(*x);
   }
 
   void destroy(np x) {
     if (!x) return;
     x->~Node();
-    auto s = reinterpret_cast<Slot*>(x);
+    Slot* s = reinterpret_cast<Slot*>(x);
     s->next = free_head;
     free_head = s;
   }
 
+  // 全 node を無効化する。
+  // 確保済み chunk は解放せず、次回以降に再利用する。
   void reset() {
     free_head = nullptr;
-    if (!chunks.empty()) {
-      cur = chunks[0].get();
-      cur_used = 0;
-    }
+    chunk_id = 0;
+    pos = 0;
   }
 
  private:
-  void alloc_chunk() {
-    chunks.emplace_back(make_unique<Slot[]>(CHUNK_SIZE));
-    cur = chunks.back().get();
-    cur_used = 0;
-  }
+  void alloc_chunk() { chunks.eb(make_unique<Slot[]>(CHUNK_SIZE)); }
 
   Slot* new_slot() {
     if (free_head) {
@@ -59,7 +54,14 @@ struct Node_Pool {
       free_head = free_head->next;
       return s;
     }
-    if (cur_used == CHUNK_SIZE) alloc_chunk();
-    return &cur[cur_used++];
+
+    if (chunk_id == len(chunks)) alloc_chunk();
+
+    Slot* s = &chunks[chunk_id][pos++];
+    if (pos == CHUNK_SIZE) {
+      ++chunk_id;
+      pos = 0;
+    }
+    return s;
   }
 };
