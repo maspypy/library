3755cde1382e68c0ee652c88767117efb05e6c09
maspypy <maspypy@gmail.com>
2026-08-30 21:41:42 +0900

    a

--- random/base.hpp
+++ random/base.hpp
@@ -1,10 +1,19 @@
 
 u64 RNG_64() {
-  static u64 x_ = u64(chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count()) * 10150724397891781847ULL;
+  static u64 x_ = u64(chrono::duration_cast<chrono::nanoseconds>(
+                      chrono::high_resolution_clock::now().time_since_epoch())
+                          .count()) *
+                  10150724397891781847ULL;
   x_ ^= x_ << 7;
   return x_ ^= x_ >> 9;
 }
 
-u64 RNG(u64 lim) { return RNG_64() % lim; }
+u64 RNG(u64 lim) {
+  assert(lim > 0);
+  return RNG_64() % lim;
+}
 
-ll RNG(ll l, ll r) { return l + RNG_64() % (r - l); }
+ll RNG(ll l, ll r) {
+  assert(l < r);
+  return l + RNG_64() % (r - l);
+}
