be8784a3ece728847e71a3f22556d8bb9bda4252
maspypy <maspypy@gmail.com>
2026-09-07 10:05:10 +0900

    a

--- template/monoid.hpp
+++ template/monoid.hpp
@@ -2,7 +2,7 @@
 struct Mono {
   using value_type = ;
   using X = value_type;
-  static X op(X L, X R) { return {}; }
+  static X op(const X& L, const X& R) { return {}; }
   static constexpr X id() { return; }
   static constexpr bool commute = ;
 };
