9ccbb80672383278f207f5a5e051fd6e89176a0e
maspypy <maspypy@gmail.com>
2026-09-15 06:05:07 +0900

    a

--- my_template.hpp
+++ my_template.hpp
@@ -73,13 +73,11 @@ using pq_min = priority_queue<T, vector<T>, greater<T>>;
 #define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
 #define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
 #define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
-#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
 #define FOR1_R(a) for (ll i = ll(a) - 1; i >= ll(0); --i)
 #define FOR2_R(i, a) for (ll i = ll(a) - 1; i >= ll(0); --i)
 #define FOR3_R(i, a, b) for (ll i = ll(b) - 1; i >= ll(a); --i)
-#define overload4(a, b, c, d, e, ...) e
 #define overload3(a, b, c, d, ...) d
-#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR(...) overload3(__VA_ARGS__, FOR3, FOR2, FOR1)(__VA_ARGS__)
 #define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
 
 #define all(x) (x).begin(), (x).end()
